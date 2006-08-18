49e1b2256f0227059d4974411c54dec809f7cab9
Nathan Johnson <njohnson@ebi.ac.uk>
2006-08-18 12:46:39 +0000

    More adaptors

---
+++ modules/Bio/EnsEMBL/Funcgen/DBSQL/ChannelAdaptor.pm
@@ -0,0 +1,362 @@
+#
+# Ensembl module for Bio::EnsEMBL::Funcgen::DBSQL::ChannelAdaptor
+#
+# You may distribute this module under the same terms as Perl itself
+
+=head1 NAME
+
+Bio::EnsEMBL::Funcgen::DBSQL::ChannelAdaptor - A database adaptor for fetching and
+storing Funcgen Channel objects.
+
+=head1 SYNOPSIS
+
+my $chan_a = $db->get_ChannelAdaptor();
+
+my @channels = @{$chan_a->fetch_all_by_ExperimentalChip($exp)};
+
+
+=head1 DESCRIPTION
+
+The ChannelAdaptor is a database adaptor for storing and retrieving
+Funcgen Channel objects.
+
+=head1 AUTHOR
+
+This module was created by Nathan Johnson.
+
+This module is part of the Ensembl project: http://www.ensembl.org/
+
+=head1 CONTACT
+
+Post comments or questions to the Ensembl development list: ensembl-dev@ebi.ac.uk
+
+=head1 METHODS
+
+=cut
+
+use strict;
+use warnings;
+
+package Bio::EnsEMBL::Funcgen::DBSQL::ChannelAdaptor;
+
+use Bio::EnsEMBL::Utils::Exception qw( warning );
+use Bio::EnsEMBL::Funcgen::Channel;
+use Bio::EnsEMBL::DBSQL::BaseAdaptor;
+
+use vars qw(@ISA);
+
+
+#May need to our this?
+@ISA = qw(Bio::EnsEMBL::DBSQL::BaseAdaptor);
+
+=head2 fetch_by_type_experimental_chip_id
+
+  Arg [1]    : string - type
+  Arg [1]    : int - dbID of Experiment
+  Example    : my $chan = $chan_a->fetch_by_type_experimental_chip_dbID('EXPERIMENTAL', $ec_dbid);
+  Description: Does what it says on the tin
+  Returntype : Bio::EnsEMBL::Funcgen::Channel object
+  Exceptions : None
+  Caller     : General
+  Status     : Medium Risk
+
+=cut
+
+sub fetch_by_type_experimental_chip_dbID {
+    my ($self, $type, $ec_dbid) = @_;
+
+	throw("Must specify a valid experiemntal dbID and type") if(! $ec_dbid || ! $type);
+
+
+	my $sth = $self->prepare("
+		SELECT c.channel_id
+		FROM channel c
+		WHERE c.experimental_chip_id = ?
+        AND c.type = ?
+	");
+
+	$sth->bind_param(1, $ec_dbid,   SQL_INTEGER);
+	$sth->bind_param(2, $type,      SQL_VARCHAR);
+
+	#can we do a generic fetch here?
+
+
+	$sth->execute();
+	my ($chan_id) = $sth->fetchrow();
+
+
+	return $self->fetch_by_dbID($chan_id) if $chan_id;
+}
+
+
+=head2 fetch_by_dye_experimental_chip_id
+
+  Arg [1]    : string - dye
+  Arg [1]    : int - dbID of Experiment
+  Example    : my $chan = $chan_a->fetch_by_type_experimental_chip_dbID('Cy5', $ec_dbid);
+  Description: Does what it says on the tin
+  Returntype : Bio::EnsEMBL::Funcgen::Channel object
+  Exceptions : None
+  Caller     : General
+  Status     : Medium Risk
+
+=cut
+
+sub fetch_by_dye_experimental_chip_dbID {
+    my $self = shift;
+    my $ec_dbid = shift;
+
+	throw("not yet impemented");
+
+	my ($chan_id, @results);
+
+	throw("Must specify an experiemntal dbID") if(! $ec_dbid);
+
+
+	my $sth = $self->prepare("
+		SELECT c.channel_id
+		FROM experimental_chip ec, channel c
+		WHERE c.experimental_chip_id = ec.experimental_chip_id
+        AND ec.experimental_chip_id = $ec_dbid
+	");
+
+
+
+	#can we do a generic fetch here?
+
+
+	$sth->execute();
+
+
+	while ($chan_id = $sth->fetchrow()){
+		push @results, $self->fetch_by_dbID($chan_id);
+	}
+
+	return \@results;
+}
+
+
+=head2 fetch_all_by_experimental_chip_dbID
+
+  Arg [1]    : int - dbID of Experiment
+  Example    : my @chans = @{$ec_a->fetch_all_by_experimental_chip_dbID($ac_dbid);
+  Description: Does what it says on the tin
+  Returntype : Listref of Bio::EnsEMBL::Funcgen::Channel object
+  Exceptions : None
+  Caller     : General
+  Status     : Medium Risk
+
+=cut
+
+sub fetch_all_by_experimental_chip_dbID {
+    my $self = shift;
+    my $ec_dbid = shift;
+
+	my ($chan_id, @results);
+
+	throw("Must specify an experiemntal dbID") if(! $ec_dbid);
+
+
+	my $sth = $self->prepare("
+		SELECT c.channel_id
+		FROM experimental_chip ec, channel c
+		WHERE c.experimental_chip_id = ec.experimental_chip_id
+        AND ec.experimental_chip_id = $ec_dbid
+	");
+
+
+	#can we do a generic fetch here?
+
+
+	$sth->execute();
+
+
+	while ($chan_id = $sth->fetchrow()){
+		push @results, $self->fetch_by_dbID($chan_id);
+	}
+
+	return \@results;
+}
+
+=head2 fetch_all_by_Experiment
+
+
+
+=cut
+
+sub fetch_all_by_Experiment{
+    my ($self, $exp) = @_;
+   
+	throw("Must provide an Experiment object") if(! $exp->isa('Bio::EnseMBL::Funcgen::Experiment'));
+
+	return $self->fetch_all_by_experimental_chip_dbID($exp->dbID());
+}
+
+
+
+=head2 _tables
+
+  Args       : None
+  Example    : None
+  Description: PROTECTED implementation of superclass abstract method.
+               Returns the names and aliases of the tables to use for queries.
+  Returntype : List of listrefs of strings
+  Exceptions : None
+  Caller     : Internal
+  Status     : Medium Risk
+
+=cut
+
+sub _tables {
+	my $self = shift;
+	
+	return ['channel', 'c'];
+}
+
+=head2 _columns
+
+  Args       : None
+  Example    : None
+  Description: PROTECTED implementation of superclass abstract method.
+               Returns a list of columns to use for queries.
+  Returntype : List of strings
+  Exceptions : None
+  Caller     : Internal
+  Status     : Medium Risk
+
+=cut
+
+sub _columns {
+	my $self = shift;
+	
+	return qw( c.channel_id c.experimental_chip_id c.sample_id c.dye c.type c.description);
+}
+
+=head2 _objs_from_sth
+
+  Arg [1]    : DBI statement handle object
+  Example    : None
+  Description: PROTECTED implementation of superclass abstract method.
+               Creates Channel objects from an executed DBI statement
+			   handle.
+  Returntype : Listref of Bio::EnsEMBL::Funcgen::Channel objects
+  Exceptions : None
+  Caller     : Internal
+  Status     : Medium Risk
+
+=cut
+
+sub _objs_from_sth {
+	my ($self, $sth) = @_;
+	
+	my (@result, $ec_id, $chan_id, $sample_id, $type, $dye, $desc);
+	
+	$sth->bind_columns(\$chan_id, \$ec_id, \$sample_id, \$dye, \$type, \$desc);
+	
+	while ( $sth->fetch() ) {
+		my $chan = Bio::EnsEMBL::Funcgen::Channel->new(
+													   -dbID                 => $chan_id,
+													   -EXPERIMENTAL_CHIP_ID => $ec_id,
+													   -SAMPLE_ID            => $sample_id,
+													   -TYPE                 => $type,
+													   -DYE                  => $dye,
+													   -DESCRIPTION          => $desc,
+													   -ADAPTOR              => $self,
+													   );
+
+		push @result, $chan;
+
+	}
+	return \@result;
+}
+
+
+
+=head2 store
+
+  Args       : List of Bio::EnsEMBL::Funcgen::Channel objects
+  Example    : $chan_a->store($c1, $c2, $c3);
+  Description: Stores given Channel objects in the database. Should only be
+               called once per array because no checks are made for duplicates.
+			   Sets dbID and adaptor on the objects that it stores.
+  Returntype : None
+  Exceptions : None
+  Caller     : General
+  Status     : Medium Risk
+
+=cut
+
+sub store {
+    my $self = shift;
+    my @args = @_;
+
+	my ($sarray);
+
+	my $sth = $self->prepare("
+			INSERT INTO channel
+			(experimental_chip_id, sample_id, dye, type, description)
+			VALUES (?, ?, ?, ?, ?)");
+
+
+
+    foreach my $chan (@args) {
+		if ( ! $chan->isa('Bio::EnsEMBL::Funcgen::Channel') ) {
+			warning('Can only store Channel objects, skipping $chan');
+			next;
+		}
+
+		if (!( $chan->dbID() && $chan->adaptor() == $self )){
+
+
+			my $s_chan = $self->fetch_by_type_experimental_chip_dbID($chan->type(), $chan->experimental_chip_id());
+
+
+			if(! $s_chan){
+				$sth->bind_param(1, $chan->experimental_chip_id(),  SQL_INTEGER);
+				$sth->bind_param(2, $chan->sample_id(),             SQL_VARCHAR);
+				$sth->bind_param(3, $chan->dye() ,                  SQL_VARCHAR);
+				$sth->bind_param(4, $chan->type(),                  SQL_VARCHAR);
+				$sth->bind_param(5, $chan->description(),           SQL_VARCHAR);
+			
+				$sth->execute();
+				my $dbID = $sth->{'mysql_insertid'};
+				$chan->dbID($dbID);
+				$chan->adaptor($self);
+			}
+			else{
+				#do some status checks here, check IMPORTED
+				#Need to account for recover in Importer?
+
+				throw("Channel already stored");
+			}
+		}
+	}
+
+	return \@args;
+
+}
+
+
+=head2 list_dbIDs
+
+  Args       : None
+  Example    : my @array_ids = @{$ec_a->list_dbIDs()};
+  Description: Gets an array of internal IDs for all ExperimentalChip objects in the
+               current database.
+  Returntype : List of ints
+  Exceptions : None
+  Caller     : ?
+  Status     : Medium Risk
+
+=cut
+
+sub list_dbIDs {
+    my ($self) = @_;
+	
+    return $self->_list_dbIDs('experimental_chip');
+}
+
+
+
+1;
+
---
+++ modules/Bio/EnsEMBL/Funcgen/DBSQL/PredictedFeatureAdaptor.pm
@@ -0,0 +1,478 @@
+#
+# Ensembl module for Bio::EnsEMBL::DBSQL::Funcgen::PredictedFeatureAdaptor
+#
+# You may distribute this module under the same terms as Perl itself
+
+=head1 NAME
+
+Bio::EnsEMBL::DBSQL::Funcgen::PredictedFeatureAdaptor - A database adaptor for fetching and
+storing PredictedFeature objects.
+
+=head1 SYNOPSIS
+
+my $pfa = $db->get_PredictedFeatureAdaptor();
+
+my $features = $pfa->fetch_all_by_Slice($slice);
+$features = $pfa->fetch_all_by_Slice_Target($slice, $target);
+
+=head1 DESCRIPTION
+
+The PredictedFeatureAdaptor is a database adaptor for storing and retrieving
+PredictedFeature objects.
+
+=head1 AUTHOR
+
+This module was created by Nathan Johnson.
+
+This module is part of the Ensembl project: http://www.ensembl.org/
+
+=head1 CONTACT
+
+Post comments or questions to the Ensembl development list: ensembl-dev@ebi.ac.uk
+
+=head1 METHODS
+
+=cut
+
+use strict;
+use warnings;
+
+package Bio::EnsEMBL::Funcgen::DBSQL::PredictedFeatureAdaptor;
+
+use Bio::EnsEMBL::Utils::Exception qw( throw warning );
+use Bio::EnsEMBL::Funcgen::PredictedFeature;
+use Bio::EnsEMBL::Funcgen::DBSQL::BaseFeatureAdaptor;
+#use Bio::EnsEMBL::Funcgen::CoordSystem;
+
+use vars qw(@ISA);
+@ISA = qw(Bio::EnsEMBL::Funcgen::DBSQL::BaseFeatureAdaptor);
+
+#fetch_all_by_Slice_Experiment
+#fetch_all_by_Slice_Analysis handled by BaseFeatureAdaptor fetch_all_by_Slice_score, which also takes logic name
+#make wrapper method for useability?
+
+#re-write the methods to take names aswell as objects? 
+
+
+=head2 fetch_all_by_Slice_FeatureType
+
+  Arg [1]    : Bio::EnsEMBL::Slice
+  Arg [2]    : Bio::EnsEMBL::FeatureType
+  Arg [3]    : (optional) string - logic name
+  Example    : my $slice = $sa->fetch_by_region('chromosome', '1');
+               my $features = $ofa->fetch_by_Slice_Target($slice, $target);
+  Description: Retrieves a list of features on a given slice, specific for a given target.
+  Returntype : Listref of Bio::EnsEMBL::PredictedFeature objects
+  Exceptions : Throws if no Target object provided
+  Caller     : General
+  Status     : Medium Risk
+
+=cut
+
+sub fetch_all_by_Slice_FeatureType {
+	my ($self, $slice, $target) = @_;
+	
+	throw('Need type as parameter') if ! $target->isa("Bio::EnsEMBL::FeatureType");
+	
+
+
+	my $constraint = qq( ft.name = '$type' );
+	
+	return $self->SUPER::fetch_all_by_Slice_constraint($slice, $constraint, $logic_name);
+}
+ 
+=head2 _tables
+
+  Args       : None
+  Example    : None
+  Description: PROTECTED implementation of superclass abstract method.
+               Returns the names and aliases of the tables to use for queries.
+  Returntype : List of listrefs of strings
+  Exceptions : None
+  Caller     : Internal
+  Status     : Medium Risk
+
+=cut
+
+sub _tables {
+	my $self = shift;
+	
+	return (
+			[ 'predicted_feature', 'pf' ],
+			[ 'experiment_prediction', 'ep'],
+			
+			#target?
+		   );
+}
+
+=head2 _columns
+
+  Args       : None
+  Example    : None
+  Description: PROTECTED implementation of superclass abstract method.
+               Returns a list of columns to use for queries.
+  Returntype : List of strings
+  Exceptions : None
+  Caller     : Internal
+  Status     : Medium Risk
+
+=cut
+
+sub _columns {
+	my $self = shift;
+	
+	return qw(
+			  pf.predicted_feature_id  pf.seq_region_id
+			  pf.seq_region_start  pf.seq_region_end
+			  pf.seq_region_strand pf.coord_system_id
+			  pf.target_id         pf.display_label
+			  pf.analysis_id	   pf.score
+			 );
+}
+
+
+#change this to experiment_prediction query?
+#do we need this?
+
+=head2 _default_where_clause
+
+  Args       : None
+  Example    : None
+  Description: PROTECTED implementation of superclass abstract method.
+               Returns an additional table joining constraint to use for
+			   queries.
+  Returntype : List of strings
+  Exceptions : None
+  Caller     : Internal
+  Status     : Medium Risk
+
+=cut
+
+sub _default_where_clause {
+	my $self = shift;
+	
+	return 'pf.predicted_feature_id = ep.predicted_feature_id';
+}
+
+=head2 _final_clause
+
+  Args       : None
+  Example    : None
+  Description: PROTECTED implementation of superclass abstract method.
+               Returns an ORDER BY clause. Sorting by oligo_feature_id would be
+			   enough to eliminate duplicates, but sorting by location might
+			   make fetching features on a slice faster.
+  Returntype : String
+  Exceptions : None
+  Caller     : generic_fetch
+  Status     : Medium Risk
+
+=cut
+
+
+sub _final_clause {
+	return ' ORDER BY pf.seq_region_id, pf.seq_region_start, pf.predicted_feature_id';
+}
+
+
+=head2 _objs_from_sth
+
+  Arg [1]    : DBI statement handle object
+  Example    : None
+  Description: PROTECTED implementation of superclass abstract method.
+               Creates PredictedFeature objects from an executed DBI statement
+			   handle.
+  Returntype : Listref of Bio::EnsEMBL::PredictedFeature objects
+  Exceptions : None
+  Caller     : Internal
+  Status     : Medium Risk
+
+=cut
+
+sub _objs_from_sth {
+	my ($self, $sth, $mapper, $dest_slice) = @_;
+
+	#For EFG this has to use a dest_slice from core/dnaDB whether specified or not.
+	#So if it not defined then we need to generate one derived from the species_name and schema_build of the feature we're retrieving.
+
+
+	
+	# This code is ugly because caching is used to improve speed
+
+	#my $sa = $self->db->get_SliceAdaptor();
+	
+	my ($sa, $old_cs_id);
+	$sa = $dest_slice->adaptor->db->get_SliceAdaptor() if($dest_slice);#don't really need this if we're using DNADBSliceAdaptor?
+
+	#Some of this in now probably overkill as we'll always be using the DNADB as the slice DB
+	#Hence it should always be on the same coord system
+
+	my $aa = $self->db->get_AnalysisAdaptor();
+	my @features;
+	my (%analysis_hash, %slice_hash, %sr_name_hash, %sr_cs_hash);
+
+	my (
+		$oligo_feature_id,  $seq_region_id,
+		$seq_region_start,  $seq_region_end,
+		$seq_region_strand, $cs_id,
+		$mismatches,        $oligo_probe_id,    
+		$analysis_id,       $oligo_probe_name,
+	);
+	$sth->bind_columns(
+					   \$oligo_feature_id,  \$seq_region_id,
+					   \$seq_region_start,  \$seq_region_end,
+					   \$seq_region_strand, \$cs_id,
+					   \$mismatches,        \$oligo_probe_id,
+					   \$analysis_id,		\$oligo_probe_name,
+	);
+
+	my $asm_cs;
+	my $cmp_cs;
+	my $asm_cs_name;
+	my $asm_cs_vers;
+	my $cmp_cs_name;
+	my $cmp_cs_vers;
+	if ($mapper) {
+		$asm_cs      = $mapper->assembled_CoordSystem();
+		$cmp_cs      = $mapper->component_CoordSystem();
+		$asm_cs_name = $asm_cs->name();
+		$asm_cs_vers = $asm_cs->version();
+		$cmp_cs_name = $cmp_cs->name();
+		$cmp_cs_vers = $cmp_cs->version();
+	}
+
+	my $dest_slice_start;
+	my $dest_slice_end;
+	my $dest_slice_strand;
+	my $dest_slice_length;
+	my $dest_slice_sr_name;
+	if ($dest_slice) {
+		$dest_slice_start   = $dest_slice->start();
+		$dest_slice_end     = $dest_slice->end();
+		$dest_slice_strand  = $dest_slice->strand();
+		$dest_slice_length  = $dest_slice->length();
+		$dest_slice_sr_name = $dest_slice->seq_region_name();
+	}
+
+	my $last_feature_id = -1;
+	FEATURE: while ( $sth->fetch() ) {
+		  #Need to build a slice adaptor cache here?
+		  #Would only ever want to do this if we enable mapping between assemblies??
+		  #Or if we supported the mapping between cs systems for a given schema_build, which would have to be handled by the core api
+		  
+	  
+		  if($old_cs_id && ($old_cs_id != $cs_id)){
+			  throw("More than one coord_system for feature query, need to implement SliceAdaptor hash?");
+		  }
+		  
+		  $old_cs_id = $cs_id;
+
+
+		  #Need to make sure we are restricting calls to Experiment and channel(i.e. the same coord_system_id)
+
+		  $sa ||= $self->db->get_DNADBSliceAdaptor($cs_id);
+
+
+
+		# This assumes that features come out sorted by ID
+		next if ($last_feature_id == $oligo_feature_id);
+		$last_feature_id = $oligo_feature_id;
+
+		# Get the analysis object
+		my $analysis = $analysis_hash{$analysis_id} ||= $aa->fetch_by_dbID($analysis_id);
+
+		# Get the slice object
+		my $slice = $slice_hash{'ID:'.$seq_region_id};
+
+		if (!$slice) {
+			$slice                            = $sa->fetch_by_seq_region_id($seq_region_id);
+			$slice_hash{'ID:'.$seq_region_id} = $slice;
+			$sr_name_hash{$seq_region_id}     = $slice->seq_region_name();
+			$sr_cs_hash{$seq_region_id}       = $slice->coord_system();
+		}
+
+		my $sr_name = $sr_name_hash{$seq_region_id};
+		my $sr_cs   = $sr_cs_hash{$seq_region_id};
+
+		# Remap the feature coordinates to another coord system if a mapper was provided
+		if ($mapper) {
+
+			throw("Not yet implmented mapper, check equals are Funcgen calls too!");
+
+			($sr_name, $seq_region_start, $seq_region_end, $seq_region_strand)
+				= $mapper->fastmap($sr_name, $seq_region_start, $seq_region_end, $seq_region_strand, $sr_cs);
+
+			# Skip features that map to gaps or coord system boundaries
+			next FEATURE if !defined $sr_name;
+
+			# Get a slice in the coord system we just mapped to
+			if ( $asm_cs == $sr_cs || ( $cmp_cs != $sr_cs && $asm_cs->equals($sr_cs) ) ) {
+				$slice = $slice_hash{"NAME:$sr_name:$cmp_cs_name:$cmp_cs_vers"}
+					||= $sa->fetch_by_region($cmp_cs_name, $sr_name, undef, undef, undef, $cmp_cs_vers);
+			} else {
+				$slice = $slice_hash{"NAME:$sr_name:$asm_cs_name:$asm_cs_vers"}
+					||= $sa->fetch_by_region($asm_cs_name, $sr_name, undef, undef, undef, $asm_cs_vers);
+			}
+		}
+
+		# If a destination slice was provided convert the coords
+		# If the destination slice starts at 1 and is forward strand, nothing needs doing
+		if ($dest_slice) {
+			unless ($dest_slice_start == 1 && $dest_slice_strand == 1) {
+				if ($dest_slice_strand == 1) {
+					$seq_region_start = $seq_region_start - $dest_slice_start + 1;
+					$seq_region_end   = $seq_region_end   - $dest_slice_start + 1;
+				} else {
+					my $tmp_seq_region_start = $seq_region_start;
+					$seq_region_start        = $dest_slice_end - $seq_region_end       + 1;
+					$seq_region_end          = $dest_slice_end - $tmp_seq_region_start + 1;
+					$seq_region_strand      *= -1;
+				}
+			}
+
+			# Throw away features off the end of the requested slice
+			next FEATURE if $seq_region_end < 1 || $seq_region_start > $dest_slice_length
+				|| ( $dest_slice_sr_name ne $sr_name );
+
+			$slice = $dest_slice;
+		}
+
+		push @features, $self->_new_fast( {
+			'start'         => $seq_region_start,
+			'end'           => $seq_region_end,
+			'strand'        => $seq_region_strand,
+			'slice'         => $slice,
+			'analysis'      => $analysis,
+			'adaptor'       => $self,
+			'dbID'          => $oligo_feature_id,
+			'mismatchcount' => $mismatches,
+			'_probe_id'     => $oligo_probe_id,
+			#'probeset'      => $probeset,#???do we need this?
+			'_probe_name'   => $oligo_probe_name
+		} );
+	}
+
+	return \@features;
+}
+
+=head2 _new_fast
+
+  Args       : Hashref to be passed to PredictedFeature->new_fast()
+  Example    : None
+  Description: Construct a PredictedFeature object using quick and dirty new_fast.
+  Returntype : Bio::EnsEMBL::Funcgen::PredictedFeature
+  Exceptions : None
+  Caller     : _objs_from_sth
+  Status     : Medium Risk
+
+=cut
+
+sub _new_fast {
+	my $self = shift;
+	
+	my $hash_ref = shift;
+	return Bio::EnsEMBL::Funcgen::PredictedFeature->new_fast($hash_ref);
+}
+
+=head2 store
+
+  Args       : List of Bio::EnsEMBL::Funcgen::PredictedFeature objects
+  Example    : $ofa->store(@features);
+  Description: Stores given PredictedFeature objects in the database. Should only
+               be called once per feature because no checks are made for
+			   duplicates. Sets dbID and adaptor on the objects that it stores.
+  Returntype : None
+  Exceptions : Throws if a list of PredictedFeature objects is not provided or if
+               an analysis is not attached to any of the objects
+  Caller     : General
+  Status     : Medium Risk
+
+=cut
+
+sub store{
+	my ($self, @ofs) = @_;
+
+	if (scalar(@ofs) == 0) {
+		throw('Must call store with a list of OligoFeature objects');
+	}
+
+	my $sth = $self->prepare("
+		INSERT INTO oligo_feature (
+			seq_region_id,  seq_region_start,
+			seq_region_end, seq_region_strand,
+            coord_system_id,
+			oligo_probe_id,  analysis_id,
+			mismatches
+		) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+	");
+
+	my $db = $self->db();
+	my $analysis_adaptor = $db->get_AnalysisAdaptor();
+
+	FEATURE: foreach my $of (@ofs) {
+
+		if( !ref $of || !$of->isa('Bio::EnsEMBL::Funcgen::OligoFeature') ) {
+			throw('Feature must be an OligoFeature object');
+		}
+
+		if ( $of->is_stored($db) ) {
+			warning('OligoFeature [' . $of->dbID() . '] is already stored in the database');
+			next FEATURE;
+		}
+
+		if ( !defined $of->analysis() ) {
+			throw('An analysis must be attached to the OligoFeature objects to be stored.');
+		}
+
+		# Store the analysis if it has not been stored yet
+		if ( !$of->analysis->is_stored($db) ) {
+			$analysis_adaptor->store( $of->analysis() );
+		}
+
+		my $original = $of;
+		my $seq_region_id;
+		($of, $seq_region_id) = $self->_pre_store($of);
+
+		$sth->bind_param(1, $seq_region_id,        SQL_INTEGER);
+		$sth->bind_param(2, $of->start(),          SQL_INTEGER);
+		$sth->bind_param(3, $of->end(),            SQL_INTEGER);
+		$sth->bind_param(4, $of->strand(),         SQL_TINYINT);
+		$sth->bind_param(5, $of->coord_system_id(),SQL_INTEGER);
+		$sth->bind_param(6, $of->probe->dbID(),    SQL_INTEGER);
+		$sth->bind_param(7, $of->analysis->dbID(), SQL_INTEGER);
+		$sth->bind_param(8, $of->mismatchcount(),  SQL_TINYINT);
+
+		$sth->execute();
+
+		$original->dbID( $sth->{'mysql_insertid'} );
+		$original->adaptor($self);
+	}
+}
+
+=head2 list_dbIDs
+
+  Args       : None
+  Example    : my @feature_ids = @{$ofa->list_dbIDs()};
+  Description: Gets an array of internal IDs for all OligoFeature objects in
+               the current database.
+  Returntype : List of ints
+  Exceptions : None
+  Caller     : ?
+  Status     : Medium Risk
+
+=cut
+
+sub list_dbIDs {
+	my $self = shift;
+	
+	return $self->_list_dbIDs('predicted_feature');
+}
+
+
+
+
+
+
+
+1;
+
