8f9bb7dea86fecc50d5d8e782fb40f0f67ed8e21
Nathan Johnson <njohnson@ebi.ac.uk>
2007-05-09 12:09:54 +0000

    fixed bug in fetch_ResultFeatures_by_Slice_ResultSet

--- modules/Bio/EnsEMBL/Funcgen/DBSQL/ResultSetAdaptor.pm
+++ modules/Bio/EnsEMBL/Funcgen/DBSQL/ResultSetAdaptor.pm
@@ -564,7 +564,7 @@ sub fetch_ResultFeatures_by_Slice_ResultSet{
   my $sql = "SELECT ec.biological_replicate, cc.chip_channel_id from experimental_chip ec, chip_channel cc 
              WHERE cc.table_name='experimental_chip'
              AND ec.experimental_id=cc.table_id
-             AND cc.table_id IN(".join(', ', (map $_->dbID(), $rset->get_ExperimentalChips())).")";
+             AND cc.table_id IN(".join(', ', (map $_->dbID(), @{$rset->get_ExperimentalChips()})).")";
 
   
   my $sth = $self->prepare($sql);
