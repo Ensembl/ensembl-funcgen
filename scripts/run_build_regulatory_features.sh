1467633c9ff82d7d91df3fa242ca7ace9ca35f50
Stefan Graef <graef@sanger.ac.uk>
2007-09-20 16:37:26 +0000

    wrapper shell script to run regulatory build v47

---
+++ scripts/run_build_regulatory_features.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+
+if [ $# -lt 4 ]; then
+
+	echo "Usage: $0 <host> <port> <user> <password>"
+	exit;
+
+fi
+
+
+HOST=$1
+shift
+PORT=$1
+shift
+USER=$1
+shift
+PASS=$1
+shift
+ARGS=$*
+
+FOCUS=\
+CD4_DNASE_IMPORT,\
+GM06990_DNASE_IMPORT
+
+TARGET=\
+CD4_CTCF,\
+CD4_H2AZ,\
+CD4_H2BK5me1,\
+CD4_H3K27me1,\
+CD4_H3K27me2,\
+CD4_H3K27me3,\
+CD4_H3K36me1,\
+CD4_H3K36me3,\
+CD4_H3K4me1,\
+CD4_H3K4me2,\
+CD4_H3K4me3,\
+CD4_H3K79me1,\
+CD4_H3K79me2,\
+CD4_H3K79me3,\
+CD4_H3K9me1,\
+CD4_H3K9me2,\
+CD4_H3K9me3,\
+CD4_H3R2me1,\
+CD4_H3R2me2,\
+CD4_H4K20me1,\
+CD4_H4K20me3,\
+CD4_H4R3me2,\
+CD4_PolII,\
+Nessie_NG_STD_2_ctcf_ren_BR1,\
+Wiggle_H3K27me3,\
+Wiggle_H3K36me3,\
+Wiggle_H3K4me3,\
+Wiggle_H3K79me3,\
+Wiggle_H3K9me3,\
+Wiggle_H4K20me3
+
+OUTDIR=/lustre/scratch1/ensembl/graef/RegBuild/v47
+
+# first dump annotated features from database 
+# for further processing
+
+bsub -o "$OUTDIR/RegulatoryBuild.log" -J RegBuild_Dump \
+$EFG_SRC/scripts/build_regulatory_features.pl \
+    -host $HOST \
+    -port $PORT \
+    -user $USER \
+    -pass $PASS \
+    -dbname homo_sapiens_funcgen_47_36i \
+    -data_version 46_36h \
+    -outdir $OUTDIR \
+    -dump \
+    -focus $FOCUS \
+    -target $TARGET \
+    $ARGS
+
+# build regulatory features and dump to file when dumping 
+# features has been sucessfully completed
+
+bsub -o "$OUTDIR/RegulatoryBuild.log" -J RegBuild_[1-25] \
+    -w 'done(RegBuild_Dump)' \
+    $EFG_SRC/scripts/build_regulatory_features.pl \
+    -host $HOST \
+    -port $PORT \
+    -user $USER \
+    -pass $PASS \
+    -dbname homo_sapiens_funcgen_47_36i \
+    -data_version 46_36h \
+    -focus $FOCUS \
+    -target $TARGET \
+    -outdir $OUTDIR \
+    -dump_features \
+    $ARGS
+
+#    -s 1 \
