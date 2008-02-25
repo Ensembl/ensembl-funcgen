e163f27fa67094c33d7d5d00843ab8b665788ce9
Nathan Johnson <njohnson@ebi.ac.uk>
2008-02-25 17:21:10 +0000

    added options and fixed db naming

--- scripts/update_DB_for_release.pl
+++ scripts/update_DB_for_release.pl
@@ -1,56 +1,63 @@
 #!/usr/local/ensembl/bin/perl -w
 
-
 use strict;
 use Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::Funcgen::Utils::HealthChecker;
+use Pod::Usage;
+use Getopt::Long;
 
-
-my $reg = "Bio::EnsEMBL::Registry";
-
-#my $species = 'mus_musculus';
-#my $species = 'homo_sapiens';
-my $species = 'drosophila_melanogaster';
-my $schema_build = '49_44';
-my $pass = shift @ARGV;
-my $port = 3306;
+my ($pass, $species, $schema_build, $skip_meta_coord, $from_ensdb, $dnadb, $check_displayable);
+my ($help, $man, $dbname);
 my $user = 'ensadmin';
+my $port = 3306;
 my $host = 'ens-genomics1';
-my $skip_mc;
+
+GetOptions( 'help'             => \$help,
+            'host|h=s'         => \$host,
+            'port=i'           => \$port,
+            'user|u=s'         => \$user,
+            'pass|p=s'         => \$pass,
+			'dbname=s'         => \$dbname,
+			#'slice=s'          => \$test_slice,
+            'species|d=s'      => \$species,
+			'data_versions=s' => \$schema_build,#mandatory as default ensembldb will not exist
+			'from_ensembldb'   => \$from_ensdb,
+			'skip_meta_coord'  => \$skip_meta_coord,
+			#skip dumps?
+			#force update
+			'check_displayable' => \$check_displayable,
+			"help|?"             => \$help,
+			"man|m"              => \$man,
+			#add opt for old, new & stable fset name
+		   );
+
+
+pod2usage(1) if $help;
+pod2usage(-exitstatus => 0, -verbose => 2) if $man;
+
+
+
 my @builds = @ARGV;
 
-#only loads v43 no v44 DBs???
-#$reg->load_registry_from_db(
-#							-host => 'ens-staging',
-#							-user => 'ensro',
-#							#-port => '3362',
-#							-verbose => 1,
-#						   );#
+$dbname = "${species}_funcgen_${schema_build}" if ! defined $dbname;
 
-
-
-
-
-
-
-my $dnadb = Bio::EnsEMBL::DBSQL::DBAdaptor->new(
-												-dbname  => "${species}_core_${schema_build}",
-												-host    => 'ens-staging',# 
-												#-host    => 'ensembldb',
-												-port    => 3306,
-												-user    =>  $user,
-												#-user    =>  'anonymous',
-												-pass    => $pass,
-												-species => $species,
+if(! $from_ensdb){
+  $dnadb = Bio::EnsEMBL::DBSQL::DBAdaptor->new(
+											   -dbname  => "${species}_core_${schema_build}",
+											   -host    => 'ens-staging',
+											   -port    => 3306,
+											   -user    =>  'ensro',
+											   #-pass    => $pass,
+											   -species => $species,
+											   -group   => 'core',
 											   );
-die ("You have not provided sufficient arguments to make a DB connection\n".
-	 "-dbname  => ${species}_funcgen_${schema_build}, host   => $host, port   => $port, user   => $user, pass   => $pass") if ! $dnadb;
+}
 
 
 #This will add the default chromosome CS, but not any other levels
 my $efg_db = Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor->new(
-														  -dbname  => "${species}_funcgen_${schema_build}",
+														  -dbname  => $dbname,
 														  -host    => $host,
 														  -port    => $port,
 														  -user    => $user,
@@ -60,10 +67,21 @@ my $efg_db = Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor->new(
 														 );
 
 
+
+#Test the db connections
+$efg_db->dbc->db_handle;
+$efg_db->dnadb->dbc->db_handle;
+
+
 my $hchecker = Bio::EnsEMBL::Funcgen::Utils::HealthChecker->new(
 																-db     => $efg_db,
 																-builds => \@builds,
+																-skip_meta_coord => $skip_meta_coord,
+																#-force_update
+																#skip_dumps
+																-check_displayable => $check_displayable,
 															   );
 
-$hchecker->update_db_for_release($skip_mc);
+$hchecker->update_db_for_release();
 $hchecker->log_data_sets();
+
