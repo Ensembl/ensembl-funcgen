d11dc45e1351b303ebe11b4705ed5aefc98fa1f2
Nathan Johnson <njohnson@ebi.ac.uk>
2008-03-11 15:00:59 +0000

    separated human only data patch to b

---
+++ sql/patch_49_50b.sql
@@ -0,0 +1,8 @@
+insert into feature_type values(NULL, 'TSS 2.5KB', 'Pseudo', 'Feature is within +/-2.5KB of the Transcript Start Site');
+insert into feature_type values(NULL, 'TTS 2.5KB', 'Pseudo', 'Feature is within +/-2.5KB of the Transcript Termination Site');
+
+
+--Human only data patch
+--This may change if we do another build for v50
+--update meta set meta_value='9,8,23,24,25,26,11,27,12,6,5,4,28,29,13,30,31,14,32,33,34,10,35,36,8,9,11,12,4,13,14,10,178400,178401' where meta_key='regulatory_string_feature_type_id';
+--End human only data patch
