1069467362443175fa08076dfe98bab4eb895b1c
Stefan Graef <graef@sanger.ac.uk>
2008-05-14 10:08:25 +0000

    get_encode_regions smartened

--- modules/Bio/EnsEMBL/Funcgen/Utils/Encode.pm
+++ modules/Bio/EnsEMBL/Funcgen/Utils/Encode.pm
@@ -39,48 +39,44 @@ use vars qw(@ISA @EXPORT_OK);
 
 use Bio::EnsEMBL::Utils::Exception qw(throw);
 use POSIX;
+use Data::Dumper;
 
 =head2 get_encode_regions
 
   Arg [1]     : Bio::EnsEMBL::DBSQL::DBAdaptor
-  Arg [2]     : assembly version string, e.g. NCBI35 (optional)
   Description : fetch encode region coordinates from core db
   Returns     : reference to hash with Encode region names as keys
-  Exceptions  : 
-  Example     : 
+  Exceptions  : Throws if no Bio::EnsEMBL::DBSQL::DBAdaptor defined
+  Example     : get_encode_regions($dnadb);
 
 =cut
 
 sub get_encode_regions {
 
-    my ($db, $version) = @_;
+    my $db = shift;
     
-    $version = undef if (! @_);
-
-    my @chromosomes=qw(1 2 3 4 5 6 7 8 9 10 11 12 13 14 
-                       15 16 17 18 19 20 21 22 X Y MT);
+    throw("Need to pass a valid Bio::EnsEMBL::DBSQL::DBAdaptor") 
+        if (! ($db  && $db->isa("Bio::EnsEMBL::DBSQL::DBAdaptor")));
 
     my $sa = $db->get_SliceAdaptor();
+    my $tls = $sa->fetch_all('toplevel');
+    #print Dumper $tls;
+
     my $mfa = $db->get_MiscFeatureAdaptor();
 
-    my @mf;
-    foreach my $c (@chromosomes)
-    {
-        my $s = $sa->fetch_by_region
-            ('chromosome', $c, undef, undef, undef, $version);
-        push @mf, @{$mfa->fetch_all_by_Slice_and_set_code($s, 'encode')};
-    }
-    
+    my @encode_regions;
+    map { push @encode_regions,
+          @{$mfa->fetch_all_by_Slice_and_set_code($_, 'encode')} } @$tls;
+    #print Dumper @encode_regions;
+
     my %encode_regions;
-    foreach my $mf (@mf) 
-    {
-        $encode_regions{$mf->display_id} = sprintf
-            ("%s:%s:%s:%d:%d:%d",
-             $mf->slice->coord_system_name, 
-             $mf->slice->coord_system()->version(),
-             $mf->slice->seq_region_name,
-             $mf->start, $mf->end, $mf->strand);
-    }
+    map { $encode_regions{$_->display_id} = sprintf
+              ("%s:%s:%s:%d:%d:%d",
+               $_->slice->coord_system_name, 
+               $_->slice->coord_system()->version(),
+               $_->slice->seq_region_name,
+               $_->start, $_->end, $_->strand);
+      } @encode_regions;
     
     return \%encode_regions;
 
