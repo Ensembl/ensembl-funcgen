b27e1504cff2c228fd7a1a044d5e1eb032b374d5
Nathan Johnson <njohnson@ebi.ac.uk>
2008-07-28 15:59:28 +0000

    Fixed table_name/object_name mismatch and added FeatureType to internal methods

--- modules/Bio/EnsEMBL/Funcgen/DBSQL/DBEntryAdaptor.pm
+++ modules/Bio/EnsEMBL/Funcgen/DBSQL/DBEntryAdaptor.pm
@@ -65,7 +65,8 @@ use strict;
 
 =cut
 
-
+#Is this different to the core code?
+#Yes we don't have a max rows limit!
 
 sub fetch_by_db_accession {
   my $self = shift;
@@ -142,6 +143,200 @@ sub fetch_by_db_accession {
   return $exDB;
 }
 
+
+=head2 _fetch_by_object_type
+
+  Arg [1]    : string $ensID
+  Arg [2]    : string $ensType
+  			   (object type to be returned) 
+  Arg [3]    : optional $exdbname (external database name)
+  Arf [4]    : optional $exdb_type (external database type)
+  Example    : $self->_fetch_by_object_type( $translation_id, 'Translation' )
+  Description: Fetches DBEntry by Object type
+  Returntype : arrayref of DBEntry objects; may be of type IdentityXref if
+               there is mapping data, or GoXref if there is linkage data.
+  Exceptions : none
+  Caller     : fetch_all_by_Gene
+               fetch_all_by_Translation
+               fetch_all_by_Transcript
+  Status     : Stable
+
+=cut
+
+#We need to modify this to return full DBEntries with ensembl_object and linkage annotation
+#for a given external_id/accession
+#Currently can only list reg feat IDs for a given external_id
+#But we want 
+
+
+sub _fetch_by_object_type {
+  my ( $self, $ensID, $ensType, $exdbname, $exdb_type ) = @_;
+
+  my @out;
+
+  if ( !defined($ensID) ) {
+    throw("Can't fetch_by_EnsObject_type without an object");
+  }
+
+  if ( !defined($ensType) ) {
+    throw("Can't fetch_by_EnsObject_type without a type");
+  }
+
+  #  my $sth = $self->prepare("
+  my $sql = (<<SSQL);
+    SELECT xref.xref_id, xref.dbprimary_acc, xref.display_label, xref.version,
+           xref.description,
+           exDB.dbprimary_acc_linkable, exDB.display_label_linkable,
+           exDB.priority,
+           exDB.db_name, exDB.db_release, exDB.status, exDB.db_display_name,
+           exDB.secondary_db_name, exDB.secondary_db_table,
+           oxr.object_xref_id,
+           es.synonym,
+           idt.query_identity, idt.target_identity, idt.hit_start,
+           idt.hit_end, idt.translation_start, idt.translation_end,
+           idt.cigar_line, idt.score, idt.evalue, idt.analysis_id,
+           gx.linkage_type,
+           xref.info_type, xref.info_text, exDB.type, gx.source_xref_id,
+           oxr.linkage_annotation
+    FROM   (xref xref, external_db exDB, object_xref oxr)
+    LEFT JOIN external_synonym es on es.xref_id = xref.xref_id 
+    LEFT JOIN identity_xref idt on idt.object_xref_id = oxr.object_xref_id
+    LEFT JOIN go_xref gx on gx.object_xref_id = oxr.object_xref_id
+    WHERE  xref.xref_id = oxr.xref_id
+      AND  xref.external_db_id = exDB.external_db_id 
+      AND  oxr.ensembl_id = ?
+      AND  oxr.ensembl_object_type = ?
+SSQL
+  $sql .= " AND exDB.db_name like '" . $exdbname . "' " if ($exdbname);
+  $sql .= " AND exDB.type like '" . $exdb_type . "' "   if ($exdb_type);
+  my $sth = $self->prepare($sql);
+
+  $sth->bind_param( 1, $ensID,   SQL_INTEGER );
+  $sth->bind_param( 2, $ensType, SQL_VARCHAR );
+  $sth->execute();
+
+  my ( %seen, %linkage_types, %synonyms );
+
+  my $max_rows = 1000;
+
+  while ( my $rowcache = $sth->fetchall_arrayref( undef, $max_rows ) ) {
+    while ( my $arrRef = shift( @{$rowcache} ) ) {
+      my ( $refID,                  $dbprimaryId,
+           $displayid,              $version,
+           $desc,                   $primary_id_linkable,
+           $display_id_linkable,    $priority,
+           $dbname,                 $release,
+           $exDB_status,            $exDB_db_display_name,
+           $exDB_secondary_db_name, $exDB_secondary_db_table,
+           $objid,                  $synonym,
+           $queryid,                $targetid,
+           $query_start,            $query_end,
+           $translation_start,      $translation_end,
+           $cigar_line,             $score,
+           $evalue,                 $analysis_id,
+           $linkage_type,           $info_type,
+           $info_text,              $type,
+           $source_xref_id,          $link_annotation
+      ) = @$arrRef;
+
+      my $linkage_key =
+        ( $linkage_type || '' ) . ( $source_xref_id || '' );
+
+      my %obj_hash = ( 'adaptor'            => $self,
+                       'dbID'               => $refID,
+                       'primary_id'         => $dbprimaryId,
+                       'display_id'         => $displayid,
+                       'version'            => $version,
+                       'release'            => $release,
+                       'info_type'          => $info_type,
+                       'info_text'          => $info_text,
+                       'type'               => $type,
+                       'secondary_db_name'  => $exDB_secondary_db_name,
+                       'secondary_db_table' => $exDB_secondary_db_table,
+                       'dbname'             => $dbname,
+					   'linkage_annotation' => $link_annotation);
+
+      # Using an outer join on the synonyms as well as on identity_xref,
+      # we now have to filter out the duplicates (see v.1.18 for
+      # original). Since there is at most one identity_xref row per
+      # xref, this is easy enough; all the 'extra' bits are synonyms.
+      if ( !$seen{$refID} ) {
+        my $exDB;
+
+        if ( ( defined($queryid) ) ) {  # an xref with similarity scores
+          $exDB = Bio::EnsEMBL::IdentityXref->new_fast( \%obj_hash );
+          $exDB->query_identity($queryid);
+          $exDB->target_identity($targetid);
+
+          if ( defined($analysis_id) ) {
+            my $analysis =
+              $self->db()->get_AnalysisAdaptor()
+              ->fetch_by_dbID($analysis_id);
+
+            if ( defined($analysis) ) { $exDB->analysis($analysis) }
+          }
+
+          $exDB->cigar_line($cigar_line);
+          $exDB->query_start($query_start);
+          $exDB->translation_start($translation_start);
+          $exDB->translation_end($translation_end);
+          $exDB->score($score);
+          $exDB->evalue($evalue);
+
+        } elsif ( defined $linkage_type && $linkage_type ne "" ) {
+          $exDB = Bio::EnsEMBL::GoXref->new_fast( \%obj_hash );
+          my $source_xref = ( defined($source_xref_id)
+                              ? $self->fetch_by_dbID($source_xref_id)
+                              : undef );
+          $exDB->add_linkage_type( $linkage_type, $source_xref || () );
+          $linkage_types{$refID}->{$linkage_key} = 1;
+
+        } else {
+          $exDB = Bio::EnsEMBL::DBEntry->new_fast( \%obj_hash );
+        }
+
+        if ( defined($desc) )        { $exDB->description($desc) }
+        if ( defined($exDB_status) ) { $exDB->status($exDB_status) }
+
+        $exDB->primary_id_linkable($primary_id_linkable);
+        $exDB->display_id_linkable($display_id_linkable);
+        $exDB->priority($priority);
+        $exDB->db_display_name($exDB_db_display_name);
+
+        push( @out, $exDB );
+        $seen{$refID} = $exDB;
+
+      } ## end if ( !$seen{$refID} )
+
+      # $exDB still points to the same xref, so we can keep adding GO
+      # evidence tags or synonyms.
+
+      if ( defined($synonym) && !$synonyms{$refID}->{$synonym} ) {
+        if ( defined($synonym) ) {
+          $seen{$refID}->add_synonym($synonym);
+        }
+        $synonyms{$refID}->{$synonym} = 1;
+      }
+
+      if (    defined($linkage_type)
+           && $linkage_type ne ""
+           && !$linkage_types{$refID}->{$linkage_key} )
+      {
+        my $source_xref = ( defined($source_xref_id)
+                            ? $self->fetch_by_dbID($source_xref_id)
+                            : undef );
+        $seen{$refID}
+          ->add_linkage_type( $linkage_type, $source_xref || () );
+        $linkage_types{$refID}->{$linkage_key} = 1;
+      }
+    } ## end while ( my $arrRef = shift...
+  } ## end while ( my $rowcache = $sth...
+
+  return \@out;
+} ## end sub _fetch_by_object_type
+
+
+
 #Placeholders to catch error
 #These now work in reverse as the Gene/Transcript/Translation 
 #is the xref not the ensembl_object as with the core code
@@ -184,21 +379,6 @@ sub fetch_all_by_Translation {
 #Haven't we replaced these for eFG feature with a direct call in the object/object_adaptor?
 
 
-=head2 list_gene_ids_by_external_db_id
-
-  Arg [1]    : string $external_id
-  Example    : @gene_ids = $dbea->list_gene_ids_by_external_db_id(1020);
-  Description: Retrieve a list of geneid by an external identifier that is
-               linked to  any of the genes transcripts, translations or the
-               gene itself. NOTE: if more than one external identifier has the
-               same primary accession then genes for each of these is returned.
-  Returntype : list of ints
-  Exceptions : none
-  Caller     : unknown
-  Status     : Stable
-
-=cut
-
 sub list_gene_ids_by_external_db_id{
    my ($self,$external_db_id) = @_;
 
@@ -213,20 +393,7 @@ throw('Not implemented in eFG, maybe you want the core DBEntryAdaptor?');
    return keys %T;
 }
 
-=head2 list_gene_ids_by_extids
 
-  Arg [1]    : string $external_name
-  Arg [2]    : (optional) string $external_db_name
-  Example    : @gene_ids = $dbea->list_gene_ids_by_extids('ARSE');
-  Description: Retrieve a list of geneid by an external identifier that is 
-               linked to  any of the genes transcripts, translations or the 
-               gene itself 
-  Returntype : list of ints
-  Exceptions : none
-  Caller     : unknown
-  Status     : Stable
-
-=cut
 
 sub list_gene_ids_by_extids {
   my ( $self, $external_name, $external_db_name ) = @_;
@@ -280,18 +447,6 @@ throw('Not implemented in eFG, maybe you want the core DBEntryAdaptor?');
 }
 
 
-=head2 list_translation_ids_by_extids
-
-  Arg [1]    : string $external_name
-  Arg [2]    : (optional) string $external_db_name
-  Example    : @tr_ids = $dbea->list_gene_ids_by_extids('GO:0004835');
-  Description: Gets a list of translation IDs by external display IDs
-  Returntype : list of Ints
-  Exceptions : none
-  Caller     : unknown
-  Status     : Stable
-
-=cut
 
 sub list_translation_ids_by_extids {
   my ( $self, $external_name, $external_db_name ) = @_;
@@ -483,22 +638,27 @@ sub _type_by_external_id {
   #      t.is_current = 1 AND
   #  );
   #}
-  #els
-  if(lc($ensType) eq 'regulatory_feature'){
+  #
+
+
+  if(lc($ensType) eq 'regulatoryfeature'){
 	$from_sql  = 'regulatory_feature rf, ';
 	$where_sql = qq( rf.regulatory_feature_id = oxr.ensembl_id AND );
-	#rf.is_current = 1 AND );
   }
-  elsif(lc($ensType) eq 'external_feature'){
+  elsif(lc($ensType) eq 'externalfeature'){
 	$from_sql  = 'external_feature ef, ';
 	$where_sql = qq( ef.external_feature_id = oxr.ensembl_id AND );
-	#rf.is_current = 1 AND );
-  }
-  elsif(lc($ensType) eq 'annotated_feature'){
+  } 
+  elsif(lc($ensType) eq 'annotatedfeature'){
 	$from_sql  = 'annotated_feature af, ';
 	$where_sql = qq( af.annotated_feature_id = oxr.ensembl_id AND );
-	#rf.is_current = 1 AND );
   }
+  elsif(lc($ensType) eq 'featuretype'){
+	$from_sql  = 'featuretype ft, ';
+	$where_sql = qq( ft.feature_type_id = oxr.ensembl_id AND );
+  }
+
+  
 
   if ( defined($external_db_name) ) {
     # Involve the 'external_db' table to limit the hits to a particular
@@ -632,23 +792,22 @@ sub _type_by_external_db_id{
   #   );
  # }els
 
-  if(lc($ensType) eq 'regulatory_feature'){
+  if(lc($ensType) eq 'regulatoryfeature'){
 	$from_sql  = 'regulatory_feature rf, ';
 	$where_sql = qq( rf.regulatory_feature_id = oxr.ensembl_id AND );
-	#rf.is_current = 1 AND );
   }
-  elsif(lc($ensType) eq 'external_feature'){
+  elsif(lc($ensType) eq 'externalfeature'){
 	$from_sql  = 'external_feature ef, ';
 	$where_sql = qq( ef.external_feature_id = oxr.ensembl_id AND );
-	#rf.is_current = 1 AND );
-  }
-  elsif(lc($ensType) eq 'annotated_feature'){
+  } 
+  elsif(lc($ensType) eq 'annotatedfeature'){
 	$from_sql  = 'annotated_feature af, ';
 	$where_sql = qq( af.annotated_feature_id = oxr.ensembl_id AND );
-	#rf.is_current = 1 AND );
   }
-
-
+  elsif(lc($ensType) eq 'featuretype'){
+	$from_sql  = 'featuretype ft, ';
+	$where_sql = qq( ft.feature_type_id = oxr.ensembl_id AND );
+  }
 
 
   my $query = 
