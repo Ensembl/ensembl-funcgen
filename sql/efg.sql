4d914673e176b77fe7963b33d1989afd812ff07e
Nathan Johnson <njohnson@ebi.ac.uk>
2008-08-06 16:01:58 +0000

    added patch and sql for associated_feature_type table

--- sql/efg.sql
+++ sql/efg.sql
@@ -12,6 +12,21 @@
 --- Use MGED terms for design_types & experimental_variables where possible.
 --- Use Brno nomelcature for target features
 
+--
+-- Table structure for table `associated_feature_type`
+--
+
+
+
+DROP TABLE IF EXISTS `associated_feature_type`;
+CREATE TABLE `associated_feature_type` (
+   `feature_id` int(10) unsigned NOT NULL,
+   `feature_table` enum('annotated', 'external', 'regulatory') default NULL,
+   `feature_type_id` int(10) unsigned NOT NULL,
+   PRIMARY KEY  (`feature_id`, `feature_table`, `feature_type_id`),
+   KEY `feature_type_index` (`feature_type_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
 
 
 
