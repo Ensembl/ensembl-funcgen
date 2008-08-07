ffd785825a7fae3521e2d6ffbd10c02eb0958388
Nathan Johnson <njohnson@ebi.ac.uk>
2008-08-07 13:38:45 +0000

    added some simple sql QC for reg build

---
+++ scripts/regulatory_build/reg_build_qc.sql
@@ -0,0 +1,9 @@
+ select count(distinct(rf.regulatory_feature_id)), sr.name, fs.name from regulatory_feature rf, feature_set fs, seq_region sr where fs.name in ('RegulatoryFeatures', 'RegulatoryFeatures_v3') and fs.feature_set_id=rf.feature_set_id and rf.seq_region_id=sr.seq_region_id group by rf.feature_set_id, rf.seq_region_id order by rf.seq_region_id, rf.feature_set_id;
+
+
+--This takes a while and can probably be done better
+
+
+select fs.name, count(*)  from regulatory_feature rf, feature_set fs where fs.feature_set_id in(69,112) and fs.feature_set_id=rf.feature_set_id group by rf.feature_set_id;
+
+
