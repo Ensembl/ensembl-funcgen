afe078bf6117e32d4f45af4366f83c3f86c988f4
Nathan Johnson <njohnson@ebi.ac.uk>
2008-08-07 13:42:27 +0000

    feature_type count

--- scripts/regulatory_build/reg_build_qc.sql
+++ scripts/regulatory_build/reg_build_qc.sql
@@ -7,3 +7,9 @@
 select fs.name, count(*)  from regulatory_feature rf, feature_set fs where fs.feature_set_id in(69,112) and fs.feature_set_id=rf.feature_set_id group by rf.feature_set_id;
 
 
+-- post analysis feature type QC
+
+
+
+
+select count(distinct(rf.regulatory_feature_id)), ft.name, fs.name from regulatory_feature rf, feature_set fs, feature_type ft where fs.name in ('RegulatoryFeatures', 'RegulatoryFeatures_v3') and fs.feature_set_id=rf.feature_set_id and rf.feature_type_id=ft.feature_type_id group by rf.feature_set_id, rf.feature_type_id order by rf.feature_type_id, rf.feature_set_id;
