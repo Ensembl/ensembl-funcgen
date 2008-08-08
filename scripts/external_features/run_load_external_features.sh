5ff782df8953b8831e8cc00757dc11334291bbef
Nathan Johnson <njohnson@ebi.ac.uk>
2008-08-08 13:09:09 +0000

    updated example script

--- scripts/external_features/run_load_external_features.sh
+++ scripts/external_features/run_load_external_features.sh
@@ -2,19 +2,28 @@
 
 #. ~/src/ensembl-efg/scripts/.efg
 
-#file for cisred is always motif file, search regions will be guessed from filename
+
+PASS=$1
+shift
+
 
 perl $EFG_SRC/scripts/external_features/load_external_features.pl\
-	-type  redfly\
-	-species drosophila_melanogaster\
+	-type  cisred\
+	-species homo_sapiens\
 	-port 3306\
 	-user ensadmin\
 	-host ens-genomics1\
 	-clobber 1\
-	-dbname not_patched_drosophila_melanogaster_funcgen_50_54a\
-	-cdbname drosophila_melanogaster_core_49_54\
-	-pass $1
+	-dbname test_homo_sapiens_funcgen_51_36m\
+	-dnadb_host ens-staging\
+    -dnadb_name homo_sapiens_core_51_36m\
+	-dnadb_user ensro\
+	-pass $PASS\
+	$@
 
+
+#$@ is files, always motifs then search for cisRED? Or should we do some pattern matching
+#separate loading of feature_sets, then we can load just one at a time?
 
 
 #	-file  /nfs/acari/nj1/src/ensembl-functgenomics/scripts/external_features/VISTA/vista.txt\
