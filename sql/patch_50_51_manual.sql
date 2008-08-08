4b6569a2337d0dd5cb601e7382e65322d79963fc
Nathan Johnson <njohnson@ebi.ac.uk>
2008-08-08 15:50:54 +0000

    updated manual mouse data patch

--- sql/patch_50_51_manual.sql
+++ sql/patch_50_51_manual.sql
@@ -7,6 +7,10 @@
 --delete s from status s, status_name sn, result_set rs where rs.name='Vienna MEFf H3K4me2' and rs.result_set_id =s.table_id and s.table_name='result_set' and s.status_name_id=sn.status_name_id and sn.name='DISPLAYABLE';
 --delete s from status s, status_name sn, data_set ds where ds.name='Vienna MEFf H3K4me2' and ds.data_set_id =s.table_id and s.table_name='data_set' and s.status_name_id=sn.status_name_id and sn.name='DISPLAYABLE';
 
+--delete s from status s, status_name sn, feature_set fs where fs.name='Vienna MEFf H3K4me2' and fs.feature_set_id =s.table_id and s.table_name='feature_set' and s.status_name_id=sn.status_name_id and sn.name='DISPLAYABLE';
+
+
+
 --remove old ftype
 -- delete from feature_type where name='cisRED';
 --correct cisRED fset ftype
