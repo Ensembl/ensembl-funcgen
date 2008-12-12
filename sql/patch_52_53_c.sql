88aacfad84703821db880c8f875c0f98f6ded0ef
Nathan Johnson <njohnson@ebi.ac.uk>
2008-12-12 10:02:01 +0000

    added object_xref Probe patch

---
+++ sql/patch_52_53_c.sql
@@ -0,0 +1,14 @@
+# patch_52_53_c.sql
+#
+# title: ensembl_object_type Probe 
+#
+# description:
+# Modify object_xref.ensembl_object_type to include Probe
+
+ALTER table object_xref modify `ensembl_object_type` enum('RegulatoryFeature','ExternalFeature','AnnotatedFeature','FeatureType', 'Probe') NOT NULL;
+
+
+# patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_52_53_c.sql|ensembl_object_type Probe');
+
+
