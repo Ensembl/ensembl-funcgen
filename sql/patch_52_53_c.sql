3af1577b666321cf9ce751a07e72dc4cb0c6209f
Nathan Johnson <njohnson@ebi.ac.uk>
2008-12-19 16:18:44 +0000

    readded Probe!

--- sql/patch_52_53_c.sql
+++ sql/patch_52_53_c.sql
@@ -5,10 +5,10 @@
 # description:
 # Modify object_xref.ensembl_object_type to include Probe
 
-ALTER table object_xref modify `ensembl_object_type` enum('RegulatoryFeature','ExternalFeature','AnnotatedFeature','FeatureType', 'ProbeSet') NOT NULL;
+ALTER table object_xref modify `ensembl_object_type` enum('RegulatoryFeature','ExternalFeature','AnnotatedFeature','FeatureType', 'ProbeSet', 'Probe') NOT NULL;
 
 
 # patch identifier
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_52_53_c.sql|ensembl_object_type ProbeSet');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_52_53_c.sql|ensembl_object_type ProbeSet Probe');
 
 
