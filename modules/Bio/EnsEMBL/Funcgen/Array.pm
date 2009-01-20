2ee69f89b09f01e225321dc0e52dad121cf3c7a4
Nathan Johnson <njohnson@ebi.ac.uk>
2009-01-20 14:07:07 +0000

    == > eq

--- modules/Bio/EnsEMBL/Funcgen/Array.pm
+++ modules/Bio/EnsEMBL/Funcgen/Array.pm
@@ -125,7 +125,7 @@ sub new {
   $self->name($name);
   $self->format($format)    if defined $format;
 
-  if(defined $format && $format == 'EXPRESSION' && ! defined $class){
+  if(defined $format && $format eq 'EXPRESSION' && ! defined $class){
 	throw('You must defined a class if you are importing and array with an EXPRESSION format');
   }
 
