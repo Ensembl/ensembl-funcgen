66e6a4527ea176dcba45b6ed7bb7971ecf0cc9f3
Nathan Johnson <njohnson@ebi.ac.uk>
2009-01-20 14:16:27 +0000

    changed key to unique name class

--- sql/patch_52_53_f.sql
+++ sql/patch_52_53_f.sql
@@ -6,7 +6,7 @@
 # Add class field to array table to define class of array e.g. AFFY_UTR, AFFY_ST etc (This sould be used to direct parsing). Format for expression arrays would be TRANSCRIPT.
 
 ALTER table array add column `class` varchar(20) default NULL;
-ALTER table array add KEY `class_idx` (`class`);
+ALTER table array add UNIQUE KEY `name_class_idx` (`name`, `class`);
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_52_53_f.sql|array.class');
