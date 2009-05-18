ec574a6b3515f68eaf1cca7cf569e417ab78f3d5
Nathan Johnson <njohnson@ebi.ac.uk>
2009-05-18 16:00:05 +0000

    Updated wrapper script, now works to setup/validate sets before submiting slice based jobs to farm

--- scripts/regulatory_build/run_build_regulatory_features.pl
+++ scripts/regulatory_build/run_build_regulatory_features.pl
@@ -33,139 +33,131 @@ Options:
 
 =head1 DESCRIPTION
 
+Wrapper script to handles build job submissions, set generation, rollback and other 
+high level functions.
 
-=head1 LICENCE
+=head1 LICENSE
 
-This code is distributed under an Apache style licence. Please see
-http://www.ensembl.org/info/about/code_licence.html for details.
+  Copyright (c) 1999-2009 The European Bioinformatics Institute and
+  Genome Research Limited.  All rights reserved.
 
-=head1 AUTHOR
+  This software is distributed under a modified Apache license.
+  For license details, please see
 
-Stefan Graf, Ensembl Functional Genomics
+    http://www.ensembl.org/info/about/code_licence.html
 
 =head1 CONTACT
 
-Please post comments/questions to the Ensembl development list
-<ensembl-dev@ebi.ac.uk>
+  Please email comments or questions to the public Ensembl
+  developers list at <ensembl-dev@ebi.ac.uk>.
+
+  Questions may also be sent to the Ensembl help desk at
+  <helpdesk@ensembl.org>.
 
 =cut
+
+#To do 
+#1 Integrate with build_regulator_features.pl and just have two modes, one to do the set validation, dumping
+#and submitting, and the other to actually run the jobs
+
+#2 Update to take standard pipeline.env params i.e. remove dataversion and define dnadb explicitly.
+
+#3 Remove dump_annotated_feature func from build_regulatory_features and submit parallelised jobs
+#to farm using export_features script/Exporter.pm
 
 use strict;
 use warnings;
 use Data::Dumper;
 
-use Bio::EnsEMBL::Utils::Exception qw(verbose throw warning info);
+use Bio::EnsEMBL::Utils::Exception qw(verbose warning info);
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor;
+use Bio::EnsEMBL::Funcgen::Utils::Helper;
 
 use Getopt::Long;
 
-my ($host,$port,$user,$pass,$dbname,$species,$data_version,
-    $cdbhost,$cdbport,$cdbuser,$cdbpass,$jobs,
-    $help,$man);
+my ($dbhost,$dbport,$dbuser,$dbpass,$dbname,$species, $fset, $outdir,
+	$cdbhost,$cdbport,$cdbuser,$cdbpass,$cdbname, $non_ref, $include_mt,
+	@focus_names, @attr_names, $cdb, $db, $jobs,$help,$man, $dump_afs);
 
 $|=1;
 
-# database parameters for eFG DB
-$host = $ENV{EFG_HOST};
-$port = $ENV{EFG_PORT};
-$user = $ENV{EFG_WRITE_USER};
-$dbname = $ENV{EFG_DBNAME};
-$species = $ENV{SPECIES};
-$data_version = $ENV{DATA_VERSION};
-
 # default core database 
-$cdbhost = 'ens-staging';
-$cdbport = 3306;
-$cdbuser = 'ensro';
-#$cdbhost = 'ensembldb.ensembl.org';
-#$cdbuser = 'anonymous';
-#$cdbport = 3306;
+$cdbhost ||= 'ens-staging';
+$cdbport ||= 3306;
+$cdbuser ||= 'ensro';
 
 # build specififc paramters
-my $outdir = '/lustre/work1/ensembl/graef/RegBuild/v52';
-
-my @focus = qw 
-    (CD4_CTCF_e52 CD4_DNASE_IMPORT GM06990_DNASE_IMPORT 
-     Nessie_NG_STD_2_ctcf_ren_BR1);
-
-my @attributes = qw
-    (
-     CD4_H2AZ_e52 CD4_H2BK5me1_e52 CD4_H3K27me1_e52 CD4_H3K27me2_e52 
-     CD4_H3K27me3_e52 CD4_H3K36me1_e52 CD4_H3K36me3_e52 CD4_H3K4me1_e52 
-     CD4_H3K4me2_e52 CD4_H3K4me3_e52 CD4_H3K79me1_e52 CD4_H3K79me2_e52 
-     CD4_H3K79me3_e52 CD4_H3K9me1_e52 CD4_H3K9me2_e52 CD4_H3K9me3_e52 
-     CD4_H3R2me1_e52 CD4_H3R2me2_e52 CD4_H4K20me1_e52 CD4_H4K20me3_e52 
-     CD4_H4R3me2_e52 CD4_PolII_e52 
-     Wiggle_H3K27me3 Wiggle_H3K36me3 Wiggle_H3K4me3 Wiggle_H3K79me3 
-     Wiggle_H3K9me3 Wiggle_H4K20me3 
-     CD4_H2AK5ac_e52 CD4_H2AK9ac_e52 CD4_H2BK120ac_e52 CD4_H2BK12ac_e52 
-     CD4_H2BK20ac_e52 CD4_H2BK5ac_e52 CD4_H3K14ac_e52 CD4_H3K18ac_e52 
-     CD4_H3K23ac_e52 CD4_H3K27ac_e52 CD4_H3K36ac_e52 CD4_H3K4ac_e52 
-     CD4_H3K9ac_e52 CD4_H4K12ac_e52 CD4_H4K16ac_e52 CD4_H4K5ac_e52 
-     CD4_H4K8ac_e52 CD4_H4K91ac_e52
-     );
-
-#     CD4_H2AZ_e51 CD4_H2BK5me1_e51 CD4_H3K27me1_e51 CD4_H3K27me2_e51 
-#     CD4_H3K27me3_e51 CD4_H3K36me1_e51 CD4_H3K36me3_e51 CD4_H3K4me1_e51 
-#     CD4_H3K4me2_e51 CD4_H3K4me3_e51 CD4_H3K79me1_e51 CD4_H3K79me2_e51 
-#     CD4_H3K79me3_e51 CD4_H3K9me1_e51 CD4_H3K9me2_e51 CD4_H3K9me3_e51 
-#     CD4_H3R2me1_e51 CD4_H3R2me2_e51 CD4_H4K20me1_e51 CD4_H4K20me3_e51 
-#     CD4_H4R3me2_e51 CD4_PolII_e51 
-#     Wiggle_H3K27me3 Wiggle_H3K36me3 Wiggle_H3K4me3 Wiggle_H3K79me3 
-#     Wiggle_H3K9me3 Wiggle_H4K20me3 
-#     CD4_H2AK5ac_e51 CD4_H2AK9ac_e51 CD4_H2BK120ac_e51 CD4_H2BK12ac_e51 
-#     CD4_H2BK20ac_e51 CD4_H2BK5ac_e51 CD4_H3K14ac_e51 CD4_H3K18ac_e51 
-#     CD4_H3K23ac_e51 CD4_H3K27ac_e51 CD4_H3K36ac_e51 CD4_H3K4ac_e51 
-#     CD4_H3K9ac_e51 CD4_H4K12ac_e51 CD4_H4K16ac_e51 CD4_H4K5ac_e51 
-#     CD4_H4K8ac_e51 CD4_H4K91ac_e51
-
-
+#my $outdir = '/lustre/work1/ensembl/graef/RegBuild/v52';
 
 GetOptions (
-            "host|h=s"       => \$host,
-            "port=s"         => \$port,
-            "user|u=s"       => \$user,
-            "pass|p=s"       => \$pass,
+			#Use pipeline env db param names
+            "dbhost|h=s"       => \$dbhost,
+            "dbport=s"         => \$dbport,
+            "dbuser|u=s"       => \$dbuser,
+            "dbpass|p=s"       => \$dbpass,
             "dbname|d=s"     => \$dbname,
             "species=s"      => \$species,
-            "data_version|v=s" => \$data_version,
-            "cdbhost=s"      => \$cdbhost,
-            "cdbport=s"      => \$cdbport,
-            "cdbuser=s"      => \$cdbuser,
-            "cdbpass=s"      => \$cdbpass,
-            "jobs=s"         => \$jobs,
+			"dnadb_host=s"      => \$cdbhost,
+            "dnadb_port=s"      => \$cdbport,
+            "dnadb_user=s"      => \$cdbuser,
+            "dnadb_pass=s"      => \$cdbpass,
+			'dnadb_name=s'   => \$cdbname,
+			'focus_sets=s{,}'     => \@focus_names, 
+			'attribute_sets=s{,}' => \@attr_names,
+
+			'outdir=s'            => \$outdir,
+
+			'non_ref'             => \$non_ref,
+			'include_mt'          => \$include_mt,
+			'dump_annotated_features' => \$dump_afs,
+			
+			#To add opts
+			#slice
+			#skip_slices
+
+
+            #"jobs=s"         => \$jobs,
             "help|?"         => \$help,
             "man|m"          => \$man,
             );
 
 ### check options ###
 
-throw("Must specify mandatory database hostname (-host).\n") if ! defined $host;
-throw("Must specify mandatory database username. (-user)\n") if ! defined $user;
-throw("Must specify mandatory database password (-pass).\n") if ! defined $pass;
+throw("Must specify mandatory database hostname (-dbhost).\n") if ! defined $dbhost;
+throw("Must specify mandatory database username. (-dbuser)\n") if ! defined $dbuser;
+throw("Must specify mandatory database password (-dbpass).\n") if ! defined $dbpass;
 throw("Must specify mandatory database name (-dbname).\n") if ! defined $dbname;
-throw("Must specify mandatory database data version, like 47_36i (-data_version).\n") 
-     if !$data_version;
 
-my $cdb = Bio::EnsEMBL::DBSQL::DBAdaptor->new
+#Need to validate whe have sets pass
+
+#Allow comma separated
+@focus_names = split(/,/,join(',',@focus_names));
+@attr_names = split(/,/,join(',',@attr_names));
+
+
+if($cdbname){
+  $cdb = Bio::EnsEMBL::DBSQL::DBAdaptor->new
     (
      -host => $cdbhost,
      -port => $cdbport,
      -user => $cdbuser,
-     -dbname => $species.'_core_'.$data_version,
+     -dbname => $cdbname,
      -species => $species,
+	 -group => 'core',
      );
+}
 
-my $db = Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor->new
+$db = Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor->new
     (
-     -host   => $host,
-     -user   => $user,
+     -host   => $dbhost,
+     -user   => $dbuser,
      -dbname => $dbname,
      -species => $species,
-     -pass   => $pass,
-     -port   => $port,
+     -pass   => $dbpass,
+     -port   => $dbport,
      -dnadb  => $cdb,
+	 -group => 'funcgen',
      );
 
 # determine the number of toplevel slices we can analyze 
@@ -174,267 +166,165 @@ my $db = Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor->new
 # by the actual run script
 
 my @slices = ();
-my $sa = $db->get_SliceAdaptor();
-#foreach my $s (sort {$a->name cmp $b->name} @{$sa->fetch_all('toplevel', undef, 1)}) {
-foreach my $s (sort {$a->name cmp $b->name} @{$sa->fetch_all('toplevel')}) {
-        
-    next if ($s->seq_region_name =~ m/^MT/);
-    push @slices, $s;
-    
+my $sa = $db->dnadb->get_SliceAdaptor();
+foreach my $s (sort {$a->name cmp $b->name} @{$sa->fetch_all('toplevel', undef, $non_ref)}){      
+  next if ($s->seq_region_name =~ m/^MT/ && ! $include_mt);
+  push @slices, $s;
+  
 }
 
-#map {print Dumper($_->name, $_->get_seq_region_id);} @slices;
 
-warn("There are ".scalar @slices." slices available to analyse.");
+print "There are ".scalar @slices." slices available to analyse.\n";
 
-$jobs = '1-'.scalar(@slices) unless (defined $jobs);
+#Don't need to batch this, there will probably be less than 30!
+#$jobs = '1-'.scalar(@slices) unless (defined $jobs);
 
+
+
+
+#Much of this may be repeated in the build script for validation?
 
 # parse both focus and attribute sets and check that they exist
 my (%focus_fsets, %attrib_fsets);
 my $fsa = $db->get_FeatureSetAdaptor();
 
-map { my $fset = $fsa->fetch_by_name($_);
+map { $fset = $fsa->fetch_by_name($_);
       throw("Focus set $_ does not exist in the DB") 
           if (! defined $fset); 
       $focus_fsets{$fset->dbID} = $fset; 
-  } @focus;
+  } @focus_names;
 #print Dumper %focus_fsets;
 
 map { 
-    my $fset = $fsa->fetch_by_name($_);
+    $fset = $fsa->fetch_by_name($_);
     throw("Attribute set $_ does not exist in the DB") 
         if (! defined $fset); 
     $attrib_fsets{$fset->dbID()} = $fset; 
-} @attributes;
+} @attr_names;
 #print Dumper %attrib_fsets;
 
 # make sure that attribute sets also contain focus sets (Do we really need this?)
 map { $attrib_fsets{$_} = $focus_fsets{$_} } keys %focus_fsets;
 
 
-my ($sql);
+my (@fset_ids, @ftype_ids);
 
-# add missing supporting sets for data_set RegulatoryFeatures (140) in v52 that 
-# haven't been stored. This needs to go into the build regulatory feature script!!!
-#
-#my $dsa = $db->get_DataSetAdaptor();
-#my $dset = $dsa->fetch_by_name('RegulatoryFeatures');
-#
-#foreach (sort {$a <=> $b} keys %attrib_fsets ) {
-#
-#    #print Dumper ($_, $attrib_fsets{$_}->name);
-#
-#    $sql = sprintf "insert into supporting_set (data_set_id, supporting_set_id, type) "
-#        ."values (%d, %d, 'feature');", $dset->dbID, $_;
-#    print Dumper $sql;
-#    eval {
-#        $db->dbc->do($sql);
-#    };
-#    throw("Couldn't store supporting_set ".$attrib_fsets{$_}->name." (".$_.") for data_set"
-#          ."'RegulatoryFeatures' (".$dset->dbID.").") if ($@);
-#
-#}
-
-### build and import regbuild strings by feature_set_id and feature_type_id
-
-my $sth = $db->dbc->prepare("select * from meta where meta_key='regbuild.feature_set_ids'");
-$sth->execute();
-my $noe = scalar @{$sth->fetchall_arrayref};
-
-if ($noe == 0) {
-
-    $sql = 'insert into meta (meta_key, meta_value) values ("regbuild.feature_set_ids", "'
-        .join(',', map {$_->dbID} sort {$a->name cmp $b->name} values %attrib_fsets).'");';
-    #print "$sql\n";
-    eval {
-        $db->dbc->do($sql);
-    };
-    throw("Couldn't store regbuild.feature_set_ids in meta table.") if ($@);
-
-} else {
-
-    throw("regbuild.feature_set_ids already exists in meta table. You need to move it\n"
-          ."  away before you can run the regulatory build.");
-    
+foreach $fset(sort {$a->name cmp $b->name} values %attrib_fsets) {
+  push @fset_ids, $fset->dbID;
+  push @ftype_ids, $fset->feature_type->dbID;
 }
 
 
-$sth = $db->dbc->prepare("select * from meta where meta_key='regbuild.feature_type_ids'");
+print "Fset string is:\t".join(',', @fset_ids)."\n";
+print "Ftype string is:\t".join(',', @ftype_ids)."\n";
+
+my ($sql, $meta_value, $reg_string, $cmd);
+
+
+### build and import regbuild strings by feature_set_id and feature_type_id
+
+my $sth = $db->dbc->prepare("select meta_value from meta where meta_key='regbuild.feature_set_ids'");
 $sth->execute();
-$noe = scalar @{$sth->fetchall_arrayref};
+($meta_value) = map "@$_", @{$sth->fetchall_arrayref};
 
-if ($noe == 0) {
 
-    $sql = 'insert into meta (meta_key, meta_value) values ("regbuild.feature_type_ids", "'
-        .join(',', map {$_->feature_type->dbID} sort {$a->name cmp $b->name} values %attrib_fsets).'");';
-    #print "$sql\n";
+
+$reg_string = join(',', map {$_->dbID} sort {$a->name cmp $b->name} values %attrib_fsets);
+
+if (! $meta_value) {
+
+
+    $sql = "insert into meta (meta_key, meta_value) values ('regbuild.feature_set_ids', '$reg_string')";
+
+    eval {
+        $db->dbc->do($sql);
+    };
+    die("Couldn't store regbuild.feature_set_ids in meta table.\n$@") if ($@);
+
+} else {
+
+  if($meta_value ne $reg_string){
+	die("regbuild.feature_set_ids already exists and does not match\nOld\t$meta_value\nNew\t$reg_string\nPlease archive previous RegulatoryBuild.\n");
+  }else{  
+	warn "Found matching regbuild.feature_set_ids meta entry, do you need to archive a previous Regulatorybuild?\n";
+	#Skip this warn with clobber?
+	#Add this warn to summary log?
+
+  }
+}
+
+
+$sth = $db->dbc->prepare("select meta_value from meta where meta_key='regbuild.feature_type_ids'");
+$sth->execute();
+($meta_value) =  map "@$_", @{$sth->fetchall_arrayref};
+$reg_string = join(',', map {$_->feature_type->dbID} sort {$a->name cmp $b->name} values %attrib_fsets);
+
+if (! $reg_string) {
+
+    $sql = "insert into meta (meta_key, meta_value) values ('regbuild.feature_type_ids', '$reg_string')";
+
     eval {
         $db->dbc->do($sql);
     };
     throw("Couldn't store regbuild.feature_type_ids in meta table.") if ($@);
 
 } else {
+ if($meta_value ne $reg_string){
+	die("regbuild.feature_type_ids already exists and do not match\nOld\t$meta_value\nNew\t$reg_string\nPlease archive previous RegulatoryBuild.\n");
+  }else{  
+	warn "Found matching regbuild.feature_type_ids meta entry, do you need to archive a previous Regulatorybuild?\n";
 
-    throw("regbuild.feature_set_ids already exists in meta table. You need to move it\n"
-          ."  away before you can run the regulatory build.");
-    
+	#Skip this warn with clobber?
+	#Add this warn to summary log?
+
+  }    
 }
+
+#Enough time to realise we might want to archive
+sleep(5);
+
 
 
 # submit jobs to the farm
 
-(my $bsubhost = $host) =~ s/-/_/g;
-my $bsub = "bsub -oo '$outdir/RegulatoryBuild_%I.log' -J 'RegulatoryBuild_[".$jobs."]' ".
-    "-R 'select[my".$bsubhost."<80] rusage[my".$bsubhost."=10:duration=10]'";
-my $cmd = "$ENV{EFG_SRC}/scripts/regulatory_build/build_regulatory_features.pl"
-    .' -host '.$host
-    .' -port '.$port
-    .' -user '.$user
-    .' -pass '.$pass
-    .' -dbname '.$dbname
-    .' -data_version '.$data_version
-    .' -focus '.join(',', @focus)
-    .' -attrib '.join(',', @attributes)
-    .' -outdir '.$outdir
-    .' -dump_regulatory_features '
-    .' -clobber '
-    .' -stats '
-    .' -write_features'
-    ;
+(my $bsubhost = $dbhost) =~ s/-/_/g;
 
-print $bsub, "    ", $cmd, "\n";
+$dump_afs = ' -dump_annotated_features ' if $dump_afs;
 
-system("$bsub $cmd") &&
-    throw ("Can't submit job array to farm");
+foreach my $slice(@slices){
+  my $sr_name = $slice->seq_region_name;
+  
 
-__END__
+  #my $bsub = "bsub -oo '$outdir/RegulatoryBuild_%I.log' -J 'RegulatoryBuild_[".$jobs."]' ".
+  #  "-R 'select[my".$bsubhost."<80] rusage[my".$bsubhost."=10:duration=10]'";
 
-#!/bin/sh
+ my $bsub = "bsub -o $outdir/RegulatoryBuild_$sr_name.log -e $outdir/RegulatoryBuild_$sr_name.err -J 'RegulatoryBuild_$sr_name' ".
+   "-R 'select[my".$bsubhost."<80] rusage[my".$bsubhost."=10:duration=10]'";
+ 
 
-if [ $# -lt 4 ]; then
+  $cmd = "$ENV{EFG_SRC}/scripts/regulatory_build/build_regulatory_features.pl"
+	.' -seq_region_name '.$slice->name
+    .' -host '.$dbhost
+	  .' -port '.$dbport
+		.' -user '.$dbuser
+		  .' -pass '.$dbpass
+			.' -dbname '.$dbname
+			  #.' -species '.$species
+				.' -dnadb_host '.$cdbhost
+				  .' -dnadb_name '.$cdbname
+					.' -focus '.join(',', @focus_names)
+					  .' -attrib '.join(',', @attr_names)
+						.' -outdir '.$outdir
+						  .' -dump_regulatory_features '
+							.' -clobber '
+							  .' -stats '
+								." -write_features $dump_afs";
 
-	echo "Usage: $0 <host> <port> <user> <password>"
-	exit;
+  #print $bsub, "    ", $cmd, "\n";
+  
+  #system($cmd);
+  #exit;
 
-fi
+  system("$bsub $cmd") && die("Can't submit job array to farm");
+}
 
-HOST=$1
-shift
-PORT=$1
-shift
-USER=$1
-shift
-PASS=$1
-shift
-
-DATA_VERSION='51_36m'
-DBNAME='sg_homo_sapiens_funcgen_'${DATA_VERSION}
-
-OUTDIR='/lustre/work1/ensembl/graef/RegBuild/v51'
-
-FOCUS=\
-CD4_CTCF_e51,\
-CD4_DNASE_IMPORT,\
-GM06990_DNASE_IMPORT,\
-Nessie_NG_STD_2_ctcf_ren_BR1
-
-TARGET=\
-CD4_H2AZ_e51,\
-CD4_H2BK5me1_e51,\
-CD4_H3K27me1_e51,\
-CD4_H3K27me2_e51,\
-CD4_H3K27me3_e51,\
-CD4_H3K36me1_e51,\
-CD4_H3K36me3_e51,\
-CD4_H3K4me1_e51,\
-CD4_H3K4me2_e51,\
-CD4_H3K4me3_e51,\
-CD4_H3K79me1_e51,\
-CD4_H3K79me2_e51,\
-CD4_H3K79me3_e51,\
-CD4_H3K9me1_e51,\
-CD4_H3K9me2_e51,\
-CD4_H3K9me3_e51,\
-CD4_H3R2me1_e51,\
-CD4_H3R2me2_e51,\
-CD4_H4K20me1_e51,\
-CD4_H4K20me3_e51,\
-CD4_H4R3me2_e51,\
-CD4_PolII_e51,\
-Wiggle_H3K27me3,\
-Wiggle_H3K36me3,\
-Wiggle_H3K4me3,\
-Wiggle_H3K79me3,\
-Wiggle_H3K9me3,\
-Wiggle_H4K20me3,\
-CD4_H2AK5ac_e51,\
-CD4_H2AK9ac_e51,\
-CD4_H2BK120ac_e51,\
-CD4_H2BK12ac_e51,\
-CD4_H2BK20ac_e51,\
-CD4_H2BK5ac_e51,\
-CD4_H3K14ac_e51,\
-CD4_H3K18ac_e51,\
-CD4_H3K23ac_e51,\
-CD4_H3K27ac_e51,\
-CD4_H3K36ac_e51,\
-CD4_H3K4ac_e51,\
-CD4_H3K9ac_e51,\
-CD4_H4K12ac_e51,\
-CD4_H4K16ac_e51,\
-CD4_H4K5ac_e51,\
-CD4_H4K8ac_e51,\
-CD4_H4K91ac_e51
-
-case "$MODE" in 
-    "dump")
-
-
-        # first dump annotated features from database 
-        # for further processing
-
-        bsub -o "$OUTDIR/RegulatoryBuild_afDump_$FOCUS.log" -J RegBuild_afDump \
-        $EFG_SRC/scripts/regulatory_build/build_regulatory_features.pl \
-            -host $HOST \
-            -port $PORT \
-            -user $USER \
-            -pass $PASS \
-            -dbname $DBNAME \
-            -data_version $DATA_VERSION \
-            -outdir $OUTDIR \
-            -dump \
-            -focus $FOCUS \
-            -target $TARGET \
-            $@
-        ;;
-    
-    * )
-
-        # build regulatory features and dump to file when dumping 
-        # features has been sucessfully completed
-
-        #            -w 'done("RegBuild_afDump")' \
-
-        #bsub -o "$OUTDIR/RegulatoryBuild_$FOCUS.log" -J RegBuild_[1-25] \
-        $EFG_SRC/scripts/regulatory_build/build_regulatory_features.pl \
-            -host $HOST \
-            -port $PORT \
-            -user $USER \
-            -pass $PASS \
-            -dbname $DBNAME \
-            -data_version $DATA_VERSION \
-            -focus $FOCUS \
-            -target $TARGET \
-            -outdir $OUTDIR \
-            -dump_features \
-            -clobber \
-            -stats \
-            -write_features \
-            $@
-
-           # -gene_signature \
-           # -seq_name 10 \
-       ;;
-esac
-          
