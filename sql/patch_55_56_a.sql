0aeee228e2819170e4d8fb21e9b1500134994211
Nathan Johnson <njohnson@ebi.ac.uk>
2009-07-03 12:55:18 +0000

    added ;

--- sql/patch_55_56_a.sql
+++ sql/patch_55_56_a.sql
@@ -5,7 +5,7 @@
 # description:
 # Extend probe_set.name field length
 
-alter table probe_set modify `name` varchar(100) NOT NULL
+alter table probe_set modify `name` varchar(100) NOT NULL;
 
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_55_56_a.sql|probe_set.name_length');
 
