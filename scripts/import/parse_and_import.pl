68e76aae01007c4bb52d87353d0d0e3147944c31
Nathan Johnson <njohnson@ebi.ac.uk>
2009-07-06 13:30:40 +0000

    Tidied params Now catches empty params more docs

--- scripts/import/parse_and_import.pl
+++ scripts/import/parse_and_import.pl
@@ -1,28 +1,24 @@
 #!/software/bin/perl -w
 
-##!/usr/local/ensembl/bin/perl -w
-
-####!/opt/local/bin/perl -w
-
 
 =head1 NAME
 
-ensembl-efg parse_and_import.pl
+ ensembl-efg parse_and_import.pl
   
 =head1 SYNOPSIS
 
-parse_and_import.pl [options]
+ parse_and_import.pl [options]
 
-Can we just point this at Importer perldoc?
+=head1 OPTIONS
 
-Options:
+ Options:
 
-Mandatory
+ Mandatory
   -name|n          Instance name
   -format|f        Data format
   -group|g         Group name
 
-Optional
+ Optional
   -registry_host Host to load registry from(defaults to ensembldb.ensembl.org)
   -registry_port Port for registry host
   -registry_user User for registry host 
@@ -34,6 +30,7 @@ Optional
   -array_set       Flag to treat all chip designs as part of same array
   -array_name      Name of the set of array chips
   -result_set      Name to give the raw/normalised result set.
+  -result_files    Space separated list of result files paths
   -fasta           Fasta dump flag
   -norm|n          Normalisation method (default=vsn)
   -species|s       Species name any standard e! species alias(will be reset to dbname/latin name e.g. "homo_sapiens")
@@ -43,66 +40,14 @@ Optional
   -log_file        Defines the log file
   -ucsc_coords     Flag to define usage of UCSC coord system in source files
   -release         Release version to load the registry from, defaults to latest release.
+  -assembly        The specific assembly version to use e.g. 36 or 37.
+
+  -update_xml      ?? Is this required, isn't this always updated now?
+
+
   -help            Brief help message
   -man             Full documentation
 
-=head1 OPTIONS
-
-=over 8
-
-=item B<-name|n>
-
-Mandatory:  Instance name for the data set, this is the directory where the native data files are located
-
-=item B<-format|f>
-
-Mandatory:  The format of the data files e.g. nimblegen
-
-=over 8
-
-=item B<-group|g>
-
-Mandatory:  The name of the experimental group
-
-=over 8
-
-=item B<-data_root>
-
-The root data dir containing native data and pipeline data, default = $ENV{'EFG_DATA'}
-
-=over 8
-
-=item B<-fasta>
-
-Flag to turn on dumping of all probe_features in fasta format for the remapping pipeline
-
-=item B<-norm>
-
-Normalisation method, deafult is the Bioconductor vsn package which performs generalised log ratio transformations
-
-=item B<-species|s>
-
-Species name for the array.
-
-=item B<-debug>
-
-Turns on and defines the verbosity of debugging output, 1-3, default = 0 = off
-
-=over 8
-
-=item B<-log_file|l>
-
-Defines the log file, default = "${instance}.log"
-
-=item B<-help>
-
-Print a brief help message and exits.
-
-=item B<-man>
-
-Prints the manual page and exits.
-
-=back
 
 =head1 DESCRIPTION
 
@@ -130,12 +75,9 @@ BEGIN{
 
 ### MODULES ###
 use Getopt::Long;
-#use Carp;#For dev only? cluck not exported by default Remove this and implement in Helper
 use Pod::Usage;
-#POSIX? File stuff
 use File::Path;
 use Bio::EnsEMBL::Funcgen::Importer;
-use Bio::EnsEMBL::Analysis;
 use Bio::EnsEMBL::Registry;
 use Bio::EnsEMBL::Utils::Exception qw( throw warning );
 
@@ -146,7 +88,7 @@ my ($input_name, $input_dir, $name, $rset_name, $output_dir, $loc, $contact, $gr
 my ($assm_ver, $help, $man, $species, $nmethod, $dnadb, $array_set, $array_name, $vendor, $exp_date, $ucsc);
 my ($ctype, $ftype, $recover, $mage_tab, $update_xml, $write_mage, $no_mage, $farm, $exp_set, $old_dvd_format);
 my ($reg_host, $reg_user, $reg_port, $reg_pass);
-my ($parser, $fanal, $release);
+my ($parser, $fanal, $release, @result_files);
 
 #to be removed
 my ($import_dir);
@@ -160,81 +102,101 @@ my $user = "ensadmin";
 my $host = 'localhost';
 my $port = '3306';
 my $fasta = 0;#Shouldn't this be on by default?
-#my $recover = 0;
 my $verbose = 0;
 
 #Definitely need some sort of Defs modules for each array?
 
 $main::_debug_level = 0;
 $main::_tee = 0;
-
-
+my @tmp_args = @ARGV;
 #Use some sort of DBDefs for now, but need  to integrate with Register, and have put SQL into (E)FGAdaptor?
 #Use ArrayDefs.pm module for some of these, class, vendor, format?
 #ArrayDefs would also contain paths to data and vendor specific parse methods?
 
+
 GetOptions (
-			"name|n=s"     => \$name,
-			"format|f=s"   => \$format,
-			"vendor|v=s"   => \$vendor,
-			"parser=s"     => \$parser,
-			"pass|p=s"     => \$pass,
-			"port|l=s"     => \$port,
-			"host|h=s"     => \$host,
-			"user|u=s"     => \$user,
+			#Experiment
+			"name|n=s"           => \$name,
+			"format|f=s"         => \$format,
+			"vendor|v=s"         => \$vendor,
+			"parser=s"           => \$parser,
+			"array_set"          => \$array_set,
+			"array_name=s"       => \$array_name,
+			"result_set=s"       => \$rset_name,
+			"experimental_set=s" => \$exp_set,
+			"feature_type=s"     => \$ftype,
+			"feature_analysis=s" => \$fanal,
+			"cell_type=s"        => \$ctype,
+			"exp_date=s"         => \$exp_date,
+			'result_files=s{,}'  => \@result_files,
+
+			#Experimental group 
+			"group|g=s"    => \$group,
+			"location=s"   => \$loc,
+			"contact=s"    => \$contact,
+
+			#Run mode
+			"fasta"          => \$fasta,
+			"farm=s"         => \$farm,
+			"recover|r"      => \$recover,
+			"norm_method=s"  => \$nmethod,
+			"old_dvd_format" => \$old_dvd_format,
+			"ucsc_coords"    => \$ucsc,
+			"interactive"    => \$interactive,
+
+			#MAGE
+			"write_mage"   => \$write_mage,
+			'update_xml'   => \$update_xml,
+			'no_mage'      => \$no_mage,
+
+			#DB connection
+			"dbname=s"        => \$dbname,
+			"pass|p=s"        => \$pass,
+			"port|l=s"        => \$port,
+			"host|h=s"        => \$host,
+			"user|u=s"        => \$user,
+			"species|s=s"     => \$species,
 			"registry_user=s" => \$reg_user,
 			"registry_host=s" => \$reg_host,
 			"registry_pass=s" => \$reg_pass,
 			"registry_port=s" => \$reg_port,
-			"ssh"          => \$ssh,
-			"dbname=s"     => \$dbname,
-			"group|g=s"    => \$group,
-			"species|s=s"      => \$species, 
-			"assembly|a=s" => \$assm_ver,
-			"array_set"    => \$array_set,
-			"array_name=s" => \$array_name,
-			"result_set=s" => \$rset_name,
-			"experimental_set=s" => \$exp_set,
-			"feature_type=s" => \$ftype,
-			"feature_analysis=s" => \$fanal,
-			"cell_type=s"    => \$ctype,
-			"write_mage"   => \$write_mage,
-			'update_xml'  => \$update_xml,
-			'no_mage' => \$no_mage,
-			"debug=i"    => \$main::_debug_level,
+			"ssh"             => \$ssh,
+			"assembly|a=s"    => \$assm_ver,
+			"release=s"       => \$release,
+		
+			#Directory overrides
 			"data_root=s"  => \$data_dir,
 			"input_dir=s"  => \$input_dir,
 			"output_dir=s" => \$output_dir,
-			"exp_date=s"   => \$exp_date,
-			"fasta"        => \$fasta,
-			"farm=s"       => \$farm,
-			"recover|r"    => \$recover,
-			"norm_method=s"       => \$nmethod,
-			"location=s"   => \$loc,
-			"contact=s"    => \$contact,
-			"old_dvd_format" => \$old_dvd_format,
-			"ucsc_coords"  => \$ucsc,
-            "release=s"    => \$release,
+		
+			#Other params
 			"tee"          => \$main::_tee,
 			"log_file=s"   => \$main::_log_file,
 			"debug_file=s" => \$main::_debug_file,
-			#should have MAGE flag here? or would this be format?
-			"interactive"  => \$interactive,
+			"debug=i"      => \$main::_debug_level,		
 			"help|?"       => \$help,
 			"man|m"        => \$man,
-			"verbose"    => \$verbose, # not implmented yet
-		   );
+			"verbose"      => \$verbose, # not implmented yet in Importer?
+		   )   or pod2usage( -exitval => 1,
+							 -message => "Params are:\t@tmp_args"
+						   );
 
+#Need to put these in an opt so we can test for the rest of ARGV i.e. we have missed an opt
 
-my @result_files = @ARGV;
+#my @result_files = @ARGV;
+
+if (@ARGV){
+  pod2usage( -exitval =>1,
+			 -message => "You have specified incomplete options:\t@tmp_args");
+}
 
 throw("Nimblegen import does not support cmdline defined result files") if (@result_files && uc($vendor) eq "NIMBELGEN");
 
 #Need to add (primary) design_type and description, or add to defs file?
 
 
-pod2usage(1) if $help;
-pod2usage(-exitstatus => 0, -verbose => 2) if $man;
+pod2usage(0) if $help;#Just synopsis
+pod2usage(-exitstatus => 0, -verbose => 2) if $man;#All Pod with highlighting
 
 #checking params used in this scripts
 die('Must provide a -vendor parameter') if ! defined $vendor;
@@ -253,6 +215,9 @@ $main::_log_file = $output_dir."/${name}.log" if(! defined $main::_log_file);
 $main::_debug_file = $output_dir."/${name}.dbg" if(! defined $main::_debug_file);
 
 ### SET UP IMPORTER (FUNCGENDB/DNADB/EXPERIMENT) ###
+
+
+
 
 my $Imp = Bio::EnsEMBL::Funcgen::Importer->new
   (
@@ -302,182 +267,6 @@ my $Imp = Bio::EnsEMBL::Funcgen::Importer->new
    #This allows input dir to be somewhere 
    #other than efg dir structure
   );
-
-
-
-#Move the following to validate_analyses.pl
-#This should read from a tab delimted text file
-
-my $anal_a = $Imp->db->get_AnalysisAdaptor();
-my $anal = Bio::EnsEMBL::Analysis->new(
-				       -logic_name      => 'VendorMap',
-				       -db              => 'NULL',
-				       -db_version      => 'NULL',
-				       -db_file         => 'NULL',
-				       -program         => 'NULL',
-				       -program_version => 'NULL',
-				       -program_file    => 'NULL',
-				       -gff_source      => 'NULL',
-				       -gff_feature     => 'NULL',
-				       -module          => 'NULL',
-				       -module_version  => 'NULL',
-				       -parameters      => 'NULL',
-				       -created         => 'NULL',
-				       -description    => 'Original mapping provided by Array Vendor',
-				       -display_label   => 'Vendor mapping',
-				       -displayable     => 1,
-				      );
-
-
-my $sanger_anal = Bio::EnsEMBL::Analysis->new(
-					      -logic_name      => 'SangerPCR',
-					      -db              => 'NULL',
-					      -db_version      => 'NULL',
-					      -db_file         => 'NULL',
-					      -program         => 'NULL',
-					      -program_version => 'NULL',
-					      -program_file    => 'NULL',
-					      -gff_source      => 'NULL',
-					      -gff_feature     => 'NULL',
-					      -module          => 'NULL',
-					      -module_version  => 'NULL',
-					      -parameters      => 'NULL',
-					      -created         => 'NULL',
-					      -description    => 'Ratio generated by standard Sanger PCR array processing',
-					      -display_label   => 'SangerPCR',
-					      -displayable     => 1,
-					     );
-
-
-my $raw_anal = Bio::EnsEMBL::Analysis->new(
-										   -logic_name      => 'RawValue',
-										   -db              => 'NULL',
-										   -db_version      => 'NULL',
-										   -db_file         => 'NULL',
-										   -program         => 'NULL',
-										   -program_version => 'NULL',
-										   -program_file    => 'NULL',
-										   -gff_source      => 'NULL',
-										   -gff_feature     => 'NULL',
-										   -module          => 'NULL',
-										   -module_version  => 'NULL',
-										   -parameters      => 'NULL',
-										   -created         => 'NULL',
-										   -description    => 'Raw value',
-										   -display_label   => 'Raw value',
-										   -displayable     => 1,
-										  );
-
-my $vsn_anal = Bio::EnsEMBL::Analysis->new(
-										   -logic_name      => 'VSN_GLOG',
-										   -db              => 'NULL',
-										   -db_version      => 'NULL',
-										   -db_file         => 'NULL',
-										   -program         => 'NULL',
-										   -program_version => 'NULL',
-										   -program_file    => 'NULL',
-										   -gff_source      => 'NULL',
-										   -gff_feature     => 'NULL',
-										   -module          => 'NULL',
-										   -module_version  => 'NULL',
-										   -parameters      => 'NULL',
-										   -created         => 'NULL',
-										   -description    => 'Generalised log transformation based on VSN variance stabilised scores',
-										   -display_label   => 'VSN_GLOG',
-					   -displayable     => 1,
-										  );
-
-my $tbw_anal = Bio::EnsEMBL::Analysis->new(
-										   -logic_name      => 'T.Biweight',
-										   -db              => 'NULL',
-										   -db_version      => 'NULL',
-										   -db_file         => 'NULL',
-										   -program         => 'NULL',
-										   -program_version => 'NULL',
-										   -program_file    => 'NULL',
-										   -gff_source      => 'NULL',
-										   -gff_feature     => 'NULL',
-										   -module          => 'NULL',
-										   -module_version  => 'NULL',
-										   -parameters      => 'NULL',
-										   -created         => 'NULL',
-										   -description     => 'Weighted mean standardisation using the one step Tukey Biweight algorithm',
-										   -display_label   => 'Tukey Biweight',
-					   -displayable     => 1,
-										  );
-
-
-
-my $pz_anal = Bio::EnsEMBL::Analysis->new(
-										   -logic_name      => 'Parzen',
-										   -db              => 'NULL',
-										   -db_version      => 'NULL',
-										   -db_file         => 'NULL',
-										   -program         => 'NULL',
-										   -program_version => 'NULL',
-										   -program_file    => 'NULL',
-										   -gff_source      => 'NULL',
-										   -gff_feature     => 'NULL',
-										   -module          => 'NULL',
-										   -module_version  => 'NULL',
-										   -parameters      => 'NULL',
-										   -created         => 'NULL',
-										   -description     => 'Solexa clusters',
-										   -display_label   => 'Parzen',
-					   -displayable     => 1,
-										  );
-
-
-my $t_anal = Bio::EnsEMBL::Analysis->new(
-									   -logic_name      => 'Nessie',
-									   -db              => 'NULL',
-									   -db_version      => 'NULL',
-									   -db_file         => 'NULL',
-									   -program         => 'NULL',
-									   -program_version => 'NULL',
-									   -program_file    => 'NULL',
-									   -gff_source      => 'NULL',
-									   -gff_feature     => 'NULL',
-									   -module          => 'NULL',
-									   -module_version  => 'NULL',
-									   -parameters      => 'NULL',
-									   -created         => 'NULL',
-									   -description    => 'Hidden Markov Model based predictions based on tiling array data',
-									   -display_label   => 'Nessie (TilingHMM)',
-					 -displayable     => 1,
-									  );
-my $lanal = Bio::EnsEMBL::Analysis->new(
-				       -logic_name      => 'LiftOver',
-				       -db              => 'NULL',
-				       -db_version      => 'NULL',
-				       -db_file         => 'NULL',
-				       -program         => 'NULL',
-				       -program_version => 'NULL',
-				       -program_file    => 'NULL',
-				       -gff_source      => 'NULL',
-				       -gff_feature     => 'NULL',
-				       -module          => 'NULL',
-				       -module_version  => 'NULL',
-				       -parameters      => 'NULL',
-				       -created         => 'NULL',
-				       -description    => 'Remapping to new assembly performed by LiftOver',
-				       -display_label   => 'LiftOver',
-				       -displayable     => 1,
-				      );
-
-
-#this checks if already stored
-$anal_a->store($anal);
-$anal_a->store($lanal);
-$anal_a->store($raw_anal);
-$anal_a->store($vsn_anal);
-$anal_a->store($t_anal);
-$anal_a->store($sanger_anal);
-$anal_a->store($pz_anal);
-$anal_a->store($tbw_anal);
-
-
-#Validate, parse and import all experiment data
 
 
 $Imp->register_experiment();
