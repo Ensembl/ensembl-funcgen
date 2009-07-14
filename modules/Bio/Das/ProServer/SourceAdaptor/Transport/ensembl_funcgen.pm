1cdb7d5aa1b424eae5d9c3fb694cca4e6bd2ab03
Nathan Johnson <njohnson@ebi.ac.uk>
2009-07-14 13:35:34 +0000

    added dnadb params

--- modules/Bio/Das/ProServer/SourceAdaptor/Transport/ensembl_funcgen.pm
+++ modules/Bio/Das/ProServer/SourceAdaptor/Transport/ensembl_funcgen.pm
@@ -4,7 +4,7 @@ efg_transport.pm
 
 =head1 DESCRIPTION
 
-Transport module to retrieve REsult features from efg database
+Transport module to retrieve Annotated/ResultFeatures from an efg database
 
 =head1 LICENCE
 
@@ -35,24 +35,25 @@ use Carp;
 use Data::Dumper;
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor;
-use Bio::EnsEMBL::Registry;
+#use Bio::EnsEMBL::Registry;
 use Bio::Das::ProServer::SourceAdaptor::Transport::generic;
 
 use vars qw(@ISA);
 @ISA = qw(Bio::Das::ProServer::SourceAdaptor::Transport::generic);
 
+
+
+#No init as data_version now either explicilty set as part of dnadb config
+#Or autoselect in the DBAdaptor
+
 sub adaptor {
     my $self = shift;
 
-
-	#We need to add dnadb creation to this!
-
-
     unless($self->{'_adaptor'}) {
 		  
-	  #We need to add dnadb creation to this!
-	  #Or can we get the DBAdaptor to autoset one according to the data_version?
         
+	  #Can we add actaully ini file to this error?
+
 	  $self->{'_adaptor'} ||= Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor->new
 		(
 		 -host    => $self->config->{'host'}     || 'localhost',
@@ -62,6 +63,14 @@ sub adaptor {
 		 -pass    => $self->config->{'password'} || '',
 		 -port    => $self->config->{'port'}     || '3306',
 		 -type    => 'funcgen',
+		 -dnadb_assembly => $self->config('dnadb_assembly'),
+		 -dnadb_host => $self->config('dnadb_host'),
+		 -dnadb_port => $self->config('dnadb_post'),
+		 -dnadb_user => $self->config('dnadb_user'),
+		 -dnadb_pass => $self->config('dnadb_pass'),
+		 
+
+
 		);
 	  print Dumper $self->{'_adaptor'} if ($self->{'debug'});
 
@@ -73,7 +82,7 @@ sub adaptor {
 
 sub chromosome_by_region {
   my ($self, $chr, $start, $end) = @_;
-  my $slice = $self->adaptor->get_SliceAdaptor->fetch_by_region
+  my $slice = $self->adaptor->dnadb->get_SliceAdaptor->fetch_by_region
       (
        'chromosome', $chr, $start, $end
        );
@@ -92,5 +101,157 @@ sub DESTROY {
   my $self = shift;
   $self->disconnect();
 }
+
+
+1;
+
+__END__
+
+#Are any of these used?
+#Or is this hangover from ensembl adaptor?
+#Using the registry will create problems
+#as we may had multiple DBs with the same species
+#which will cause the registry to redefine the species name
+
+sub version {
+  my ($self) = @_;
+  return Bio::EnsEMBL::Registry->software_version();
+}
+
+sub last_modified {
+  my ($self) = @_;
+  my $dbc = $self->adaptor()->dbc();
+  my $sth = $dbc->prepare(q(SHOW TABLE STATUS));
+  $sth->execute();
+  my $server_text = [sort { $b cmp $a } ## no critic
+                     keys %{ $sth->fetchall_hashref('Update_time') }
+                    ]->[0]; # server local time
+  $sth->finish();
+  $sth = $dbc->prepare(q(SELECT UNIX_TIMESTAMP(?) as 'unix'));
+  $sth->execute($server_text); # sec since epoch
+  my $server_unix = $sth->fetchrow_arrayref()->[0];
+  $sth->finish();
+  return $server_unix;
+}
+
+
+sub chromosomes {
+  my ($self, $species, $group) = @_;
+  return $self->slice_adaptor($species, $group)->fetch_all('chromosome');
+}
+
+
+sub _load_registry {
+  my ($self) = @_;
+  if (!$self->config->{'skip_registry'}) {
+    Bio::EnsEMBL::Registry->load_registry_from_db(
+      -host => 'ensembldb.ensembl.org',
+      -user => 'anonymous',
+      -verbose => $self->{'debug'} );
+  }
+  Bio::EnsEMBL::Registry->set_disconnect_when_inactive();
+  return;
+}
+
+sub _apply_override {
+  my ($self) = @_;
+  my $dbname = $self->config->{'dbname'};
+
+  if ($dbname) {
+
+    my ($species, $group) = $dbname =~ m/([a-z_]+)_([a-z]+)_\d+/mx;
+    if ($species  eq 'ensembl') {
+      $species = 'multi';
+    }
+    $self->{'_species'} ||= $species;
+    $self->{'_group'}   ||= $group;
+
+    if (!$self->{'_species'} || !$self->{'_group'}) {
+      croak "Unable to parse database species and group: $dbname";
+    }
+
+    $self->{'debug'} && carp sprintf "Overriding database with %s (%s,%s)\n",
+                                     $dbname, $self->{'_species'}, $self->{'_group'};
+
+    # This is a map from group names to Ensembl DB adaptors.
+    # Taken from Bio::EnsEMBL::Registry
+    my %group2adaptor = (
+      'blast'   => 'Bio::EnsEMBL::External::BlastAdaptor',
+      'compara' => 'Bio::EnsEMBL::Compara::DBSQL::DBAdaptor',
+      'core'    => 'Bio::EnsEMBL::DBSQL::DBAdaptor',
+      'estgene' => 'Bio::EnsEMBL::DBSQL::DBAdaptor',
+      'funcgen' => 'Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor',
+      'haplotype' =>
+        'Bio::EnsEMBL::ExternalData::Haplotype::DBAdaptor',
+      'hive' => 'Bio::EnsEMBL::Hive::DBSQL::DBAdaptor',
+      'lite' => 'Bio::EnsEMBL::Lite::DBAdaptor',
+      'otherfeatures' => 'Bio::EnsEMBL::DBSQL::DBAdaptor',
+      'pipeline' =>
+        'Bio::EnsEMBL::Pipeline::DBSQL::DBAdaptor',
+      'snp' =>
+        'Bio::EnsEMBL::ExternalData::SNPSQL::DBAdaptor',
+      'variation' =>
+        'Bio::EnsEMBL::Variation::DBSQL::DBAdaptor',
+      'vega' => 'Bio::EnsEMBL::DBSQL::DBAdaptor',
+    );
+
+    my $adaptorclass = $group2adaptor{ $self->{'_group'} } || croak 'Unknown database group: '.$self->{'_group'};
+    # Creating a new connection will add it to the registry.
+    eval "require $adaptorclass";
+    $@ && die $@;
+    $adaptorclass->new(
+      -host    => $self->config->{'host'}     || 'localhost',
+      -port    => $self->config->{'port'}     || '3306',
+      -user    => $self->config->{'username'} || 'ensro',
+      -pass    => $self->config->{'password'},
+      -dbname  => $dbname,
+      -species => $self->{'_species'},
+      -group   => $self->{'_group'},
+    );
+  }
+  return;
+}
+
+
+
+sub gene_by_id {
+  my ($self, $id, $species, $group) = @_;
+  return $self->gene_adaptor($species, $group)->fetch_by_stable_id($id);
+}
+
+sub genes {
+  my ($self, $species, $group) = @_;
+  return $self->gene_adaptor($species, $group)->fetch_all();
+}
+
+sub version {
+  my ($self) = @_;
+  return Bio::EnsEMBL::Registry->software_version();
+}
+
+sub last_modified {
+  my ($self) = @_;
+  my $dbc = $self->adaptor()->dbc();
+  my $sth = $dbc->prepare(q(SHOW TABLE STATUS));
+  $sth->execute();
+  my $server_text = [sort { $b cmp $a } ## no critic
+                     keys %{ $sth->fetchall_hashref('Update_time') }
+                    ]->[0]; # server local time
+  $sth->finish();
+  $sth = $dbc->prepare(q(SELECT UNIX_TIMESTAMP(?) as 'unix'));
+  $sth->execute($server_text); # sec since epoch
+  my $server_unix = $sth->fetchrow_arrayref()->[0];
+  $sth->finish();
+  return $server_unix;
+}
+
+sub disconnect {
+  my $self = shift;
+  Bio::EnsEMBL::Registry->disconnect_all();
+  $self->{'debug'} and carp "$self performed disconnect\n";
+  return;
+}
+
+
 
 1;
