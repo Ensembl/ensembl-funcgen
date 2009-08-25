2978de5b89932ae32dcec16242d0c21b2f69bc6e
Nathan Johnson <njohnson@ebi.ac.uk>
2009-08-25 13:15:27 +0000

    patch script to fix seq_region problem with DB which have non-standard names

---
+++ sql/patch_seq_region_schema_build.sql
@@ -0,0 +1,25 @@
+--This patch is to correct a problem with DBs which do not follow
+--the standard naming convention(drosophila, c elegans). Non standard names result in new
+--coord systems and seq_regions being generated when they are in fact the 
+--same as a previous release.  This can cause data loaded on an old coord system
+--not to be included in the new coord system, hence data will not be accessable using 
+--the latest dnadb.
+
+--The fix is to simple patch the seq_region_ids in the feature tables affected.
+--osr = old_seq_region
+--nsr = new_seq_region
+ 
+--This is a self join
+--will give large product due to nr records!
+
+--select osr.seq_region_id, nsr.seq_region_id, nsr.name from (select seq_region_id, name from seq_region where schema_build='55_54c') osr, (select seq_region_id, name from seq_region where schema_build='56_513a') nsr where osr.name=nsr.name; 
+
+--Could we nest a self join rather than deriving both tables?
+--Or is this just hte same?
+
+--select sr.old_seq_region_id, sr.new_seq_region_id, sr.name from (select sr2.seq_region_id as old_seq_region_id, sr1.seq_region_id as new_seq_region_id, sr2.name from seq_region sr2 join seq_region sr1 on sr1.name=sr2.name and sr2.schema_build='55_54c' and sr1.schema_build='56_513a') sr;
+
+--No, this uses a key and one less derived table!
+
+
+update external_feature ef, (select sr.old_seq_region_id as osr, sr.new_seq_region_id as nsr from (select sr2.seq_region_id as old_seq_region_id, sr1.seq_region_id as new_seq_region_id, sr2.name from seq_region sr2 join seq_region sr1 on sr1.name=sr2.name and sr2.schema_build='55_54c' and sr1.schema_build='56_513a') sr) sr3 set ef.seq_region_id=sr3.nsr where ef.seq_region_id=sr3.osr; 
