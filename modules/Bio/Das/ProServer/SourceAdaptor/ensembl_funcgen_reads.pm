c257dd0b007d798ac9f54478ced210a658f1c04d
Nathan Johnson <njohnson@ebi.ac.uk>
2009-09-07 14:17:45 +0000

    merged ensemb_feature/result_set to ensembl_set created ensembl_funcgen_reads removed some old adaptors

---
+++ modules/Bio/Das/ProServer/SourceAdaptor/ensembl_funcgen_reads.pm
@@ -0,0 +1,293 @@
+=head1 NAME
+
+Bio::Das::ProServer::SourceAdaptor::ensembl_funcgen_reads
+
+=head1 DESCRIPTION
+
+This is a SourceAdaptor module to retrieve raw read alignments and profiles.
+
+=head1 LICENSE
+
+  Copyright (c) 1999-2009 The European Bioinformatics Institute and
+  Genome Research Limited.  All rights reserved.
+
+  This software is distributed under a modified Apache license.
+  For license details, please see
+
+    http://www.ensembl.org/info/about/code_licence.html
+
+=head1 CONTACT
+
+  Please email comments or questions to the public Ensembl
+  developers list at <ensembl-dev@ebi.ac.uk>.
+
+  Questions may also be sent to the Ensembl help desk at
+  <helpdesk@ensembl.org>.
+
+
+=cut
+
+package Bio::Das::ProServer::SourceAdaptor::ensembl_funcgen_reads;
+
+use strict;
+use vars qw(@ISA);
+use Bio::Das::ProServer::SourceAdaptor;
+@ISA = qw(Bio::Das::ProServer::SourceAdaptor);
+
+
+#To Do
+
+
+#Let's get this set up with just reads/profile first
+#Then what?
+#Implement colour hash?
+
+
+sub init {
+    my $self                = shift;
+    $self->{'capabilities'} = {
+        'features' => '1.0',
+        'stylesheet' => '1.0',
+    };
+    $self->{'dsnversion'} = '1.1';
+    #$self->{'coordinates'} = {
+    #	'http://www.dasregistry.org/dasregistry/coordsys/CS_DS40' => '7:140079754,140272033'
+    #};
+	
+	my %valid_data_formats = (
+							  reads   => 1,#can we make this more useful?
+							  profile => 1,
+							 );
+	
+	#Should check transport here?
+
+	if(defined $self->hydra){
+	  #Set title based on table name
+ 	  #This need to be generic for standard and hydra source
+
+	  my $hydra_name = $self->config->{'hydraname'};
+	  my $full_name = $self->dsn;	  
+	  #Leave dsn as is?
+	  #Causes failure when querying
+	  ($self->{'title'} = $full_name) =~ s/${hydra_name}_//;
+	  #reconstitute table name here
+	  $self->{'table_name'} = $self->config->{'basename'}.'_'.$self->{'title'};
+
+
+	  #This assumes first token of title is display/data type
+	  #e.g. reads or profile
+	  my $dtype = (split/_/, $self->{'title'})[0];
+	  $self->{'title'} =~ s/${dtype}_//;
+	  $self->{'description'} = $self->{'title'}.' '.$dtype;	
+	  $self->{'title'} =  $self->{'title'}."_${dtype}";
+
+
+	  #strip the trailing s if reads
+	  #This is not really the DAS type, which is the type of annotation
+	  #And is normally used to retrieve additional info from the annotation server
+	  #Should we change this to the feature type name?
+
+	  ($self->{'display_type'} = $dtype) =~ s/reads$/read/;
+
+
+	  if(! exists $valid_data_formats{$dtype}){
+		warn 'The table name '.$self->{'table_name'}.' does not specify a valid format ('.join(',', keys %valid_data_formats).')e.g. bed_reads_ES_DNase';
+		return;
+	  }
+	  
+	  #Can we set title to same name for two different types? e.g. reads or profile?
+	  #Yes, so long as we set the description to differentiate between the two		
+	  #$self->{'title'} =~ s/${dtype}_//;
+	  #$self->{'description'} = $self->{'title'}.' '.$dtype;	
+	  #No? It seems we may not be able to display two track with the same name???
+
+
+	  #We need to be selective about the tables we use here
+	  #Looks like default is to use all tables
+	  #Need to set config basename as bed\_
+	  #wild cards screw up dsn name generation so handle here instead
+	}
+	else{
+	  #All the above should be set in the config
+	}
+
+#	warn "dsn is ".$self->dsn;
+#	warn "title is ".$self->title;
+#	warn "table_name is ".$self->{'table_name'};
+
+
+	#Now set feature colour here based on feature_type
+
+}
+
+sub title {
+    my $self = shift;
+
+    return $self->{'title'};
+}
+
+sub build_features {
+    my ($self, $opts) = @_;
+    
+    my $segment       = $opts->{'segment'};
+    my $gStart        = $opts->{'start'};
+    my $gEnd          = $opts->{'end'};
+
+    ### using max bins
+    #if( $opts->{'maxbins'} && $gStart && $gEnd) {
+    #    return $self->merge_features($opts);
+    #}
+
+    my $dsn           = $self->{'dsn'};
+    my $dbtable       =  $self->{'table_name'};#$dsn;
+    #print Dumper $dbtable;
+    
+    #########
+    # if this is a hydra-based source the table name contains the hydra name and needs to be switched out
+    #
+    #my $hydraname     = $self->config->{'hydraname'};
+    #if ($hydraname) {
+    #    my $basename = $self->config->{'basename'};
+    #    $dbtable =~ s/$hydraname/$basename/;
+    #}
+  
+  
+    my $qsegment      = $self->transport->dbh->quote($segment);
+    my $qbounds       = qq(AND start <= '$gEnd' AND end >= '$gStart') if($gStart && $gEnd);
+	#feature_id | seq_region | start   | end     | name                   | score | strand | note
+    my $query         = qq(SELECT * FROM $dbtable WHERE  seq_region = $qsegment $qbounds); # ORDER BY start);
+    my $ref           = $self->transport->query($query);
+    my @features      = ();
+    
+    for my $row (@{$ref}) {
+
+        my ($start, $end, $score) = ($row->{'start'}, $row->{'end'}, $row->{'score'});
+        my $type;
+
+		#Can we change this to use ori instead?
+		#Remove type or use fill-in?
+		
+        if ($self->{'display_type'} eq 'profile') {
+		  $type = 'profile_read';
+		} 
+		elsif ($self->{'display_type'} eq 'read') {
+		  $type = $row->{'strand'} eq '+' ? 'forward' : 'reverse';
+		  $type .= '_read';
+		  $row->{'strand'} = '';#???? Populating this leads to display on +/-ve strand, we want it just on the -ve strand
+        }
+		#Not used anymore
+		#else {
+        #    $type = ($score > 0) ? 'uniq' : 'non-uniq';y
+        #}
+		
+
+		#The less data we push here the quicker the display.
+		#Omit method, label?
+
+		push @features, {
+            'id'          => $row->{'feature_id'},
+            'label'       => $row->{'name'} !~ m/^.$/ ? $row->{'name'} : $row->{'feature_id'},
+            #'method'      => $self->config->{'method'},
+            'type'        => $type,#$self->{'type'},
+            'typecategory'=> $self->config->{'category'} || 'sequencing',#required for style sheet
+            'start'       => $row->{'start'},
+            'end'         => $row->{'end'},
+            'ori'         => $row->{'strand'},#Can we just set this to ''?
+            'score'       => $row->{'score'},
+            #'note' => [],
+        };
+
+    }    
+   
+    #warn "No. of features: ".scalar(@features)."\n";
+    return @features;
+}
+
+
+#We need to define a base base_funcgen.pm which will have method to access colours dependant on 
+#modification.
+#This should mirror what the main site uses
+
+
+sub das_stylesheet
+{
+    my $self = shift;
+
+
+	#This should use a format or type config rather than the name
+
+	#These need to use the same config as the reg feat panel
+	#There fore we need to encode the feature type in the file name
+
+    if ($self->{'dsn'} =~ m/_profile/) {
+        return <<EOT;
+<!DOCTYPE DASSTYLE SYSTEM "http://www.biodas.org/dtd/dasstyle.dtd">
+<DASSTYLE>
+    <STYLESHEET version="1.0">
+ <CATEGORY id="sequencing">
+            <TYPE id="profile_read">
+                <GLYPH>
+                    <TILING>
+                        <LABEL>no</LABEL>
+                        <HEIGHT>30</HEIGHT>
+                        <COLOR1>brown4</COLOR1>
+                        <MIN>0</MIN>
+                        <MAX>15</MAX>
+                    </TILING>
+                </GLYPH>
+            </TYPE>
+ </CATEGORY>
+    </STYLESHEET>
+</DASSTYLE>
+EOT
+
+    } else {
+
+	  #Can we use FARROW and RARROW here?
+	  #These will set orientation, so will be displayed in separate tracks?
+	  #SOUTHWEST & NORTHEAST define direction of arrow
+	  #Can we harcode <ORIENTATION> here, will this override the feature ori? No it doesn't
+	  #Turn off labels by default..this is not yet supported by the browser
+	  #Do we need a default in here?
+	  
+
+        return <<EOT;
+<!DOCTYPE DASSTYLE SYSTEM "http://www.biodas.org/dtd/dasstyle.dtd">
+<DASSTYLE>
+    <STYLESHEET version="1.0">
+    <CATEGORY id="sequencing">
+             <TYPE id="forward_read">
+                <GLYPH>
+                    <ARROW>
+                        <LABEL>no</LABEL>
+                        <HEIGHT>10</HEIGHT>
+                        <FGCOLOR>black</FGCOLOR>
+                        <BUMP>1</BUMP>
+                        <BAR_STYLE>full</BAR_STYLE>
+                        <SOUTHWEST>no</SOUTHWEST>
+                    </ARROW>
+                </GLYPH>
+            </TYPE>
+            <TYPE id="reverse_read">
+                <GLYPH>
+                    <ARROW>
+                        <LABEL>no</LABEL>
+                        <HEIGHT>10</HEIGHT>
+                        <FGCOLOR>darkgreen</FGCOLOR>
+                        <BUMP>1</BUMP>
+                        <BAR_STYLE>full</BAR_STYLE>
+                        <NORTHEAST>no</NORTHEAST>
+                    </ARROW>
+                </GLYPH>
+            </TYPE>
+        </CATEGORY>
+    </STYLESHEET>
+</DASSTYLE>
+EOT
+    }
+}
+
+1;
+
+
+
---
+++ modules/Bio/Das/ProServer/SourceAdaptor/ensembl_funcgen_set.pm
@@ -0,0 +1,307 @@
+=head1 NAME
+
+ensembl_funcgen_set.pm
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+Proserver module to access an ensembl funcgen Result/FeatureSet as a DAS source.
+
+=head1 LICENSE
+
+  Copyright (c) 1999-2009 The European Bioinformatics Institute and
+  Genome Research Limited.  All rights reserved.
+
+  This software is distributed under a modified Apache license.
+  For license details, please see
+
+    http://www.ensembl.org/info/about/code_licence.html
+
+=head1 CONTACT
+
+  Please email comments or questions to the public Ensembl
+  developers list at <ensembl-dev@ebi.ac.uk>.
+
+  Questions may also be sent to the Ensembl help desk at
+  <helpdesk@ensembl.org>.
+
+=cut
+
+
+#To do
+# Rename ensembl_set and integrate result_set support?
+# Move transport stuff to transport? This module should only be for config parsing and display functions
+
+package Bio::Das::ProServer::SourceAdaptor::ensembl_funcgen_set;
+
+use warnings;
+use strict;
+use Data::Dumper;
+#use IO::File;
+
+use base qw(Bio::Das::ProServer::SourceAdaptor);
+
+sub init
+{
+    my ($self) = @_;
+
+    $self->{'capabilities'} = {
+        'features' => '1.0',
+        'stylesheet' => '1.0'
+        };
+
+	
+	#Set the set name here if hydra
+	my $set_name;
+
+	if($self->hydra){
+	  my $hydraname = $self->config->{'hydraname'};
+	  ($set_name = $self->dsn) =~ s/${hydraname}_//;
+	}
+
+	#Then explicitly fetch the set using the name
+	$self->{'set'} = $self->transport->fetch_set($set_name);
+
+
+	$self->{'title'} = 	$self->config->{'title'} || $self->set->name;
+
+	#Will this be valid for result_sets?
+
+	$self->{'description'} = $self->config->{'description'} || 
+	  ($self->config->{'set_type'} eq 'result') ? $self->set->name.' tiling' : $self->set->display_label;
+
+	return;
+}
+
+sub title {
+    my $self = shift;
+  
+    return $self->{'title'};
+}
+
+sub set{
+  my $self = shift;
+  
+  #Never required as we don this in init.
+  #if(! defined $self->{'set'}){
+#	$self->{'set'} = $self->transport->fetch_set;
+#  }
+
+  return $self->{'set'};
+}
+
+
+sub build_features{
+  my ( $self, $args ) = @_;
+  print Dumper $args if ($self->{'debug'});
+  
+  my $segment = $args->{'segment'};
+  my $start   = $args->{'start'};
+  my $end     = $args->{'end'};
+  
+  if(! ($segment && $start && $end)){
+	warn "Insufficient loci params:\t${segment}:${start}:${end}";
+	return;
+  }
+  
+  my $build_method = 'build_'.$self->config->{'set_type'}.'_set_features';
+  return $self->$build_method($segment, $start, $end);
+}
+
+
+sub build_result_set_features{
+  my ($self, $segment, $start, $end) = @_;
+
+  my ($id, $label, %score, @features);
+  my $slice        = $self->transport->chromosome_by_region($segment, $start, $end);
+  print Dumper $slice if ($self->{'debug'});
+  my $features     = $self->set->get_ResultFeatures_by_Slice($slice);
+  print "Number of features: ".scalar(@{$features})."\n" if ($self->{'debug'});
+
+  my $type     = $self->config()->{'type'} || 'default';
+  my $source   =  $self->config()->{'source'} || $self->set->analysis->display_label;
+  my $type_cat =  $self->config()->{'typecategory'} || 'result_set';
+
+  foreach my $ft(@{$features}){
+ 
+        my $ft_start = $start+$ft->start();
+        my $ft_end = $start+$ft->end();
+		
+        my $id = sprintf( "%s:%s,%s",
+                          $segment,
+                          $ft_start,
+                          $ft_end);
+       
+        push @features, {
+            
+						 'id'          => $id,
+						 'label'       => $id,
+						 'start'       => $ft_start,
+						 'end'         => $ft_end,
+						 #'ori'         => 1,
+						 'score'       => $ft->score,
+						 'method'      => $source,
+						 'type'        => $type,
+						 'typecategory'=> $type_cat,
+						 #'note'        => $note,
+						 #'link'        => '',
+						 #'linktxt'     => '',
+						 
+						}
+
+	  };
+
+  return @features;
+}
+
+
+
+sub build_feature_set_features{
+  my ($self, $segment, $start, $end) = @_;
+
+  my ($id, $label, %score, @features);
+  my $slice        = $self->transport->chromosome_by_region($segment, $start, $end);
+  print Dumper $slice if ($self->{'debug'});
+  my $features     = $self->set->get_Features_by_Slice($slice);
+  print "Number of features: ".scalar(@{$features})."\n" if ($self->{'debug'});
+  my $set_type = $self->config->{'set_type'};
+
+
+  my $type = $self->config->{'type'}         || 'peak';
+  #Need a way of setting this to DNA met, or even reg feats!
+
+  my $cat  = $self->config->{'typecategory'} ||  'feature_set';
+  my $label_method = ($set_type eq 'regulatory') ? 'stable_id' : 'display_label';
+  my $source   =  $self->config()->{'source'} || $self->set->analysis->display_label;
+
+
+  foreach my $ft (@{$features}) {
+
+	$id = sprintf( "%s:%s,%s",
+				   $segment,
+				   $ft->seq_region_start,
+				   $ft->seq_region_end,
+				 );
+	  
+	#because we don't want to pass empty score?
+	%score = ($set_type eq 'annotated') ? (score => $ft->score) : ();
+
+
+	push @features,
+	  {
+	   'id'          => $id,
+	   'label'       => $ft->$label_method,
+	   #'segment'     => $segment,#Does not return to DAS features test page???
+	   'start'       => $ft->seq_region_start,
+	   'end'         => $ft->seq_region_end,
+	   'ori'         => $ft->seq_region_strand,
+	   'method'      => $source,
+	   #Can change this to be more informative? Have display label/feature_type here
+	   #And extended info(reg atts) in note?
+	   'type'        => $type,
+	   'typecategory'=> $cat,
+	   #'note'        => $ft->display_label,
+	   #            'link'        => 'http://www.sanger.ac.uk/PostGenomics/epigenome/',
+	   #            'linktxt'     => 'Human Epigenome Project (HEP)'
+	   %score
+	  };
+  }
+  
+  return @features;
+}
+
+
+
+sub das_stylesheet{
+  my $self = shift;
+
+  
+  #need to define this based on config
+  #And some defaults
+  #Will this be possible with hydra config?
+  
+  
+  #print Dumper $self->{'set_name'};
+  
+  if ($self->set->isa('Bio::EnsEMBL::Funcgen::ResultSet')){
+	return <<EOT;
+<!DOCTYPE DASSTYLE SYSTEM "http://www.biodas.org/dtd/dasstyle.dtd">
+<DASSTYLE>
+    <STYLESHEET version="0.01">
+        <CATEGORY id="result_set">
+            <TYPE id="default">
+                <GLYPH>
+                    <TILING>
+                        <HEIGHT>30</HEIGHT>
+                        <COLOR1>brown3</COLOR1>
+                    </TILING>
+                </GLYPH>
+            </TYPE>
+        </CATEGORY>
+    </STYLESHEET>
+</DASSTYLE>
+EOT
+  }
+  elsif ($self->set->name =~ m/_BR/) {
+	return <<EOT;
+<!DOCTYPE DASSTYLE SYSTEM "http://www.biodas.org/dtd/dasstyle.dtd">
+<DASSTYLE>
+    <STYLESHEET version="0.01">
+        <CATEGORY id="feature_set">
+            <TYPE id="default">
+                <GLYPH>
+                    <BOX>
+                        <FGCOLOR>black</FGCOLOR>
+                        <BGCOLOR>brown3</BGCOLOR>
+                        <HEIGHT>5</HEIGHT>
+                    </BOX>
+                </GLYPH>
+            </TYPE>
+        </CATEGORY>
+    </STYLESHEET>
+</DASSTYLE>
+EOT
+
+  } else {
+        
+	return <<EOT;
+<!DOCTYPE DASSTYLE SYSTEM "http://www.biodas.org/dtd/dasstyle.dtd">
+<DASSTYLE>
+    <STYLESHEET version="0.01">
+        <CATEGORY id="feature_set">
+            <TYPE id="default">
+                <GLYPH>
+                    <BOX>
+                        <FGCOLOR>red</FGCOLOR>
+                        <BGCOLOR>black</BGCOLOR>
+                    </BOX>
+                </GLYPH>
+            </TYPE>
+            <TYPE id="peak">
+                <GLYPH>
+                    <BOX>
+                        <FGCOLOR>black</FGCOLOR>
+                        <BGCOLOR>brown4</BGCOLOR>
+                        <HEIGHT>5</HEIGHT>
+                    </BOX>
+                </GLYPH>
+            </TYPE>
+            <TYPE id="DNA methylation">
+                <GLYPH>
+                    <BOX>
+                        <FGCOLOR>blue</FGCOLOR>
+                        <BGCOLOR>black</BGCOLOR>
+                    </BOX>
+                </GLYPH>
+            </TYPE>
+        </CATEGORY>
+    </STYLESHEET>
+</DASSTYLE>
+EOT
+  }
+}
+
+1;
+
