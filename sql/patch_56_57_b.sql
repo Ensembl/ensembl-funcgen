89809ecfdda1f916c87de62536cb5d788ec9c26d
Nathan Johnson <njohnson@ebi.ac.uk>
2009-11-03 11:45:15 +0000

    added uo key patch

---
+++ sql/patch_56_57_b.sql
@@ -0,0 +1,13 @@
+# patch_56_57_a.sql
+#
+# title: unmapped_object object_type key
+#
+# description:
+# Add a ensembl_object_type/id key to the unmapped_object table
+
+ALTER TABLE object_xref ADD KEY `object_type_idx` (`ensembl_object_type`,`ensembl_id`);
+
+# patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_56_57_b.sql|uo.object_type_id_key');
+
+
