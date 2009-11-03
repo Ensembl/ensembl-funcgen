b9ddb7789b7e484fe1d56d8bb047b2cadecbcfb0
Nathan Johnson <njohnson@ebi.ac.uk>
2009-11-03 11:47:31 +0000

    corrected to unmapped_object

--- sql/patch_56_57_b.sql
+++ sql/patch_56_57_b.sql
@@ -5,7 +5,7 @@
 # description:
 # Add a ensembl_object_type/id key to the unmapped_object table
 
-ALTER TABLE object_xref ADD KEY `object_type_idx` (`ensembl_object_type`,`ensembl_id`);
+ALTER TABLE unmapped_object ADD KEY `object_type_idx` (`ensembl_object_type`,`ensembl_id`);
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_56_57_b.sql|uo.object_type_id_key');
