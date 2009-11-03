b2c3c5ce8f23491fd63570ebefb503238ddf064a
Nathan Johnson <njohnson@ebi.ac.uk>
2009-11-03 13:16:54 +0000

    commented out data patch

--- sql/patch_56_57_c.sql
+++ sql/patch_56_57_c.sql
@@ -7,8 +7,8 @@
 
 alter table unmapped_object modify `type` enum('xref','probe2transcript', 'array_mapping') NOT NULL;
 
-#Do data patch
-update unmapped_object set type='array_mapping' where type='' and (identifier='genomics' or identifier='transcript');
+#Optionally do data patch
+#update unmapped_object set type='array_mapping' where type='' and (identifier='genomics' or identifier='transcript');
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_56_57_c.sql|uo.object_type_enum');
