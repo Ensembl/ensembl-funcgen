cbe3f90823c28e12cbbd51f3531420a5810423ff
Nathan Johnson <njohnson@ebi.ac.uk>
2009-11-11 12:57:40 +0000

    added get_coord_system_versions

--- modules/Bio/Das/ProServer/SourceAdaptor/Transport/ensembl_funcgen.pm
+++ modules/Bio/Das/ProServer/SourceAdaptor/Transport/ensembl_funcgen.pm
@@ -1,6 +1,6 @@
 =head1 NAME
 
-efg_transport.pm
+ensembl_funcgen.pm
 
 =head1 DESCRIPTION
 
@@ -27,17 +27,12 @@ Transport module to retrieve Annotated/ResultFeatures from an efg database
 
 =cut
 
+#To do
+# 1 More debug stuff
+# 2 Docs
+
+
 package Bio::Das::ProServer::SourceAdaptor::Transport::ensembl_funcgen;
-
-
-
-#BEGIN {
-#  my ($eroot) = $ENV{'ENS_ROOT'}     =~ m|([a-zA-Z0-9_/\.\-]+)|;
-#  my ($broot) = $ENV{'BIOPERL_HOME'} =~ m|([a-zA-Z0-9_/\.\-]+)|;
-#
-#  unshift(@INC,"$eroot/ensembl/modules");
-#  unshift(@INC,"$broot");
-#}
 
 use strict;
 use Carp;
@@ -54,28 +49,44 @@ use vars qw(@ISA);
 
 #No init as data_version now either explicilty set as part of dnadb config
 #Or autoselect in the DBAdaptor
+#Add some debug stuff?
 
 sub adaptor {
-    my $self = shift;
+  my $self = shift;
 
 
     if(! defined $self->{'_adaptor'}) {
 		  
 	  #Can get problems here with Registry persisting after transport has gone out of scope 
 	  #and been destroyed.
-	  #Creating a new adaptor will cause the species to be incremented
-	  #in turn causing the dnadb never to be found by the DBAdaptor
-	  
-	  #Will this cause a problem when we want to use more than one core DB for the same species?
+	  #This happens for every source after the 1st DB, as the DB comparison in ConfigRegistry is
+	  #always against the first stored
 
 	  #Ensembl API was never designed to run like this!
 
-	  #Let's test the registry version to see if it matches what we want and reinstate it
+	  #Let's test the registry version to see if it matches what we want and reinstate it?
 	  #Otherwise we are going to have to deal with incremented species names in the DBAdaptor
-
-	  
+	  #No, this should be done in ConfigRegistry
 
 	  #do we need to parse coordinates here to set dnadb_assembly?
+	  #We should really be able to run this from the same dnadb so long as it supports it
+	  #This mean we need to set a version to pass to SliceAdaptor->fetch_by_region
+	  #We also need to test wether it is available in the dnadb that is defined or detected
+
+	  #Should we make this optional?  Just rely on default dnadb chosen by efg?
+	  my $dnadb = Bio::EnsEMBL::DBSQL::DBAdaptor->new
+		( 
+		 -host   => $self->config->{'dnadb_host'},
+		 -dbname => $self->config->{'dnadb_name'}, 
+		 -port   => $self->config->{'dnadb_port'},
+		 -user   => $self->config->{'dnadb_user'},
+		 -pass   => $self->config->{'dnadb_pass'},
+		 -group  => 'core',
+		);
+
+	  #Now check we have the required CS version in the dnadb?
+	  #This is already done in general_DAS_config.pl
+	  
 	  $self->{'_adaptor'} ||= Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor->new
 		(
 		 -host    => $self->config->{'host'}     || 'localhost',
@@ -84,13 +95,20 @@ sub adaptor {
 		 -species => $self->config->{'species'}  || carp('No species defined for '.$self->{'dsn'}.' in your das_source.ini file'),
 		 -pass    => $self->config->{'password'},
 		 -port    => $self->config->{'port'},
-		 -group   => 'funcgen',
-		 -dnadb_assembly => $self->config->{'dnadb_assembly'},
-		 -dnadb_host => $self->config->{'dnadb_host'},
-		 -dnadb_port => $self->config->{'dnadb_post'},
-		 -dnadb_user => $self->config->{'dnadb_user'},
-		 -dnadb_pass => $self->config->{'dnadb_pass'},
+		 -dnadb   => $dnadb,
+		 #-group   => 'funcgen',#Now set as default
+		 #The following are never set by GenerateConfig
+		 #So ensembldb would be used, which is fine for just fetching slice info
+		 #Will need to set this if we don't have it on ensembldb!
+
+		 #-dnadb_assembly => $self->config->{'dnadb_assembly'},
+		 #-dnadb_host => $self->config->{'dnadb_host'},
+		 #-dnadb_port => $self->config->{'dnadb_post'},
+		 #-dnadb_user => $self->config->{'dnadb_user'},
+		 #-dnadb_pass => $self->config->{'dnadb_pass'},
+		 #Need to add dnadbname here!
 		);
+
 	  print Dumper $self->{'_adaptor'} if ($self->{'debug'});
 
 	  #Test the connection
@@ -99,17 +117,40 @@ sub adaptor {
 	  #Set disconnect when inactive to 1 to enable autoreconnect
 	  $self->{'_adaptor'}->dbc->disconnect_when_inactive(1);;
     }
-
-	return $self->{'_adaptor'};	
+  
+  return $self->{'_adaptor'};	
 }
 
+
+#Do we even really need this?
+#Ensembl API replaces what should be in here
+
 sub chromosome_by_region {
-  my ($self, $chr, $start, $end) = @_;
-  my $slice = $self->adaptor->dnadb->get_SliceAdaptor->fetch_by_region
-      (
-       'chromosome', $chr, $start, $end
-       );
-  return $slice;
+  my ($self, $chr, $start, $end, $strand) = @_;
+
+  #$cs_version should be set once for this transport based on the coordinates config or the set name?
+
+  #This should really be region by name, and use the toplevel?
+  #However, we can't use top level for old assemblies
+  
+  return $self->adaptor->dnadb->get_SliceAdaptor->fetch_by_region('chromosome', $chr, $start, $end, $strand, $self->coord_system_version);
+}
+
+
+sub get_coord_system_versions{
+  my ($self) = @_;
+
+  if(! defined $self->{'_coord_versions'}){
+	$self->{'_coord_versions'} = [];
+	my $coords = $self->config->{'coordinates'};
+	$coords =~ s/;$//;
+	my @cs_versions = split /;\s*/, $coords;
+	map { $_ =~ s/\s*,.*//} @cs_versions;
+	map { $_ =~ s/_//} @cs_versions;
+	@{$self->{'_coord_versions'}} = @cs_versions;
+  }
+
+  return $self->{'_coord_versions'};
 }
 
 sub disconnect {
@@ -183,18 +224,6 @@ sub fetch_set{
   return $set;
 }
 
-
-
-
-#DO NOT NEED THIS
-#And in fact seems to cause problems with mysterious disconnections on and autreconnect handle
-#sub DESTROY {
-#  my $self = shift;
-
-#  $self->disconnect();
-#}
-
-
 sub last_modified {
   my ($self) = @_;
   my $dbc = $self->adaptor()->dbc();
@@ -211,28 +240,37 @@ sub last_modified {
   return $server_unix;
 }
 
+sub version {
+  my ($self) = @_;
+  return Bio::EnsEMBL::Registry->software_version();
+}
+
 
 
 1;
 
 __END__
 
-#Are any of these used?
-#Or is this hangover from ensembl adaptor?
-#Using the registry will create problems
-#as we may had multiple DBs with the same species
-#which will cause the registry to redefine the species name
+#DO NOT NEED THIS
+#And in fact seems to cause problems with mysterious disconnections on and autreconnect handle
+#sub DESTROY {
+#  my $self = shift;
 
-sub version {
-  my ($self) = @_;
-  return Bio::EnsEMBL::Registry->software_version();
-}
+#  $self->disconnect();
+#}
+
+
+
+
+#These are from the standard ensembl transport
+#We could resurrect these(or inherit from standard ensembl transport)
+#Currently not being used
+
 
 sub chromosomes {
   my ($self, $species, $group) = @_;
   return $self->slice_adaptor($species, $group)->fetch_all('chromosome');
 }
-
 
 sub _load_registry {
   my ($self) = @_;
