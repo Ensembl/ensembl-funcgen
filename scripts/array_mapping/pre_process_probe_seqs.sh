25f05204e3ecd9d89c506d6bb2a2f7d01cc47085
Nathan Johnson <njohnson@ebi.ac.uk>
2009-11-12 10:10:09 +0000

    fixed illumina txt array name bug

--- scripts/array_mapping/pre_process_probe_seqs.sh
+++ scripts/array_mapping/pre_process_probe_seqs.sh
@@ -55,7 +55,7 @@ elif [ $file_type = TXT ]; then
 	if [ $vendor = ILLUMINA ]; then
 		awk "BEGIN { FS = \"\t\" }; {if (\$1==\"Species\") probes=1;
 else if (\$1==\"[Controls]\") exit;
-else if (probes == 1) print \">${name}:\" \$14 \"\n\" \$18}" $file_path > ${file_path}.tmp
+else if (probes == 1) print \">${array_name}:\" \$14 \"\n\" \$18}" $file_path > ${file_path}.tmp
 
 	elif [ $vendor = CODELINK ]; then
 awk "{if (\$1==\"CUSTOMER_PROBE_NAME\") probes=1;
