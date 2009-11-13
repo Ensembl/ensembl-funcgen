17faa24e28b0077719003514040f64c6a8173065
Nathan Johnson <njohnson@ebi.ac.uk>
2009-11-13 13:49:35 +0000

    Updated to use InputSet

--- modules/Bio/EnsEMBL/Funcgen/Parsers/Bed.pm
+++ modules/Bio/EnsEMBL/Funcgen/Parsers/Bed.pm
@@ -30,22 +30,46 @@ Post questions to the EnsEMBL development list ensembl-dev@ebi.ac.uk
 
 package Bio::EnsEMBL::Funcgen::Parsers::Bed;
 
-use Bio::EnsEMBL::Funcgen::ExperimentalSet;
+use Bio::EnsEMBL::Funcgen::Parsers::InputSet;
 use Bio::EnsEMBL::Funcgen::FeatureSet;
 use Bio::EnsEMBL::Funcgen::AnnotatedFeature;
 use Bio::EnsEMBL::Utils::Exception qw( throw warning deprecate );
-use Bio::EnsEMBL::Funcgen::Utils::EFGUtils qw(species_chr_num open_file);
+use Bio::EnsEMBL::Funcgen::Utils::EFGUtils qw(open_file);
 use Bio::EnsEMBL::Utils::Argument qw( rearrange );
 use Bio::EnsEMBL::Funcgen::Utils::Helper;
+use File::Basename;
 use strict;
 
 use Data::Dumper;
 
 use vars qw(@ISA);
-@ISA = qw(Bio::EnsEMBL::Funcgen::Utils::Helper);
+@ISA = qw(Bio::EnsEMBL::Funcgen::Parsers::InputSet);
+
+#To do
+# Extend this so we can import features to annotated_feature
+# and profiles/reads to result_feature
+# This will replace individual tables for current bed das sources
+# Altho' idea of separate tables for each set is not necessarily a bad one
+# We would have to have extra table or fields details registration
+# Easier admin i.e. drop tables
+# What about partitioning? Irrelevant if we are moving to matrix
+# Not easy to patch dynamically named tables? Can't assign values to user varaible from query!
+# Would need to be a stored procedure
+# Could have separate matrix files for result_features as we probably wouldn't want 
+# to do any cross set querying at this level.
+# Could we also use this in the RunnableDBs?
+# Simply separate those methods required by both normal bed annotated_feature import
+# and RunnableDB based annotated_feature import
+# Importing into result_feature requires a result_set which assumes a chip experiment
+# We need to alter result_set such that it can be optionally associated with ExperimentalSets
+# Or should we just go for a different table? read_feature?
+# NOTE: PARITION by key may run into problems if different sets have different windows
+# Would also need to modify ResultFeatureAdaptor?
 
 =head2 new
 
+  Arg[0]     : hash containing optional attributes:
+                          -bed_reads => 0|1, #Set input as read alignments rather than peak calls (default is 0)
   Example    : my $self = $class->SUPER::new(@_);
   Description: Constructor method for Bed class
   Returntype : Bio::EnsEMBL::Funcgen::Parsers::Bed
@@ -55,13 +79,21 @@ use vars qw(@ISA);
 
 =cut
 
-
 sub new{
   my $caller = shift;
   
   my $class = ref($caller) || $caller;
-  my $self  = $class->SUPER::new();
+
+  my $self = $class->SUPER::new(@_);
   
+
+  #Bed specific params
+  my ($bed_reads) = rearrange(['BED_READS'], @_);
+
+  #This should be annotated or result which can then be used in define_and_validate_sets in Helper
+
+  $self->{'set_feature_type'} = 'result' if $bed_reads;
+
   throw("This is a skeleton class for Bio::EnsEMBL::Importer, should not be used directly") 
 	if(! $self->isa("Bio::EnsEMBL::Funcgen::Importer"));
   
@@ -71,7 +103,7 @@ sub new{
 	  #remove method arrays and execute serially?
       array_data   => [],#['experiment'],
       probe_data   => [],#["probe"],
-      results_data => ["and_import_bed"],
+      results_data => ["and_import"],
       norm_method => undef,
 
 	  #Need to make these definable?
@@ -100,273 +132,135 @@ sub new{
 sub set_config{
   my $self = shift;
 
-  throw('Must provide an ExperimentalSet name for a Bed import') if ! defined $self->experimental_set_name();
+  $self->SUPER::set_config;
 
-  #Mandatory checks
-  if(! defined $self->feature_analysis){
-	throw('Must define a -feature_analysis parameter for Bed imports');
-  }
-
-
-  #We need to undef norm emthod as it has been set to the env var
-  $self->{'norm_method'} = undef;
+  #Probably need to add MAQ/BOWTIE/BWA as analyses
 
   #dir are not set in config to enable generic get_dir method access
+  #Need to define output dir if we are processing reads
+  
+
 
   return;
 }
 
 
+sub pre_process_file{
+  my ($self, $filepath) = @_;
 
- 
-sub read_and_import_bed_data{
-    my $self = shift;
-    
-    $self->log("Reading and importing ".$self->vendor()." data");
-    my (@header, @data, @design_ids, @lines);
-    my ($anal, $fh, $file);
-    
-    my $eset_adaptor = $self->db->get_ExperimentalSetAdaptor();
-    my $af_adaptor = $self->db->get_AnnotatedFeatureAdaptor();
-    my $fset_adaptor = $self->db->get_FeatureSetAdaptor();
-    my $dset_adaptor = $self->db->get_DataSetAdaptor();
-	my $new_data = 0;
-	my $eset = $eset_adaptor->fetch_by_name($self->experimental_set_name());
-    
-    if(! defined $eset){
-        $eset = Bio::EnsEMBL::Funcgen::ExperimentalSet->new(
-                                                            -name         => $self->experimental_set_name(),
-                                                            -experiment   => $self->experiment(),
-                                                            -feature_type => $self->feature_type(),
-                                                            -cell_type    => $self->cell_type(),
-                                                            -vendor       => $self->vendor(),
-                                                            -format       => $self->format(),
-															-analysis     => $self->feature_analysis,
-                                                            );
-        ($eset)  = @{$eset_adaptor->store($eset)};
-    }
+  #We really just need to set file_operator here dependant on compression
 
-    #we need a way to define replicates on a file basis when we have no meta file!
-    #can we make this generic for application to array imports?
-    #currently we have to do a separate import for each replicate, specifying the result files each time
-    #we need to add a experimental_set_name option
-    #Actually ExperimentalSet is a little redundant as we can do the roll back which is exactly what this is designed to facilitate
-    #It does however allow incremental addition of new subsets
-
-    #Now define FeatureSet
-    
-    #shouldn't we be using the exp name?
-    my $fset = $fset_adaptor->fetch_by_name($self->experiment->name());
-
-    if(! defined $fset){
-
-        #currently hardcoded, but we should probably add feature_analysis_name
-	  $fset = Bio::EnsEMBL::Funcgen::FeatureSet->new(
-													 -name         => $self->experiment->name(),
-													 -feature_type => $self->feature_type(),
-													 -cell_type    => $self->cell_type(),
-													 -type         => 'annotated',
-													 -analysis     => $self->feature_analysis,
-													);
-        ($fset)  = @{$fset_adaptor->store($fset)};
-    }
-
-    #Define DataSet
-    my $dset = $dset_adaptor->fetch_by_name($self->experiment->name());
-    
-    if(! defined $dset){
-
-        $dset = Bio::EnsEMBL::Funcgen::DataSet->new(
-                                                    -name                => $self->experiment->name(),
-                                                    -supporting_sets     => [$eset],
-                                                    -feature_set         => $fset,
-                                                    -displayable         => 1,
-                                                    -supporting_set_type => 'experimental',
-                                                    );
-        ($dset)  = @{$dset_adaptor->store($dset)};
-    }
-    
-
-    #Get file
-    if (! @{$self->result_files()}) {
-        my $list = "ls ".$self->input_dir().'/'.$self->name().'*.bed';
-        my @rfiles = `$list`;
-        $self->result_files(\@rfiles);
-    }
-    
-    if (scalar(@{$self->result_files()}) >1) {
-        warn("Found more than one bed file:\n".
-             join("\n", @{$self->result_files()})."\nBed does not yet handle replicates.".
-             "  If they are from the same replicate cat them and retry\n(We need to resolve how we are going handle replicates with random cluster IDs)");
-        #do we even need to?
-    }
-
-	#Here were are tracking the import of individual bed files by adding them as ExperimentalSubSets
-	#Recovery would never know what to delete
-	#So would need to delete all, Hence no point in setting status?
+  #If we are loading the reads then we want to gunzip anyway?
+  #Not if we are using ensembl style table (i.e. does not match bed format)
+  #Therefore need to write another output file?
+  #Or do we just incorporate this as 0 size window in result_feature?
 
 
+  #Will these %'s be interpolated here and fail in openfile?
 
-	### VALIDATE FILES ###
-	#We need validate all the files first, so the import doesn't fall over half way through
-	#Or if we come across a rollback halfway through
-	my %new_data;
-	my $roll_back = 0;
+  if($self->input_gzipped){
+	$self->input_file_operator("gzip -dc %s | sort -k 1,3 |");
+  }
+  else{
+	$self->input_file_operator("sort -k 1,3 %s |");
+  }
 
-	foreach my $filepath(@{$self->result_files()}) {
-	  chomp $filepath;
-	  my $filename;
-	  ($filename = $filepath) =~ s/.*\///;
-	  my $sub_set;
-	  $self->log("Found bed file\t$filename");
-	  
-	  if($sub_set = $eset->get_subset_by_name($filename)){
-		
-		if ($sub_set->has_status('IMPORTED')){
-		  $self->log("ExperimentalSubset(${filename}) has already been imported");
-		} 
-		else {
-		  $self->log("Found partially imported ExperimentalSubset(${filename})");
-		  $roll_back = 1;
-
-		  #remove roll_back from these tests?
-		  if ($self->recovery() && $roll_back) {
-			$self->log("Rolling back results for ExperimentalSubset:\t".$filename);
-			warn "Cannot yet rollback for just an ExperimentalSubset, rolling back entire set\n";
-			
-			$self->rollback_FeatureSet($fset);
-			$self->rollback_ExperimentalSet($eset);
-			last;
-		  }
-		  elsif($roll_back){
-			throw("Found partially imported ExperimentalSubSet:\t$filepath\n".
-				  "You must specify -recover  to perform a full roll back for this ExperimentalSet:\t".$eset->name);
-		  }
-		}
-	  }
-	  else{
-		$self->log("Found new ExperimentalSubset(${filename})");
-		$new_data{$filepath} = undef;
-		$sub_set = $eset->add_new_subset($filename);
-		$eset_adaptor->store_ExperimentalSubsets([$sub_set]);
-	  }
-	}
+  #We also need to set optional output filehandle here
+  #So we can mysqlimport instead of using API to import
+  #This should be put in output_dir
+  #So we can point to an input file anywhere, and have our outputs in our own directory
+  #We should also create links to input files in input_dir!
+  #Let's just have one big file so there is no risk of having competing processes between imports
 
 
-	### READ AND IMPORT FILES ###
-	foreach my $filepath(@{$self->result_files()}) {
-	  chomp $filepath;
-	  my $filename;
-	  my $count = 0;
-	  ($filename = $filepath) =~ s/.*\///;
+  
+  if(! defined  $self->output_file && $self->set_feature_type eq 'result'){
+	#output_file is currently only required for read mysqlimport loading
+	#This could also be used by SAM/BAM so put in InputSet
 
-	  if($roll_back || exists $new_data{$filepath}){
+	#Or do we need a file for each discrete set?
+	#Each import consititutes one discrete data set
+	#So this is okay for replicates in a result set
+	#But not for different cell/feature types
+	#Use one file
+	#This is imposed case in INputSet::validate_files
+	my ($name) = fileparse($filepath);
 
-		 $self->log("Reading bed file:\t".$filename);
-		 my $fh = open_file($filepath);
-		 my @lines = <$fh>;
-		 close($fh);
+	$name =~ s/\.gz// if $self->input_gzipped;
+	#We can't pipe this to gzip as we need to mysqlimport it, which does not support gzip?
+	#gzip -dc file.sql | mysql would work but would be much slower due ti individual inserts
+   	$self->output_file($self->get_dir('output')."/result_feature.${name}.txt");
+  }
+
+
+  #return $new_path;
+  return $filepath;
+}
+
+
+sub parse_line{
+  my ($self, $line) = @_;
+
+  return if $line !~ /^chr/io;
+
+  my ($chr, $start, $end, $name, $score, $strand, @other_fields) = split/\t/o, $line;				  
+  #my ($chr, $start, $end, $score) = split/\t/o, $line;#Mikkelson hack	
+  #Validate variables types here beofre we get a nasty error from bind_param?
+
+  #Any more valid BED fields here?
+  # thickStart - The starting position at which the feature is drawn thickly (for example, the start codon in gene displays).
+  # thickEnd - The ending position at which the feature is drawn thickly (for example, the stop codon in gene displays).
+  # itemRgb - An RGB value of the form R,G,B (e.g. 255,0,0). If the track line itemRgb attribute is set to "On", this RBG value will determine the display color of the data contained in this BED line. NOTE: It is recommended that a simple color scheme (eight colors or less) be used with this attribute to avoid overwhelming the color resources of the Genome Browser and your Internet browser.
+  # blockCount - The number of blocks (exons) in the BED line.
+  # blockSizes - A comma-separated list of the block sizes. The number of items in this list should correspond to blockCount.
+  # blockStarts - A comma-separated list of block starts. All of the blockStart positions should be calculated relative to chromStart. The number of items in this list should correspond to blockCount. 
+  
+  $strand = $self->set_strand($strand);			   
+
+  if($self->ucsc_coords){
+	$start +=1;
+  }
+		   
+  if(!  $self->cache_slice($chr)){
+	warn "Skipping AnnotatedFeature import, cound non standard chromosome: $chr";
+  }
+  else{
+	
+	#This is generic count handled in InputSet 
+	$self->count('features');
 		 
-		 my ($line, $f_out);
-		 my $fasta = '';
-		 
-		 #warn "we need to either dump the pid rather than the dbID or dump the fasta in the DB dir";
-		 my $fasta_file = $self->get_dir('fastas')."/".$self->experiment->name().'.'.$filename.'.fasta';
+	#This is currently only loading peaks?
+	#We need to plug the collection code in here for reads?
+	#Also need to optionally have as result set or feature sets dependant on bed_type = reads | peaks
+	
+	#Need to convert all of this to use InputSet parser methods
+	#This is single line format, so we don't need to use $self->feature_params or record_separator
 
-		 if($self->dump_fasta()){
-		   $self->backup_file($fasta_file);
-		   $f_out = open_file($fasta_file, '>');
-		 }
-		
-		 foreach my $line (@lines) {
-		   $line =~ s/\r*\n//o;
-
-		   #Need to parse header here e.g.
-		   #track name=pairedReads description="Clone Paired Reads" useScore=1
-
-		   next if $line =~ /^\#/o;	
-		   next if $line =~ /^$/o;
-		   next if $line !~ /^chr/io;
-		   #next if $line =~ /^chr/i;#Mikkelson hack
-		   
-		   #chr start end name score strand thickStart thickEnd itemRgb blockCount blockSizes blockStarts
-
-		   #Score should be between 0-1000 for ucsc gradient colouring, no such requirement for e!
-		   #Need to set itemRgb as TRACK_RGB colour status
-		   
-
-
-		   my ($chr, $start, $end, $name, $score) = split/\t/o, $line;				  
-		   #my ($chr, $start, $end, $score) = split/\t/o, $line;#Mikkelson hack	
-		   #Validate variables types here beofre we get a nasty error from bind_param?
-		   
-
-		   if($self->ucsc_coords){
-			 $start +=1;
-		   }
-		   
-		   if(!  $self->cache_slice($chr)){
-			 warn "Skipping AnnotatedFeature import, cound non standard chromosome: $chr";
-		   }
-		   else{
-			 
-			 #This is currently only loading peaks?
-			 #We need to plug the collection code in here for reads?
-			 #Also need to optionally have as result set or feature sets dependant on bed_type = reads | peaks
-			 
-			 my $feature = Bio::EnsEMBL::Funcgen::AnnotatedFeature->new
-			   (
-				-START         => $start,
-				-END           => $end,
-				-STRAND        => 1,
-				-SLICE         => $self->cache_slice($chr),
-				-ANALYSIS      => $fset->analysis,
-				-SCORE         => $score,
-				-DISPLAY_LABEL => $name,
-				-FEATURE_SET   => $fset,
-			   );
+	my $feature = Bio::EnsEMBL::Funcgen::AnnotatedFeature->new
+	  (
+	   -START         => $start,
+	   -END           => $end,
+	   -STRAND        => $strand,
+	   -SLICE         => $self->cache_slice($chr),
+	   -ANALYSIS      => $self->data_set->product_FeatureSet->analysis,
+	   -SCORE         => $score,
+	   -DISPLAY_LABEL => $name,
+	   -FEATURE_SET   => $self->data_set->product_FeatureSet,
+	  );
 
 					 
-			 $af_adaptor->store($feature);
+	$self->annotated_feature_adaptor->store($feature);
 			 
-			 $count++;
-
-			 #dump fasta here
-			 if ($self->dump_fasta()){
-			   #We need to prefix this with the dbID?
-			   #And put the fasta in a host:port:dbname specfific dir?
-			   $fasta .= '>'.$name."\n".$self->cache_slice($chr)->sub_Slice($start, $end, 1)->seq()."\n";
-			 }
-		   }
-		 }
-		 
-		 
-		 if ($self->dump_fasta()){
-		   print $f_out $fasta;
-		   close($f_out);
-		 }
-
-
-		 $self->log("Finished importing $count features:\t$filepath");
-		 my $sub_set = $eset->get_subset_by_name($filename);
-		 $sub_set->adaptor->store_status('IMPORTED', $sub_set);
-	   }
-    }
-
-	#Now store default states for FeatureSet
-	$self->set_imported_states_by_Set($fset);
-	$self->log("No new data, skipping result parse") if ! keys %new_data && ! $roll_back;   
-    $self->log("Finished parsing and importing results");
-
-
-	#Retrieve DataSet here as sanity check it is valid
-	#We had one which had a mismatched feature_type between the feature_set and the experimental_set
-	#How was this possible, surely this should have failed on generation/storage?
-
-    
-    return;
-}
+	#Unlikely we will ever need this
+	#dump fasta here
+	#if ($self->dump_fasta()){
+	#  #We need to prefix this with the dbID?
+	#  #And put the fasta in a host:port:dbname specfific dir?
+	#  $fasta .= '>'.$name."\n".$self->cache_slice($chr)->sub_Slice($start, $end, 1)->seq()."\n";
+	#}
+  }
   
-
+  return;
+}
 
 1;
