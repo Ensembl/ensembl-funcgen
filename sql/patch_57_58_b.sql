1e1adb57e42178f73138d3fa2efeafcf42ed950b
Nathan Johnson <njohnson@ebi.ac.uk>
2009-12-02 15:25:13 +0000

    added more 57>58 patches

--- sql/patch_57_58_b.sql
+++ sql/patch_57_58_b.sql
@@ -1,4 +1,4 @@
-# patch_56_57_a.sql
+# patch_56_57_b.sql
 #
 # title: unmapped_object object_type key reorder
 #
@@ -9,6 +9,6 @@ ALTER TABLE unmapped_object DROP KEY object_type_idx;
 ALTER TABLE unmapped_object ADD KEY `object_type_idx` (`ensembl_id`, `ensembl_object_type`);
 
 # patch identifier
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_56_57_b.sql|uo.object_type_id_key_reorder');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_57_58_b.sql|uo.object_type_id_key_reorder');
 
 
---
+++ sql/patch_57_58_c.sql
@@ -0,0 +1,14 @@
+# patch_56_57_c.sql
+#
+# title: status.table_name length increase
+#
+# description:
+# Increase max length of status.table_name to accomodate custom data tables
+
+
+ALTER TABLE unmapped_object MODIFY `table_name` varchar(32) NOT NULL DEFAULT '',
+
+# patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_57_58_c.sql|s.table_name_length');
+
+
