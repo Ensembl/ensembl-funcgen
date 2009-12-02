5eac1449480b8310880f9ab6366d91dd1a783000
Nathan Johnson <njohnson@ebi.ac.uk>
2009-12-02 17:16:51 +0000

    added CS status handling for bed tables

--- modules/Bio/Das/ProServer/SourceHydra/ensembl_funcgen.pm
+++ modules/Bio/Das/ProServer/SourceHydra/ensembl_funcgen.pm
@@ -49,7 +49,7 @@ use Data::Dumper;
 use Carp;
 use Readonly;
 
-our $VERSION       = do { my ($v) = (q$Revision: 1.4 $ =~ /\d+/mxg); $v; };
+our $VERSION       = do { my ($v) = (q$Revision: 1.5 $ =~ /\d+/mxg); $v; };
 Readonly::Scalar our $CACHE_TIMEOUT => 30;
 
 
@@ -70,47 +70,100 @@ sub sources {
     $self->{'_sourcecache_timestamp'} = $now;
   }
   
+
+  #Add support for basename eq bed and nothing else
+
+
+
   # Use the configured query to find the names of the sources
   if(!exists $self->{'_sources'}) {
     $self->{'_sources'} = [];
     eval {
-      $self->{'debug'} and carp('Fetching sources using '.ucfirst($self->config->{'set_type'}).'SetAdaptor');
-	  my $adaptor_method = 'get_'.ucfirst($self->config->{'set_type'}).'SetAdaptor';
-	  my $adaptor = $self->transport->adaptor->$adaptor_method;
 
-	  #We need to get coord system here from source name and use this to bring back sets 
-	  #with IMPORTED_ASSEMBLY information
-	  #Need to do this for every coords version!
+	  my $basename = $self->config->{'basename'};	
+	  ####CAN'T ACTUALLY HAVE MULTIPLE COORDS FOR THE SAME HYDRA
+	  ####AS THE COORD SYSTEM IS AT HYDRA/TRANSPORT LEVEL
+	  ####NOT THE SOURCE LEVEL, SO THIS GET'S RESET FOR SUBSEQUENT
+	  ####SOURCES
+	  my @cs_versions = @{$self->transport->get_coord_system_versions};
 
-	  #map is slow here?
-	#  @{$self->{'_sources'}} = map {
+	  if(scalar(@cs_versions) > 1){
+		carp("Hydra sources can currently only handle one coordinate system, please define separate config for each assembly:\t".join("\t", @cs_versions))
+	  }
+
+	  my $cs_version = $cs_versions[0];
+
+	  if ($basename){
+
+		if($basename eq 'bed'){
+		  my $l = length $basename;
+		  $self->{'debug'} and carp qq(Fetching tables like $basename%);
+		  my $dbh =  $self->transport->adaptor->dbc->db_handle;
+		  my $sth = $dbh->prepare(qq(SHOW TABLES LIKE "$basename%"));
+		  $sth->execute();
+	
+		  
+		  foreach my $table_ref(@{$sth->fetchall_arrayref()}){
+			my ($table) = @$table_ref;
+			#We now use the status table for custom bed tables
+			#Which ties their use to the efg schema
+			#Check for DAS_DISPLAYABLE here?
+			#This is going to change when we import as result_features
+			
+			#Now check for IMPORTED_CSVERSION status
+			#Do we need to bind here to protext against injection from $basename and coordinates?
+			#Use default table_id of 1
+			my $sql = "select sn.name from status s, status_name sn where s.table_name='$table' and s.table_id=1 and s.status_name_id=sn.status_name_id and sn.name='IMPORTED_${cs_version}'";
+			my ($status_name) = $dbh->selectrow_array($sql);
+			push @{$self->{'_sources'}}, $hydraname.($table =~ /^.{$l}(.*)$/mx)[0] if $status_name;
+		  }
+		}
+		else{
+		  #throw here as we don't support any other basenames
+		  carp "The ensembl_funcgen hydra only supports a basename of bed, omit basename from config for set support";
+		}
+	  }
+	  else{
+		
+		$self->{'debug'} and carp('Fetching sources using '.ucfirst($self->config->{'set_type'}).'SetAdaptor');
+		my $adaptor_method = 'get_'.ucfirst($self->config->{'set_type'}).'SetAdaptor';
+		my $adaptor = $self->transport->adaptor->$adaptor_method;
+		
+		#We need to get coord system here from source name and use this to bring back sets 
+		#with IMPORTED_ASSEMBLY information
+		#Need to do this for every coords version!
+		
+		#map is slow here?
+		#  @{$self->{'_sources'}} = map {
 		#We need to add CS version here to differentiate between the same set on different assemblies
 		#Let's do this only to those which are available on two versions?
 		#Or should we add this by default?
-	  #How will transport know which assembly to use?
+		#How will transport know which assembly to use?
 		#Can we see in Ensembl that a source is on a different assembly?
-	#	$hydraname.'_'.$_->name;
+		#	$hydraname.'_'.$_->name;
 	
-	  #} @{$adaptor->fetch_all('DAS_DISPLAYABLE')};
+		#} @{$adaptor->fetch_all('DAS_DISPLAYABLE')};
+		
 
+		#This should now only ever return one which should match the hydra source name
+		#Can't have multiple coordinates entries unless all sources are present on both?
+		#Would also need to support assembly in features query! 
 
-	  #This should now only ever return one which should match the hydra source name
-	  #Can't have multiple coordinates entries unless all sources are present on both?
-	  #Would also need to support assembly in features query! 
-	  my @cs_versions = @{$self->transport->get_coord_system_versions};
-
-	  foreach my $set(@{$adaptor->fetch_all('DAS_DISPLAYABLE')}){
-
-		foreach my $cs_version(@cs_versions){
-
-		  if($set->has_status("IMPORTED_${cs_version}")){
-			#Have to add the version to the end of the source name?
-			push @{$self->{'_sources'}}, $hydraname.'_'.$set->name.':'.$cs_version;
+	
+	
+		foreach my $set(@{$adaptor->fetch_all('DAS_DISPLAYABLE')}){
+		 
+		  foreach my $cs_version(@cs_versions){
+			
+			if($set->has_status("IMPORTED_${cs_version}")){
+			  #Have to add the version to the end of the source name?
+			  push @{$self->{'_sources'}}, $hydraname.'_'.$set->name.':'.$cs_version;
+			}
 		  }
 		}
 	  }
 
-      $self->{'debug'} and carp qq(@{[scalar @{$self->{'_sources'}}]} sources found);
+	  $self->{'debug'} and carp qq(@{[scalar @{$self->{'_sources'}}]} sources found);
 	  1;#To make the eval return true
 
     } or do {
