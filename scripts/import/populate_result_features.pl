9a7d5222847a9287f9f076173911a073930e516f
Nathan Johnson <njohnson@ebi.ac.uk>
2010-01-06 15:53:46 +0000

    farm mode force mode skip and slice modes

--- scripts/import/populate_result_features.pl
+++ scripts/import/populate_result_features.pl
@@ -41,9 +41,9 @@ use Getopt::Long;
 
 
 my ($db_name, $db_host, $db_port, $db_user, $db_pass, $farm, $species);
-my ($dnadb_name, $dnadb_host, $dnadb_port, $dnadb_user, $dnadb_pass);
+my ($dnadb_name, $dnadb_host, $dnadb_port, $dnadb_user, $dnadb_pass, $force);
 my ($slice_name, $rset_name, $old_assm, $new_assm, $dnadb, @slices, @skip_slices);#, $delete
-my $output_dir;
+my ($output_dir, $skip_zero_window, @window_sizes);
 
 my @tmp_args = @ARGV;
 
@@ -57,6 +57,7 @@ GetOptions(
 		   'result_set=s'     => \$rset_name,
 		   'slice_name=s'     => \$slice_name,
 		   'farm'             => \$farm,
+		   'force'            => \$force,#force continue, stop jobs hanging on farm if RESULT_FEATURE_SET
            #'transcript_species_id=i' => \$transcript_species_id, #See probe2transcript.pl for how to implement multi species support
            #'transcript_multi_species' => \$transcript_multi_species,
 
@@ -71,6 +72,8 @@ GetOptions(
 		   'old_assm=s'       => \$old_assm,
 		   'new_assm=s'       => \$new_assm, 
 		   'skip_slices=s{,}' => \@skip_slices,
+		   'skip_zero_window' => \$skip_zero_window,
+		   'window_sizes=s{,}' => \@window_sizes,
 		   #Do we need window_sizes here for projected windows?
 	 	   'species=s'      => \$species,#Not really needed
 
@@ -98,6 +101,13 @@ if(! $farm && ! $slice_name){
   die("To run with all slices you should specify the -farm option, or to run locally specify one -slice.\n");
 }
 
+if($new_assm && $skip_zero_window){
+  die('You cannot specify both -new_assm and -skip_zero_window');
+}
+
+if(@window_sizes){
+  warn 'You have specified custom window sizes, if these do no match those in Bio::EnsEMBL::Collector::ResultFeautre then this may lead to missing data, should be used for testing only!';
+}
 
 if($dnadb_name){
   $dnadb = Bio::EnsEMBL::DBSQL::DBAdaptor->new(
@@ -154,6 +164,49 @@ else{
 my ($rset) = @{$rset_adaptor->fetch_all_by_name($rset_name)};
 die("Could not fetch ResultSet using name:\t$rset_name") if ! $rset;
 
+
+#Sanity check that we have a RESULT_FEATURE_SET
+#If we are skipping_zero_level
+#Do this here instead of in Collector to avoid submitting doomed jobs
+#Move all this to the Collector?
+
+
+if($skip_zero_window && ! $rset->has_status('RESULT_FEATURE_SET')){
+  die("You are trying to -skip_zero_window for a ResultSet which is not yet a RESULT_FEATURE_SET.\n".
+	  "It is likely you are trying to generate bins from assembly projected ResultFeatures.\n".
+	  "Please check your 0 window size projected ResultFeatures and set the RESULT_FEATURE_SET status.\n".
+	  "Or if you really want to omit the natural resolution, please do this in \@Bio::EnsEMBL::Utils::Collector::window_sizes\n");
+  #Could do it here, but it is probably more safe like this
+  #Altho new_assm automatically uses 0 window_size even if it is not set.
+}
+elsif($rset->has_status('RESULT_FEATURE_SET') &&
+	  ! $skip_zero_window){
+  die("You are trying to load ResultSet which is already a RESULT_FEATURE_SET, maybe you want to rollback_ResultFeatures or -skip_zero_window if you are trying to regenerate windows from a projected ResultSet");
+  
+}
+elsif($skip_zero_window && $rset->has_status('RESULT_FEATURE_SET')){
+  print "\nYou have specified -skip_zero_window for a RESULT_FEATURE_SET. This may duplicate data for window_sizes != 0\n";
+
+
+  if(! $force){
+
+	my $msg = "Continue?[y|n]"; 
+	my $response;
+
+	while (! defined $response){
+	  print $msg;
+	  $response = <STDIN>;
+	  chomp $response;
+	  exit if $response eq 'n';
+	  $response = undef if $response ne 'y';
+	}
+	
+	push @tmp_args, '-force';
+	#$force = 1;
+  }
+}
+
+
 #Set job parameters
 if($farm){
   $output_dir ||= $ENV{'EFG_DATA'}.'/result_features/'.$db_name;
@@ -162,18 +215,20 @@ if($farm){
 	system("mkdir -p $output_dir") == 0 || die("cannot make output directory:\t$output_dir\n$?");
   }
 
-  #We basically want to strip out farm, slice_name and skip slices
+  #We basically want to strip out farm, slice_name and skip_slices
   #This would be easier with something hash based
   my $seen_opt = 0;
 
   foreach my $i(0..$#tmp_args){
-	
-	$tmp_args[$i] = '' if $tmp_args[$i] =~ /^[-]+farm/;
+	$seen_opt = 0 if $tmp_args[$i] =~ /^[-]+/;#Reset seen opt if we seen a new one
+
+	$tmp_args[$i] = '' if $tmp_args[$i] =~ /^[-]+farm/;#Only remove current flag
 	$seen_opt = 1 if $tmp_args[$i] =~ /^[-]+skip_slices/;
-	$seen_opt = 1 if $tmp_args[$i] =~ /^[-]+slice_name/;
-	$seen_opt = 0 if $seen_opt &&  $tmp_args[$i] =~ /^[-]+s/;
-	$tmp_args[$i] = '' if $seen_opt;
+	$seen_opt = 1 if $tmp_args[$i] =~ /^[-]+slice/;#Don't have full param name incase we have just specified -slice
+	
+	$tmp_args[$i] = '' if $seen_opt;#Remove args following option
   }
+
 }
 
 
@@ -203,9 +258,16 @@ SLICE: foreach my $slice(@slices){
 	warn "Need to wait here and add RESULT_FEATURE_SET status if no errors found";
 	#This is a pipeline thing, no?
 	warn "Need to kick of 2nd stage of project bin generation";
+	#We need to manually check output and 
+
   }
   else{
-	my %config = (-NEW_ASSEMBLY => $new_assm);
+
+	#Expose more config through the options?
+	my %config = (-NEW_ASSEMBLY => $new_assm, 
+				  -SKIP_ZERO_WINDOW => $skip_zero_window,
+				  -WINDOW_SIZES => \@window_sizes
+				 );
 
 	foreach my $slice(@slices){
 	  $rfeat_adaptor->store_window_bins_by_Slice_ResultSet($slice, $rset, %config);
@@ -213,3 +275,10 @@ SLICE: foreach my $slice(@slices){
   }
 }
 
+if($farm){
+  warn "You must manually check all jobs have run correctly before setting RESULT_FEATURE_SET status for ".$rset->name."\n";
+
+  if($new_assm){
+	warn "To generate the remaning window sizes your must now resubmit the jobs with -skip_zero_window\n";
+  }
+}
