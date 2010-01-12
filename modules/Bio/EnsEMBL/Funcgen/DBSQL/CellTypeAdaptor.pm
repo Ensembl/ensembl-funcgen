a303c3abe56be5749a0096f65a086a3c3210c218
Nathan Johnson <njohnson@ebi.ac.uk>
2010-01-12 10:08:38 +0000

    added gender support

--- modules/Bio/EnsEMBL/Funcgen/DBSQL/CellTypeAdaptor.pm
+++ modules/Bio/EnsEMBL/Funcgen/DBSQL/CellTypeAdaptor.pm
@@ -116,7 +116,7 @@ sub _tables {
 sub _columns {
   my $self = shift;
 	
-  return qw( ct.cell_type_id ct.name ct.display_label ct.description);#ct.type/class
+  return qw( ct.cell_type_id ct.name ct.display_label ct.description ct.gender);#ct.type/class
 }
 
 =head2 _objs_from_sth
@@ -136,16 +136,17 @@ sub _columns {
 sub _objs_from_sth {
 	my ($self, $sth) = @_;
 	
-	my (@result, $ct_id, $name, $dlabel, $desc);
+	my (@result, $ct_id, $name, $dlabel, $desc, $gender);
 	
-	$sth->bind_columns(\$ct_id, \$name, \$dlabel, \$desc);
+	$sth->bind_columns(\$ct_id, \$name, \$dlabel, \$desc, \$gender);
 	
 	while ( $sth->fetch() ) {
 		my $ctype = Bio::EnsEMBL::Funcgen::CellType->new(
 														 -dbID          => $ct_id,
 														 -NAME          => $name,
 														 -DISPLAY_LABEL => $dlabel,
-														 -DESCRIPTION    => $desc,
+														 -DESCRIPTION   => $desc,
+														 -GENDER        => $gender,
 														 -ADAPTOR       => $self,
 														);
 	  
@@ -176,8 +177,8 @@ sub store {
   
   my $sth = $self->prepare("
 			INSERT INTO cell_type
-			(name, display_label, description)
-			VALUES (?, ?, ?)");
+			(name, display_label, description, gender)
+			VALUES (?, ?, ?, ?)");
     
   
   
@@ -196,6 +197,8 @@ sub store {
 	  $sth->bind_param(1, $ct->name(),           SQL_VARCHAR);
 	  $sth->bind_param(2, $ct->display_label(),  SQL_VARCHAR);
 	  $sth->bind_param(3, $ct->description(),    SQL_VARCHAR);
+	  $sth->bind_param(4, $ct->gender(),         SQL_VARCHAR);
+	  
 	  
 	  $sth->execute();
 	  $ct->dbID($sth->{'mysql_insertid'});
