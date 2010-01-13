27adc67f4b2a90392db338c013d5cb408d1e98b4
Nathan Johnson <njohnson@ebi.ac.uk>
2010-01-13 10:44:07 +0000

    *** empty log message ***

--- sql/patch_56_57_b.sql
+++ sql/patch_56_57_b.sql
@@ -5,7 +5,9 @@
 # description:
 # Add a ensembl_object_type/id key to the unmapped_object table
 
-ALTER TABLE unmapped_object ADD KEY `object_type_idx` (`ensembl_id`, `ensembl_object_type`,);
+#ALTER TABLE unmapped_object DROP KEY `object_type_idx`;
+
+ALTER TABLE unmapped_object ADD KEY `object_type_idx` (`ensembl_id`, `ensembl_object_type`);
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_56_57_b.sql|uo.object_type_id_key');
