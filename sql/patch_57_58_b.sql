e8a7f35f5059c4e04d34e77ec2f79e98a6b62887
Nathan Johnson <njohnson@ebi.ac.uk>
2010-02-08 17:02:35 +0000

    updated to add key

--- sql/patch_57_58_b.sql
+++ sql/patch_57_58_b.sql
@@ -7,7 +7,7 @@
 
 
 ALTER table regulatory_feature ADD column `cell_type_id` int(10) unsigned DEFAULT NULL AFTER feature_type_id;
-
+ALTER table regulatory_feature ADD KEY `cell_type_idx` (`cell_type_id`);
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_57_58_b.sql|regulatory_feature.cell_type');
