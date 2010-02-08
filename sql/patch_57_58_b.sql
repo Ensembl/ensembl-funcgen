91cb478efb174d05d8117c116ae2b807d0a32d83
Nathan Johnson <njohnson@ebi.ac.uk>
2010-02-08 16:56:07 +0000

    Is now rf.cell_type patch

--- sql/patch_57_58_b.sql
+++ sql/patch_57_58_b.sql
@@ -1,14 +1,15 @@
-# patch_56_57_b.sql
+# patch_57_58_b.sql
 #
-# title: unmapped_object object_type key reorder
+# title: regulatory_feature.cell_type
 #
 # description:
-# Change order of key so ho highest cardinality field first
+# Add cell_type_id and associated indexes to regulatory_feature
 
-ALTER TABLE unmapped_object DROP KEY object_type_idx;
-ALTER TABLE unmapped_object ADD KEY `object_type_idx` (`ensembl_id`, `ensembl_object_type`);
+
+ALTER table regulatory_feature ADD column `cell_type_id` int(10) unsigned DEFAULT NULL AFTER feature_type_id;
+
 
 # patch identifier
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_57_58_b.sql|uo.object_type_id_key_reorder');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_57_58_b.sql|regulatory_feature.cell_type');
 
 
