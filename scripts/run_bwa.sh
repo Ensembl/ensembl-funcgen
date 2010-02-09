4bee30836967520219570ffd26d0260046b3d012
Nathan Johnson <njohnson@ebi.ac.uk>
2010-02-09 12:40:02 +0000

    *** empty log message ***

--- scripts/run_bwa.sh
+++ scripts/run_bwa.sh
@@ -72,15 +72,12 @@ fi
 
 if [[ $dir ]]; then
 
-	#Do we not have a function for this?
+	#Do we not have a function for this unzipping/cating?
 
 	#strip last dir name off for input
 	file_name=$(echo $dir | sed -r 's/\/$//')
 	file_name=$(echo $dir | sed -r 's/.*\///')
 	file="${dir}/${file_name}.fastq"
-
-
-
 
 	if [[ -f $file ]]; then
 
@@ -136,41 +133,42 @@ fi
 
 lc_species=$(echo $species | tr [A-Z] [a-z])
 uc_species=$(echo $species | tr [a-z] [A-Z])
-index_name="${lc_species}_${gender}_${assembly}${mask}"
+index_name="${lc_species}_${gender}_${assembly}${mask}.fasta"
+fasta_file=${index_home}/${uc_species}/${index_name}
 
 #Check index files
 for ext in amb ann bwt pac rbwt rpac rsa sa; do
-	CheckFile "${index_home}/${uc_species}/${index_name}.${ext}"
+	error=$(CheckFile "${index_home}/${uc_species}/${index_name}.${ext}")
+
+	if [ $? -ne 0 ]; then
+		echo -e $error
+		
+		if [[ ! -f $fasta_file ]]; then
+			echo -e "\nYou need to create the fasta file to index:\t$fasta_file"
+			#Add how to dump here?
+			echo -e "\ne.g. bsub -J ${lc_species}_top_level_unmasked -e ${lc_species}_top_level_unmasked.err -o ${lc_species}_top_level_unmasked.out -R\"select[mem>3500] rusage[mem=3500]\" -M3500000 $EFG_PERL $SRC/ensembl-analysis/scripts/sequence_dump.pl -dbhost ens-livemirror -dbuser ensro -dbname ${lc_species}_core_SCHEMA_BUILD -species $lc_species -coord_system_name toplevel -filename $lc_species_male_ASSEMBLY_unmasked.fasta"
+			echo -e "Then create the female file as follows:\n\tcat ${lc_species}_male_ASSEMBLY_unmasked.fasta | /nfs/users/nfs_d/dkeefe/bin/fastagrep -t -v -P -p 'chromosome:ASSEMBLY:Y' > ${lc_species}_female_ASSEMBLY_unmasked.fasta"
+		fi
+
+		echo -e "\nYou need to generate the bwa indexes using:\n\tbsub -J ${lc_species}_bwa_indexes -o ${lc_species}_bwa_indexes.out  -e ${lc_species}_bwa_indexes.err $resource bwa index -a bwtsw $fasta_file"
+		
+		exit
+	fi
 done
 
 
 file_name=$(echo $file | sed 's/\.fastq$//')
 
 echo -e "Running bwa with following options:\n\tIndex name\t= $index_name\n\tInput file\t= $file\n\tAlignment type\t= $align_type\n"
-
-#do we need to extend the job name here to 
-#make it more unique?
-
 #This is again truncating the bsub cmd and failing?!
 #bjob_cmd="bsub -J bwa_${file_name} $resource -o ${file_name}.bwa.out -e ${file_name}.bwa.err 'bwa aln $index_home/${uc_species}/${index_name}  $file > ${file_name}.${align_type}.sai; bwa $align_type ${index_home}/${uc_species}/${index_name} ${file_name}.${align_type}.sai $file > ${file_name}.${align_type}.sam'"
 
 #Use submitJob
 job_name="bwa_${file_name}"
 bsub_cmd="$resource -o ${file_name}.bwa.out -e ${file_name}.bwa.err"
-job_cmd="'bwa aln $index_home/${uc_species}/${index_name}  $file > ${file_name}.${align_type}.sai; bwa $align_type ${index_home}/${uc_species}/${index_name} ${file_name}.${align_type}.sai $file > ${file_name}.${align_type}.sam'"
+job_cmd="'bwa aln $fasta_file  $file > ${file_name}.${align_type}.sai; bwa $align_type $fasta_file ${file_name}.${align_type}.sai $file > ${file_name}.${align_type}.sam'"
 
-#bjob_cmd="bsub -J bwa_${file_name} $resource -o ${file_name}.bwa.out -e ${file_name}.bwa.err 'bwa aln $index_home/${uc_species}/${index_name}  $file > ${file_name}.${align_type}.sai; bwa $align_type ${index_home}/${uc_species}/${index_name} ${file_name}.${align_type}.sai $file > ${file_name}.${align_type}.sam'"
-
-#Add this so submitJob does not fail
-#Move to efg.config?
+#Add this so submitJob does not fail. Move to efg.config?
 export QUEUE_MANAGER=LSF
 submitJob "$job_name" "$bsub_cmd" "$job_cmd" 
-
-
-#exit
-
-#$bjob_cmd
-
-
-
 
