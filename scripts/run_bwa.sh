b9f438e5a545edcd3a4dfc5fa0e349cb107f2892
Nathan Johnson <njohnson@ebi.ac.uk>
2010-03-10 16:27:06 +0000

    Now handles mutliple input file as args rather than single -f file option Added optional -e(xperiment_name) Added mandatory -n(set name option) Not outputs to standardised alignment directory structure Added alignment.log file to track input and settings for each alignment run

--- scripts/run_bwa.sh
+++ scripts/run_bwa.sh
@@ -12,12 +12,18 @@ fi
 
 
 #To do
-#Integrate this into pipeline.env?
-#This requires set up of a instance file and input dir
-#Also is less visible? Need to fettle with Help to list funcs better?
-#As we have no structure to the functions, but at least we have a dir structure when we have separate scripts
-#add outdir ?
-#remove hardcoded fastagrep path
+#1 Integrate this into pipeline.env?
+#  This requires set up of a instance file and input dir. Also is less visible? 
+#  Need to fettle with Help to list funcs better?
+#  As we have no structure to the functions, but at least we have a dir 
+#  structure when we have separate scripts, add outdir ?
+#2 remove hardcoded fastagrep path
+#3 Done redirect output to standardised alignments output directory structure
+#4 Done Take any input path, so we don't have to have standardised structure for fastq dirs as this often mirrors source repository structure
+#5 Add -cell_type and -feature_type opts and cat to CellType_FeatureType(_.*).fastq, or just add set_name?
+#6 DONE Finish files handling
+#7 DONE Add fastq.list file to log the true uncat'd input fastq file(s)  
+#8 Optionally wait for job success and remove cat'd file if present?
 
 OPTIND=1    #Reset the getopts if we have used it previously
 VALID_GENDERS='male female'
@@ -31,8 +37,12 @@ align_type='samse'
 resource='-R"select[mem>5000] rusage[mem=5000]" -M5000000'
 dir=
 clean=
+experiment_name=
+outdir=
+#Change this to $EFG_DATA
+scratch_dir=/lustre/scratch103/ensembl/funcgen
 
-usage="Usage:\t\trun_bwa.sh  <options>
+usage="Usage:\t\trun_bwa.sh  <options> [ file1 file2 ]
 \nDescription:\tThis script will take either a fastq file or a directory as input, unzip cat/rename 
 \n\t\tand rezip as necessary to generate a single fastq file. The relevant bwa indexes will be validated 
 \n\t\tbefore submitting the bwa job to the farm.
@@ -40,8 +50,10 @@ usage="Usage:\t\trun_bwa.sh  <options>
 \nMandatory options:
 \n\t-s(pecies e.g. homo_sapiens)
 \n\t-g(ender e.g male|female)
-\n\t-f(ile e.g. your_file.fastq)\tor\t-d(ir input dir containing fastq files)
+\n\t-n(set name i.e. CELLTYPE_FEATURETYPE(_.*), in line with expected input for peaks.env)
 \nOther options:
+\n\t-e(xperiment name, required if input files are split over different directories)
+\n\t-o(utdir overrides default $scratch_dir/alignments/\$species/\$experiment_name)
 \n\t-a(ssembly default is $assembly)
 \n\t-p(aired default is single reads)
 \n\t-i(ndex_home default=$index_home)
@@ -49,12 +61,24 @@ usage="Usage:\t\trun_bwa.sh  <options>
 \n\t-c(lean away cat'd fastq file)
 \n\t-h(elp)"
 
-while getopts ":g:s:f:d:i:r:a:cph" opt; do
+
+#Add opts:
+#ftype
+#ctype
+#experiment name
+
+#build alignments dir based on $EFG_DATA/alignments/species/assembly/exp_name
+#remove file option and just have as args?
+
+
+while getopts ":g:s:e:n:d:o:i:r:a:cph" opt; do
 	case $opt in 
 		g  ) gender=$OPTARG ;; 
 		s  ) species=$OPTARG ;;
-        f  ) file=$OPTARG ;;
-        d  ) dir=$OPTARG ;;
+		e  ) experiment_name=$OPTARG ;;
+		n  ) name=$OPTARG ;;
+		d  ) dir=$OPTARG ;;
+        o  ) outdir=$OPTARG ;;
 		i  ) index_file=$OPTARG ;;
         r  ) resource=$OPTARG ;;
 		a  ) assembly=$OPTARG ;;
@@ -65,75 +89,186 @@ while getopts ":g:s:f:d:i:r:a:cph" opt; do
 	esac 
 done
 
+
+#Get trailing file name arguments
+idx=1
+
+while [[ $idx -lt $OPTIND ]]; do 
+	idx=$(($idx + 1))
+	shift 
+done
+
+files=($*)
+
 #Parameter checks
-CheckVariablesOrUsage "$usage" gender species index_home
+CheckVariablesOrUsage "$usage" gender species index_home name
 ValidateVariableOrUsage "$usage" gender VALID_GENDERS
 
-if [[ ! $file ]] && [[ ! $dir ]]; then
-	echo "You must supply either a -f(ile) or -d(ir) parameter"
+if [[ ! "$files" ]] && [[ ! $dir ]]; then
+	echo -e "ERROR:\tYou must supply either a -d(ir) parameter or some file arguments\n"
+	echo -e $usage
+	exit
+elif [[ "$files" ]] && [[ $dir ]]; then
+	echo -e "ERROR:You have specified -d(irectory) and input files, please use one or the other\n"
+	echo -e $usage
 	exit
 fi
 
-if [[ $dir ]]; then
 
-	#Do we not have a function for this unzipping/cating?
 
-	#strip last dir name off for input
-	file_name=$(echo $dir | sed -r 's/\/$//')
-	file_name=$(echo $dir | sed -r 's/.*\///')
-	file="${dir}/${file_name}.fastq"
 
+
+
+if [ "$dir" ]; then
+	
+	if [ ! -d $dir ]; then
+		echo "-d $dir is not a valid directory"
+		exit
+	fi
+
+    #Set outdir and experiment_name if not defined
+	if [ ! "$experiment_name" ]; then
+	    #Get exp name from dir name
+		experiment_name=$(echo $dir | sed -r 's/\/$//')
+		experiment_name=$(echo $experiment_name | sed -r 's/.*\///')
+	fi
+
+	if [ ! "$outdir" ]; then
+		outdir="${scratch_dir}/alignments/${species}/${assembly}/${experiment_name}"
+	fi
+
+	
+	file="${outdir}/${name}.fastq"
+
+	#Test for pre-cat'd file
 	if [[ -f $file ]]; then
 
 		if [ $clean ]; then
-			echo -e "Removing old fastq file:\t${file}"
+			echo -e "Removing previously fastq file:\t${file}"
 			rm -f $file
 		else
-			echo -e "Using old cat'd fastq file:\t$file"
+			echo -e "WARNING:\tUsing previously cat'd fastq file:\t$file"
+			echo -e "Specify -c(lean) to override this behaviour"
+			files=($file)
+			sleep 10
 		fi
 	fi
 
+
+	#Get files
 	if [[ ! -f $file ]]; then
-		echo "Generating fastq file:\t$file"
-		#gfastq_files=($(ls ${dir}/*fastq.gz))
-		#Do it this way to avoid STDERR
-		fastq_files=($(ls ${dir}/*fastq*))
-		
-		if [[ ! $fastq_files ]]; then
-			echo -e "No fastq files found in input dir:\t$dir"
-		elif [[ ! -z $(echo ${fastq_files[*]} | grep '.fastq.gz') ]]; then
-			echo "Unzipping fastq files"
-			Execute gunzip $dir/*.fastq.gz
+		files=($(ls ${dir}/*fastq*))
+
+		if [[ ! "$files" ]]; then
+			echo -e "ERROR:\tNo fastq files found in input dir:\t$dir\n"
+			echo -e $usage
+			exit
 		fi
-
-		fastq_files=$(ls ${dir}/*fastq)
-		echo -e "Using:\t"
-
-		for fastq_file in $fastq_files; do
-			fastq_file=$(echo $fastq_file | sed -r 's/.*\///')
-			echo -e "\t$fastq_file"
-		done
-
-
-		echo -e "Cat'ing files to:\t $file"
-		Execute cat ${dir}/*fastq > $file
-		echo "Gzipping all source fastq files"
-		Execute gzip $fastq_files		
 	fi
 
-	#Need to rezip here really
-	#echo "Gzipping all source fastq files"
-	#Execute gzip $fastq_files
+else  # files
+
+	#Test exp_name and set outdir and file
+	if [ ! "$experiment_name" ]; then
+		echo -e "ERROR:\tYou must explcitly set an -e(experiment name) when specify multiple file paths\n"
+		echo -e $usage
+		exit
+	fi
+
+	if [ ! "$outdir" ]; then
+		outdir="${scratch_dir}/alignments/${species}/${assembly}/${experiment_name}"
+	fi
+
+	
+	file="${outdir}/${name}.fastq"
+
+	#Test for pre-cat'd file
+	if [[ -f $file ]]; then
+
+	 	if [ $clean ]; then
+		 	echo -e "Removing previously cat'd fastq file:\t${file}"
+			rm -f $file
+		else 
+ 			echo -e "WARNING:\tUsing previously cat'd fastq file:\t$file"
+			echo -e "Specify -c(lean) to override this behaviour"
+	 		files=($file)
+			sleep 10
+	 	fi
+ 	fi
 fi
 
 
+#Do we not have a function for this unzipping/cating?
+tmp_files=
 
-CheckFile $file
+for f in ${files[*]}; do
+	
+	if [[ $f != *fastq* ]]; then
+		echo -e "Ignoring non-fastq file:\t$f"
+	else
+		CheckFile $f
+		echo -e "Using fastq file:\t$f"
+		
+		funzipped=$f
+		
+	 	    #Unzip those which are zipped
+		if [[ $f =  *.gz ]]; then
+			echo -e "Unzipping..."
 
-if [[ $file != *fastq ]]; then
-	echo "Your file argument needs to be a fastq file:\t$file"
+			exit
+
+			Execute gunzip $f
+			funzipped=$(echo $f | sed 's/\.gz//')
+		fi 
+		
+		tmp_files=(${tmp_files[*]} $funzipped)
+	fi
+done
+
+if [ ! "$tmp_files" ]; then
+	echo -e "Could not find any valid input fastq files from list:\n${files[*]}"
 	exit
 fi
+
+#Cat if required and log inputs
+alignment_log="${output_dir}/${name}.alignment.log"
+
+#file up until now is the expected cat'd/cache fastq file 
+
+if [[ ${#tmp_files[*]} -gt 1 ]]; then
+	MakeDirs $outdir			
+	echo -e "Cat'ing files to:\t$file"
+	#This is not catching failure!
+	Execute cat ${tmp_files[*]} > $file
+
+	echo 'Input fastq files for $experiment_name $name alignments:' > $alignment_log
+	
+	for f in ${tmp_files[*]}; do
+		echo $f >> $alignment_log
+	done
+
+	echo "Gzipping all source fastq files"
+	Execute gzip ${tmp_files[*]}
+else
+	#Run from single input file
+
+	#This may not be the full path? So match may fail
+	#need to ls full patch for comparison
+
+	if [[ ${tmp_files[0]} != $file ]]; then
+		#Only log input when we know
+		#we are not running with the cache file
+		#otherwise preserve original inputs
+		echo 'Input fastq file for $experiment_name $name alignments:' > $alignment_log
+		echo  ${tmp_files[0]} >> $alignment_log
+	fi
+
+	file=${tmp_files[0]}
+fi
+
+
+
+#Could also log other run params here?
 
 lc_species=$(echo $species | tr [A-Z] [a-z])
 uc_species=$(echo $species | tr [a-z] [A-Z])
@@ -162,18 +297,25 @@ done
 
 
 file_name=$(echo $file | sed 's/\.fastq$//')
-echo -e "Running bwa with following options:\n\tIndex name\t= $index_name\n\tInput file\t= $file\n\tAlignment type\t= $align_type\n"
+run_txt="\nRunning bwa with following options:\n\tIndex name\t= $index_name\n\tInput file\t= $file\n\tAlignment type\t= $align_type\n\tOutput dir\t= $outdir\n"
+
+echo -e $run_txt
+echo -e $run_txt >> $alignment_log
 
 #Is the index job still running?
-checkJob ${index_name}_indexes
+checkJob ${index_name}_indexes exit_if_running
+
 
 #Use submitJob to avoid truncation of bsub cmd
-job_name=$(echo $file_name | sed 's/.*\///')
-job_name="bwa_${align_type}_${job_name}"
-bsub_cmd="-q long $resource -o ${file_name}.bwa.out -e ${file_name}.bwa.err"
-job_cmd="'bwa aln $fasta_file  $file > ${file_name}.${align_type}.sai; bwa $align_type $fasta_file ${file_name}.${align_type}.sai $file > ${file_name}.${align_type}.sam'"
+job_name="bwa_${align_type}_${experiment_name}_${name}";
+bsub_cmd="-q long $resource -o ${outdir}/${file_name}.bwa.out -e ${outdir}/${file_name}.bwa.err"
+job_cmd="'bwa aln $fasta_file  $file > ${outdir}/${file_name}.${align_type}.sai; bwa $align_type $fasta_file ${outdir}/${file_name}.${align_type}.sai $file > ${outdir}/${file_name}.${align_type}.sam'"
 
-#Add this so submitJob does not fail. Move to efg.config?
-export QUEUE_MANAGER=LSF
+#echo $bsub_cmd $job_cmd
+
 submitJob "$job_name" "$bsub_cmd" "$job_cmd" 
 
+#Could really do with submitting this to farm if we are waiting
+#Wait for job success here and remove cache file if present?
+
+echo -e "\nNOTE:\tNeed to remove the cat'd fastq file after the job has successfully completed"
