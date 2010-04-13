cc1609b4a6cb8bf89658874c30470509d701eade
Nathan Johnson <njohnson@ebi.ac.uk>
2010-04-13 12:57:51 +0000

    updated POD and added methods param

--- scripts/update_DB_for_release.pl
+++ scripts/update_DB_for_release.pl
@@ -1,3 +1,84 @@
+=head1 NAME
+
+update_DB_for_release.pl
+  
+
+=head1 SYNOPSIS
+
+This script performs several updates to the eFG DBs as part of the release cycle.
+
+ perl update_DB_for_release.pl <mandatory paramters> [ optional parameters ]
+
+
+=head1 DESCRIPTION
+
+This script use the Bio::EnsEMBL::Funcgen::Utils::Healthchecker modules to perform critical updates to efg DBs during the release cycle. By default this script calls the following methods:
+
+  update_db_for_release - Wrapper method for several standard updates/checks
+  log_data_sets         - Check and logs available DataSets dependant on -check_displayable
+  check_stable_ids      - Checks stable_ids for DBs which have a RegualtoryFeatures set
+
+
+This default behaviour can be over-ridden by specifying a methods arrays e.g.
+
+  -methods validate_new_seq_regions update_meta_schema_version check_meta_strings check_meta_species analyse_and_optimise_tables set_current_coord_system update_meta_coord clean_xrefs log_data_sets check_stable_ids
+
+
+See Bio::EnsEMBL::Funcgen::Utils::HealthChecker for more details.
+
+
+=head1 OPTIONS
+
+ Mandatory
+  -species      Latin name as used in DB name or meta table e.g. homo_sapiens
+  -host         DB host
+  -user         DB user
+  -pass         DB pass
+  -data_version e.g. 58_37k
+  
+
+ Optional
+  -dbname            DB name (default production name will be used if not specified)
+  -dnadb_host        DNADB host
+  -skip_meta_coord
+  #-skip_analyse 
+  #-skip_xref_cleanup 
+  -check_displayable Forces log_data_sets to only use DISPLAYABLE sets
+  -methods           Over-rides defaults behaviour and only run specified methods (see above)
+  #-tee              Tees output to STDOUT(not yet implemented)
+  -log_file           
+  -help              Prints a short help page
+  -man               Prints the entire POD documentation
+
+
+=head1 EXAMPLE
+
+perl update_DB_for_release.pl -species homo_sapiens -host ens-genomics1 -user $USER -data_version 58_37c -dbname homo_sapiens_funcgen_58_37c -dnadb_host ens-staging -check_displayable -pass $PASS
+
+=head1 SEE ALSO
+
+Bio:EnsEMBL::Funcgen::Utils::HealthChecker
+
+=head1 LICENSE
+
+  Copyright (c) 1999-2009 The European Bioinformatics Institute and
+  Genome Research Limited.  All rights reserved.
+
+  This software is distributed under a modified Apache license.
+  For license details, please see
+
+    http://www.ensembl.org/info/about/code_licence.html
+
+=head1 CONTACT
+
+  Please email comments or questions to the public Ensembl
+  developers list at <ensembl-dev@ebi.ac.uk>.
+
+  Questions may also be sent to the Ensembl help desk at
+  <helpdesk@ensembl.org>.
+
+
+=cut
 
 use strict;
 use Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor;
@@ -7,25 +88,29 @@ use Pod::Usage;
 use Getopt::Long;
 
 my ($pass, $species, $schema_build, $skip_meta_coord, $dnadb_host, $dnadb, $check_displayable);
-my ($help, $man, $dbname);
+my ($help, $man, $dbname, @methods);
 my $user = 'ensadmin';
 my $port = 3306;
 my $host = 'ens-genomics1';
 my @tmp_args = @ARGV;
 
+#define here to avoid warnings
+$main::_tee = 0;
+
 GetOptions( 
-		   'host|h=s'         => \$host,
-		   'port=i'           => \$port,
-		   'user|u=s'         => \$user,
-		   'pass|p=s'         => \$pass,
-		   'dbname=s'         => \$dbname,
-		   'species|d=s'      => \$species,
-		   'data_versions=s' => \$schema_build,#mandatory as default ensembldb will not exist
-		   'dnadb_host=s'     => \$dnadb_host,
-		   'skip_meta_coord'  => \$skip_meta_coord,
+		   'host|h=s'          => \$host,
+		   'port=i'            => \$port,
+		   'user|u=s'          => \$user,
+		   'pass|p=s'          => \$pass,
+		   'dbname=s'          => \$dbname,
+		   'species|d=s'       => \$species,
+		   'data_versions=s'   => \$schema_build,#mandatory as default ensembldb will not exist
+		   'dnadb_host=s'      => \$dnadb_host,
+		   'skip_meta_coord'   => \$skip_meta_coord,
 		   'check_displayable' => \$check_displayable,
-		   'help|?'             => \$help,
-		   'man|m'              => \$man,
+		   'methods=s{,}'      => \@methods,
+		   'help|?'            => \$help,
+		   'man|m'             => \$man,
 		   'log_file=s'        => \$main::_log_file,
 		   'tee'               => \$main::_tee,
 		   #'slice=s'          => \$test_slice,
@@ -53,7 +138,7 @@ if($dnadb_host){
 											   -host    => $dnadb_host,
 											   #-host => 'ensdb-1-13',
 											   #-port => 5307,
-											   -port    => 3306,
+											   #-port    => 3306,
 											   -user    =>  'ensro',
 											   #-pass    => $pass,
 											   -species => $species,
@@ -86,8 +171,13 @@ my $hchecker = Bio::EnsEMBL::Funcgen::Utils::HealthChecker->new(
 																#-force_update
 																#skip_dumps
 																-check_displayable => $check_displayable,
+																-methods => \@methods,
 															   );
 
-$hchecker->update_db_for_release();
-$hchecker->log_data_sets();
-$hchecker->check_stable_ids;
+
+
+if(! @methods){
+  $hchecker->update_db_for_release();
+  $hchecker->log_data_sets();
+  $hchecker->check_stable_ids;
+}
