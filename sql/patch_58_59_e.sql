23ccd96b93f16a314ba23faad4469802c113229d
Nathan Johnson <njohnson@ebi.ac.uk>
2010-06-08 10:24:21 +0000

    added meta schema_type patch

---
+++ sql/patch_58_59_e.sql
@@ -0,0 +1,12 @@
+# patch_58_59_e.sql
+#
+# title: add meta schema_type
+#
+# description:
+# Add meta entry denoting funcgen schema_type
+
+INSERT INTO meta (meta_key, meta_value) VALUES ('schema_type', 'funcgen');
+# patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_58_59_e.sql|meta.schema_type');
+
+
