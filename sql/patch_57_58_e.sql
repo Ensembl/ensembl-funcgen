e0189f6e981ab16de8e9302f5cdd45de5c2ff9d5
Nathan Johnson <njohnson@ebi.ac.uk>
2010-07-05 12:44:19 +0000

    added NULL species_id for schema_type

--- sql/patch_57_58_e.sql
+++ sql/patch_57_58_e.sql
@@ -10,4 +10,3 @@ ALTER table probe ADD KEY `name_idx` (`name`);
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_57_58_e.sql|probe.name_key');
 
-
