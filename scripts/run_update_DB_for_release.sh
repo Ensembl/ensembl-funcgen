9eaed3086bd16c643d81f1c4b49399792085d593
Nathan Johnson <njohnson@ebi.ac.uk>
2010-07-15 09:48:27 +0000

    updated script path

--- scripts/run_update_DB_for_release.sh
+++ scripts/run_update_DB_for_release.sh
@@ -20,53 +20,48 @@ shift
 PASS=$1
 shift
 
+
+
 if [ ! $PASS ] || [ ! $USER ]
-then echo "Must provide at a user and password argument"; echo "Write run script usage in .efg"; exit; fi
+then echo "Must provide at a user and password argument"; exit; fi
 
-
-#| bos_taurus_funcgen_56_4e                | DONE
-#| caenorhabditis_elegans_funcgen_56_200   | DONE
-#| canis_familiaris_funcgen_56_2m          | DONE
-#| ciona_intestinalis_funcgen_56_2m        | DONE 
-#| danio_rerio_funcgen_56_8b               | DONE
-#| drosophila_melanogaster_funcgen_56_513a | DONE
-#| gallus_gallus_funcgen_56_2m             | DONE
-#| homo_sapiens_funcgen_56_37a             | RUNNING
-#| macaca_mulatta_funcgen_56_10l           | DONE
-#| mus_musculus_funcgen_56_37i             | RUNNING on ens-genomics1 copied to staging
-#| ornithorhynchus_anatinus_funcgen_56_1k  | DONE
-#| pan_troglodytes_funcgen_56_21l          | DONE on ens-genomics1 copied to staging
-#| rattus_norvegicus_funcgen_56_34x        | RUNNING in screen
-#| saccharomyces_cerevisiae_funcgen_56_1j  | DONE
-#| xenopus_tropicalis_funcgen_56_41n       | DONE
-
-
-#Need to do this for all by submitting these jobs to the farm.
-
-#WARNING: This loop only works for standard DB names i.e. no prefixes!
+#WARNING: This loop only works for standard DB names (i.e. only dev prefix allowed)
 
 echo "Please check edit the script before running, by adding your dbnames and checking the host parameters"
 exit;
 
+host='ens-staging2'
+dnadb_host='ens-staging2'
+dbs='mus_musculus_funcgen_59_37l'
 
-for db in sus_scrofa_funcgen_57_9a; do
+
+for db in $dbs; do
+
 	latin=$(echo $db | sed 's/_funcgen_.*//')
+	latin=$(echo $latin | sed 's/dev_//')
 	data_version=$(echo $db | sed 's/.*funcgen_//')
 
+	bsub_cmd="bsub -o $HOME/logs/update_DB_for_release.${latin}_${data_version}.out  -e $HOME/logs/update_DB_for_release.${latin}_${data_version}.err -J update_DB_for_release.${latin}_${data_version} -q basement "
 
-bsub_cmd="bsub -o $HOME/logs/update_DB_for_release.${latin}.out  -e $HOME/logs/update_DB_for_release.${latin}.err -J update_DB_for_release.${latin} -q basement "
 
-
-$bsub_cmd time perl -w $EFG_SRC/scripts/update_DB_for_release.pl\
+#	job_cmd="$bsub_cmd time perl -w $EFG_SRC/scripts/update_DB_for_release.pl\
+job_cmd="time perl -w $EFG_SRC/scripts/release/update_DB_for_release.pl\
 	-species $latin\
 	-port 3306 \
-	-host ens-genomics1 \
+	-host $host\
 	-user $USER \
 	-data_version $data_version\
 	-dbname $db\
-	-dnadb_host ens-staging2\
+	-dnadb_host $dnadb_host\
 	-check_displayable \
-	-pass $PASS \
-	-tee \
+	-pass $PASS $*"
+
+	echo -e "\n$job_cmd"
+	
+	$job_cmd
 
 done
+
+exit
+
+
