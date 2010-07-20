f4817caa8503bc1091fc3464118e4dca1aaaffea
Nathan Johnson <njohnson@ebi.ac.uk>
2010-07-20 14:49:20 +0000

    removed motif_id and added feature_type_id for now

--- sql/patch_59_60_b.sql
+++ sql/patch_59_60_b.sql
@@ -1,34 +1,41 @@
 # patch_59_60_b.sql
 #
-# title: add pwm_feature
+# title: add motif_feature
 #
 # description:
-# Add pwm_feature table, and associated link table
+# Add motif_feature table, and associated link table
 
-DROP TABLE IF EXISTS `pwm_feature`;
-CREATE TABLE `pwm_feature` (
-  `pwm_feature_id` int(10) unsigned NOT NULL auto_increment,
+DROP TABLE IF EXISTS `motif_feature`;
+CREATE TABLE `motif_feature` (
+  `motif_feature_id` int(10) unsigned NOT NULL auto_increment,
   `seq_region_id` int(10) unsigned NOT NULL,
   `seq_region_start` int(10) unsigned NOT NULL,
   `seq_region_end` int(10) unsigned NOT NULL,
   `seq_region_strand` tinyint(1) NOT NULL,
   `display_label` varchar(60) default NULL,
   `score` double default NULL,
-  `pwm_id` int(10) unsigned NOT NULL,	
-  PRIMARY KEY  (`pwm_feature_id`),
+  `feature_type_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY  (`motif_feature_id`),
   KEY `seq_region_idx` (`seq_region_id`,`seq_region_start`),
-  KEY `pwm_idx` (`pwm_id`),     
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 MAX_ROWS=100000000;
-
-
-DROP TABLE IF EXISTS `associated_pwm_feature`;
-CREATE TABLE `associated_pwm_feature` (
-   `annotated_feature_id` int(10) unsigned NOT NULL,
-   `pwm_feature_id` int(10) unsigned NOT NULL,
-   PRIMARY KEY  (`annotated_feature_id`, `pwm_feature_id`)
+  KEY `feature_type_idx` (`feature_type_id`),
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 
+-- Add motif_id/binding_matrix_id here
+-- move/replicate feature_type_id to binding_matrix
+
+-- analysis or type for pwm should be in the binding_matrix table
+-- should we capture analysis here to i.e. MOODS?
+
+DROP TABLE IF EXISTS `associated_motif_feature`;
+CREATE TABLE `associated_motif_feature` (
+   `annotated_feature_id` int(10) unsigned NOT NULL,
+   `motif_feature_id` int(10) unsigned NOT NULL,
+   PRIMARY KEY  (`annotated_feature_id`, `motif_feature_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- also load pwm_features as reg_attrs to avoid more queries when drawing reg_feat track?
+-- Or store the precomputed start stop structure as a BLOB for display?
 
 
 
@@ -41,6 +48,6 @@ CREATE TABLE `associated_pwm_feature` (
 
 
 # patch identifier
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_58_59_b.sql|probe.description');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_59_60_b.sql|motif_feature');
 
 
