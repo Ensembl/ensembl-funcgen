43b58a511a8c56866e461361d33758a820b22850
Nathan Johnson <njohnson@ebi.ac.uk>
2010-09-16 13:31:21 +0000

    corrected header name

--- sql/patch_59_60_c.sql
+++ sql/patch_59_60_c.sql
@@ -5,6 +5,8 @@
 # description:
 # Add motif_feature table, and associated link table
 
+-- add new motif_feature table
+
 DROP TABLE IF EXISTS `motif_feature`;
 CREATE TABLE `motif_feature` (
   `motif_feature_id` int(10) unsigned NOT NULL auto_increment,
@@ -21,11 +23,9 @@ CREATE TABLE `motif_feature` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 
--- Add motif_id/binding_matrix_id here
--- move/replicate feature_type_id to binding_matrix
 
--- analysis or type for pwm should be in the binding_matrix table
--- should we capture analysis here to i.e. MOODS?
+
+-- Add link table
 
 DROP TABLE IF EXISTS `associated_motif_feature`;
 CREATE TABLE `associated_motif_feature` (
@@ -34,14 +34,8 @@ CREATE TABLE `associated_motif_feature` (
    PRIMARY KEY  (`annotated_feature_id`, `motif_feature_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
--- also load pwm_features as reg_attrs to avoid more queries when drawing reg_feat track?
--- Or store the precomputed start stop structure as a BLOB for display?
 
 
-
--- pwm_feature to feature_set/cell_type may be 1 to many relationship
--- pwm_feature to annotated_feature may be many to 1 relationship
--- remove feature_set_id or change to cell_type_id?
 -- cell_type style query will have to be done via query extension to feature_set using cell_type_id.
 
 DROP TABLE IF EXISTS `binding_matrix`;
@@ -60,8 +54,7 @@ CREATE  TABLE `binding_matrix` (
 -- Need to add AVG_ROW_LENGTH here for free text/blob field.
 -- frequencies to be recast as blob
 -- change type to analysis_id to avoid enum limitations
--- Then have wrappers for Jaspar/Inferred?
-
+-- Then have wrapper methods for Jaspar/Inferred?
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_59_60_c.sql|motif_feature_binding_matrix');
