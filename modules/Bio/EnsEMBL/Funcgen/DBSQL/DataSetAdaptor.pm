b3812fe2200f0289bafac3260f2ca47d27965dc9
Daniel Sobral <ds19@sanger.ac.uk>
2010-11-26 17:17:12 +0000

    Just adding a comment

--- modules/Bio/EnsEMBL/Funcgen/DBSQL/DataSetAdaptor.pm
+++ modules/Bio/EnsEMBL/Funcgen/DBSQL/DataSetAdaptor.pm
@@ -208,6 +208,17 @@ sub fetch_all_by_product_FeatureSet_type {
 }
 
 
+=head2 fetch_by_product_FeatureSet
+
+  Arg [1]    : Bio::EnsEMBL::Funcgen::FeatureSet
+  Example    : my @dsets = $fs_adaptopr->fetch_by_product_FeatureSet($fset);
+  Description: Retrieves DataSet objects from the database based on the FeatureSet.
+  Returntype : ARRAYREF of Bio::EnsEMBL::Funcgen::DataSet objects
+  Exceptions : Throws if arg is not a valid FeatureSet
+  Caller     : General
+  Status     : Deprecated - use fetch_all_by_product_FeatureSet
+
+=cut
 
 sub fetch_all_by_FeatureSet {
   my $self = shift;
