e9bc69ab0c8900ed435b848c9e6dcc07d3f51652
Daniel Sobral <ds19@sanger.ac.uk>
2010-12-06 15:42:10 +0000

    Updated patch to also cover change in index

--- sql/patch_60_61_b.sql
+++ sql/patch_60_61_b.sql
@@ -6,8 +6,14 @@
 # Add analysis_id field to binding_matrix table and drop type field
 
 
+ALTER table binding_matrix DROP KEY `name_type_idx`;
+
 ALTER table binding_matrix ADD `analysis_id` int(10) unsigned NOT NULL;
 ALTER table binding_matrix DROP `type`;
+
+ALTER table binding_matrix ADD KEY `name_analysis_idx` (`name`, `analysis_id`);
+
+OPTIMIZE table binding_matrix;
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_60_61_b.sql|binding_matrix.analysis_id');
