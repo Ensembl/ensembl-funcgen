35c9b2789e20a6c131a5e100c16a156bcb2ddd83
Daniel Sobral <ds19@sanger.ac.uk>
2011-01-20 10:41:42 +0000

    Schema version change

---
+++ sql/patch_61_62_a.sql
@@ -0,0 +1,13 @@
+# patch_61_62_a.sql
+#
+# title: update schema version
+#
+# description:
+# Update schema_version in meta table to 62
+
+UPDATE meta SET meta_value='62' WHERE meta_key='schema_version';
+
+# patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_61_62_a.sql|schema_version');
+
+
