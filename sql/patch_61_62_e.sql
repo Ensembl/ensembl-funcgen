e91160948e105c9a06292427ea1a6a90b70e8862
Nathan Johnson <njohnson@ebi.ac.uk>
2011-02-11 13:19:54 +0000

    updated dbfile registry

--- sql/patch_61_62_e.sql
+++ sql/patch_61_62_e.sql
@@ -1,9 +1,27 @@
-# patch_61_62_e.sql
-#
-# title:  dbfile_registry
-#
-# description:
-# Add generic dbfile_registry table to store filepaths
+/** 
+@header patch_61_62_e.sql - dbfile_registry
+@desc   Add generic dbfile_registry table to store filepaths
+
+@table  dbfile_registry
+@desc   This generic table contains a simple registry of paths to support 
+        flat file(DBFile) access. This should be left joined from the
+        relevant adaptor e.g. ResultSetAdaptor
+@column table_id   Primary key of linked dbfile entity e.g. result_set or 
+        analysis
+@column table_name Name of linked table
+@column path       Either a full filepath or a directory which the API will 
+                   use to build the filepath
+*/
+
+
+DROP TABLE IF EXISTS `dbfile_registry`;
+CREATE TABLE `status` (
+   `table_id` int(10) unsigned NOT NULL,
+   `table_name` varchar(32)NOT NULL,	
+   `path` varchar(255) NOT NULL,
+   PRIMARY KEY  (`table_id`, `table_name`, `status_name_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
 
 
 # patch identifier
