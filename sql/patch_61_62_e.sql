2d0956bde8aca3285a031e5b3a5d5232a5fe2c47
Nathan Johnson <njohnson@ebi.ac.uk>
2011-02-11 12:39:33 +0000

    added dbfile registry

---
+++ sql/patch_61_62_e.sql
@@ -0,0 +1,12 @@
+# patch_61_62_e.sql
+#
+# title:  dbfile_registry
+#
+# description:
+# Add generic dbfile_registry table to store filepaths
+
+
+# patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_61_62_e.sql|dbfile_registry');
+
+
