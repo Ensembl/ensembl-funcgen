819942715ce60a5155f4096668eb2fe54110f8d8
Nathan Johnson <njohnson@ebi.ac.uk>
2011-02-28 16:02:30 +0000

    now includes db_release

--- sql/patch_61_62_h.sql
+++ sql/patch_61_62_h.sql
@@ -1,11 +1,11 @@
 /** 
-@header patch_61_62_h - external_db.db_name_idx 
+@header patch_61_62_h - external_db.db_name_release_idx 
 
 @desc   Add unique index to db_name field in external_db table.
 */
 
-CREATE INDEX db_name_idx ON external_db(db_name);
+CREATE UNIQUE INDEX db_name_release_idx ON external_db(db_name, db_release);
 
 # Patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value)
-  VALUES (NULL, 'patch', 'patch_61_62_h.sql|external_db.db_name_idx');
+  VALUES (NULL, 'patch', 'patch_61_62_h.sql|external_db.db_name_release_idx');
