4f406de085df06c616eeb5ee5e4153b6dfb3a726
Nathan Johnson <njohnson@ebi.ac.uk>
2011-02-28 16:02:06 +0000

    update to include external features

--- sql/patch_61_62_b.sql
+++ sql/patch_61_62_b.sql
@@ -1,6 +1,6 @@
 /** 
-@header patch_61_62_c.sql - motif_feature.interdb_stable_id
-@desc   Add 'internal' motif_feature stable_ids.  
+@header patch_61_62_c.sql - interdb_stable_id
+@desc   Add 'internal' motif_feature/external_feature stable_ids.  
         Required for inter-DB linking without using internal db ids
         i.e variation consequences
 */
@@ -23,5 +23,21 @@ analyze table motif_feature;
 optimize table motif_feature;
 
 
+ALTER table external_feature ADD `interdb_stable_id` mediumint(8) unsigned DEFAULT NULL;
+
+set @n = 0;
+update external_feature mf
+  join (select external_feature_id, @n := @n + 1 new_stable_id
+          from external_feature
+          order by external_feature_id) v
+    on mf.external_feature_id = v.external_feature_id
+  set mf.interdb_stable_id = v.new_stable_id;
+
+ALTER table external_feature ADD UNIQUE KEY `interdb_stable_id_idx` (`interdb_stable_id`);
+
+
+analyze table external_feature;
+optimize table external_feature
+
 # patch identifier
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_61_62_b.sql|motif_feature.interdb_stable_id');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_61_62_b.sql|interdb_stable_id');
