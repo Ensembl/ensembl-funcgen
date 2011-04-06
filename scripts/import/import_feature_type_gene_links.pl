534d71327ebabbd85f9013f0c44152ba2e53a3b1
Daniel Sobral <ds19@sanger.ac.uk>
2011-04-06 13:15:19 +0000

    Adds external_db when needed

--- scripts/import/import_feature_type_gene_links.pl
+++ scripts/import/import_feature_type_gene_links.pl
@@ -34,17 +34,19 @@ Mandatory
 
   -species|s       Species name
 
-  -dbpass|p        The password for the EFG DB
+  -pass|p        The password for the EFG DB
 
   -dbname          Defines the eFG dbname
+
+  -file            Specifies file with data
   
 Optional
 
-  -dbhost|h        Defines the eFG db host [ens-genomics1]
+  -host|h        Defines the eFG db host [ens-genomics1]
 
-  -dbport|l        Defines the eFG db port [3306]
+  -port|l        Defines the eFG db port [3306]
 
-  -dbuser|u        Defines the eFG db user [ensadmin]
+  -user|u        Defines the eFG db user [ensadmin]
 
   -help            Brief help message
 
@@ -58,7 +60,7 @@ Optional
 
 Species name
 
-=item B<-dbpass|p>
+=item B<-pass|p>
 
 Database password
 
@@ -66,17 +68,34 @@ Database password
 
 Database name
 
-=item B<-dbhost|h>
+=item B<-host|h>
 
 Database host (defaults to ens-genomics1)
 
-=item B<-dbport|l>
+=item B<-port|l>
 
 Database port (defaults to 3306)
 
-=item B<-dbuser|u>
+=item B<-user|u>
 
 Database user (defaults to 'ensadmin')
+
+=item B<-dnadb_host>
+
+Core Database host (defaults to ens-livemirror)
+
+=item B<-dnadb_port>
+
+Core Database port (defaults to 3306)
+
+=item B<-dnadb_user>
+
+Core Database user (defaults to 'ensro')
+
+
+=item B<-file|f>
+
+Data file (usually in types/$species.FeatureType_Genes.txt)
 
 =item B<-help>
 
@@ -98,28 +117,36 @@ use Getopt::Long;
 use Pod::Usage;
 use Bio::EnsEMBL::DBEntry;
 use Bio::EnsEMBL::Funcgen::Utils::Helper;
+use Bio::EnsEMBL::Funcgen::Utils::EFGUtils qw(add_external_db);
 use Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::Utils::Exception qw( throw warning );
 
-my ($pass,$port,$host,$user,$dbname,$species,$help);
+my ($dnadb_host,$dnadb_port,$dnadb_user);
+my ($pass,$port,$host,$user,$dbname,$species,$help,$file);
 $port = 3306;
 $host='ens-genomics1';
 $user='ensadmin';
 
 GetOptions (
-	    "dbpass|p=s"   => \$pass,
-	    "dbport|l=s"   => \$port,
-	    "dbhost|h=s"   => \$host,
-	    "dbuser|u=s"   => \$user,
+	    "dnadb_host=s" => \$dnadb_host,
+	    "dnadb_port=s" => \$dnadb_port,
+	    "dnadb_user=s" => \$dnadb_user,
+	    "pass|p=s"     => \$pass,
+	    "port|l=s"     => \$port,
+	    "host|h=s"     => \$host,
+	    "user|u=s"     => \$user,
 	    "dbname=s"     => \$dbname,
 	    "species|s=s"  => \$species, 
+	    "file|f=s"     => \$file,
 	    "help|?"       => \$help,
 	   );
 pod2usage(1) if $help;
 pod2usage(0) if !defined($species);
 pod2usage(0) if !defined($dbname);
 pod2usage(0) if !defined($pass);
+pod2usage(0) if !defined($file);
 
+#Core db not really important here but may pass core db as parameters to avoid errors?
 my $efg_dba = Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor->new
     (
      -host    => $host,
@@ -127,50 +154,62 @@ my $efg_dba = Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor->new
      -dbname  => $dbname,
      -species => $species,  
      -port    => $port,
-     -pass	  => $pass,
+     -pass    => $pass,
+     -dnadb_host => $dnadb_host,
+     -dnadb_port => $dnadb_port,
+     -dnadb_user => $dnadb_user,
      );
+$efg_dba->dbc->db_handle;
+
+print ":: Loading FeatureType Gene Links from file $file\n";
+
 my $fta	= $efg_dba->get_FeatureTypeAdaptor();     
 my $dbentry_adaptor = $efg_dba->get_DBEntryAdaptor();
 
 my $analysis_adaptor = $efg_dba->get_AnalysisAdaptor();
-my $analysis = $analysis_adaptor->fetch_by_logic_name("Ensembl Manual Annotation");
-if(!$analysis){ throw "Analysis 'Ensembl Manual Annotation' not found"; }
+my $analysis = $analysis_adaptor->fetch_by_logic_name("Manual Antibody-Gene Annotation");
+if(!$analysis){ throw "Analysis 'Manual Antibody-Gene Annotation' not found"; }
 my $helper = Bio::EnsEMBL::Funcgen::Utils::Helper->new(
-													   no_log => 1,#default to STDOUT
-													  );
-
-open(FILE,"types/".$species.".FeatureType_Genes.txt");
+						       no_log => 1,#default to STDOUT
+						      );
+my %releases;
+open(FILE,$file);
 <FILE>; #Title
 while(<FILE>){
-	chomp;
-	my ($feature_type, $gene_stable_id, $release) = split(/\t/);
-		
-	eval{	
-		my $ft = $fta->fetch_by_name($feature_type);
-		if(!$ft){ warn $feature_type." does not exist"; next; }
+  chomp;
+  my ($feature_type, $gene_stable_id, $release) = split(/\t/);
 
-		my $gene_name = $helper->get_core_display_name_by_stable_id($efg_dba->dnadb(),$gene_stable_id,'Gene');
-
-		my $dbentry = Bio::EnsEMBL::DBEntry->new(
-			-dbname             => $species.'_core_Gene',
-			-release            => $release,
-			-status             => 'KNOWNXREF',
-			#-display_label_linkable => 1,
-	   	 	-db_display_name    => 'EnsemblGene',
-			-type               => 'MISC', 
-			-primary_id         => $gene_stable_id,
-			-display_id         => $gene_name,
-			-info_type          => 'MISC',
-			-info_text          => 'GENE',
-			-linkage_annotation => 'Ensembl Manual Curation',
-			-analysis           => $analysis,
-			#-description        => 'Ensembl Gene associated to Feature Type',
-		);
-
-		#1 as the last argument to ignore Gene release
-		$dbentry_adaptor->store( $dbentry, $ft->dbID, 'FeatureType');
-	};
-	if($@){ warn $@; };
+  eval{	
+    if(!defined($releases{$release})){
+      #Try to create it...
+      add_external_db($efg_dba,$species.'_core_Gene',$release,'EnsemblGene');
+      $releases{$release}=1;
+    }
+    my $ft = $fta->fetch_by_name($feature_type);
+    if(!$ft){ warn $feature_type." does not exist"; next; }
+    
+    my $gene_name = $helper->get_core_display_name_by_stable_id($efg_dba->dnadb(),$gene_stable_id,'Gene');
+    
+    my $dbentry = Bio::EnsEMBL::DBEntry->new(
+					     -dbname             => $species.'_core_Gene',
+					     -release            => $release,
+					     -status             => 'KNOWNXREF',
+					     #-display_label_linkable => 1,
+					     -db_display_name    => 'EnsemblGene',
+					     -type               => 'MISC', 
+					     -primary_id         => $gene_stable_id,
+					     -display_id         => $gene_name,
+					     -info_type          => 'MISC',
+					     -info_text          => 'GENE',
+					     -linkage_annotation => $analysis->logic_name,
+					     -analysis           => $analysis,
+					     #-description        => 'Ensembl Gene associated to Feature Type',
+					    );
+    
+    #1 as the last argument to ignore Gene release
+    $dbentry_adaptor->store( $dbentry, $ft->dbID, 'FeatureType');
+  };
+  if($@){ warn $@; };
 }
 
 close FILE;
