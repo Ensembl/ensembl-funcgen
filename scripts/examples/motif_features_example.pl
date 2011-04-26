f3b7c41e0875fced96000f73a34eb070765d6dcb
Daniel Sobral <ds19@sanger.ac.uk>
2011-04-26 10:51:56 +0000

    Motif Features Example

---
+++ scripts/examples/motif_features_example.pl
@@ -0,0 +1,49 @@
+use strict;
+use warnings;
+use Bio::EnsEMBL::Registry;
+
+my $registry = 'Bio::EnsEMBL::Registry';
+
+$registry->load_registry_from_db(
+    -host => 'ensembldb.ensembl.org',
+    -user => 'anonymous'
+);
+
+my $regfeat_adaptor = $registry->get_adaptor('Human', 'funcgen', 'regulatoryfeature');
+
+my $rf = $regfeat_adaptor->fetch_by_stable_id('ENSR00000165384'); 
+my @motif_features = @{$rf->regulatory_attributes('motif')};
+
+#An example to print motif feature properties
+foreach my $motif_feature (@motif_features) {
+	print_feature($motif_feature);
+	print "\tBinding Matrix: ".$motif_feature->binding_matrix->name."\n";
+	print "\tMotif Sequence: ".$motif_feature->seq."\n";
+	print "\tMotif Score: ".$motif_feature->score."\n";
+	my $afs = $motif_feature->associated_annotated_features();	
+	print "\tSupporting Annotated Features\n";
+	foreach my $feat (@$afs){
+		print "\t\t";
+		#Each feature is an annotated feature
+		print_feature($feat); 
+	}
+}
+
+sub print_feature {
+	my $feature = shift;
+	print 	$feature->display_label. 	
+	 	"\t(".$feature->seq_region_name.":".
+		$feature->seq_region_start."-".$feature->seq_region_end.")\n";
+}
+
+#We can also get motifs via the annotated features.
+$rf = $regfeat_adaptor->fetch_by_stable_id('ENSR00000354060'); 
+my @annotated_features = @{$rf->regulatory_attributes('annotated')};
+foreach my $annot_feat (@annotated_features){
+	#One motif feature may appear many times with different annotated features
+	my @motif_feats = @{$annot_feat->get_associated_MotifFeatures()};
+	if(scalar(@motif_feats)>0){
+		print "Annotated Feature with motif feature(s) - ";
+		print_feature($annot_feat);
+	}
+}
