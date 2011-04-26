701d8f84791f1f15c78310a660aa6712ec7c9665
Daniel Sobral <ds19@sanger.ac.uk>
2011-04-26 12:38:13 +0000

    Feature Types example

---
+++ scripts/examples/feature_types_example.pl
@@ -0,0 +1,25 @@
+use strict;
+use warnings;
+use Bio::EnsEMBL::Registry;
+
+my $registry = 'Bio::EnsEMBL::Registry';
+
+$registry->load_registry_from_db(
+    -host => 'ensembldb.ensembl.org',
+    -user => 'anonymous'
+);
+
+my $ftype_adaptor = $registry->get_adaptor('Human', 'funcgen', 'featuretype');
+my $fset_adaptor = $registry->get_adaptor('Human', 'funcgen', 'featureset');
+
+#Print all feature sets for the 
+my @tfs = @{$ftype_adaptor->fetch_all_by_class('Transcription Factor')}; 
+foreach my $ft (@tfs){
+	print "Feature Type: ".$ft->name."\n";
+	my @fsets = @{$fset_adaptor->fetch_all_by_FeatureType($ft)};
+	print "\t".scalar(@fsets)." Feature Sets available:\n";
+	foreach my $fset (@fsets){ 
+		print "\t\t".$fset->name."\n"; 
+	}
+}
+
