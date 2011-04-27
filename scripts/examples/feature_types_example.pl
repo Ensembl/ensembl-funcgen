e271e6dd8a15bea9b2fac1bf57ac3bae97b16f78
Daniel Sobral <ds19@sanger.ac.uk>
2011-04-27 08:49:44 +0000

    small update

--- scripts/examples/feature_types_example.pl
+++ scripts/examples/feature_types_example.pl
@@ -12,7 +12,7 @@ $registry->load_registry_from_db(
 my $ftype_adaptor = $registry->get_adaptor('Human', 'funcgen', 'featuretype');
 my $fset_adaptor = $registry->get_adaptor('Human', 'funcgen', 'featureset');
 
-#Print all feature sets for the 
+#Print all feature sets for Transcription Factors (note that this does not include CTCF, an insulator)
 my @tfs = @{$ftype_adaptor->fetch_all_by_class('Transcription Factor')}; 
 foreach my $ft (@tfs){
 	print "Feature Type: ".$ft->name."\n";
