ca2dfe563ac6c8e778db648f8019109c724b0a96
Nathan Johnson <njohnson@ebi.ac.uk>
2011-07-29 10:26:52 +0000

    changed accession_id to archive_id

--- sql/patch_63_64_c.sql
+++ sql/patch_63_64_c.sql
@@ -1,13 +1,13 @@
 /** 
 @header patch_63_64_c.sql - experiment.accession 
-@desc   Add Accession ID and Data URL fields to experiment
+@desc   Add archive_id and data_url fields to experiment
 */
 
-ALTER table experiment ADD accession_id varchar(20) default NULL;
+ALTER table experiment ADD archive_id varchar(20) default NULL;
 ALTER table experiment ADD data_url varchar(255) default NULL;
-ALTER table experiment ADD UNIQUE KEY `accession_idx`(`accession_id`);
+ALTER table experiment ADD UNIQUE KEY `archive_idx`(`archive_id`);
 
 # patch identifier
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_63_64_c.sql|experiment.accession');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_63_64_c.sql|experiment.archive_id_data_url');
 
 
