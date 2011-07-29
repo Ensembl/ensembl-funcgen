24e0cddb4e9aa57135e55244ba68b0cc15ecd38a
Daniel Sobral <ds19@sanger.ac.uk>
2011-07-29 10:21:13 +0000

    Patch for experimental group

---
+++ sql/patch_63_64_d.sql
@@ -0,0 +1,11 @@
+/** 
+@header patch_63_64_d.sql - experimental_group.url 
+@desc   Add URL field to experimental_group
+*/
+
+ALTER table experimental_group ADD url varchar(255) default NULL;
+
+# patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_63_64_d.sql|experimental_group.url');
+
+
