561e08213409b4b3567e994429188e12df5cde13
Daniel Sobral <ds19@sanger.ac.uk>
2011-08-08 15:27:44 +0000

    Added boolean is_project in the experimental_group table

--- sql/patch_63_64_d.sql
+++ sql/patch_63_64_d.sql
@@ -4,6 +4,7 @@
 */
 
 ALTER table experimental_group ADD url varchar(255) default NULL;
+ALTER table experimental_group ADD is_project boolean default False;
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_63_64_d.sql|experimental_group.url');
