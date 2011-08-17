54bb83a9e12a5756c34996a99a00e354e001e80c
Nathan Johnson <njohnson@ebi.ac.uk>
2011-08-17 10:29:45 +0000

    updated uo keys

---
+++ sql/patch_63_64_g.sql
@@ -0,0 +1,30 @@
+/** 
+@header patch_63_64_g.sql - index_tidy_up
+@desc   Tidy indexes on unmapped_object to reflect the core schema
+*/
+
+
+
+
+ALTER table unmapped_object drop KEY id_idx;
+ALTER table unmapped_object drop KEY  anal_idx;
+ALTER table unmapped_object drop KEY anal_exdb_idx;
+ALTER table unmapped_object drop KEY object_type_idx;
+
+ALTER table unmapped_object add UNIQUE KEY `unique_unmapped_obj_idx` (`identifier`,`ensembl_id`,`parent`,`unmapped_reason_id`,`ensembl_object_type`,`external_db_id`);
+ALTER table unmapped_object add KEY `anal_exdb_idx` (`analysis_id`,`external_db_id`);
+ALTER table unmapped_object add  KEY `id_idx` (`identifier`(50));
+
+/** 
+reduce identifier index to 50 characters as most ids are under 30, index will be faster
+add index for queries using external_db_id in the where clause
+*/
+
+ALTER table unmapped_object add  KEY `ext_db_identifier_idx` (`external_db_id`,`identifier`);
+
+
+
+# patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_63_64_g.sql|index_tidy_up');
+
+
