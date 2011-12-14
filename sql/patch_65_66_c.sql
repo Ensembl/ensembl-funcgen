2df37327fc4e771af98431779d731490328d509d
Nathan Johnson <njohnson@ebi.ac.uk>
2011-12-14 14:22:34 +0000

    *** empty log message ***

--- sql/patch_65_66_c.sql
+++ sql/patch_65_66_c.sql
@@ -6,8 +6,8 @@
 ALTER TABLE array_chip MODIFY `design_id` varchar(100) default NULL; 
 ALTER TABLE array_chip MODIFY  `name` varchar(100) default NULL;
 
-analyze table array_design;
-optimize table array_design;
+analyze table array_chip;
+optimize table array_chip;
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_65_66_c.sql|array_chip.name_design');
