d12cb288ed07350a7b0493f84af40a3c594b5e77
Nathan Johnson <njohnson@ebi.ac.uk>
2012-05-08 13:42:16 +0000

    new easier question 1

--- scripts/examples/solutions/regulatory_features_1.pl
+++ scripts/examples/solutions/regulatory_features_1.pl
@@ -11,57 +11,74 @@ $registry->load_registry_from_db(
 );
 
 # Regulatory Features 
-# Create a script which gets all the RegulatoryFeatures within 2KB of the 'HOXA4' gene.
-# Hint: Use the Gene->feature_Slice method, then use the expand method.
-# Print out their stable IDs, bound_start/end and start/end values, name of the cell and feature types.
+# Using the human DB, fetch the all the cell type specific regulatory features with stable ID 'ENSR00000623613'.
+# Print out the stable ID, bound_start/end and start/end values, name of the cell and feature type for each.
+# HINT: To get all the cell type specific RegulatoryFeatures use the fetch_all_by_stable_ID method.
 
-#Grab the eFG adaptor
-my $gene_adaptor = $registry->get_adaptor('Human', 'core', 'gene');
-my $regfeat_adaptor = $registry->get_adaptor('Human', 'funcgen','regulatoryfeature');
+#Grab the regfeat adaptor
+my $regfeat_adaptor = $registry->get_adaptor('Human', 'funcgen', 'regulatoryfeature');
 
-my @genes = @{$gene_adaptor->fetch_all_by_external_name('HOXA4')};
-print scalar(@genes)." human gene(s) named Hox4\n";
 
-my $hox4 = $genes[0];
-my $slice = $hox4->feature_Slice;
-print "Slice corresponding to Hox4: ".$slice->seq_region_name."\t".$slice->start."\t".$slice->end."\t".$slice->strand."\n";
-$slice = $slice->expand(2000,2000);
-print "Slice 2Kb around Hox4: ".$slice->seq_region_name."\t".$slice->start."\t".$slice->end."\t".$slice->strand."\n";
+#Fetch ENSR00000623613 reg feats for all cell types.
+my @rfs = @{$regfeat_adaptor->fetch_all_by_stable_ID('ENSR00000623613')};
 
-my @reg_feats = @{$regfeat_adaptor->fetch_all_by_Slice($slice)};
-print scalar(@reg_feats)." Regulatory Features 2kb around Hox4\n";
 
-foreach my $rf (@reg_feats){
-	print $rf->stable_id.": \n";
-	print "\tRelative Position: ".$rf->bound_start."..".$rf->start."-".$rf->end."..".$rf->bound_end."\n";
-	print "\tCell: ".$rf->cell_type->name."\n";
-	print "\tFeature Type: ".$rf->feature_type->name."\n";
+# Print out the details
+foreach my $rf(@rfs){
+  print $rf->stable_id.": \n";
+  print "\tRelative Position: ".$rf->bound_start."..".$rf->start."-".$rf->end."..".$rf->bound_end."\n";
+  print "\tCell: ".$rf->cell_type->name."\n";
+  print "\tFeature Type: ".$rf->feature_type->name."\n";
 }
-
 
 __END__
 
 
->perl regulatory_features_1.pl
-
-1 human gene(s) named Hox4
-Slice corresponding to Hox4: 7  27168126        27170418        -1
-Slice 2Kb around Hox4: 7        27166126        27172418        -1
-
-4 Regulatory Features 2kb around Hox4
-ENSR00001558218:
-        Relative Position: 5695..5695-5950..5950
+ENSR00000623613: 
+        Relative Position: 27178100..27178438-27185061..27186300
+        Cell: K562
+        Feature Type: Unclassified
+ENSR00000623613: 
+        Relative Position: 27123400..27178438-27185061..27297450
+        Cell: H1ESC
+        Feature Type: Unclassified
+ENSR00000623613: 
+        Relative Position: 27177575..27178438-27185061..27187569
+        Cell: HUVEC
+        Feature Type: Gene Associated
+ENSR00000623613: 
+        Relative Position: 27177907..27178438-27185061..27185882
+        Cell: HMEC
+        Feature Type: Promoter Associated
+ENSR00000623613: 
+        Relative Position: 27126300..27178438-27185061..27270800
+        Cell: NH-A
+        Feature Type: Unclassified
+ENSR00000623613: 
+        Relative Position: 27178438..27178438-27185061..27185061
+        Cell: HeLa-S3
+        Feature Type: Unclassified
+ENSR00000623613: 
+        Relative Position: 27178438..27178438-27185061..27187350
+        Cell: HepG2
+        Feature Type: Unclassified
+ENSR00000623613: 
+        Relative Position: 27178050..27178438-27185061..27187700
+        Cell: IMR90
+        Feature Type: Unclassified
+ENSR00000623613: 
+        Relative Position: 27178438..27178438-27185061..27185061
+        Cell: HSMM
+        Feature Type: Promoter Associated
+ENSR00000623613: 
+        Relative Position: 27178438..27178438-27185061..27185061
         Cell: MultiCell
         Feature Type: Unclassified
-ENSR00001049354:
-        Relative Position: 4335..4335-4845..4845
-        Cell: MultiCell
+ENSR00000623613: 
+        Relative Position: 27177500..27178438-27185061..27187050
+        Cell: NHEK
         Feature Type: Unclassified
-ENSR00000623608:
-        Relative Position: 1188..1188-2849..2849
-        Cell: MultiCell
-        Feature Type: Unclassified
-ENSR00001049355:
-        Relative Position: 296..296-560..560
-        Cell: MultiCell
+ENSR00000623613: 
+        Relative Position: 27177650..27178438-27185061..27185061
+        Cell: GM12878
         Feature Type: Unclassified
