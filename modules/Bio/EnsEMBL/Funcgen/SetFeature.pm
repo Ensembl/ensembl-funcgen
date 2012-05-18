346272ad80973c846f9758b279281a547d8159cd
Nathan Johnson <njohnson@ebi.ac.uk>
2012-05-18 09:05:34 +0000

    code review changes, general tidy up and Pod improvement

--- modules/Bio/EnsEMBL/Funcgen/SetFeature.pm
+++ modules/Bio/EnsEMBL/Funcgen/SetFeature.pm
@@ -24,11 +24,14 @@
 
 =head1 NAME
 
-Bio::EnsEMBL::Funcgen::SetFeature - Ensembl specific set feature.
+Bio::EnsEMBL::Funcgen::SetFeature - Base class ???
 
 =head1 SYNOPSIS
 
-    my $feat = new Bio::EnsEMBL::Feature(-start         => 100,
+   ??? Update to real use cases and correct namespsace
+
+    my $feat = new Bio::EnsEMBL::Feature(
+                     -start         => 100,
                                          -end           => 220,
                                          -strand        => -1,
                                          -slice         => $slice,
@@ -55,48 +58,52 @@ Bio::EnsEMBL::Funcgen::SetFeature - Ensembl specific set feature.
 
 =head1 DESCRIPTION
 
+??? Update
 This is a simple wrapper method for the core Feature class to contain common generic
 Funcgen SetFeature methods.
 
 =cut
 
 
+package Bio::EnsEMBL::Funcgen::SetFeature;
+
 use strict;
 use warnings;
-
-package Bio::EnsEMBL::Funcgen::SetFeature;
 
 use Bio::EnsEMBL::Feature;
 use Bio::EnsEMBL::Funcgen::Storable;
 use Bio::EnsEMBL::Utils::Argument qw(rearrange);
-use Bio::EnsEMBL::Utils::Exception qw(throw deprecate warning);
-use vars qw(@ISA);
+# ??? Does AY have a replacemnt, can we remove/replace?
 
+use Bio::EnsEMBL::Utils::Exception qw(throw);
+
+use vars qw(@ISA);
 @ISA = qw(Bio::EnsEMBL::Feature Bio::EnsEMBL::Funcgen::Storable);
 
 
-#Bio::EnsEMBL::Funcgen::Storable
-#gives access to DBEntry type methods
-#This only works as the DBEntry methods in the core API are at the Gene/Transcript/Translation level
-#This would not be a problem if this was integrated back into the core code
-
 =head2 new
 
+#??? Desribe passing hash
+
   Arg [-SET]          : Bio::EnsEMBL::Funcgen::ResultSet or FeatureSet.
-  Arg [-FEATURE_TYPE] : optional Bio::EnsEMBL::Funcgen::FeatureType. Defaults to Set FeatureType.
-  Arg [-ANALYSIS]     : Bio::EnsEMBL::Analysis. Defaults to Set Analysis.
+  Arg [-FEATURE_TYPE] : (optional) Bio::EnsEMBL::Funcgen::FeatureType. Defaults to Feature/ResultSet FeatureType.
+  Arg [-ANALYSIS]     : (optional) Bio::EnsEMBL::Analysis. Defaults to Feature/ResultSet Analysis.
   Arg [-SLICE]        : Bio::EnsEMBL::Slice - The slice on which this feature is.
-  Arg [-START]        : int - The start coordinate of this feature relative to the start of the slice
+  Arg [-START]        : Int - The start coordinate of this feature relative to the start of the slice
 		                it is sitting on. Coordinates start at 1 and are inclusive.
-  Arg [-END]          : int -The end coordinate of this feature relative to the start of the slice
+  Arg [-END]          : Int -The end coordinate of this feature relative to the start of the slice
 	                    it is sitting on. Coordinates start at 1 and are inclusive.
-  Arg [-DISPLAY_LABEL]: string - Display label for this feature
-  Arg [-STRAND]       : int - The orientation of this feature. Valid values are 1, -1 and 0.
-  Arg [-dbID]         : (optional) int - Internal database ID.
-  Arg [-ADAPTOR]      : (optional) Bio::EnsEMBL::DBSQL::BaseAdaptor - Database adaptor.
+  Arg [-DISPLAY_LABEL]: String - Display label for this feature
+  Arg [-STRAND]       : Int - The orientation of this feature. Valid values are 1, -1 and 0.
+#??? Is this optional in core Feature
+
+  Arg [-dbID]         : (optional) Int - Internal database ID.
+  Arg [-ADAPTOR]      : (optional) Bio::EnsEMBL::Funcgen::DBSQL::BaseFeatureAdaptor
 
   Arg [-FEATURE_SET]  : Bio::EnsEMBL::Funcgen::FeatureSet (Obsolete)
 
+
+#??? AF inherits from SF
   Example             : my $feature = Bio::EnsEMBL::Funcgen::AnnotatedFeature->new
                               (
                                -SLICE         => $chr_1_slice,
@@ -107,35 +114,35 @@ use vars qw(@ISA);
 							   -SET           => $fset,
                               );
 
-  Description: Constructor for SetFeature objects. Should never be called directly, only by children.
+  Description: Constructor for SetFeature objects. Should never be called directly, only by its children.
   Returntype : Bio::EnsEMBL::Funcgen::SetFeature
-  Exceptions : None
-  Caller     : General
+  Exceptions : Throws if ???
+  Caller     : ???
   Status     : At Risk - FEATURE_SET arg replaced by SET in v67
 
 =cut
 
 sub new {
   my $caller = shift;
-	
   my $class = ref($caller) || $caller;
 
   my ($display_label, $fset, $ftype, $set)
     = rearrange(['DISPLAY_LABEL', 'FEATURE_SET', 'FEATURE_TYPE', 'SET'], @_);
-  
-  #Don't test for set vs fset here as this will slow things down.
-  #We use new_fast from the adaptors to avoid this
+
   $set ||= $fset;
 
-  #Grab FeatureSet first so we can pass analysis to base Feature class
   if( ( ref($set) ne 'Bio::EnsEMBL::Funcgen::FeatureSet') &&
 	  ( ref($set) ne 'Bio::EnsEMBL::Funcgen::ResultSet') ){
 	throw("Must pass valid Bio::EnsEMBL::Funcgen::FeatureSet or ResultSet object");
   }
 
-  my $self = $class->SUPER::new(@_, -analysis => $fset->analysis);
+  #Grab FeatureSet first so we can pass analysis to base Feature class
   
-   
+  #??? This fset->analysis will over-write feature analysis
+  #can't remove as part of core Feature. catch and warn or ignore for speed?
+
+  my $self = $class->SUPER::new(@_, -analysis => $fset->analysis);
+
 
   if($ftype){
 	
@@ -143,7 +150,7 @@ sub new {
 	  throw('feature_type param must be a valid Bio::EnsEMBL::Funcgen::FeatureType');
 	}
   
-	$self->{'feature_type'} = $ftype;
+	$self->{feature_type} = $ftype;
   }
  
   #Setting attrs directly removes the need for setter code in methods
@@ -172,6 +179,8 @@ sub new {
 
 #Remove and use Bio::EnsEMBL::Feature::new_fast? - This 'weakens' adaptor
 
+#??? Just use core method
+
 sub new_fast {
   return bless ($_[1], $_[0]);
 }
@@ -190,13 +199,16 @@ sub new_fast {
 =cut
 
 sub feature_set {
-  return $_[0]->{'set'};
+  #??? deprecate
+  #check webcode?
+
+  return $_[0]->{set};
 }
 
 
 =head2 set
 
-  Example    : my $fset = $set_feature->set();
+  Example    : my $set = $set_feature->set();
   Description: Getter for the set attribute for this feature.
   Returntype : Bio::EnsEMBL::Funcgen::FeatureSet or ResultSet
   Exceptions : None
@@ -206,13 +218,15 @@ sub feature_set {
 =cut
 
 sub set {
-  return $_[0]->{'set'};
+  return $_[0]->{set};
 }
 
 =head2 cell_type
 
-  Example    : my $cell_name = $efeature->cell_type()->name();
-  Description: Getter for the cell_type attribute for this feature.
+  Example    : my $cell_name = $set_feature->cell_type->name;
+  Description: Getter for the CellType attribute for this feature.
+
+#??? Comes from set
                May not always be set for ExternalFeatures.
   Returntype : Bio::EnsEMBL::Funcgen::CellType
   Exceptions : None
@@ -222,13 +236,15 @@ sub set {
 =cut
 
 sub cell_type{
-	return $_[0]->set->cell_type();
+	return $_[0]->set->cell_type;
 }
 
 =head2 feature_type
 
-  Example    : my $ft_name = $efeature->feature_type()->name();
-  Description: Getter for the feature_type attribute for this feature.
+  Example    : my $ft_name = $set_feature->feature_type->name;
+  Description: Getter for the FeatureType attribute for this feature.
+
+#??? defaults to set ftype if not explcitly set for thsi feature
   Returntype : Bio::EnsEMBL::Funcgen::FeatureType
   Exceptions : None
   Caller     : General
@@ -238,14 +254,16 @@ sub cell_type{
 
 sub feature_type{
   my $self = shift; 
-  return (defined $self->{'feature_type'}) ?  $self->{'feature_type'} : $self->set->feature_type();
+  return (defined $self->{feature_type}) ?  $self->{feature_type} : $self->set->feature_type;
 }
 
 
+#??? Remove this
+#what about MFs? add as feature_set as MOODS/PWM analysis not represented
 
 =head2 analysis
 
-  Example    : my $analysis = $setfeature->analysis);
+  Example    : my $analysis = $setfeature->analysis;
   Description: Getter for the Analysis attribute for this feature.
   Returntype : Bio::EnsEMBL::Analysis
   Exceptions : None
@@ -256,24 +274,24 @@ sub feature_type{
 
 sub analysis{
   my $self = shift;
-  return (defined $self->{'analysis'}) ? $self->{'analysis'} : $self->set->analysis();
+  return (defined $self->{analysis}) ? $self->{analysis} : $self->set->analysis();
 }
 
 
 =head2 display_label
 
-  Example    : my $label = $feature->display_label();
+  Example    : my $label = $feature->display_label;
   Description: Getter for the display label of this feature.
-               This will most likely be over-ridden by child class
+               This will most likely be over-ridden by inheriting class
   Returntype : String
   Exceptions : None
   Caller     : General
-  Status     : Medium Risk
+  Status     : Stable
 
 =cut
 
 sub display_label{
-  return $_[0]->{'display_label'};
+  return $_[0]->{display_label};
 }
 
 
