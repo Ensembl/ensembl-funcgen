1dca15d04bd0013e7d5dfc75087be09d899524e2
Ikhlak Ahmed <ia3@sanger.ac.uk>
2012-08-02 12:56:14 +0000

    corrected for meta_value name in the insert statement to meta table

--- sql/patch_68_69_b.sql
+++ sql/patch_68_69_b.sql
@@ -1,10 +1,11 @@
 /** 
-@header patch_68_69_b.sql - result_set.feature_class
+@header patch_68_69_b.sql - DNAMethylationFeature support
 @desc   add a feature_class field to result_set
 */
 
 
 -- Move currently misplaced fields from experiment table
+
 ALTER TABLE result_set ADD `feature_class` enum('result','DNAMethylation') DEFAULT NULL;
 ALTER TABLE feature_type MODIFY class enum('Insulator','DNA','Regulatory Feature','Histone','RNA','Polymerase','Transcription Factor','Transcription Factor Complex','Regulatory Motif','Enhancer','Expression','Pseudo','Open Chromatin','Search Region','Association Locus','Segmentation State', 'DNAMethylation') DEFAULT NULL;
 ALTER TABLE input_set MODIFY type  enum('annotated','result','segmentation','DNAMethylation') DEFAULT NULL;
@@ -22,6 +23,6 @@ OPTIMIZE TABLE input_set;
 ANALYZE TABLE input_set;
 
 # patch identifier
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_68_69_b.sql|result_set.feature_class');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_68_69_b.sql|DNAMethylationFeature support');
 
 
