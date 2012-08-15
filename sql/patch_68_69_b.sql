2008b3b6a5a3135d5cab31f46b4e30c8428ad265
Nathan Johnson <njohnson@ebi.ac.uk>
2012-08-15 14:12:57 +0000

    updated to use table name format for enum and DNA Modification ftype class

--- sql/patch_68_69_b.sql
+++ sql/patch_68_69_b.sql
@@ -6,9 +6,13 @@
 
 -- Move currently misplaced fields from experiment table
 
-ALTER TABLE result_set ADD `feature_class` enum('result','DNAMethylation') DEFAULT NULL;
-ALTER TABLE feature_type MODIFY class enum('Insulator','DNA','Regulatory Feature','Histone','RNA','Polymerase','Transcription Factor','Transcription Factor Complex','Regulatory Motif','Enhancer','Expression','Pseudo','Open Chromatin','Search Region','Association Locus','Segmentation State', 'DNAMethylation') DEFAULT NULL;
-ALTER TABLE input_set MODIFY type  enum('annotated','result','segmentation','DNAMethylation') DEFAULT NULL;
+ALTER TABLE result_set ADD `feature_class` enum('result','dna_methylation') DEFAULT NULL;
+
+ALTER TABLE feature_type MODIFY class enum('Insulator','DNA','Regulatory Feature','Histone','RNA','Polymerase','Transcription Factor','Transcription Factor Complex','Regulatory Motif','Enhancer','Expression','Pseudo','Open Chromatin','Search Region','Association Locus','Segmentation State', 'DNA Modification') DEFAULT NULL;
+
+-- actully want to remove input_set feature class as input_set can support more than one feature class
+
+ALTER TABLE input_set MODIFY type  enum('annotated','result','segmentation','dna_methylation') DEFAULT NULL;
 
 UPDATE result_set set feature_class = 'result';
 
