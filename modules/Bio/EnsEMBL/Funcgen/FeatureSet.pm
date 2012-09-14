75007fb1e4746a9f7d3de693ddfbfeb8761f21d4
Nathan Johnson <njohnson@ebi.ac.uk>
2012-09-14 09:06:17 +0000

    variosu tweaks and now uses biuild_feature_class_name

--- modules/Bio/EnsEMBL/Funcgen/FeatureSet.pm
+++ modules/Bio/EnsEMBL/Funcgen/FeatureSet.pm
@@ -101,7 +101,7 @@ sub new {
   my $self   = $class->SUPER::new(@_);
 
   my ( $desc, $dlabel, $iset_id, $iset, $exp_id, $exp ) =
-    rearrange( [ 
+    rearrange( [
                 'DESCRIPTION',   'DISPLAY_LABEL',
                 'INPUT_SET_ID', 'INPUT_SET', 'EXPERIMENT_ID', 'EXPERIMENT'
                ],
@@ -117,27 +117,27 @@ sub new {
 
   #Mandatory params checks here (setting done in Set.pm)
   throw('Must provide a FeatureType')
-    if ( !defined $self->feature_type );
+    if ( ! defined $self->feature_type );
 
  #explicit type check here to avoid invalid types being imported as NULL
  #subsequently throwing errors on retrieval
   my $type = $self->feature_class;
 
-  if ( !( $type && exists $valid_classes{$type} ) ) {
+  if ( ! ( $type && exists $valid_classes{$type} ) ) {
     throw( 'You must define a valid FeatureSet type e.g. ' .
            join( ', ', keys %valid_classes ) );
   }
 
   #Direct assignment to prevent need for set arg test in method
 
-  $self->{'description'}   = $desc   if defined $desc;
-  $self->{'display_label'} = $dlabel if defined $dlabel;
-  $self->{'input_set_id'} = $iset_id if defined $iset_id;
+  $self->{description}   = $desc   if defined $desc;
+  $self->{display_label} = $dlabel if defined $dlabel;
+  $self->{input_set_id} = $iset_id if defined $iset_id;
 
   if ( defined $iset ) {
     #Exp obj is only passed during object storing
     #so let the adaptor do is_stored_and_valid
-    $self->{'input_set'} = $iset;
+    $self->{input_set} = $iset;
   }
 
   return $self;
@@ -176,7 +176,7 @@ sub new_fast {
 =cut
 
 sub description {
-  return $_[0]->{'description'};
+  return $_[0]->{description};
 }
 
 
@@ -195,21 +195,20 @@ sub description {
 sub display_label {
   my $self = shift;
 
-  if ( !$self->{'display_label'} ) {
+  if ( ! defined $self->{display_label} ) {
 
-    if ( $self->feature_type->class() eq 'Regulatory Feature' ) {
-      $self->{'display_label'} = $self->name;
+    if ( $self->feature_class eq 'regulatory' ) {
+      $self->{display_label} = $self->name;
     }
     else {
       #This still fails here if we don't have a class or a cell_type set
 
-      $self->{'display_label'} =
-        $self->feature_type->name() . " - " . $self->cell_type->name() .
-        " Enriched Sites";
+      $self->{display_label} =
+        $self->feature_type->name.' - '. $self->cell_type->name.' enriched sites';
     }
   }
 
-  return $self->{'display_label'};
+  return $self->{display_label};
 }
 
 
@@ -233,7 +232,7 @@ sub get_FeatureAdaptor{
   if(! exists $self->{'adaptor_refs'}){
 
     foreach my $valid_class(keys %valid_classes){
-      my $method = 'get_'.$self->build_feature_class_name($valid_class).'Adaptor';
+      my $method = 'get_'.$self->adaptor->build_feature_class_name($valid_class).'Adaptor';
       
       $self->{'adaptor_refs'}{$valid_class} = $self->adaptor->db->$method;
     }
@@ -409,7 +408,6 @@ sub source_label{
     my @source_labels;
 
     if ($input_set) {
-
 
       foreach my $isset(@{$input_set->get_InputSubsets}){
 
