96ab3c61344a3d944d970c3315fa18ff1e54d993
Nathan Johnson <njohnson@ebi.ac.uk>
2012-10-02 09:24:43 +0000

    tidied for CVS

--- scripts/release/run_create_ftp_dir_links.sh
+++ scripts/release/run_create_ftp_dir_links.sh
@@ -1,23 +1,19 @@
 #!/bin/sh
 
-#. ~/src/ensembl-efg/scripts/.efg
-
-#$* would be list of builds if none specifed will use DEFAULT build for the update DB
-
-#GetOpts here with
-
-
 USER=$1
 shift
-#PASS=$1
-#shift
 
-dbname=homo_sapiens_funcgen_68_37
-#dbname=mus_musculus_funcgen_68_38
-dbhost=ens-staging1
-dnadb_host=$dbhost
+
+
+dbname=your_binomial_speciesname_funcgen_SCHEMA_BUILD
+dbhost=your_funcgen_mysql_host
+
+#Optional
+dnadb_host=your_dnadb_mysql_host
 dnadb_user=$USER
 
+nfs_root='/nfs/root/staging/source/data/directory/species/assmebly';
+ftp_root='/ftp/mirror/root/directory'
 
 if [ ! $USER ]; then
 	echo "Must provide a user argument"; exit; 
@@ -25,17 +21,16 @@ fi
 
 
 job_cmd="perl -w $EFG_SRC/scripts/release/create_ftp_dir_links.pl\
+  -nfs_root $nfs_root\
+  -ftp_root $ftp_root\
 	-dbhost $dbhost\
 	-dbuser $USER \
 	-dbname $dbname\
 	-dnadb_host $dnadb_host\
 	-dnadb_user $dnadb_user $@"
 
-#echo -e $job_cmd
-
 $job_cmd
 
-#-pass $PASS
 
 
 
