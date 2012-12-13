312f83cf816ac291048362271768256770c8098f
Nathan Johnson <njohnson@ebi.ac.uk>
2012-12-13 13:52:42 +0000

    added and corrected more docs, removed internal paths, tidied code

--- scripts/examples/dna_methylation_features.pl
+++ scripts/examples/dna_methylation_features.pl
@@ -1,31 +1,39 @@
 =head1 DESCRIPTION
 
-This is an example script to retrieve and use DNAMethylationFeature objects from UCSC BigBed file format. 
-The Ensembl methylation data can be downloaded via FTP from the Ensembl site (http://www.ensembl.org/info/data/ftp/index.html ). 
-The BigBed files (.bb) in this directory represent processed DNA methylation information from whole genome (WGBS) and reduced 
-representation (RRBS) bisulphite sequencing experiments. 
-For information about BigBed files please see http://genome.ucsc.edu/FAQ/FAQformat.html. Each line of the source bed file 
-represents one genomic cytosine (DNAMethylationFeature). The fourth field of each line contains information about cytosine 
-context and total coverage, and the fifth field gives percentage of methylated reads multiplied by a factor of 10 that generates 
-a methylation score value for the DNAMethylationFeature and ranges from 0-1000. Strand orientation of the feature is present in the 
-sixth field. An example line that represents a cytosine in CG context with 19 methylated reads out of a total of 20 reads would be as under:
+This is an example script for the DNAMethylationFeature API.  This class is supported by the database and also by
+UCSC BigBed format files, and hence has some additional requirements:
 
-chr1    3000573 3000574 CG/20   950     +
+    ensembl-external       http://www.ensembl.org/info/docs/api/api_cvs.html
+    ensembl-webcode        http://www.ensembl.org/info/docs/api/api_cvs.html
+    Bio-BigFile-1.07       http://search.cpan.org/~lds/Bio-BigFile-1.07/lib/Bio/DB/BigFile.pm
+    UCSC Kent source tree  http://hgdownload.cse.ucsc.edu/admin/jksrc.zip
+      or more docs here    https://sites.google.com/site/xzhou82/Home/bioinformatics-computational-biology/ucsc-genome-browser
 
-Access to these data requires installation of EnsEMBL::ExternalData modules from  Ensembl CVS and Bio::DB::BigFile from CPAN. 
-DNAMethylationFeatureAdaptor::fetch_all_by_Slice_ResultSet API method can be used to fetch the data. This requires that the 
-ResultSetAdaptor:dbfile_data_root is set explicitly before fetching any ResultSets.
+The Ensembl methylation data can be downloaded from the Ensembl FTP site via the 'data_files' link available here:
+    www.ensembl.org/info/data/ftp/index.html
+Or more preferably direct access e.g.:
+    ftp.ensembl.org/pub/release-69/data_files/homo_sapiens/GRCh37/dna_methylation_feature/
 
-It is important that the directory structure provided here for the files
-is maintained wherever you wish to store them, i.e they must be kept in
+It is important that the sub directory structure provided here for the files is maintained i.e. dna_methylation_feature and 
+all of it's contents. This is because these paths are referenced from the database table 'dbfile_registry'. More information 
+regarding the file format is available in the README file on the FTP site.
 
-species_name/assembly_name/feature_class/
+For API access, the location (or dbfile_data_root) of the dna_methylation_feature directory will require configuration in the 
+ResultSetAdaptor.
 
 e.g. if you download the files to the following directory:
 
-/home/ensembl/data_files/homo_sapiens/GRCh37/dna_methylation_feature 
+    /home/ensembl/data_files/homo_sapiens/GRCh37/dna_methylation_feature
 
+<<<<<<< dna_methylation_features.pl
+Then
+
+   my $dbfile_data_root='/home/ensembl/data_files/homo_sapiens/GRCh37';
+
+See below for how this is set and how features are retrieved.
+=======
 Your dbfile_data_root is /home/ensembl/data_files/homo_sapiens/GRCh37/
+>>>>>>> 1.2
 
 =cut
 
@@ -34,42 +42,47 @@ use warnings;
 use Bio::EnsEMBL::Registry;
 
 my $registry         = 'Bio::EnsEMBL::Registry';
+<<<<<<< dna_methylation_features.pl
+my $dbfile_data_root = '/home/ensembl/data_files/homo_sapiens/GRCh37/';
+
+$registry->load_registry_from_db
+  (
+   -host => 'ensembldb.ensembl.org',
+   -user => 'anonymous'
+  );
+
+my $dmf_adaptor        = $registry->get_adaptor( 'Human', 'funcgen', 'DNAMethylationFeature' );
+my $slice_adaptor      = $registry->get_adaptor( 'Human', 'core', "slice" );
+my $result_set_adaptor = $registry->get_adaptor( 'Human', 'funcgen', "resultset" );
+=======
 
 #change for db_file_data_root
 my $dbfile_data_root = '/nfs/ensnfs-dev/staging/homo_sapiens/GRCh37/';
+>>>>>>> 1.2
 
-$registry->load_registry_from_db(
-    -host => 'ensembldb.ensembl.org',
-    -user => 'anonymous'
-);
-
-my $dmf_adaptor =
-  $registry->get_adaptor( 'Human', 'funcgen', 'DNAMethylationFeature' );
-my $slice_adaptor = $registry->get_adaptor( 'Human', 'core', "slice" );
-my $result_set_adaptor =
-  $registry->get_adaptor( 'Human', 'funcgen', "resultset" );
+#Set the dbfile_data_root!
 $result_set_adaptor->dbfile_data_root($dbfile_data_root);
 
-#fetch all result_sets for DNAMethylation data
-my @result_sets = @{
-    $result_set_adaptor->fetch_all_by_feature_class( 'dna_methylation',
-        { status => 'DISPLAYABLE' } )
-};
 
-#fetch result_set by name
-my ($rset) =
-  @{ $result_set_adaptor->fetch_all_by_name('AG04449_5mC_ENCODE_Uw') };
+#fetch ResultSet by name
+my ($rset) = @{ $result_set_adaptor->fetch_all_by_name('AG04449_5mC_ENCODE_Uw') };
 
+#pick a genomic region of interest
 my $slice = $slice_adaptor->fetch_by_region( 'chromosome', 1, 1, 3011550 );
-my $constraints = {
-    min_read_depth  => 15,
-    context         => 'CG',
-    min_methylation => '25',
-    max_methylation => '75.5',
-};
-my $dna_meth_features =
-  $dmf_adaptor->fetch_all_by_Slice_ResultSet( $slice, $rset, $constraints );
 
+#Optionally define some contraints
+my $constraints = 
+  {
+   min_read_depth  => 15,
+   context         => 'CG',
+   min_methylation => 25,
+   max_methylation => '75.5',
+  };
+
+#Fetch the features
+my $dna_meth_features = $dmf_adaptor->fetch_all_by_Slice_ResultSet( $slice, $rset, $constraints );
+
+#Print some information
 foreach my $dmf ( @{$dna_meth_features} ) {
     print "\n-------------------------------------\n";
     print "Display label:\t" . $dmf->display_label . "\n";
