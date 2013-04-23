30c8b1a5b9da40fcd3ca80e20e407cbc58723027
Nathan Johnson <njohnson@ebi.ac.uk>
2013-04-23 09:26:51 +0000

    removed usage of add_supporting_sets in favour of passing directly to new, deprecated store_updated_sets

--- modules/Bio/EnsEMBL/Funcgen/DBSQL/DataSetAdaptor.pm
+++ modules/Bio/EnsEMBL/Funcgen/DBSQL/DataSetAdaptor.pm
@@ -40,82 +40,18 @@ DataSet objects.
 
 =cut
 
+package Bio::EnsEMBL::Funcgen::DBSQL::DataSetAdaptor;
+
 use strict;
 use warnings;
-
-package Bio::EnsEMBL::Funcgen::DBSQL::DataSetAdaptor;
 
 use Bio::EnsEMBL::Utils::Exception qw( throw warning deprecate );
 use Bio::EnsEMBL::Funcgen::DataSet;
 use Bio::EnsEMBL::Funcgen::DBSQL::BaseAdaptor;
 
 use vars qw(@ISA);
-use strict;
-use warnings;
+@ISA = qw(Bio::EnsEMBL::Funcgen::DBSQL::BaseAdaptor);
 
-@ISA = qw(Bio::EnsEMBL::Funcgen::DBSQL::BaseAdaptor); #do we need this?
-#we can't pass the slice through the BaseAdaptor
-#So we either don't use it, or use the slice on all the DataSet calls?
-
-
-
-#Generates DataSet contains info about DataSet content
-#do we need to accomodate different classes of data or multiple feature types in one set?  i.e. A combi experiment (Promot + Histone mod)?
-#schema can handle this...API? ignore for now but be mindful. 
-#This is subtley different to handling different experiments with different features in the same DataSet.  
-#Combi will have same sample.
-#See DataSet for definitions of set types
-
-
-#This needs one call to return all displayable sets, grouped by cell_line and ordered by FeatureType
-#needs to be restricted to cell line, feature type, but these fields have to be disparate from result_feature 
-#as this is only a simple linker table, and connections may not always be present
-#so cell tpye and feature type constraints have to be performed on load, then can assume that associated features and results
-# have same cell type/feature
-#so we need to group by cell_type in sql and then order by feature_type_id in sql or rearrange in code?
-#This will not know about chip sets, just that a feature set is linked to various result sets
-#There fore we need to use the chip_set_id or link back to the experimental_chip chip_set_ids
-#this would require a self join on experimental_chip
-
-
-
-#what other methods do we need? all with displayable option
-#fetch by Experiment_Slice?
-#fetch by FeatureType_Slice?
-#fetch by CellType_Slice?
-#set_chips (duplicates)?  We are now using result_set_id as the chip_set key, so if we didn't know the sets previosuly, then we would have to alter the result_set_id retrospectively i.e. change the result_set_id.  This would also require a check on result_feature to see if any feature_sets had been associated, and cleaning up of duplicate result_feature entries if the same feature were attached to both of  the previously separate result sets.
-#this may require an on duplicate key call....delete one.
-
-#wouldn't it be better to associate the chip set info with the ec's rather than the result set?
-
-#how are we going to accomodate a combi exp?  Promot + Histone mods?
-#These would lose their exp set association, i.e. same exp & sample different exp method
-#we're getting close to defining the regulon here, combined results features from the same exp
-#presently want them displayed as a group but ordered appropriately
-#was previously treating each feature as a separate result set
-
-
-#for storing/making link we don't need the Slice context
-#store should check all 
-#so do we move the slice context to the object methods or make optional
-#then object method can check for slice and throw or take a Slice as an optional argument
-#this will enable generic set to be created to allow loading and linking of features to results
-#we still need to know which feature arose from which chip!!!!  Not easy to do and may span two.
-#Need to genericise this to the chip_set(or use result_set_id non unique)
-#We need to disentangle setting the feature to chip/set problem from the displayable problem.
-#change the way StatusAdaptor works to accomodate result_set_id:table_name:table_id, as this will define unique results
-#
-
-#can we extend this to creating skeleton result sets and loading raw results too?
-#
-
-#Result.pm should be lightweight by default to enable fast web display, do we need oligo_probe_id?
-
-
-#how are we going to overcome unlinked but displayable sets?
-#incomplete result_feature records will be hack to update/alter?
-#could have attach_result to feature method?
-#force association when loading features
 
 =head2 fetch_by_name
 
@@ -135,7 +71,7 @@ sub fetch_by_name {
   
   throw("Must provide a name argument") if (! defined $name);
   
-  my $sql = "ds.name='".$name."'";
+  my $sql = "ds.name='${name}'";
   
   if ($status) {
     my $constraint = $self->status_to_constraint($status) if $status;
@@ -145,6 +81,7 @@ sub fetch_by_name {
   return (defined $sql) ? $self->generic_fetch($sql)->[0] : [];
   
 }
+
 
 =head2 fetch_all_by_supporting_set_type
 
@@ -175,44 +112,7 @@ sub fetch_all_by_supporting_set_type {
   
 }
 
-=head2 fetch_all_by_product_FeatureSet_type
-
-  Arg [1]    : string - product feature set type for this data_set e.g. 'annotated', 'regulatory'
-  Arg [2]    : (optional) string - status e.g. 'DISPLAYABLE'
-  Example    : my $dsets = $dset_adaptor->fetch_all_by_product_FeatureSet_type('regulatory');
-  Description: Fetch all DataSets of a given product feature set type
-  Returntype : ARRAYREF of Bio::EnsEMBL::Funcgen::DataSet objects
-  Exceptions : Throws if no product feaure set type passed
-  Caller     : General
-  Status     : At Risk - not yet implmented
-
-=cut
-
-sub fetch_all_by_product_FeatureSet_type {
-  my ($self, $type, $status) = @_;
-  
-  throw('Not yet implemented');
-  #left join? same for result_set?
-  #Or do we do a sneaky workaround and use the FeatureSet adaptor here to get the feature sets we require
-  #and then call self?
-  #or do we just use feature_sets for reg feats and forget about the possiblity of displaying the suppoorting sets?
-  #will we ever want to display supporting sets in contig/neighbourhood view?
-  #or will we only use expanded view in a feature context, hence we can just use the Regulatory Attribs?
-  #don't really want to expand regulatory feats on contig view, so access FeatureSet directly
-
-  throw("Must provide a supporting_set type argument") if (! defined $type);
-  
-  my $sql = "ss.type='".$type."'";
-  
-  if ($status) {
-    my $constraint = $self->status_to_constraint($status) if $status;
-    $sql = (defined $constraint) ? $sql." ".$constraint : undef;
-  }
-
-  return (defined $sql) ? $self->generic_fetch($sql) : [];
-  
-}
-
+#add fetch_all_by_product_FeatureSet_type/FeatureType?
 
 =head2 fetch_by_product_FeatureSet
 
@@ -248,10 +148,6 @@ sub fetch_all_by_FeatureSet {
 
 =cut
 
-
-#This is main FeatureSet, i.e. the result of the analysis of the supporting_sets
-#Supporting sets could also be FeatureSets!!!  Confusion!
-
 sub fetch_by_product_FeatureSet {
   my $self = shift;
   my $fset = shift;
@@ -266,41 +162,6 @@ sub fetch_by_product_FeatureSet {
 
   return $self->generic_fetch($sql)->[0];	
 }
-
-
-=head2 fetch_all_by_ResultSet
-
-  Arg [1]    : Bio::EnsEMBL::Funcgen::ResultSet
-  Example    : my @dsets = $fs_adaptopr->fetch_all_by_ResultSet($rset);
-  Description: Retrieves DataSet objects from the database based on the ResultSet.
-  Returntype : ARRAYREF of Bio::EnsEMBL::Funcgen::DataSet objects
-  Exceptions : Throws if arg is not a valid ResultSet
-  Caller     : General
-  Status     : At Risk - to be removed
-
-=cut
-
-sub fetch_all_by_ResultSet {
-  my $self = shift;
-  my $rset = shift;
-
-  deprecate('Use fetch_all_by_supporting_set');
-
-  return $self->fetch_all_by_supporting_set($rset);
-
-
-  #if(! ($rset && $rset->isa("Bio::EnsEMBL::Funcgen::ResultSet") && $rset->dbID())){
-  #  throw("Must provide a valid stored Bio::EnsEMBL::Funcgen::ResultSet object");
-  #}
-	
-
-  ##self join here to make sure we get all linked result_sets
-  #my $sql = 'ds.data_set_id IN (SELECT ds.data_set_id from data_set ds where result_set_id='.$rset->dbID().')';
-
-
-  #return $self->generic_fetch($sql);	
-}
-
 
 
 =head2 fetch_all_by_supporting_set
@@ -333,9 +194,6 @@ sub fetch_all_by_supporting_set {
 	
   return $self->generic_fetch($sql);	
 }
-
-
-
 
 
 =head2 fetch_all_by_feature_type_class
@@ -377,6 +235,7 @@ sub fetch_all_by_feature_type_class {
   return \@dsets;	
 }
 
+
 =head2 fetch_all_displayable_by_feature_type_class
 
   Arg [1]    : string - class of associated FeatureSet FeatureType
@@ -391,7 +250,6 @@ sub fetch_all_by_feature_type_class {
 
 sub fetch_all_displayable_by_feature_type_class {
   my ($self, $class) = @_;
-  
   return $self->fetch_all_by_feature_type_class($class, 'DISPLAYABLE');	
 }
 
@@ -410,8 +268,6 @@ sub fetch_all_displayable_by_feature_type_class {
 =cut
 
 sub _tables {
-  my $self = shift;
-  
   return (
 		  [ 'data_set',    'ds' ],
 		  [ 'supporting_set', 'ss'],
@@ -432,11 +288,6 @@ sub _tables {
 =cut
 
 sub _columns {
-  #my $self = shift;
-
-  #will this work? May have multiple record/result_set_id
-  
-  
   return qw(
 			ds.data_set_id	    ds.feature_set_id
 			ds.name             ss.type
@@ -460,34 +311,9 @@ sub _columns {
 =cut
 
 sub _left_join {
-  my $self = shift;
-	
+  my $self = shift;	
   return (['supporting_set', 'ds.data_set_id = ss.data_set_id']);
 }
-
-
-#=head2 _final_clause
-#
-#  Args       : None
-#  Example    : None
-#  Description: PROTECTED implementation of superclass abstract method.
-#               Returns an ORDER BY clause. Sorting by oligo_feature_id would be
-#			   enough to eliminate duplicates, but sorting by location might
-#			   make fetching features on a slice faster.
-#  Returntype : String
-#  Exceptions : None
-#  Caller     : generic_fetch
-#  Status     : At Risk
-#
-#=cut
-
-
-#this should be another left join? on feature_set and a join on feature_type so we can sort lexically on class, name
-#should we implement a default sort in the data_set adaptor which could be superceeded by a custom list?
-
-#sub _final_clause {
-#	return ' ORDER BY fs.feature_type_id, fs.cell_type_id'; #group on cell_type_id
-#}
 
 
 =head2 _objs_from_sth
@@ -498,47 +324,42 @@ sub _left_join {
                Creates Array objects from an executed DBI statement
 			   handle.
   Returntype : ARRAYREF of Bio::EnsEMBL::Funcgen::DataSet objects
-  Exceptions : None
+  Exceptions : Throws if supporting set cannot be fetched
   Caller     : Internal
-  Status     : At Risk
+  Status     : Stable
 
 =cut
 
 sub _objs_from_sth {
   my ($self, $sth) = @_;
-  
   my (@data_sets, @supporting_sets, $data_set, $dbID, $set_id);
-  my ($fset_id, $fset, $set, $name, $ss_type, $ss_id);
+  my ($fset_id, $fset, $set, $name, $ss_type, $ss_id, %params);
   
   my %set_adaptors = (
 					  feature      => $self->db->get_FeatureSetAdaptor(),
 					  result       => $self->db->get_ResultSetAdaptor(),
 					  input        => $self->db->get_InputSetAdaptor(),
 					 );
-
   $sth->bind_columns(\$dbID, \$fset_id, \$name, \$ss_type, \$ss_id);
-  
+   
   while ( $sth->fetch() ) {
 	
-    if ((! $data_set) || ($data_set->dbID() != $dbID)) {
+    if ((! %params) || ($params{-DBID} != $dbID)) {
 
-	  if ($data_set) {
-		$data_set->add_supporting_sets(\@supporting_sets);
-		push @data_sets, $data_set;
+	  if (%params) {
+		push @data_sets, Bio::EnsEMBL::Funcgen::DataSet->new
+		                   (%params, '-SUPPORTING_SETS', \@supporting_sets);
 		#do not set to empty array as this will cause failure of check in DataSet->new
 		undef @supporting_sets;
 	  }
 
-	  #handle absent sets, dbIDs of 0
-      $fset = ($fset_id) ? $set_adaptors{'feature'}->fetch_by_dbID($fset_id) : undef;
-
-      $data_set = Bio::EnsEMBL::Funcgen::DataSet->new(
-													  -DBID                => $dbID,
-													  -NAME                => $name,
-													  -FEATURE_SET         => $fset,
-													  -ADAPTOR             => $self,
-													 );
-
+	  %params = 
+	   (
+	    -DBID                => $dbID,
+		-NAME                => $name,
+		-FEATURE_SET         => $set_adaptors{'feature'}->fetch_by_dbID($fset_id),
+	    -ADAPTOR             => $self
+       );
 	}
 	
 	if ($ss_id) {	  
@@ -552,39 +373,20 @@ sub _objs_from_sth {
 	}
   }
 
-  
   #handle last set
-  if ($data_set) {
-	#	warn "ssets are @supporting_sets";
-	$data_set->add_supporting_sets(\@supporting_sets);
-	push @data_sets, $data_set;
+  if (%params) {
+	push @data_sets, Bio::EnsEMBL::Funcgen::DataSet->new
+		                   (%params, '-SUPPORTING_SETS', \@supporting_sets);
   }
 
-
-	#As we're not (quite) constraining how DataSets are associated
-	#it's valid to have a combined exp, i.e. Promoter plus Histone features and results?
-	#It can be valid to have several feature_types in the same set?
-	#This is entirely dependent on sensible experimental design and how we want to view the data.
-	#We could then have multiple cell types too? Getting too many dimensions to display sensibly within e!  
-	#Logically we need one constant to key on, cell_type, feature_type, but also allow support combined info 
-	#i.e. all Histone mods for one cell_type, but also have promoter data too?
-	#This is getting close to a 'regulon', as we're incorporating all sorts of supporting data
-	#There should be an order of display for and within each feature class 
-	#(or if we have same feature across several cell types then we order alphabetically?)
-	#Other possible variables to order on:  
-	#analysis_id, this would also separate the features as we can't base a feature on mutliple analyses of the same data
-	#So we either accomodate everything, where the only contraint is that we have one constant in the set
-	#Or we restrict the Set to handle just one feature_set and it's supporting result_sets
-	#Start simple, let's just take the one feature/data set problem first
- 
-	return \@data_sets;
-  }
+  return \@data_sets;
+}
 
 
 =head2 store
 
   Args       : List of Bio::EnsEMBL::Funcgen::DataSet objects
-  Example    : $dsa->store(@dsets);
+  Example    : my @dsets = @{$dsa->store(@dsets)};
   Description: Stores given DataSet objects in the database. Sets dbID and adaptor 
                on the objects that it stores.
   Returntype : ARRAYREF of stored DataSet objects
@@ -595,57 +397,50 @@ sub _objs_from_sth {
   Status     : At Risk
 
 =cut
+  
+sub store{
+  my ($self, @dsets) = @_;
+  throw('Must pass a list of DataSet objects to store') if(! @dsets || $#dsets < 0);
 
-  sub store{
-	my ($self, @dsets) = @_;
-
-	throw('Must pass a list of DataSet objects to store') if(! @dsets || $#dsets < 0);
-
-
-	my $sth = $self->prepare("INSERT INTO data_set (feature_set_id, name) 
-                            VALUES (?, ?)");
-	my $sth2 = $self->prepare("INSERT INTO supporting_set (data_set_id, supporting_set_id, type) 
-                            VALUES (?, ?, ?)");
-
-	my ($fset_id);
-
-	my $db = $self->db();
-
-
-	foreach my $dset (@dsets) {
-
-	  throw('Must pass a DataSet object to store') if( ! ( ref $dset && 
-														   $dset->isa('Bio::EnsEMBL::Funcgen::DataSet')));
-
-	  if ( $dset->is_stored($db) ) {
-		throw('DataSet [' . $dset->dbID() . '] is already stored in the database,'.
-			  'use store_updated_sets method to add new supporting sets in this DataSet');
-	  }
-		
-	  $fset_id = (defined $dset->product_FeatureSet()) ? $dset->product_FeatureSet->dbID() : 0;
-	
-	  $sth->bind_param(1, $fset_id,                     SQL_INTEGER);
-	  $sth->bind_param(2, $dset->name(),                SQL_VARCHAR);
-	  $sth->execute();
-	  $dset->dbID( $sth->{'mysql_insertid'} );
-	  $dset->adaptor($self);
-	
-
+  my $sth = $self->prepare("INSERT INTO data_set (feature_set_id, name) VALUES (?, ?)");
+  my $sth2 = $self->prepare
+    ("INSERT INTO supporting_set (data_set_id, supporting_set_id, type) VALUES (?, ?, ?)");
+  my ($fset_id);
+  my $db = $self->db();
     
-	  foreach my $sset (@{$dset->get_supporting_sets()}) {
-		
-		throw("All supporting Feature and ResultSets must be stored previously.".
-			  " Use store_updated_sets method if your DataSet has been stored") if(! $sset->is_stored($db));
-
-		$sth2->bind_param(1, $dset->dbID(),                SQL_INTEGER);
-		$sth2->bind_param(2, $sset->dbID(),                SQL_INTEGER);
-		$sth2->bind_param(3, $sset->set_type(),            SQL_VARCHAR); #enum feature/result/experimental
-		$sth2->execute();
-	  }
-	}
+  foreach my $dset (@dsets) {
       
-	return \@dsets
+    if( ! (defined $dset && 
+          (ref($dset) eq 'Bio::EnsEMBL::Funcgen::DataSet'))){
+      throw('Must pass a DataSet object to store') 
+    }
+    
+    if ( $dset->is_stored($db) ) {
+      throw('DataSet [' . $dset->dbID() . '] is already stored in the database');
+    }
+		
+    $sth->bind_param(1, $dset->product_FeatureSet->dbID, SQL_INTEGER);
+    $sth->bind_param(2, $dset->name(),                   SQL_VARCHAR);
+    $sth->execute();
+    $dset->dbID( $sth->{'mysql_insertid'} );
+    $dset->adaptor($self);
+	 
+    foreach my $sset (@{$dset->get_supporting_sets()}) {
+
+      if(! $sset->is_stored($db)){
+        #Set check already done in _set_Sets_and_types
+        throw('All supporting Feature and ResultSets must be stored previously');
+      }
+      
+      $sth2->bind_param(1, $dset->dbID(),                SQL_INTEGER);
+      $sth2->bind_param(2, $sset->dbID(),                SQL_INTEGER);
+      $sth2->bind_param(3, $sset->set_type(),            SQL_VARCHAR); #enum feature/result/experimental
+      $sth2->execute();
+    }
   }
+      
+  return \@dsets
+}
 
 
 =head2 store_updated_sets
@@ -666,8 +461,10 @@ sub _objs_from_sth {
   #This needs to cahnge to an arrayref of dset and an overwrite flag
 
 
-  sub store_updated_sets{
+sub store_updated_sets{
 	my ($self, $dsets, $overwrite) = @_;
+	
+	throw('store_updated_sets is not longer support, please rollback and recreate the DataSet');
 
 	throw('Must pass a list of DataSet objects to store') if(! @$dsets || $#{$dsets} < 0);
 	my ($sql);
