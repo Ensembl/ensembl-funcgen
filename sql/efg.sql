0655da127ee512f5f4d3f2198567baaa6a1db8c9
Thomas Juettemann <juettemann@ebi.ac.uk>
2013-05-08 18:44:41 +0000

    patches for r72

--- sql/efg.sql
+++ sql/efg.sql
@@ -1420,6 +1420,49 @@ CREATE TABLE `meta_coord` (
 
 
 /**
+@table  associated_xref
+@desc	Create table associated_xref for associating object xrefs with an associated annotation (eg Gene Ontology Annotation Extensions) given a source xref and condition.
+@colour  #000000
+
+@column associated_xref_id Primary key
+@column object_xref_id
+@column xref_id
+@column source_xref_id
+@column condition_type
+@column associated_group_id
+@column rank
+*/
+CREATE TABLE `associated_xref` (
+  `associated_xref_id`  int(10)       unsigned NOT NULL AUTO_INCREMENT,
+  `object_xref_id`      int(10)       unsigned NOT NULL DEFAULT '0',
+  `xref_id`             int(10)       unsigned NOT NULL DEFAULT '0',
+  `source_xref_id`      int(10)       unsigned          DEFAULT NULL,
+  `condition_type`      varchar(128)                    DEFAULT NULL,
+  `associated_group_id` int(10)       unsigned          DEFAULT NULL,
+  `rank` int(10)                      unsigned          DEFAULT '0',
+  PRIMARY KEY (`associated_xref_id`),
+  UNIQUE KEY `object_associated_source_type_idx` (`object_xref_id`,`xref_id`,`source_xref_id`,`condition_type`,`associated_group_id`),
+  KEY `associated_source_idx` (`source_xref_id`),
+  KEY `associated_object_idx` (`object_xref_id`),
+  KEY `associated_idx`        (`xref_id`),
+  KEY `associated_group_idx`  (`associated_group_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 |
+
+/**
+@table  associated_group
+@desc	Create table associated_xref for associating object xrefs with an associated annotation (eg Gene Ontology Annotation Extensions) given a source xref and condition.
+@colour  #000000
+
+@column associated_group_id
+@column description
+*/
+
+CREATE TABLE `associated_group` (
+  `associated_group_id` int(10)      unsigned NOT NULL AUTO_INCREMENT,
+  `description`         varchar(128)          DEFAULT NULL,
+  PRIMARY KEY (`associated_group_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1
+/**
 @table identity_xref
 @desc Describes how well a particular xref object matches the EnsEMBL object.
 @colour  #000000
