535805a8033d2a4e5c24517412cded706def5907
Nathan Johnson <njohnson@ebi.ac.uk>
2013-05-22 12:23:07 +0000

    renumbered and removed features_5

--- scripts/examples/solutions/features_1.pl
+++ scripts/examples/solutions/features_1.pl
@@ -10,51 +10,97 @@ $registry->load_registry_from_db(
     -user => 'anonymous'
 );
 
-#1. External Features
-#The funcgen DB contains some data directly imported from external sources, like predicted miRNA targets from miRanda, predicted regulatory regions from cisRED, or experimentally verified regulatory regions from EnhancerDB
-#Create a script which gets all the 'cisRED motifs' features for the Human region 7:27136000-27139000.
-#List their display_labels. List all active regions from EnhancerDB ('VISTA enhancer set') for Human chromosome 7.
-#Hint - use their feature type to know which ones are active
-
+#2. Annotated Features
+#Annotated Features represents the results of an analysis of raw or processing signal data. These correspond to regions in the genome enriched for specific events (like TF binding or Histone Marks) i.e.they are 'peak calls'.
+#Compare the number of annotated features in the region Y:5000000-40000000 between the Human feature sets 'K562_DNase1_ENCODE_Duke_SWEmbl_R0025_D150' and 'HepG2_DNase1_ENCODE_Duke_SWEmbl_R0025_D150'.
+#Investigate the differences. Hint: check cell types etc.
+#Print the position, score and summit of annotated features for the Human feature set 'K562_CTCF_ENCODE_Broad_SWEmbl_R015_D150' within the region 1:100000-150000
+#Note: the summit is in the coordinates of the full chromosome.
+#Print the properties of the analysis used in the previous feature sets.
 
 #Grab the eFG adaptor
 my $sa = $registry->get_adaptor('Human', 'core', 'slice');
 my $fsa = $registry->get_adaptor('Human', 'funcgen', 'featureset');
 
-my $cisred_slice = $sa->fetch_by_region('chromosome','7',27136000,27139000);
-my $cisred_fset = $fsa->fetch_by_name('cisRED motifs');
-my @cisred_afs = @{$cisred_fset->get_Features_by_Slice($cisred_slice)};
-print "Found ".scalar(@cisred_afs)." cisRED features\n";
+# You can get it by the name
+#Carefull: some parts of the Y chomosome are PARs i.e. same as the X...
+my $slice = $sa->fetch_by_region('chromosome','Y',5000000,40000000);
 
-foreach my $af (@cisred_afs){
-	print $af->display_label."\n";
+# Compare the number of annotated features in the region Y:5000000-40000000 between the Human feature sets 'K562_DNase1_ENCODE_Duke_SWEmbl_R0025_D150' and 'HepG2_DNase1_ENCODE_Duke_SWEmbl_R0025_D150'.Investigate the differences (hint: check cell types).
+my $K562_DNAse_fset = $fsa->fetch_by_name('K562_DNase1_ENCODE_Duke_SWEmbl_R0025_D150');
+my @K562_DNAse_afs = @{$K562_DNAse_fset->get_Features_by_Slice($slice)};
+
+print "K562 contains ".scalar(@K562_DNAse_afs)." features\n";
+print $K562_DNAse_fset->cell_type->name." has gender ".$K562_DNAse_fset->cell_type->gender."\n";
+
+my $HepG2_DNAse_fset = $fsa->fetch_by_name('HepG2_DNase1_ENCODE_Duke_SWEmbl_R0025_D150');
+my @HepG2_DNAse_afs = @{$HepG2_DNAse_fset->get_Features_by_Slice($slice)};
+
+print "HepG2 contains ".scalar(@HepG2_DNAse_afs)." features\n";
+print $HepG2_DNAse_fset->cell_type->name." has gender ".$HepG2_DNAse_fset->cell_type->gender."\n";
+
+# Print the position, score and summit of annotated features for feature set 'K562_CTCF_ENCODE_Broad_SWEmbl_R015_D150' within the region 1:100000-150000
+print "Features for K562_CTCF_ENCODE_Broad_SWEmbl_R015_D150\n";
+my $K562_CTCF_fset = $fsa->fetch_by_name('K562_CTCF_ENCODE_Broad_SWEmbl_R015_D150');
+
+$slice = $sa->fetch_by_region('chromosome','1',100000,150000);
+my @K562_CTCF_afs = @{$K562_CTCF_fset->get_Features_by_Slice($slice)};
+
+foreach my $af (@K562_CTCF_afs){
+	print "Start: ".$af->seq_region_start." End: ".$af->seq_region_end." Score: ".$af->score." Summit: ".$af->summit."\n";
 }
+print "\n";
 
 
-my $vista_fset = $fsa->fetch_by_name('VISTA enhancer set');
-my $vista_slice = $sa->fetch_by_region('chromosome','7');
-my @vista_afs = @{$vista_fset->get_Features_by_Slice($vista_slice)};
 
-print "Found ".scalar(@vista_afs)." Vista features\n";
+# (optional) Print the properties of the analysis used in the previous feature sets
+print "K562 DNAse\n";
+print_analysis($K562_DNAse_fset->analysis);
+print "\n";
 
-foreach my $af (@vista_afs){
-	if($af->feature_type->name =~ /Enhancer/){ print $af->display_label."\n"; }
+print "HepG2 DNAse\n";
+print_analysis($HepG2_DNAse_fset->analysis);
+print "\n";
+
+print "K562 CTCF\n";
+print_analysis($K562_CTCF_fset->analysis);
+print "\n";
+
+sub print_analysis {
+	my $an = shift;
+	print "Logic Name: ".$an->logic_name."\n";
+	print "Display Label: ".$an->display_label."\n";
+	print "Program: ".$an->program."\t".$an->parameters."\n";
 }
 
 __END__
 
->perl features_1.pl
 
-Found 5 cisRED features
-craHsap152694
-craHsap152679
-craHsap152667
-craHsap152648
-craHsap152626
+>perl features_2.pl
 
-Found 5 Vista features
-hs169
-hs170
-hs79
-hs174
-hs183
+K562 contains 0 features
+K562 has gender female
+HepG2 contains 54 features
+HepG2 has gender male
+
+Features for K562_CTCF_ENCODE_Broad_SWEmbl_R015_D150
+Start: 104785 End: 105286 Score: 73.603595 Summit: 105023
+Start: 134367 End: 134792 Score: 26.516135 Summit: 134592
+Start: 137299 End: 137689 Score: 57.620595 Summit: 137493
+Start: 138722 End: 139482 Score: 207.230595 Summit: 139034
+
+K562 DNAse
+Logic Name: SWEmbl_R0025_D150
+Display Label: SWEmbl
+Program: SWEmbl -f 150 -R 0.0025 -d 150
+
+HepG2 DNAse
+Logic Name: SWEmbl_R0025_D150
+Display Label: SWEmbl
+Program: SWEmbl -f 150 -R 0.0025 -d 150
+
+K562 CTCF
+Logic Name: SWEmbl_R015_D150
+Display Label: SWEmbl
+Program: SWEmbl -f 150 -R 0.015 -d 150
+
--- scripts/examples/solutions/features_3.pl
+++ scripts/examples/solutions/features_3.pl
@@ -12,54 +12,65 @@ $registry->load_registry_from_db(
 
 my $rfa = $registry->get_adaptor('Human', 'funcgen', 'regulatoryfeature');
 
-#3. Motif Features
-#Motif features represent putative binding sites based on alignments of PWMs from JASPAR. MotifFeatures are always associated to AnnotatedFeatures representing Transcription Factor (TF) Binding. More information about how we integrate these into the regulatory build process can be found here.
-#Get the 'motif' regulatory attributes associated to the Human Regulatory Feature 'ENSR00001227187'. Print their properties.
-#Hint: use 'motif' as a parameter for regulatory_attributes.
-#Print the properties of the annotated features associated to the motif feature.
+#4. Binding Matrices and motif strength
+#Each MotifFeature is associated with a PWM, which are represented by the 'BindingMatrix' class. The MotifFeature score represents the relative binding affinity with respect to the PWM defined in the BindingMatrix.
+#Using the Motif feature obtained in exercise 3, get the associated Binding Matrix and print some details.
+#Check potential effect of changes in the sequence of the motif feature on the relative strength of that motif feature.
+#Check the GERP conservation scores along the motif. Compare with the JASPAR matrix.
 
 my $rf = $rfa->fetch_by_stable_id('ENSR00001227187');
+my $mf = $rf->regulatory_attributes('motif')->[0];
+my $slice = $mf->feature_Slice;
+my $seq = $mf->seq;
 
-foreach my $mf ( @{$rf->regulatory_attributes('motif')}){
-	print "Display Label: ".$mf->display_label.";";
-	print " Position: ".$mf->seq_region_name.":".$mf->start."-".$mf->end.";";
-	print " Score: ".$mf->score.";";
-	print "\n";
-	
-	# Print the properties of the annotated features associated to each motif feature.
-	# Print the name of the feature set associated to these annotated features.
-	foreach my $af (@{$mf->associated_annotated_features()}){
-		print "\tAssociated Feature Set: ".$af->feature_set->name."\n";
-		print_feature($af);		
-	}
-	
-};
+my $bm = $mf->binding_matrix;
+print "Relative affinity of the ".$bm->name." motif\t$seq :".$bm->relative_affinity($seq)."\n";
+print "Relative affinity of changed motif\tTGGTGTGCAGAGGGCATGA :".$bm->relative_affinity("TGGTGTGCAGAGGGCATGA")."\n";
+print "                                            ^\n";
+print "Relative affinity of changed motif\tTGGTCTGCGGAGGGCATGA :".$bm->relative_affinity("TGGTCTGCGGAGGGCATGA")."\n";
+print "                                                ^\n";
 
-sub print_feature {
-	my $af = shift;
-	print "\tCell: ".(defined($af->cell_type) ? $af->cell_type->name : "Undetermined").";"; 
-	print " Feature Type: ".$af->feature_type->name.";";
-	print " Position: ".$af->seq_region_name.":".$af->start."-".$af->end.";";
-	print " Score: ".$af->score.";";
-	print " Summit: ".$af->summit.";";
-	print "\n";	
-}
+
+
+
+#http://jaspar.genereg.net/cgi-bin/jaspar_db.pl?ID=MA0139.1&rm=present&collection=CORE
+
+
+#get method_link_species_set object for gerp conservation scores for mammals
+my $mlss_adaptor = $registry->get_adaptor("Multi", "compara", "MethodLinkSpeciesSet");
+my $mlss = $mlss_adaptor->fetch_by_method_link_type_species_set_name("GERP_CONSERVATION_SCORE", "mammals");
+
+#Get cons scores for given slice
+my $display_size = $slice->end - $slice->start + 1; 
+my $cs_adaptor = $registry->get_adaptor("Multi", 'compara', 'ConservationScore');
+my $scores = $cs_adaptor->fetch_all_by_MethodLinkSpeciesSet_Slice($mlss, $slice, $display_size);
+print join("\t",split('',$seq))."\n";
+
+
+map { printf("%.3f\t", $_->diff_score); } @$scores;
+print "\n";
+print "                                ^\n";
+
+print $bm->frequencies;
+print "                  ^\n";
+print "                                  ^\n";
 
 __END__
 
 
->perl features_3.pl
+>perl features_4.pl
 
-Display Label: CTCF:MA0139.1; Position: 6:132128805-132128823; Score: 0.863;
-        Associated Feature Set: HUVEC_CTCF_ENCODE_Uw_SWEMBL_R015
-        Cell: HUVEC; Feature Type: CTCF; Position: 6:132128612-132128993; Score: 22.126581; Summit: 132128815;
-        Associated Feature Set: H1ESC_CTCF_ENCODE_Uta_SWEMBL_R015
-        Cell: H1ESC; Feature Type: CTCF; Position: 6:132128668-132129004; Score: 50.586062; Summit: 132128831;
-        Associated Feature Set: HepG2_CTCF_ENCODE_Uta_SWEmbl_R015_D150
-        Cell: HepG2; Feature Type: CTCF; Position: 6:132128676-132128987; Score: 27.973154; Summit: 132128824;
-        Associated Feature Set: HepG2_CTCF_ENCODE_Uw_SWEmbl_R015_D150
-        Cell: HepG2; Feature Type: CTCF; Position: 6:132128678-132129034; Score: 82.49168; Summit: 132128828;
-
-Display Label: Nrsf:MA0138.1; Position: 6:132129499-132129517; Score: 0.805;
-        Associated Feature Set: H1ESC_Nrsf_ENCODE_Hudsonalpha_SWEMBL_R015
-        Cell: H1ESC; Feature Type: Nrsf; Position: 6:132129342-132129713; Score: 103.256328; Summit: 132129519;
+Relative affinity of the MA0139.1 motif TGGTCTGCAGAGGGCATGA :0.863462959414993
+Relative affinity of changed motif      TGGTGTGCAGAGGGCATGA :0.747783950261183
+                                            ^
+Relative affinity of changed motif      TGGTCTGCGGAGGGCATGA :0.821835750551094
+                                                ^
+T       G       G       T       C       T       G       C       A       G       A       G       G       G       C       A       T       G       A
+2.650   2.650   2.650   1.520   2.650   1.450   1.970   2.850   0.292   0.950   1.380   2.850   0.940   2.850   2.850   1.670   2.850   3.020   3.110
+                                ^
+A  [ 87 167 281  56   8 744  40 107 851   5 333  54  12  56 104 372  82 117 402 ]
+C  [291 145  49 800 903  13 528 433  11   0   3  12   0   8 733  13 482 322 181 ]
+G  [ 76 414 449  21   0  65 334  48  32 903 566 504 890 775   5 507 307  73 266 ]
+T  [459 187 134  36   2  91  11 324  18   3   9 341   8  71  67  17  37 396  59 ]
+                  ^
+                                  ^
