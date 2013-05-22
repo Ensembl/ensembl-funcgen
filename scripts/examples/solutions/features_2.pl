720d2e705d2b6d4f7304895451a675c9bb27f75c
Nathan Johnson <njohnson@ebi.ac.uk>
2013-05-22 13:13:27 +0000

    bulk changes from v71 later checkins

--- scripts/examples/solutions/features_2.pl
+++ scripts/examples/solutions/features_2.pl
@@ -10,97 +10,56 @@ $registry->load_registry_from_db(
     -user => 'anonymous'
 );
 
-#2. Annotated Features
-#Annotated Features represents the results of an analysis of raw or processing signal data. These correspond to regions in the genome enriched for specific events (like TF binding or Histone Marks) i.e.they are 'peak calls'.
-#Compare the number of annotated features in the region Y:5000000-40000000 between the Human feature sets 'K562_DNase1_ENCODE_Duke_SWEmbl_R0025_D150' and 'HepG2_DNase1_ENCODE_Duke_SWEmbl_R0025_D150'.
-#Investigate the differences. Hint: check cell types etc.
-#Print the position, score and summit of annotated features for the Human feature set 'K562_CTCF_ENCODE_Broad_SWEmbl_R015_D150' within the region 1:100000-150000
-#Note: the summit is in the coordinates of the full chromosome.
-#Print the properties of the analysis used in the previous feature sets.
+my $rfa = $registry->get_adaptor('Human', 'funcgen', 'regulatoryfeature');
 
-#Grab the eFG adaptor
-my $sa = $registry->get_adaptor('Human', 'core', 'slice');
-my $fsa = $registry->get_adaptor('Human', 'funcgen', 'featureset');
+#3. Motif Features
+#Motif features represent putative binding sites based on alignments of PWMs from JASPAR. MotifFeatures are always associated to AnnotatedFeatures representing Transcription Factor (TF) Binding. More information about how we integrate these into the regulatory build process can be found here.
+#Get the 'motif' regulatory attributes associated to the Human Regulatory Feature 'ENSR00001227187'. Print their properties.
+#Hint: use 'motif' as a parameter for regulatory_attributes.
+#Print the properties of the annotated features associated to the motif feature.
 
-# You can get it by the name
-#Carefull: some parts of the Y chomosome are PARs i.e. same as the X...
-my $slice = $sa->fetch_by_region('chromosome','Y',5000000,40000000);
+my $rf = $rfa->fetch_by_stable_id('ENSR00001227187');
 
-# Compare the number of annotated features in the region Y:5000000-40000000 between the Human feature sets 'K562_DNase1_ENCODE_Duke_SWEmbl_R0025_D150' and 'HepG2_DNase1_ENCODE_Duke_SWEmbl_R0025_D150'.Investigate the differences (hint: check cell types).
-my $K562_DNAse_fset = $fsa->fetch_by_name('K562_DNase1_ENCODE_Duke_SWEmbl_R0025_D150');
-my @K562_DNAse_afs = @{$K562_DNAse_fset->get_Features_by_Slice($slice)};
+foreach my $mf ( @{$rf->regulatory_attributes('motif')}){
+	print "Display Label: ".$mf->display_label.";";
+	print " Position: ".$mf->seq_region_name.":".$mf->start."-".$mf->end.";";
+	print " Score: ".$mf->score.";";
+	print "\n";
+	
+	# Print the properties of the annotated features associated to each motif feature.
+	# Print the name of the feature set associated to these annotated features.
+	foreach my $af (@{$mf->associated_annotated_features()}){
+		print "\tAssociated Feature Set: ".$af->feature_set->name."\n";
+		print_feature($af);		
+	}
+	
+};
 
-print "K562 contains ".scalar(@K562_DNAse_afs)." features\n";
-print $K562_DNAse_fset->cell_type->name." has gender ".$K562_DNAse_fset->cell_type->gender."\n";
-
-my $HepG2_DNAse_fset = $fsa->fetch_by_name('HepG2_DNase1_ENCODE_Duke_SWEmbl_R0025_D150');
-my @HepG2_DNAse_afs = @{$HepG2_DNAse_fset->get_Features_by_Slice($slice)};
-
-print "HepG2 contains ".scalar(@HepG2_DNAse_afs)." features\n";
-print $HepG2_DNAse_fset->cell_type->name." has gender ".$HepG2_DNAse_fset->cell_type->gender."\n";
-
-# Print the position, score and summit of annotated features for feature set 'K562_CTCF_ENCODE_Broad_SWEmbl_R015_D150' within the region 1:100000-150000
-print "Features for K562_CTCF_ENCODE_Broad_SWEmbl_R015_D150\n";
-my $K562_CTCF_fset = $fsa->fetch_by_name('K562_CTCF_ENCODE_Broad_SWEmbl_R015_D150');
-
-$slice = $sa->fetch_by_region('chromosome','1',100000,150000);
-my @K562_CTCF_afs = @{$K562_CTCF_fset->get_Features_by_Slice($slice)};
-
-foreach my $af (@K562_CTCF_afs){
-	print "Start: ".$af->seq_region_start." End: ".$af->seq_region_end." Score: ".$af->score." Summit: ".$af->summit."\n";
-}
-print "\n";
-
-
-
-# (optional) Print the properties of the analysis used in the previous feature sets
-print "K562 DNAse\n";
-print_analysis($K562_DNAse_fset->analysis);
-print "\n";
-
-print "HepG2 DNAse\n";
-print_analysis($HepG2_DNAse_fset->analysis);
-print "\n";
-
-print "K562 CTCF\n";
-print_analysis($K562_CTCF_fset->analysis);
-print "\n";
-
-sub print_analysis {
-	my $an = shift;
-	print "Logic Name: ".$an->logic_name."\n";
-	print "Display Label: ".$an->display_label."\n";
-	print "Program: ".$an->program."\t".$an->parameters."\n";
+sub print_feature {
+	my $af = shift;
+	print "\tCell: ".(defined($af->cell_type) ? $af->cell_type->name : "Undetermined").";"; 
+	print " Feature Type: ".$af->feature_type->name.";";
+	print " Position: ".$af->seq_region_name.":".$af->start."-".$af->end.";";
+	print " Score: ".$af->score.";";
+	print " Summit: ".$af->summit.";";
+	print "\n";	
 }
 
 __END__
 
 
->perl features_2.pl
+>perl features_3.pl
 
-K562 contains 0 features
-K562 has gender female
-HepG2 contains 54 features
-HepG2 has gender male
+Display Label: CTCF:MA0139.1; Position: 6:132128805-132128823; Score: 0.863;
+        Associated Feature Set: HUVEC_CTCF_ENCODE_Uw_SWEMBL_R015
+        Cell: HUVEC; Feature Type: CTCF; Position: 6:132128612-132128993; Score: 22.126581; Summit: 132128815;
+        Associated Feature Set: H1ESC_CTCF_ENCODE_Uta_SWEMBL_R015
+        Cell: H1ESC; Feature Type: CTCF; Position: 6:132128668-132129004; Score: 50.586062; Summit: 132128831;
+        Associated Feature Set: HepG2_CTCF_ENCODE_Uta_SWEmbl_R015_D150
+        Cell: HepG2; Feature Type: CTCF; Position: 6:132128676-132128987; Score: 27.973154; Summit: 132128824;
+        Associated Feature Set: HepG2_CTCF_ENCODE_Uw_SWEmbl_R015_D150
+        Cell: HepG2; Feature Type: CTCF; Position: 6:132128678-132129034; Score: 82.49168; Summit: 132128828;
 
-Features for K562_CTCF_ENCODE_Broad_SWEmbl_R015_D150
-Start: 104785 End: 105286 Score: 73.603595 Summit: 105023
-Start: 134367 End: 134792 Score: 26.516135 Summit: 134592
-Start: 137299 End: 137689 Score: 57.620595 Summit: 137493
-Start: 138722 End: 139482 Score: 207.230595 Summit: 139034
-
-K562 DNAse
-Logic Name: SWEmbl_R0025_D150
-Display Label: SWEmbl
-Program: SWEmbl -f 150 -R 0.0025 -d 150
-
-HepG2 DNAse
-Logic Name: SWEmbl_R0025_D150
-Display Label: SWEmbl
-Program: SWEmbl -f 150 -R 0.0025 -d 150
-
-K562 CTCF
-Logic Name: SWEmbl_R015_D150
-Display Label: SWEmbl
-Program: SWEmbl -f 150 -R 0.015 -d 150
-
+Display Label: Nrsf:MA0138.1; Position: 6:132129499-132129517; Score: 0.805;
+        Associated Feature Set: H1ESC_Nrsf_ENCODE_Hudsonalpha_SWEMBL_R015
+        Cell: H1ESC; Feature Type: Nrsf; Position: 6:132129342-132129713; Score: 103.256328; Summit: 132129519;
