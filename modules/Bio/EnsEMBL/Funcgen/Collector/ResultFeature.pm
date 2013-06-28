a31e6b608841c6fba1de5697ae760a8c12467e69
Nathan Johnson <njohnson@ebi.ac.uk>
2013-06-28 14:55:52 +0000

    added merge_and_index_slice_collections

--- modules/Bio/EnsEMBL/Funcgen/Collector/ResultFeature.pm
+++ modules/Bio/EnsEMBL/Funcgen/Collector/ResultFeature.pm
@@ -1,8 +1,8 @@
-# $Id: ResultFeature.pm,v 1.11 2011-02-18 14:35:41 nj1 Exp $
+# $Id: ResultFeature.pm,v 1.12 2013-06-28 14:55:52 nj1 Exp $
 
 =head1 LICENSE
 
-  Copyright (c) 1999-2011 The European Bioinformatics Institute and
+  Copyright (c) 1999-2013 The European Bioinformatics Institute and
   Genome Research Limited.  All rights reserved.
 
   This software is distributed under a modified Apache license.
@@ -38,20 +38,19 @@ use base qw(Bio::EnsEMBL::Utils::Collector Bio::EnsEMBL::DBFile::CollectionAdapt
 
 ### Global config variables
 # See associated Collector methods for more info
-
 $Bio::EnsEMBL::Utils::Collector::bin_model  = 'SIMPLE';
 
-#Default is for read coverage, array intensity config defined in set_collection_defs_by_ResultSet
-#Kept here as example of basic package config
+
+#WARNING Need to change this package var as it is persisting and growing between instances
+#within the same process! i.e. workers in the hive!
 $Bio::EnsEMBL::Utils::Collector::window_sizes = [30, 65, 130, 260, 450, 648, 950, 1296];
 
-
-#May need to drop 30 here due to RPKM float value doubling packed size
 
 # Tuning 900(Most used display size) - 172(non-drawable area) = 772 pixels/bins
 # Optimal window sizes over zoom levels
 # 1kb 5kb 10kb 50kb 100kb 200kb 500kb 1mb
 # 2   7   13   65   130   260   648   1296
+
 #Theoretical max for default 16MB max_allowed_packet_size is 30(~23kb) - sufficient min for read coverage.
 #0 will be used for array intensies until 65 is more appropriate(see adaptor)
 #450 and 950 added to handle middle ground, but are these really needed?
@@ -60,30 +59,26 @@ $Bio::EnsEMBL::Utils::Collector::window_sizes = [30, 65, 130, 260, 450, 648, 950
 #Largest size is essential so we aren't retrieving/drawing more features than pixels
 #Smaller sizes are desirable as more resolution required when zoomed in.
 
-$Bio::EnsEMBL::Utils::Collector::bin_method   = 'RPKM';#'count';#only used by collector
-#small integers(little endian)
-#RPKM is now float(native)
-$Bio::EnsEMBL::Utils::Collector::packed_size   = 4;#2;#per score
-$Bio::EnsEMBL::Utils::Collector::pack_template = 'f';#'v';#per score
+#count - v small(4) integers(little endian)
+#RPKM is now a f(loat).  Which is native, not option to force endianess with packed floats
+#So may cause problems if using a host machine with a different endian order
+$Bio::EnsEMBL::Utils::Collector::bin_method   = 'RPKM'; #only used by collector
+$Bio::EnsEMBL::Utils::Collector::packed_size   = 4;     #per score
+$Bio::EnsEMBL::Utils::Collector::pack_template = 'f';   #per score
 
 
-#Length is completely unreliable here
-#returns 2 for single v and 5 for single f?
-
-#Does appear to be 4 from read bytes
 
 ### Mandatory methods required by the base Collector
-
-
-
-
 # You must have a methods like this to perform the store
 
 sub store_window_bins_by_Slice_ResultSet {
   my ($self, $slice, $rset, %config) = @_;
 
   $self->source_set_type('result');#required by get_Feature_by_Slice   
-  $self->set_collection_defs_by_ResultSet($rset);  
+  $self->set_collection_defs_by_ResultSet($rset);
+
+  
+
   $self->set_config(%config);
 
 
@@ -99,7 +94,12 @@ sub store_window_bins_by_Slice_ResultSet {
 sub store_window_bins_by_Slice_Parser{
   my ($self, $slice, $imp, %config) = @_;
 
-
+  
+  #warn " store_window_bins_by_Slice_Parser ".$slice->name;
+  #warn "wsizes before set ".join(', ', @{$self->window_sizes});
+  #warn "wsizes before set ".join(', ', @{$Bio::EnsEMBL::Utils::Collector::window_sizes});
+  
+  
   #We need to test for new_assm if we have a reads result set and fail
   #currently don't have access to new assm for validation?
   #Can project to new_assm as this would require two passes storing initially on the 0 window level
@@ -133,32 +133,40 @@ sub store_window_bins_by_Slice_Parser{
     
   if(! $skip_zero_window){
 	#Assume we only have one set here(enforced in define_and_validate_sets)
-	my ($iset) = @{$imp->result_set->get_InputSets};
+	my ($iset) = @{$imp->result_set->get_support};
 
 	if($iset->format eq 'SEQUENCING'){
 	  $config{'-skip_zero_window'} = 1;
 	}
   }
  
-  $self->set_config(%config, (-method_config => {
-												 #RPKM method config
-												 -dnadb          => $imp->db->dnadb,
-												 -total_features => $imp->total_features,
-												 -gender         => $imp->cell_type->gender,
-												 -window_sizes   => $self->window_sizes,
-												 #pass here as Collector is currently 'readding' 0 wsize
-												}
-							 ));
+  warn "wsizes ".join(', ', @{$self->window_sizes});
+
+  $self->set_config(%config, (-method_config => 
+                              {
+                               #RPKM method config
+                               -dnadb          => $imp->db->dnadb,
+                               -total_features => $imp->total_features,
+                               -gender         => $imp->result_set->cell_type->gender,
+                               -window_sizes   => $self->window_sizes,
+                               #pass here as Collector is currently 'readding' 0 wsize
+                              }
+                             ));
 
  
   ### Check for existing dbfile dir/files
   my $rset_dir        = $self->result_set->dbfile_data_dir;
   my $dbfile_data_dir = $imp->get_dir('output');
 
+  #This is causing problems with mismatch between
+  #output dir and staging dir generated from meta dbfile_data_dir
+  #Need to use force!
+
   if((! $force ) &&
 	 $rset_dir   &&
 	 ($rset_dir ne $dbfile_data_dir)){
 	throw("ResultSet dbfile_data_dir($rset_dir) and -output_dir($dbfile_data_dir) do not match. Please rectify or specify -force to update");
+	
   }
   
   #Update/set ResultSet dbfile_data_dir
@@ -169,7 +177,7 @@ sub store_window_bins_by_Slice_Parser{
 	$self->result_set->dbfile_data_dir($dbfile_data_dir);
 	$self->result_set->adaptor->store_dbfile_data_dir($self->result_set);
   }
- 
+
   if((! defined $dbfile_data_dir) ||
 	 (! -d $dbfile_data_dir)){
 	throw('Your -dbfile_data_dir is either not set or not a valid directory');
@@ -190,14 +198,19 @@ sub store_window_bins_by_Slice_Parser{
 	  if(-e $col_file_name && $force){
 		unlink($col_file_name) or warn("Failed to remove exisiting col file:\t$col_file_name\n$!");
 		#no throw as file will be over-written anyway
+
+
+		#This was removing open file an existing fh when there are duplicate wsizes!
+		#Do we need to some sort of cache check first?
+
 	  }
 
 	  #Generate and cache filhandle here to avoid failing after data has been generated
 	  my $fh = $self->get_filehandle($col_file_name, {-file_operator => '>'});
 	  
-	  if(! defined $fh){
-		throw("Could not get_filehandle for >${col_file_name}");
-	  }
+	  #if(! defined $fh){
+	  #throw("Could not get_filehandle for >${col_file_name}");
+	  #}
 	  
 	  #Set AUTOFLUSH to enable validate_file_length in store_collection
 	  $fh->autoflush;
@@ -210,34 +223,7 @@ sub store_window_bins_by_Slice_Parser{
   return;
 }
 
-
-=head2 rollback_Features_by_Slice
-
-  Args[0]    : Bio::EnsEMBL::Slice
-  Example    : $collector->get_Feature_by_Slice($slice);
-  Description: Wrapper method to fetch input features for building the Collections
-  Returntype : ARRAYREF of Bio::EnsEMBL::Funcgen::Collection::ResultFeatures
-  Exceptions : None
-  Caller     : Bio::EnsEMBL::Utils::Collector::store_window_bins_by_Slice
-  Status     : At Risk
-
-=cut
-
-#optional method to perform feature rollback
-
-sub rollback_Features_by_Slice{
-  my ($self, $slice) = @_;
-
-  #Point to Helper here
-  #This is already done in the InputSet importer for
-  #bed/sam seq imports
-  #but not for array based imports
-  #Need to take account of wsizes
-  
-
-}
-
-
+# add rollback method here
 
 =head2 get_Features_by_Slice
 
@@ -257,6 +243,8 @@ sub rollback_Features_by_Slice{
 #we could however manually pass this when we call the method?
 #self will either be a collector or a parser
 #Just maintain this wrapper for now for simplicity
+
+#CR Put stub in Colelctor.pm
 
 sub get_Features_by_Slice{
   my ($self, $slice) = @_;
@@ -328,7 +316,8 @@ sub write_collection{
   
 
   if(defined $scores){
-	my $new_cps = $self->current_packed_size($wsize) + (scalar(@$scores)*$self->packed_size);	
+	my $new_cps = $self->current_packed_size($wsize) + 
+	                                         (scalar(@$scores) * $self->packed_size);	
 	#This loop is very similar to the _obj_from_sth loop
 	#We need to set the slice and modify the start end appropriately
 	
@@ -452,7 +441,7 @@ sub reinitialise_input{
 sub store_collection{
   my ($self, $wsize, $full_slice, $slice_start, $slice_end, $strand) =  @_;
 	  
-  warn "Storing collection $wsize, $full_slice, $slice_start, $slice_end, $strand";
+  #warn "Storing collection $wsize, $full_slice, $slice_start, $slice_end, $strand";
 
   my $sr_start = $slice_start;
   my $sr_end   = $slice_end;
@@ -534,7 +523,12 @@ sub store_collection{
 
 	my $col_file_name = $self->result_set->get_dbfile_path_by_window_size($wsize, $store_slice);
 	my $fh = $self->get_filehandle($col_file_name);
-	#, {-file_operator => '>'});#not strictly needed here as it should be open for writing.
+
+	
+	#, {-file_operator => '>'});#not strictly needed here as it should be open for writing?
+	#Is this true? We are getting some file not exist errors on validate_file_length
+	#this should fail before then anyway
+	#get_filehandle just warn if it fails to acquire $fh
 	
 
 	# STORE SEQ_REGION
@@ -551,10 +545,11 @@ sub store_collection{
 	
 	# PACK AND PRINT
 	my $pack_template = '('.$self->pack_template.')'.scalar(@{$scores_ref});
-	warn "Pack template is $pack_template";
+	#warn "Pack template is $pack_template";
 	
+	warn "Writing to file $fh :\t".$col_file_name;
 	print $fh pack($pack_template, @{$scores_ref});
-	  
+	
 	# VALIDATE
    	my $total_packed_size = $self->get_total_packed_size($sr_start, $sr_end, $wsize);
 	#This will only work due to autoflush set when opening in store_window_bins_by_Slice_Parser{
@@ -622,6 +617,8 @@ sub set_collection_defs_by_ResultSet{
    
   $self->result_set($rset);
 
+  warn "set_collection_defs_by_ResultSet ".join(', ', @{$Bio::EnsEMBL::Utils::Collector::window_sizes});
+
   if(defined $rset){
 	#This is most likely already done in the caller
 	#$self->db->is_stored_and_valid('Bio::EnsEMBL::Funcgen::ResultSet', $rset);
@@ -640,7 +637,7 @@ sub set_collection_defs_by_ResultSet{
 	  $Bio::EnsEMBL::Utils::Collector::window_sizes->[0] = 30;
 
 	  #Currently only expecting int from InputSet
-	  my @isets = @{$rset->get_InputSets};
+	  my @isets = @{$rset->get_support};
 	  my @tmp_isets = grep(!/result/, (map $_->feature_class, @isets));
 	  
 	  if(@tmp_isets){
@@ -664,6 +661,9 @@ sub set_collection_defs_by_ResultSet{
   #Do we need to validate the smallest non-0 window size
   #against the max pack size?
   #This should be done in the Collector
+
+   warn "set_collection_defs_by_ResultSet ".join(', ', @{$Bio::EnsEMBL::Utils::Collector::window_sizes});
+
 
   return;
 }
@@ -748,5 +748,214 @@ sub source_set_type{
   return $self->{source_set_type};
 }
 
+
+sub merge_and_index_slice_collections {
+  my ($self, $rset, $slices, $data_dir, $force_overwrite) = @_;  
+  
+  $self->db->is_stored_and_valid('Bio::EnsEMBL::Funcgen::ResultSet', $rset);
+  
+
+  if(! -d $data_dir){
+    die("data_dir argument is not valid:'t$data_dir\n".
+      "This should contains the slice/seq_region .col files");
+  }
+  
+
+  
+  if(! (defined $slices &&
+        ref($slices)    &&
+        (ref($slices) eq 'ARRAY') &&
+        (scalar(@$slices) > 0)  ) ){
+    throw("Slices argument must be a valid Arrayref of Bio::EnsEMBL::Slice objects:\t$slices"); 
+  }
+  
+  
+  
+  $self->set_collection_defs_by_ResultSet($rset);
+  my $rset_name = $rset->name;
+  my $packed_size = $self->packed_size;
+  my @wsizes      = @{$self->window_sizes};
+  my $first_wsize = 1;
+  my (%index, $fh, @rm_files);
+  
+  
+  foreach my $wsize(@wsizes){
+  
+    #Check for target file first
+    my $merged_fname = join('.', ('result_features',  $rset_name, $wsize, 'col'));
+    my $merged_file =  "${data_dir}/${merged_fname}";
+  
+    if(-f $merged_file){
+  	  
+  	  if(! $force_overwrite){
+    	throw("Failed to over-write existing output file:\t$merged_file\n$!");
+      }else{
+    	  #print STDOUT "Over-writing existing output file:\t$merged_file\n";
+  	  }
+    }
+  
+    my $total_length = 0;
+    my $off_set      = 0;
+    my $last_length  = 0;
+    my $seen_y       = 0;
+    my (@efg_sr_ids, @file_list);
+     
+    for my $slice(@$slices){
+
+      my $sr_name    = $slice->seq_region_name;
+      my $slice_file = $data_dir.'/'.join('.', ('result_features',  $rset_name, $wsize, $sr_name, 'col'));
+  
+      if(! -f $slice_file){
+
+        if(($sr_name =~ /^[0-9]+$/) ||
+           ($sr_name eq 'X') ||
+           ($sr_name eq 'Y')) {
+          throw("Found absent col file for mandatory seq_region $sr_name:\t$merged_file");
+        }
+        
+        next;
+	  }
+	   
+	     
+      my $efg_sr_id = $self->get_seq_region_id_by_Slice($slice);
+      push @efg_sr_ids, $efg_sr_id;
+      push @file_list, $slice_file; 
+  
+      #Set new byte off set for this slice
+      $off_set           = $off_set + ($last_length);
+      $index{$efg_sr_id} = $off_set;
+  
+      #Calculate last length
+      $last_length     = $slice->length / $wsize;#rather than end, just in case we don't start at 1
+      my $tmp_last_length = int($last_length);
+      $last_length     = $tmp_last_length + 1 if ($last_length != $tmp_last_length);
+      $last_length    *= $packed_size; 
+      $total_length    = $off_set + $last_length;
+
+      #Now validate file length
+      #This should have already been done in the Collector
+      $self->validate_file_length($slice_file, $last_length, 1);#1 is binmode;
+    }
+ 
+    #Could probably do this via the dump by ordering by seq_region_name/id
+    #But mysql sort is not reliably the same as unix/perl sort
+    #So let's do it here to be sure.
+    
+    print "Total $wsize window_size data length:\t\t$total_length\n";
+  
+    #Encode release version in the file name for visibility.
+    #How big does the index need to be? I (32 bit unsigned) endian order depends on arch
+    #v for size of index
+    #v for index key i.e. sr_id
+    #V for offset (4bytes?, max unsigned is 4,294,967,295)
+    #Max offset is??
+    #=>Nchrs * (max chr length) / (min window size) * packed_size
+    #=> ~23(ignore non-chr for now) * 249250621 / 30 * 4 (also ignore index length)
+    #=>764368568 < 4294967295
+    #              404840556
+    #Plenty of room for ~ 6* more data!
+    
+    #Encode index as key(sr_id - v 2 bytes) value(offset - V 4 bytes) pairs
+  
+    
+    
+    my $num_keys      = scalar(keys %index);
+    my $index_size    = scalar(keys %index)*(6);
+    #Full index is index_size + key-values pairs
+    #Could add file length here to enable validation
+    #i.e. seek to full length and make sure we don't get and EOF until we seek one byte more
+  
+    #perl -V:{short,int,long{,long}}size
+    #shortsize='2';
+    #intsize='4';
+    #longsize='8';
+    #longlongsize='8';
+    #Actual size can change due to compiler
+    #	   use Config;
+    #       print $Config{shortsize},    "\n";
+    #       print $Config{intsize},      "\n";
+    #       print $Config{longsize},     "\n";
+    #       print $Config{longlongsize}, "\n";
+  
+    
+    #Does this also mean that changing data between perl 32bit -> 64bit will screw the reads?   
+    
+    my $total_index_size = 2 + ($num_keys * (2 + 4));
+    
+    #Adjust the offsets to account for the total index size
+    foreach my $key(keys %index){
+    	#warn "Adjusting $key index value from $index{$key}\t";
+    	$index{$key} += $total_index_size;
+    	#warn "to ".$index{$key};
+    }
+  
+  
+    my $pack_template = 'v(vV)'.$num_keys;
+    #warn "pack template is $pack_template";
+    my @index = %index;
+    my $packed_index  = pack($pack_template, ($index_size, @index));
+    
+      
+    #Let's just validate the index here
+    #my @unpacked_index = unpack($pack_template, $packed_index);
+    #my $unpacked_index_size = shift(@unpacked_index);
+    #warn "Index size($index_size) unpacked is $unpacked_index_size";
+    #my %unpacked_index = @unpacked_index;
+    #warn "Keys ".$num_keys.' unpacked is '.scalar(keys(%unpacked_index));
+    #for my $key(keys(%unpacked_index)){
+    #  warn "key $key value ".$index{$key}.' unpacked is '.$unpacked_index{$key};
+    #}
+  
+  
+    #Dumps the index to file and add it to the start of the cat list
+    my $idx_name   = join('.', ('result_features',  $rset->name, $wsize, 'idx')); 
+    my $index_file = "${data_dir}/${idx_name}";
+    print "Writing index:\t\t\t$idx_name\n";
+    open($fh, '>', $index_file) or die("Cannot open $index_file");
+    binmode($fh);
+    print $fh $packed_index;
+    close($fh);
+  
+  
+    #Validate index file
+    $fh           = $self->get_filehandle($index_file, {-binmode => 1});
+    #Do this directly here as we have little need for grabbing the whole index normally
+    my $index_ref = $self->{file_cache}{$index_file}{off_sets};
+  
+    #### Now validate written index
+  
+    #Test num keys is same
+    if($num_keys != scalar(keys %{$index_ref})){
+      throw("Original index has $num_keys whilst read and unpacked index has ".scalar(keys %{$index_ref}));
+    }
+    
+    for my $key(keys(%{$index_ref})){ #Test values match
+  	
+      if($index{$key} != $index_ref->{$key}){
+        throw("Original key $key value ".$index{$key}.
+         ' does not match read and unpacked value '.$index_ref->{$key});
+      }
+    }
+    
+  
+    #Generate merged file
+    my $cat = "cat $index_file @file_list > $merged_file";
+    print "Generating indexed file:\t$merged_file\n";
+    system($cat) == 0 or die ("Cannot cat files:\n$cat");
+  
+    #Validate length
+    $total_length += $total_index_size;
+    $self->validate_file_length($merged_file, $total_length, 1);#1 is binmode;
+  
+    push @rm_files, ($index_file, @file_list);
+  }
+  
+  		
+  #Remove index and slice files
+  #print STDOUTT "Removing index and sr_name col files\n";
+  unlink(@rm_files) || throw("Failed to remove tmp files:\n\t".join("\n\t", @rm_files)."\n$!");
+    
+  return 1;
+}
 
 1;
