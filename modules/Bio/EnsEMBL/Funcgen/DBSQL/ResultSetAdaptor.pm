49b8f4f455d9f89e767f0265a386bd578cb5ebc9
Thomas Juettemann <juettemann@ebi.ac.uk>
2013-10-16 13:25:12 +0000

    refactoring

--- modules/Bio/EnsEMBL/Funcgen/DBSQL/ResultSetAdaptor.pm
+++ modules/Bio/EnsEMBL/Funcgen/DBSQL/ResultSetAdaptor.pm
@@ -35,9 +35,9 @@ my @rsets = @{$rset_adaptor->fetch_all_by_Experiment()};
 =head1 DESCRIPTION
 
 The ResultSetAdaptor is a database adaptor for storing and retrieving
-ResultSet objects which encapsulate ResultFeatures defining individual points of 
-experimental 'signal' data. This can be raw signal(Channel) or 
-normalised(ExperimentalChip) data from an Array Experiment. A ResultSet can also 
+ResultSet objects which encapsulate ResultFeatures defining individual points of
+experimental 'signal' data. This can be raw signal(Channel) or
+normalised(ExperimentalChip) data from an Array Experiment. A ResultSet can also
 encapsulate processed signal/read data(InputSet) from a sequencing Experiment e.g RPKM.
 
 =cut
@@ -61,14 +61,14 @@ use base qw(Bio::EnsEMBL::Funcgen::DBSQL::SetAdaptor); #@ISA
   Arg [2]    : HASH of parameters (optional) containing contraint config e.g.
 
                    $result_set_adaptor->fetch_all_displayable_by_feature_class
-                                           ('dna_methylation', 
-                                             {'constraints' => 
+                                           ('dna_methylation',
+                                             {'constraints' =>
                                                {
                                                cell_types     => [$cell_type],  #Bio::EnsEMBL::Funcgen::CellType
                                                #projects       => ['ENCODE'],
                                                feature_types  => [$ftype],      #Bio::EnsEMBL::Funcgen::FeatureType
                                                status         => 'DISPLAYABLE',
-                                               } 
+                                               }
                                              });
 
   Example    : my @result_sets = @{$rs_adaptopr->fetch_all_by_feature_class('result')};
@@ -82,10 +82,10 @@ use base qw(Bio::EnsEMBL::Funcgen::DBSQL::SetAdaptor); #@ISA
 
 sub fetch_all_by_feature_class {
   my ($self, $fclass, $params) = @_;
-  
-  throw('Must provide a feature_set type') if(! defined $fclass);	
+
+  throw('Must provide a feature_set type') if(! defined $fclass);
   my $sql = 'rs.feature_class = "'.$fclass.'"';
-    
+
   #Deal with params constraints
   my $constraint = $self->compose_constraint_query($params);
   $sql .=  " AND $constraint " if $constraint;
@@ -102,7 +102,6 @@ sub fetch_all_by_feature_class {
 =head2 fetch_all_linked_by_ResultSet
 
   Arg [1]    : Bio::EnsEMBL::Funcgen::ResultSet
-  Arg [2]    : Bio::EnsEMBL::Analysis
   Example    : my @rsets = @{$rset_adaptor->fetch_all_linked_by_ResultSet($rset)};
   Description: Retrieves a list of Bio::EnsEMBL::Funcgen::ResultSets which are linked
                to the supplied ResultSet (i.e. replicate relationships/import sets)
@@ -119,8 +118,17 @@ sub fetch_all_linked_by_ResultSet{
   $self->db->is_stored_and_valid('Bio::EnsEMBL::Funcgen::ResultSet', $rset);
 
 
-  my $constraint = ' rsi.result_set_id in (SELECT distinct(result_set_id) from result_set_input where result_set_input_id in('.join(', ', @{$rset->result_set_input_ids}).')) ';
-  
+  my $constraint = '
+    rsi.result_set_id IN (
+      SELECT DISTINCT(
+          result_set_id)
+      FROM
+        result_set_input
+      WHERE
+        result_set_input_id
+      IN(
+        '.join(', ', @{$rset->result_set_input_ids}).')) ';
+
   my @tmp = @{$self->generic_fetch($constraint)};
 
   #Now remove query set
@@ -154,7 +162,7 @@ sub fetch_all_by_Experiment_Analysis{
   }
 
   my $join = $self->_get_Experiment_join_clause($exp);
-	
+
   return ($join) ?  $self->generic_fetch($join." AND rs.analysis_id=".$analysis->dbID) : [];
 }
 
@@ -168,27 +176,27 @@ sub _get_Experiment_join_clause{
   if (@ecs) { # We have an Array based experiment
 
     my $ec_ids = join(', ', (map $_->dbID, @ecs)); #get ' separated list of ecids
-	
-	
+
+
     my @chans = map @$_, (map $_->get_Channels(), @ecs);
     my $chan_ids = join(', ', (map $_->dbID(), @chans)); #get ' separated list of chanids
     #These give empty strings which are defined
     #This will not work for single IDs of 0, but this will never happen.
-	
+
     if ($ec_ids && $chan_ids) {
       $constraint = '(((rsi.table_name="experimental_chip" AND rsi.table_id IN ('.$ec_ids.
         ')) OR (rsi.table_name="channel" AND rsi.table_id IN ('.$chan_ids.'))))';
       #This could probably be sped up using UNION
       #But result set is too small for cost of implementation
-    } 
+    }
     elsif ($ec_ids) {
       $constraint = 'rsi.table_name="experimental_chip" AND rsi.table_id IN ('.$ec_ids.')';
-    } 
+    }
     elsif ($chan_ids) {
       $constraint = 'rsi.table_name="channel" AND rsi.table_id IN ('.$chan_ids.')';
     }
 
-  } 
+  }
   else {     #We have an InputSet Experiment
     #We could possibly have an experiment with an array and an input set
     #Currently nothing to stop this, but would most likely be loaded as separate experiments
@@ -218,13 +226,11 @@ sub fetch_all_by_Experiment{
   #my $constraint = "ec.experiment_id=".$exp->dbID();
   #This was much easier with the more complicated default where join
   #should we reinstate and just have a duplication of cell/feature_types?
-	
+
   my $join = $self->_get_Experiment_join_clause($exp);
-  
+
   return ($join) ? $self->generic_fetch($join) : [];
 }
-
-
 
 =head2 fetch_all_by_name
 
@@ -235,7 +241,7 @@ sub fetch_all_by_Experiment{
   Example    : ($rset) = @{$rseta->fetch_all_by_name($exp->name().'_IMPORT')};
   Description: Retrieves ResultSets based on the name attribute
   Returntype : ARRAYREF of Bio::EnsEMBL::Funcgen::ResultSet objects
-  Exceptions : Throws if no name provided or optional arguments are not valid   
+  Exceptions : Throws if no name provided or optional arguments are not valid
   Caller     : General
   Status     : At Risk
 
@@ -250,7 +256,7 @@ sub fetch_all_by_name{
   if ( ! defined $name) {
     throw('Need to pass a ResultSet name');
   }
-	
+
   my $constraint = 'rs.name = ?';
   $self->bind_param_generic_fetch($name, SQL_VARCHAR);
 
@@ -272,7 +278,7 @@ sub fetch_all_by_name{
     $self->bind_param_generic_fetch($anal->dbID, SQL_INTEGER);
   }
 
-	
+
   return $self->generic_fetch($constraint);
 }
 
@@ -363,7 +369,7 @@ sub _columns {
 sub _default_where_clause {
   return 'rs.result_set_id = rsi.result_set_id';
 }
-  
+
 
 =head2 _final_clause
 
@@ -409,16 +415,16 @@ sub _objs_from_sth {
   my ($sql, $table_name, $cc_id, $ftype_id, $ctype_id, $rf_set, $dbfile_dir,$feat_class);
   my $a_adaptor = $self->db->get_AnalysisAdaptor();
   my $ft_adaptor = $self->db->get_FeatureTypeAdaptor();
-  my $ct_adaptor = $self->db->get_CellTypeAdaptor(); 
-  $sth->bind_columns(\$dbid, \$anal_id, \$table_name, \$cc_id, 
+  my $ct_adaptor = $self->db->get_CellTypeAdaptor();
+  $sth->bind_columns(\$dbid, \$anal_id, \$table_name, \$cc_id,
 					 \$table_id, \$name, \$ctype_id, \$ftype_id, \$feat_class, \$dbfile_dir);
-  
+
     #Need c/ftype cache here or rely on query cache?
 
   while ( $sth->fetch() ) {
-    
+
     if(! $rset || ($rset->dbID() != $dbid)){
-      
+
       push @rsets, $rset if $rset;
 
       $anal  = (defined $anal_id)  ? $a_adaptor->fetch_by_dbID($anal_id)   : undef;
@@ -429,7 +435,7 @@ sub _objs_from_sth {
       if(defined $dbfile_dir){
         $dbfile_dir = $self->build_dbfile_path($dbfile_dir);
       }
-	 
+
       $rset = Bio::EnsEMBL::Funcgen::ResultSet->new
         (
          -DBID            => $dbid,
@@ -443,21 +449,21 @@ sub _objs_from_sth {
          -DBFILE_DATA_DIR => $dbfile_dir,
         );
     }
-    
-  
-    if(defined $rset->feature_type()){    
+
+
+    if(defined $rset->feature_type()){
       throw("ResultSet does not accomodate multiple FeatureTypes") if ($ftype_id != $rset->feature_type->dbID());
     }
-    
+
     if(defined $rset->cell_type()){
       throw("ResultSet does not accomodate multiple CellTypes") if ($ctype_id != $rset->cell_type->dbID());
     }
 
-    $rset->_add_table_id($table_id, $cc_id);  
+    $rset->_add_table_id($table_id, $cc_id);
   }
 
   push @rsets, $rset if $rset;
-  
+
   return \@rsets;
 }
 
@@ -477,7 +483,7 @@ sub build_dbfile_path{
 
   Args       : List of Bio::EnsEMBL::Funcgen::ResultSet objects
   Example    : $rsa->store(@rsets);
-  Description: Stores or updates previously stored ResultSet objects in the database. 
+  Description: Stores or updates previously stored ResultSet objects in the database.
   Returntype : None
   Exceptions : Throws if a List of ResultSet objects is not provided or if
                an analysis is not attached to any of the objects
@@ -490,23 +496,23 @@ sub store{
   my ($self, @rsets) = @_;
 
   throw("Must provide a list of ResultSet objects") if(scalar(@rsets == 0));
-  
+
   my (%analysis_hash);
-  
+
   my $sth = $self->prepare('INSERT INTO result_set '.
                            '(analysis_id, name, cell_type_id, feature_type_id, feature_class) '.
                            'VALUES (?, ?, ?, ?, ?)');
-  
+
   my $db = $self->db();
   my $analysis_adaptor = $db->get_AnalysisAdaptor();
-  
+
  FEATURE: foreach my $rset (@rsets) {
-    
+
     if ( ! (ref $rset && $rset->isa('Bio::EnsEMBL::Funcgen::ResultSet') )) {
-      throw('Must be an ResultSet object to store');
+      throw('Must be an ResultSet object to store. Passed: ' . ref($rset) );
     }
-    
-        
+
+
     if ( $rset->is_stored($db) ) {
       throw('ResultSet [' . $rset->dbID() . '] is already stored in the database\nResultSetAdaptor does not yet accomodate updating ResultSets');
       #would need to retrive stored result set and update table_ids
@@ -516,17 +522,17 @@ sub store{
     #Need to check table_id and analysis and that it has the correct status
 
 
-    
+
     if ( ! defined $rset->analysis() ) {
       throw('An analysis must be attached to the ResultSet objects to be stored.');
     }
-    
+
     # Store the analysis if it has not been stored yet
     if ( ! $rset->analysis->is_stored($db) ) {
       warn("Will this not keep storing the same analysis if we keep passing the same unstored analysis?");
       $analysis_adaptor->store( $rset->analysis() );
     }
-   
+
 
     my $ct_id = (defined $rset->cell_type)    ? $rset->cell_type->dbID    : undef;
     my $ft_id = (defined $rset->feature_type) ? $rset->feature_type->dbID : undef;
@@ -537,15 +543,15 @@ sub store{
     $sth->bind_param(4, $ft_id,                  SQL_INTEGER);
     $sth->bind_param(5, $rset->feature_class,    SQL_VARCHAR);
     $sth->execute;
-    
+
     $rset->dbID( $sth->{mysql_insertid} );
     $rset->adaptor($self);
-    
+
     $self->store_states($rset);
     $self->store_chip_channels($rset);
     $self->store_dbfile_data_dir($rset) if $rset->dbfile_data_dir;
   }
-  
+
   return \@rsets;
 }
 
@@ -578,7 +584,7 @@ sub dbfile_data_root{
 
     #Grab it from the meta table
     $self->{dbfile_data_root} = $self->db->get_MetaContainer->single_value_by_key('dbfile.data_root');
-	
+
     #set to empty string if undef, so we don't keep querying the meta table
     $self->{dbfile_data_root} ||= '';
   }
@@ -620,7 +626,7 @@ sub store_dbfile_data_dir{
   if(! $data_dir){
 	throw('You need to pass a dbfile_data_dir argument to update the ResultSet');
   }
-  
+
   #Check we have a record
   my $sql = 'SELECT path from dbfile_registry where table_name="result_set" and table_id=?';
   my $sth = $self->prepare($sql);
@@ -642,10 +648,10 @@ sub store_dbfile_data_dir{
 	$sth = $self->prepare($sql);
 	$sth->bind_param(1, $rset->dbID, SQL_INTEGER);
 	$sth->bind_param(2, $data_dir,   SQL_VARCHAR);
-	
+
 	$sth->execute;
   }
-  
+
   return;
 }
 
@@ -654,7 +660,7 @@ sub store_dbfile_data_dir{
 
   Args       : Bio::EnsEMBL::Funcgen::ResultSet
   Example    : $rsa->store_chip_channel(@rset);
-  Description: Convinience methods extracted from store to allow updating of chip_channel entries 
+  Description: Convinience methods extracted from store to allow updating of chip_channel entries
                during inline result processing which would otherwise be troublesome due to the need
                for a chip_channel_id in the result table before the ResultSet would normally be stored
                i.e. after it has been fully populated with data.
@@ -668,15 +674,15 @@ sub store_dbfile_data_dir{
 
 sub store_chip_channels{
   my ($self, $rset) = @_;
-  
+
   if(! ($rset && $rset->isa("Bio::EnsEMBL::Funcgen::ResultSet"))){
     throw("You must pasas a valid Bio::EnsEMBL::Funcgen::ResultSet");
   }
-  
+
   if ( ! $rset->is_stored($self->db()) ) {
     throw('ResultSet must be stored in the database before storing chip_channel entries');
   }
-  
+
   my $sth = $self->prepare("
 		INSERT INTO result_set_input (
 			result_set_id, table_id, table_name
@@ -688,7 +694,7 @@ sub store_chip_channels{
 			result_set_input_id, result_set_id, table_id, table_name
 		) VALUES (?, ?, ?, ?)
 	");
-  
+
 
   #Store and set all previously unstored table_ids
   foreach my $table_id(@{$rset->table_ids()}){
@@ -698,7 +704,7 @@ sub store_chip_channels{
       $sth->bind_param(1, $rset->dbID(),       SQL_INTEGER);
       $sth->bind_param(2, $table_id,           SQL_INTEGER);
       $sth->bind_param(3, $rset->table_name(), SQL_VARCHAR);
-      
+
       $sth->execute();
 
 	  $cc_id = $sth->{'mysql_insertid'};
@@ -732,12 +738,12 @@ sub store_chip_channels{
   Arg[2]     : optional string - STATUS e.g. 'DIPLAYABLE'
   Example    : my @rfeatures = @{$rsa->fetch_ResultFeatures_by_Slice_ResultSet($slice, $rset, 'DISPLAYABLE')};
   Description: Gets a list of lightweight ResultFeatures from the ResultSet and Slice passed.
-               Replicates are combined using a median of biological replicates based on 
+               Replicates are combined using a median of biological replicates based on
                their mean techinical replicate scores
   Returntype : List of Bio::EnsEMBL::Funcgen::ResultFeature
   Exceptions : Warns if not experimental_chip ResultSet
                Throws if no Slice passed
-               Warns if 
+               Warns if
   Caller     : general
   Status     : At risk
 
@@ -775,17 +781,8 @@ sub fetch_ResultFeatures_by_Slice_ResultSet{
   return $self->db->get_ResultFeatureAdaptor->fetch_all_by_Slice_ResultSet($slice, $rset, $ec_status, $with_probe);
 
 }
-
-
-
 ### GENERIC CONSTRAIN METHODS ###
 #See Base/SetAdaptor
-
-
-
-
-
-
 
 ### DEPRECATED METHODS ###
 
