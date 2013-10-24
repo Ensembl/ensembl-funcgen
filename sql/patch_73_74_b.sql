dd567d8aae3536a66124a88f845ea6ffde42d007
Thomas Juettemann <juettemann@ebi.ac.uk>
2013-10-24 15:28:09 +0000

    InputSubset & analysis

--- sql/patch_73_74_b.sql
+++ sql/patch_73_74_b.sql
@@ -2,51 +2,53 @@
 @header patch_73_74_b.sql - schema version
 @desc   Update for new funcgen schema layout
 */
-ALTER TABLE status_name MODIFY name varchar(60);
+ALTER TABLE `status_name` MODIFY `name` varchar(60);
 
-
-INSERT INTO analysis (created, logic_name) values (NOW(), 'ChIP-Seq');
-INSERT INTO analysis (created, logic_name) values (NOW(), 'DNase-Seq');
-INSERT INTO analysis (created, logic_name) values (NOW(), 'eQTL');
-INSERT INTO analysis (created, logic_name) values (NOW(), 'RRBS');
-INSERT INTO analysis (created, logic_name) values (NOW(), 'WGBS');
-INSERT INTO analysis (created, logic_name) values (NOW(), 'FAIRE');
+INSERT INTO `analysis` (`created`, `logic_name`) values (NOW(), 'ChIP-Seq');
+INSERT INTO `analysis` (`created`, `logic_name`) values (NOW(), 'DNase-Seq');
+INSERT INTO `analysis` (`created`, `logic_name`) values (NOW(), 'eQTL');
+INSERT INTO `analysis` (`created`, `logic_name`) values (NOW(), 'FAIRE');
+INSERT INTO `analysis` (`created`, `logic_name`) values (NOW(), 'PolIII');
+INSERT INTO `analysis` (`created`, `logic_name`) values (NOW(), 'RRBS');
+INSERT INTO `analysis` (`created`, `logic_name`) values (NOW(), 'WGBS');
 
 ALTER TABLE `input_set`    ADD `analysis_id`   int(10) unsigned NOT NULL;
 
-UPDATE input_set iset, analysis a SET iset.analysis_id = a.analysis_id WHERE a.logic_name = 'chromhmm.segway.GM12878.comb11.concord4' and iset.name = 'Segmentation:GM12878';
-UPDATE input_set iset, analysis a SET iset.analysis_id = a.analysis_id WHERE a.logic_name = 'chromhmm.segway.H1ESC.comb11.concord4'   and iset.name = 'Segmentation:H1ESC';
-UPDATE input_set iset, analysis a SET iset.analysis_id = a.analysis_id WHERE a.logic_name = 'chromhmm.segway.HeLa-S3.comb11.concord4' and iset.name = 'Segmentation:HeLa-S3';
-UPDATE input_set iset, analysis a SET iset.analysis_id = a.analysis_id WHERE a.logic_name = 'chromhmm.segway.HepG2.comb11.concord4'   and iset.name = 'Segmentation:HepG2';
-UPDATE input_set iset, analysis a SET iset.analysis_id = a.analysis_id WHERE a.logic_name = 'chromhmm.segway.HUVEC.comb11.concord4'   and iset.name = 'Segmentation:HUVEC';
-UPDATE input_set iset, analysis a SET iset.analysis_id = a.analysis_id WHERE a.logic_name = 'chromhmm.segway.K562.comb11.concord4'    and iset.name = 'Segmentation:K562';
-UPDATE input_set iset, analysis a SET iset.analysis_id = a.analysis_id WHERE a.logic_name = 'eQTL'                                    and iset.vendor = 'EQTL';
 
 UPDATE
-  input_set iset
+  `input_set`
 SET
-  analysis_id = (
+  `analysis_id` = (
     SELECT
-      analysis_id
+      `analysis_id`
     FROM
       analysis
     WHERE
-      logic_name = 'ChIP-Seq'
+      `logic_name` = 'ChIP-Seq'
       )
 WHERE
-  feature_type_id IN (
+  `feature_type`_id` IN (
     SELECT
-      feature_type_id
+      `feature_type`_id`
     FROM
-      feature_type
+      `feature_type`
     WHERE
-      feature_type.class IN ('HISTONE', 'TRANSCRIPTION FACTOR', 'TRANSCRIPTION FACTOR COMPLEX')
+      `feature_type`.class IN ('HISTONE', 'TRANSCRIPTION FACTOR', 'TRANSCRIPTION FACTOR COMPLEX')
     )
   ;
 
+UPDATE `input_set` SET `analysis_id` = ( SELECT `analysis_id` FROM `analysis` WHERE `logic_name` = 'DNase-Seq') WHERE `feature_type`_id` IN ( SELECT `feature_type`_id` FROM `feature_type` WHERE `name` = 'DNase1';) ;
+UPDATE `input_set` SET `analysis_id` = ( SELECT `analysis_id` FROM `analysis` WHERE `logic_name` = 'FAIRE')     WHERE `feature_type`_id` IN ( SELECT `feature_type`_id` FROM `feature_type` WHERE `name` = 'FAIRE';) ;
+UPDATE `input_set` SET `analysis_id` = ( SELECT `analysis_id` FROM `analysis` WHERE `logic_name` = 'PolIII')    WHERE `feature_type`_id` IN ( SELECT `feature_type`_id` FROM `feature_type` WHERE `name` = 'PolIII';) ;
 
-ALTER TABLE `status` ADD `timestamp` datetime DEFAULT NULL;
-ALTER TABLE `status` ADD `release` int(4)     DEFAULT NULL;
+UPDATE `input_set` SET `analysis_id` = ( SELECT `analysis_id` FROM `analysis` WHERE `logic_name` = 'chromhmm.segway.GM12878.comb11.concord4') WHERE `name` = 'Segmentation:GM12878';
+UPDATE `input_set` SET `analysis_id` = ( SELECT `analysis_id` FROM `analysis` WHERE `logic_name` = 'chromhmm.segway.H1ESC.comb11.concord4')   WHERE `name` = 'Segmentation:H1ESC';
+UPDATE `input_set` SET `analysis_id` = ( SELECT `analysis_id` FROM `analysis` WHERE `logic_name` = 'chromhmm.segway.HeLa-S3.comb11.concord4') WHERE `name` = 'Segmentation:HeLa-S3';
+UPDATE `input_set` SET `analysis_id` = ( SELECT `analysis_id` FROM `analysis` WHERE `logic_name` = 'chromhmm.segway.HepG2.comb11.concord4')   WHERE `name` = 'Segmentation:HepG2';
+UPDATE `input_set` SET `analysis_id` = ( SELECT `analysis_id` FROM `analysis` WHERE `logic_name` = 'chromhmm.segway.HUVEC.comb11.concord4')   WHERE `name` = 'Segmentation:HUVEC';
+UPDATE `input_set` SET `analysis_id` = ( SELECT `analysis_id` FROM `analysis` WHERE `logic_name` = 'chromhmm.segway.K562.comb11.concord4')    WHERE `name` = 'Segmentation:K562';
+
+UPDATE `input_set` SET `analysis_id` = ( SELECT `analysis_id` FROM `analysis` WHERE `logic_name` = 'eQTL')                                    WHERE `vendor` = 'EQTL';
 
 
 DROP TABLE IF EXISTS `input_set_input_subset`;
@@ -57,7 +59,11 @@ CREATE TABLE `input_set_input_subset` (
     ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 INSERT INTO input_set_input_subset ( input_set_id, input_subset_id ) SELECT input_set_id, input_subset_id FROM input_subset ;
-ALTER TABLE `input_subset` ADD `experiment_id` int(10) unsigned DEFAULT NULL AFTER `input_subset_id`;
+
+ALTER TABLE `input_subset` ADD `cell_type_id`     int(10) unsigned DEFAULT NULL AFTER `input_subset_id`;
+ALTER TABLE `input_subset` ADD `experiment_id`    int(10) unsigned DEFAULT NULL AFTER `cell_type_id`;
+ALTER TABLE `input_subset` ADD `feature_type_id`  int(10) unsigned DEFAULT NULL AFTER `experiment_id`;
+
 UPDATE input_set iset, input_subset iss SET iss.experiment_id = iset.experiment_id WHERE iss.input_set_id = iset.input_set_id;
 
 ALTER TABLE input_subset DROP INDEX set_name_dx;
