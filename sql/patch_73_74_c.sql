d1ef4f8caf92bfcba41c6dae5581db85871c33f0
Nathan Johnson <njohnson@ebi.ac.uk>
2013-10-28 16:49:23 +0000

    updated result_set_input enum for input_subset

--- sql/patch_73_74_c.sql
+++ sql/patch_73_74_c.sql
@@ -3,6 +3,7 @@
 @desc   Add replicate field to result_set
 */
 ALTER TABLE `result_set` ADD `replicate` tinyint(3) unsigned NOT NULL;
+ALTER TABLE `result_set_input` MODIFY `table_name` enum('experimental_chip','channel','input_set', 'input_subset') DEFAULT NULL;
 
 # patch identifier
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_73_74_c.sql|result_set.replicate');
