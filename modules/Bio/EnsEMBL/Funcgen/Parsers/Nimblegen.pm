48a1b7170b26221aba9b7d790f8c182a6397bf08
Nathan Johnson <njohnson@ebi.ac.uk>
2013-11-26 11:56:44 +0000

    updated to new license

--- modules/Bio/EnsEMBL/Funcgen/Parsers/Nimblegen.pm
+++ modules/Bio/EnsEMBL/Funcgen/Parsers/Nimblegen.pm
@@ -4,13 +4,19 @@
 
 =head1 LICENSE
 
-  Copyright (c) 1999-2013 The European Bioinformatics Institute and
-  Genome Research Limited.  All rights reserved.
+Copyright [1999-2013] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
 
-  This software is distributed under a modified Apache license.
-  For license details, please see
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-    http://www.ensembl.org/info/about/code_licence.html
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 =head1 CONTACT
 
@@ -56,7 +62,7 @@ use Bio::EnsEMBL::Funcgen::ExperimentalChip;
 use Bio::EnsEMBL::Funcgen::ArrayChip;
 use Bio::EnsEMBL::Funcgen::Channel;
 
-use parent qw(Bio::EnsEMBL::Funcgen::Parsers::MAGE);
+use parent qw( Bio::EnsEMBL::Funcgen::Parsers::MAGE );
 
 =head2 new
 
