41831134d0aeefcaf3c3499088eed8a6319bb3c7
Nathan Johnson <njohnson@ebi.ac.uk>
2013-11-26 14:25:17 +0000

    updated licenses

--- sql/patch_63_64_f.sql
+++ sql/patch_63_64_f.sql
@@ -1,3 +1,17 @@
+-- Copyright [1999-2013] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--      http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 /** 
 @header patch_63_64_f.sql - feature_set.experiment_id
 @desc   Add experimenta_id to feature_set to enable easy recall of experiment
