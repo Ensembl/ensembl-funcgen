e0783e74a0d0dc2b30d09abdf1032759e0725582
Nathan Johnson <njohnson@ebi.ac.uk>
2013-11-26 10:51:41 +0000

    updated to new license bioler plate

--- modules/Bio/EnsEMBL/Funcgen/DBSQL/ProbeSetAdaptor.pm
+++ modules/Bio/EnsEMBL/Funcgen/DBSQL/ProbeSetAdaptor.pm
@@ -4,13 +4,19 @@
 
 =head1 LICENSE
 
-  Copyright (c) 1999-2013 The European Bioinformatics Institute and
-  Genome Research Limited.  All rights reserved.
+Copyright [1999-2013] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
 
-  This software is distributed under a modified Apache license.
-  For license details, please see
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-    http://www.ensembl.org/info/about/code_licence.html
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 =head1 CONTACT
 
@@ -22,7 +28,7 @@
 
 =head1 NAME
 
-Bio::EnsEMBL::DBSQL::ProbeSetAdaptor - A database adaptor for fetching and
+Bio::EnsEMBL::Funcgen::DBSQL::ProbeSetAdaptor - A database adaptor for fetching and
 storing ProbeSet objects.
 
 =head1 SYNOPSIS
@@ -54,6 +60,7 @@ ProbeSet objects.
 
 =head1 SEE ALSO
 
+  Bio::EnsEMBL::Funcgen::Probe
   Bio::EnsEMBL::Funcgen::ProbeSet
   ensembl-functgenomics/scripts/examples/microarray_annotation_example.pl
 
