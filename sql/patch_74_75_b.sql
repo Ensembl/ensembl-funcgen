8d9ba276afaa65b03d239a1f886a27d4b2b6072b
Nathan Johnson <njohnson@ebi.ac.uk>
2013-12-09 10:57:08 +0000

    added 75 patch

---
+++ sql/patch_74_75_b.sql
@@ -0,0 +1,27 @@
+/**
+@header patch_74_75_b.sql - result_set.name_unique
+@desc   Patch to make name field of result_set table unique, by appending analysis
+        logic_name.
+*/
+
+UPDATE result_set rs, analysis a set rs.name=concat(rs.name, '_', a.logic_name) where rs.analysis_id=a.analysis_id;
+
+ALTER TABLE result_set DROP INDEX unique_dx;
+ALTER TABLE result_set ADD UNIQUE KEY name_idx (name);
+
+-- Now add back in other keys.
+
+ALTER TABLE result_set ADD KEY cell_type_idx (cell_type_id);
+ALTER TABLE result_set ADD KEY feature_type_idx (feature_type_id);
+
+-- 
+
+
+analyze table result_set;
+optimize table result_set;
+
+  
+# patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_74_75_b.sql|result_set.name_unique');
+
+
