d79a76658cef344404ee64fad44816e586efad6a
Nathan Johnson <njohnson@ebi.ac.uk>
2014-01-13 13:55:56 +0000

    added patch licenses and patch_74_75_d

---
+++ sql/patch_74_75_d.sql
@@ -0,0 +1,30 @@
+-- Copyright [1999-2014] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--      http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+/**
+@header patch_74_75_d.sql - result_set_input.table_name_input_subset
+@desc   input_subset was actually added to the enum in patch_73_74_c
+        This patch is simply converting the result_set_input records 
+        from input_set to input_subset
+*/
+
+insert into result_set_input(result_set_id, table_name, table_id) select  rsi.result_set_id, 'input_subset', isiss.input_subset_id from input_set inp, input_set_input_subset isiss, result_set_input rsi where rsi.table_id=inp.input_set_id and rsi.table_name='input_set' and inp.input_set_id=isiss.input_set_id;
+ 
+analyze table  result_set_input;
+optimize table result_set_input;
+ 
+# patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_74_75_d.sql|table_name_input_subset');
+
+
