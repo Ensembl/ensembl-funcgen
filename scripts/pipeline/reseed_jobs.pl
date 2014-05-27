596c9ea92bf2a00f62a771ef2663c2da5aca6692
Nathan Johnson <njohnson@ebi.ac.uk>
2014-05-27 15:27:11 +0100

    added delete/ignore_downstream job functionality

--- scripts/pipeline/reseed_jobs.pl
+++ scripts/pipeline/reseed_jobs.pl
@@ -33,26 +33,32 @@ limitations under the License.
   reseed_jobs.pl -url <String> (-job_ids <Int> ... | -logic_name <String>) \
     [ -ignore_retries -inlcude_ready -help -man ]
 
+  #Add some real use case examples here
+  
 =head1 PARAMETERS
 
   Mandatory:
-    -url              <String>    Hive DB URL
+    -url              <String>    Hive DB URL (Normally set by environment)
+    -append           <String>    Quoted hash of params to append e.g. '{rollback=>"result_set"}'
     -jobs_ids         <Int> ...   List of job IDs
     OR  
     -logic_name       <String>    Analysis logic_name
+    
   Optional:
     
-    WARNING - To be completely safe, all workers should be 
-              stopped before using the following 'ignore' options.
-    -ignore_retries   This will reseed jobs even when the max_retry_count has not been exceeded.
-    -include_ready    This will also reseed jobs which are in the READY status
+    WARNING - To be completely safe, all workers should be stopped before using 
+              the following 'ignore' options.
+    -ignore_retries    This will reseed jobs even when the max_retry_count has not been exceeded.
+    -include_ready     This will also reseed jobs which are in the READY status
+    -ignore_downstream This will ignore any downstream jobs
+    -delete_downstream This will delete any downstream jobs
     
-    -help             Prints a helpful message
-    -man              Prints the man page
+    -help              Prints a helpful message
+    -man               Prints the man page
 
 =head1 DESCRIPTION
 
-This script appends to the input_id for a given FAILED job and reset the job
+This script appends to the input_id for a given FAILED job and resets the job
 status to READY. This is primarily aimed at augmenting failing jobs which require
 a slight update to the their input_ids to run successfully.
 
@@ -81,9 +87,14 @@ use Bio::EnsEMBL::Hive::DBSQL::AnalysisJobAdaptor;
 use Bio::EnsEMBL::Hive::Utils      qw( destringify stringify );
 use Bio::EnsEMBL::Utils::Exception qw( throw );
 
-#Inject methods into the hive adaptor symbol tables/typeglobs
 
-*Bio::EnsEMBL::Hive::DBSQL::AnalysisDataAdaptor::fetch_dbID_by_data = sub {
+no strict 'refs'; #To allow quoting of symbol table of smbol table refs
+
+
+#Inject methods into the hive adaptor symbol tables/typeglobs
+#quote to avoid only used once warnings
+
+*{'Bio::EnsEMBL::Hive::DBSQL::AnalysisDataAdaptor::fetch_dbID_by_data'} = sub {
   my ($self, $data) = @_;
  
   if(! defined $data){
@@ -102,7 +113,7 @@ use Bio::EnsEMBL::Utils::Exception qw( throw );
   return $data_id;
 };
 
-*Bio::EnsEMBL::Hive::DBSQL::AnalysisDataAdaptor::update_if_needed = sub {
+*{'Bio::EnsEMBL::Hive::DBSQL::AnalysisDataAdaptor::update_if_needed'} = sub {
   my ($self, $data, $job_id) = @_;
 
   if(! (defined $data && defined $job_id)){
@@ -133,22 +144,42 @@ use Bio::EnsEMBL::Utils::Exception qw( throw );
    
   return $input_id;
 };    
+
+
+
+#This is used in both AnalysisDataAdaptor and AnalysisJobAdaptor
+#Note this is injecting into the core API!
+#USE WITH CAUTION!!!
+#Probably safer to inject into specific adaptor
   
-*Bio::EnsEMBL::Hive::DBSQL::AnalysisDataAdaptor::delete_by_dbID = sub {
-  my ($self, $dbID) = @_;
-  if(! defined $dbID){ throw('Mandatory dbID argument not met'); }
+*{'Bio::EnsEMBL::Hive::DBSQL::AnalysisDataAdaptor::delete_by_dbID'} = sub {
+  my $self         = shift;
+  my $dbID         = shift or throw('Mandatory dbID argument not met');
   
-  eval  { $self->dbc->db_handle->do("DELETE from analysis_data where analysis_data_id=$dbID") };
-  if($@){ throw("Failed to delete_by_dbID:\t$dbID\n$@"); }
+  if(! eval  { $self->dbc->db_handle->do("DELETE from analysis_data where analysis_data_id=$dbID"); 1 } ){
+    throw("Failed to delete_by_dbID:\t$dbID\n$@"); 
+  }
+  
   return;
 };
 
+  
+*{'Bio::EnsEMBL::Hive::DBSQL::AnalysisJobAdaptor::delete_by_dbID'} = sub {
+  my $self         = shift;
+  my $dbID         = shift or throw('Mandatory dbID argument not met');
+  
+  if(! eval  { $self->dbc->db_handle->do("DELETE from job where job_id=$dbID"); 1 } ){
+    throw("Failed to delete_by_dbID:\t$dbID\n$@"); 
+  }
+  
+  return;
+};
 
 #This is used in the update method below
-*Bio::EnsEMBL::Hive::DBSQL::DBAdaptor::is_stored_and_valid = 
+*{'Bio::EnsEMBL::Hive::DBSQL::DBAdaptor::is_stored_and_valid'} = 
   \&Bio::EnsEMBL::Funcgen::DBSQL::DBAdaptor::is_stored_and_valid;
 
-*Bio::EnsEMBL::Hive::DBSQL::AnalysisJobAdaptor::update = sub {
+*{'Bio::EnsEMBL::Hive::DBSQL::AnalysisJobAdaptor::update'} = sub {
   my ($self, $job) = @_;  
   $self->db->is_stored_and_valid('Bio::EnsEMBL::Hive::AnalysisJob', $job);
 
@@ -231,7 +262,7 @@ use Bio::EnsEMBL::Utils::Exception qw( throw );
 
 
 
-*Bio::EnsEMBL::Hive::DBSQL::AnalysisJobAdaptor::fetch_all_by_input_id = sub {
+*{'Bio::EnsEMBL::Hive::DBSQL::AnalysisJobAdaptor::fetch_all_by_input_id'} = sub {
   my ($self, $input_id) = @_;
   
   if(! defined $input_id){
@@ -252,10 +283,26 @@ use Bio::EnsEMBL::Utils::Exception qw( throw );
     } 
   }
   
-  return defined $input_id ? $self->generic_fetch("input_id=$input_id") : [];
+  return defined $input_id ? $self->generic_fetch("input_id='$input_id'") : [];
 };  
 
 
+*{'Bio::EnsEMBL::Hive::DBSQL::AnalysisJobAdaptor::fetch_all_by_prev_job_id'} = sub {
+  my $self         = shift;
+  my $id           = shift or throw("fetch_by_prev_job_id required an id argument");
+  my @tabs         = $self->_tables;
+  my ($name, $syn) = @{$tabs[0]};
+
+  #construct a constraint like 't1.table1_id = 1'
+  #my $constraint = "${syn}.${name}_id = $id";
+  #This could be hardcoded in fetch_by_dbID, or moved to a base adaptor?  
+  
+  return $self->_generic_fetch("${syn}.prev_${name}_id = $id");
+};
+
+
+
+use strict;
 
 $| = 1;#for debug
 
@@ -264,25 +311,37 @@ my $updated_cnt     = 0;
 my $skipped_cnt     = 0;
 my $update_fail_cnt = 0;
 
+#TODO
+# 1 Handle states a bit better, as we may just want to reset all states, especially if we are specifying job_ids
+#   Although this is arguably slighlty risky for all jobs for a logic_name/analysis
+
 sub main{
-  my (@job_ids, $url, $hive_script_dir, $logic_name, $ignore_retries, $include_ready);
-  my $append_string = '{recover => 1}';
   my @tmp_args = @ARGV;
+
+  my (@job_ids, $url, $hive_script_dir, $logic_name, $ignore_downstream, 
+      $delete_downstream, $ignore_retries, $inc_ready, $inc_done);
+  my $downstream_mode = '';
+  my $append_hash     = '{recover => 1}';
+  #This is to allow use of existing sets, but should probably be removed
+  #as it is Set specific, and might not always be required
+  
 
   GetOptions (
               #Mandatory
-              'url=s'             => \$url,
-              'job_ids=i{,}'      => \@job_ids,
-              'logic_name=s'      => \$logic_name,
-     
+              'url=s'          => \$url,
+              'job_ids=i{,}'   => \@job_ids,
+              'logic_name=s'   => \$logic_name,
+              'append=s'       => \$append_hash,
               #Optional
-              'append=s'           => \$append_string,
-              'ignore_retries'     => \$ignore_retries, 
-              'include_ready'      => \$include_ready,        
-              'help'               => sub { pod2usage(-exitval => 0); }, 
+              'ignore_retries'    => \$ignore_retries, 
+              'include_ready'     => \$inc_ready,
+              'include_done'      => \$inc_done,  
+              'ignore_downstream' => \$ignore_downstream,
+              'delete_downstream' => \$delete_downstream,     
+              'help'           => sub { pod2usage(-exitval => 0); }, 
               #removed ~? frm here as we don't want to exit with 0 for ?
               
-              'man|m'              => sub { pod2usage(-exitval => 0, -verbose => 2); },
+              'man|m'          => sub { pod2usage(-exitval => 0, -verbose => 2); },
              ) or pod2usage(-exitval => 1, -message => "Specified parameters are:\t@tmp_args"); 
              
   #Do we want to catch the rest of ARGV here by using -- in the input and assign these to init_pipeline as extra args?
@@ -300,6 +359,16 @@ sub main{
     pod2usage(-exitval => 1, -message  => 'Mandatory -url parameter is missing.');
   }
   
+  if($ignore_downstream && $delete_downstream){
+    pod2usage(-exitval => 1, -message  => '-ignore_downstream and -delete_downstream are mutually exclusive paramters');     
+  }
+  elsif($ignore_downstream){
+    $downstream_mode = 'ignore';  
+  }
+  elsif($delete_downstream){
+    $downstream_mode = 'delete';
+  }
+  
   my $hive_dba   = Bio::EnsEMBL::Hive::DBSQL::DBAdaptor->new(-url => $url);
   my $job_a      = $hive_dba->get_AnalysisJobAdaptor;
   my $analysis_a = $hive_dba->get_AnalysisAdaptor;
@@ -311,14 +380,15 @@ sub main{
     foreach my $job(@jobs){
          
       if(! (($job->status eq 'FAILED') ||
-          ($include_ready && ($job->status eq 'READY')) )){
+            ($inc_ready && ($job->status eq 'READY')) ||
+            ($inc_done && ($job->status eq 'DONE')) )){
         warn 'Skipping job_id '.$job->dbID.' as it is marked as '.$job->status."\n";
         $skipped_cnt ++;
         push @skipped_jobs, $job->dbID;
         next; #job       
       }
       
-      &reseed_and_reset_job($job, $append_string, $job_a, $analysis_a, $ignore_retries);
+      &reseed_and_reset_job($job, $append_hash, $job_a, $analysis_a, $ignore_retries, $downstream_mode);
     }
   }
   else{ #$logic_name
@@ -326,17 +396,21 @@ sub main{
     my $analysis = $analysis_a->fetch_by_logic_name($logic_name);
     my @jobs = @{$job_a->fetch_all_by_analysis_id_status($analysis->dbID, 'FAILED')};
     
-    if($include_ready){
+    if($inc_ready){
       push @jobs, @{$job_a->fetch_all_by_analysis_id_status($analysis->dbID, 'READY')};  
+    }
+    
+    if($inc_done){
+      push @jobs, @{$job_a->fetch_all_by_analysis_id_status($analysis->dbID, 'DONE')};  
     }
      
     foreach my $job (@jobs){
-      &reseed_and_reset_job($job, $append_string, $job_a, $analysis_a, $ignore_retries);
+      &reseed_and_reset_job($job, $append_hash, $job_a, $analysis_a, $ignore_retries, $downstream_mode);
     }
   }
 
   print "\nUDPATE REPORT\n".
-    "Update string:\t\t\t\t\t\t${append_string}\n".
+    "Update string:\t\t\t\t\t\t${append_hash}\n".
     "Updated job count:\t\t\t\t\t$updated_cnt\n";
     
   print "Update failed count (likely due to duplicates):\t\t$update_fail_cnt\n" if $update_fail_cnt;
@@ -347,10 +421,14 @@ sub main{
 }# end of main
 
  
+#Do we need to handle downstream jobs here?
+#These may have finished successfully already?
+#We probably want to specify and ignore or delete flag?
+#
 
 
 sub reseed_and_reset_job {
-  my ($job, $append_string, $job_a, $analysis_a, $ignore_retries) = @_;
+  my ($job, $append_string, $job_a, $analysis_a, $ignore_retries, $downstream_mode) = @_;
   
   if(! $ignore_retries){
     
@@ -367,10 +445,29 @@ sub reseed_and_reset_job {
     }
   }
   
+  if($downstream_mode ne 'ignore'){
+    my @downstream_jobs = @{$job_a->fetch_all_by_prev_job_id($job->dbID)};
+    
+    if(@downstream_jobs){
+      #This may run into problems if the job id list has upstream/downstream jobs
+      #which may have already been deleted    
+
+ 
+      if($downstream_mode eq 'delete'){
+        #47453 and 47465 got blown away by mistake
+       
+        #The only way to find this is to do a left join on the previous 
+        #analysis (DefineMergedDataSet)to the downstream job
+        
+        map { $job_a->delete_by_dbID($_->dbID) } @downstream_jobs;  
+      }
+      else{
+        throw('Found downstream jobs for job '.$job->dbID.
+          "\nPlease specify -ignore_downstream or -delete_downstream as required");  
+      }      
+    }      
+  }
   
-  $job->status('READY');
-  $job->retry_count(1); 
-   
   my $input_id = $job->input_id || {}; 
   #input_id is normally a string here
   $input_id    = destringify($input_id) if ! ref($input_id);  
@@ -385,6 +482,13 @@ sub reseed_and_reset_job {
   else{
     $updated_cnt ++;
   } 
+  
+  #Now update the status, so we don't update to READY first, then fail to reseed
+  $job->status('READY');
+  $job->retry_count(1); 
+  $job_a->update($job);
+  
+  
   
   return;
 }# end of reseed_and_reset_job
