9484aa6693ee25206d7673fea49561ef2a1c3f15
Daniel Zerbino <zerbino@ebi.ac.uk>
2014-07-02 14:19:37 +0100

    Documented segmentation unpacking

--- scripts/regulatory_build/unpack_swilder_segmentation.pl
+++ scripts/regulatory_build/unpack_swilder_segmentation.pl
@@ -1,5 +1,46 @@
 #!/usr/bin/env perl
 
+=head1 LICENSE
+
+Copyright [1999-2013] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=head1 CONTACT
+
+  Please email comments or questions to the public Ensembl
+  developers list at <ensembl-dev@ebi.ac.uk>.
+
+  Questions may also be sent to the Ensembl help desk at
+  <helpdesk@ensembl.org>.
+
+=head1 NAME
+
+unpack_segmentation.pl
+
+=head1 SYNOPSIS
+
+unpack_segmentation.pl $dir
+
+=head1 DESCRIPTION
+
+Splits a concatenated segmentation into a set of bed files.
+
+The directory given as parameter must contain one Bed file with all
+the segmentations and one text file with the emissions.
+
+=cut
+
 use strict;
 use File::Copy;
 
