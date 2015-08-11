96ed87dff2729a4b6ded9ee790e0fc221c44fe38
Nathan Johnson <njohnson@ebi.ac.uk>
2015-08-11 10:12:31 +0100

    changed throws to croaks

--- scripts/pipeline/configure_hive.pl
+++ scripts/pipeline/configure_hive.pl
@@ -158,17 +158,17 @@ sub main{
   }
 
   if(! defined $hive_script_dir){
-    throw('-hive_script_dir is a mandatory parameter');
+    croak('-hive_script_dir is a mandatory parameter');
   }
   elsif(! -d $hive_script_dir){
-    throw("-hive_script_dir is not a valid directory:\t${hive_script_dir}");
+    croak("-hive_script_dir is not a valid directory:\t${hive_script_dir}");
   }
 
   if(! defined $data_root){
-    throw('-data_root is a mandatory parameter');
+    croak('-data_root is a mandatory parameter');
   }
   elsif(! -d $data_root){
-    throw("-data_root is not a valid directory:\t${data_root}");
+    croak("-data_root is not a valid directory:\t${data_root}");
   }
 
   my $arch_params = '';
@@ -176,7 +176,7 @@ sub main{
   if(defined $archive_root){
 
     if(! -d $archive_root){
-      throw("-archive_root is not a valid directory:\t${archive_root}");
+      croak("-archive_root is not a valid directory:\t${archive_root}");
     }
 
     $arch_params .= " -archive_root $archive_root ";
@@ -215,7 +215,7 @@ sub main{
     $cmd = "perl $hive_script_dir/init_pipeline.pl ".
       "Bio::EnsEMBL::Funcgen::Hive::Config::${first_conf} $pipeline_params";
 
-    print "\n\nINITIALISING DATABASE:\t".$pdb_params->{'-dbname'}."\n";
+    print "\n\nDATABASE NOT FOUND...CREATING & INITIALISING HIVE DATABASE:\t".$pdb_params->{'-dbname'}."\n";
     run_system_cmd($cmd);
 
     $pdb      = create_DBAdaptor_from_params($pdb_params, 'hive');
@@ -362,7 +362,7 @@ sub _register_conf_in_meta{
   my $conf = shift;
 
   if(! eval { $ntable_a->store({meta_key => 'hive_conf', meta_value => $conf}); 1}){
-    throw("Failed to store hive conf meta entry:\t$conf\n$@");
+    croak("Failed to store hive conf meta entry:\t$conf\n$@");
   }
 
   return;
