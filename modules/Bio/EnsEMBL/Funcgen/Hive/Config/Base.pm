3308d90812e74df91144b74528960512d68badb4
Michael Nuhn <mnuhn@ebi.ac.uk>
2015-10-01 15:02:15 +0100

    Removed resource definitions that caused problems

--- modules/Bio/EnsEMBL/Funcgen/Hive/Config/Base.pm
+++ modules/Bio/EnsEMBL/Funcgen/Hive/Config/Base.pm
@@ -241,33 +241,24 @@ sub resource_classes {
      #urgent                  => { 'LSF' => '-q yesterday' },
      #Should never use this in the pipline, best to bswitch after submission if required
      normal_2GB              => { 'LSF' => ' -M2000 -R"select[mem>2000] rusage[mem=2000]"' },
-     normal_monitored        => { 'LSF' => " -R\"select[$ENV{DB_HOST_LSFNAME}<1000] ".
-                                            "rusage[$ENV{DB_HOST_LSFNAME}=10:duration=10:decay=1]\"" },
+     normal_monitored        => { 'LSF' => "" },
      normal_high_mem         => { 'LSF' => ' -M5000 -R"select[mem>5000] rusage[mem=5000]"' },
      normal_high_mem_2cpu    => { 'LSF' => ' -n2 -M5000 -R"select[mem>5000] rusage[mem=5000] span[hosts=1]"' },
-     normal_monitored_2GB    => {'LSF' => " -M2000 -R\"select[$ENV{DB_HOST_LSFNAME}<1000 && mem>2000]".
-                                                " rusage[$ENV{DB_HOST_LSFNAME}=10:duration=10:decay=1,mem=2000]\"" },
-     normal_monitored_4GB    => {'LSF' => " -M4000 -R\"select[$ENV{DB_HOST_LSFNAME}<1000 && mem>4000]".
-                                            " rusage[$ENV{DB_HOST_LSFNAME}=10:duration=10:decay=1,mem=4000]\"" },  
-     normal_monitored_8GB    => {'LSF' => " -M8000 -R\"select[$ENV{DB_HOST_LSFNAME}<1000 && mem>8000]".
-                                            " rusage[$ENV{DB_HOST_LSFNAME}=10:duration=10:decay=1,mem=8000]\"" },   
-     normal_monitored_16GB   => {'LSF' => " -M16000 -R\"select[$ENV{DB_HOST_LSFNAME}<1000 && mem>16000]".
-                                            " rusage[$ENV{DB_HOST_LSFNAME}=10:duration=10:decay=1,mem=16000]\"" }, 
-
+     normal_monitored_2GB    => {'LSF' => " -M2000 -R\"select[mem>2000]".
+                                                " rusage[mem=2000]\"" },
+     normal_monitored_4GB    => {'LSF' => " -M4000 -R\"select[mem>4000] rusage[mem=4000]\"" },  
+     normal_monitored_8GB    => {'LSF' => " -M8000 -R\"mem>8000] rusage[mem=8000]\"" },   
+     normal_monitored_16GB   => {'LSF' => " -M16000 -R\"select[mem>16000] rusage[mem=16000]\"" }, 
      normal_16GB_2cpu        => {'LSF' => ' -n2 -M16000 -R"select[mem>16000] rusage[mem=16000] span[hosts=1]"' },
      normal_20GB_2cpu        => {'LSF' => ' -n2 -M20000 -R"select[mem>20000] rusage[mem=20000] span[hosts=1]"' }, 
      normal_25GB_2cpu        => {'LSF' => ' -n2 -M25000 -R"select[mem>25000] rusage[mem=25000] span[hosts=1]"' }, 
      normal_30GB_2cpu        => {'LSF' => ' -n2 -M30000 -R"select[mem>30000] rusage[mem=30000] span[hosts=1]"' },      
-     normal_10gb_monitored   => {'LSF' => " -M10000 -R\"select[$ENV{DB_HOST_LSFNAME}<1000 && mem>10000]".
-                                           " rusage[$ENV{DB_HOST_LSFNAME}=10:duration=10:decay=1,mem=10000]\"" },                                                                         
-     normal_5GB_2cpu_monitored => {'LSF' => " -n2 -M5000 -R\"select[$ENV{DB_HOST_LSFNAME}<1000 && mem>5000]".
-                                                " rusage[$ENV{DB_HOST_LSFNAME}=10:duration=10:decay=1,mem=5000] span[hosts=1]\"" },
+     normal_10gb_monitored   => {'LSF' => " -M10000 -R\"select[mem>10000] rusage[mem=10000]\"" },
+     normal_5GB_2cpu_monitored => {'LSF' => " -n2 -M5000 -R\"select[mem>5000] rusage[mem=5000] span[hosts=1]\"" },
      normal_10gb             => { 'LSF' => ' -M10000 -R"select[mem>10000] rusage[mem=10000]"' },
-     long_monitored          => { 'LSF' => "-q long -R\"select[$ENV{DB_HOST_LSFNAME}<1000] ".
-                                            "rusage[$ENV{DB_HOST_LSFNAME}=10:duration=10:decay=1]\"" },
+     long_monitored          => { 'LSF' => "-q long " },
      long_high_mem           => { 'LSF' => '-q long -M4000 -R"select[mem>4000] rusage[mem=4000]"' },
-     long_monitored_high_mem => { 'LSF' => "-q long -M4000 -R\"select[$ENV{DB_HOST_LSFNAME}<1000 && mem>4000]".
-                                                " rusage[$ENV{DB_HOST_LSFNAME}=10:duration=10:decay=1,mem=4000]\"" },
+     long_monitored_high_mem => { 'LSF' => "-q long -M4000 -R\"select[mem>4000] rusage[mem=4000]\"" },
     };
 }
 
