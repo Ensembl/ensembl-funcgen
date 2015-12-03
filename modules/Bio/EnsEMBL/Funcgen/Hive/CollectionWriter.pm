2c7164dfab6018b81ed909288079a24441016345
Michael Nuhn <mnuhn@ebi.ac.uk>
2015-12-03 15:35:34 +0000

    Use bam2bed and some refactoring

--- modules/Bio/EnsEMBL/Funcgen/Hive/CollectionWriter.pm
+++ modules/Bio/EnsEMBL/Funcgen/Hive/CollectionWriter.pm
@@ -84,41 +84,75 @@ sub fetch_input {   # fetch parameters...
     my $exp = $rset->experiment(1);  # ctrl flag
     # But only in PreprocessAlignments no in WriteCollections 
     
-    if(! $exp->has_status('CONTROL_CONVERTED_TO_BED')){
-      #Make this status specific for now, just in case
-    
-      if($exp->has_status('CONVERTING_CONTROL_TO_BED')){
-         $self->input_job->transient_error(0); #So we don't retry  
-          #Would be nice to set a retry delay of 60 mins
-          throw($exp->name.' is in the CONVERTING_CONTROL_TO_BED state, another job may already be converting these controls'.
-            "\nPlease wait until ".$exp->name.' has the CONTROL_CONVERTED_TO_BED status before resubmitting this job');
-      }
-      else{
-        #Potential race condition here will fail on store
-        $exp->adaptor->store_status('CONVERTING_CONTROL_TO_BED', $exp); 
+#     if(! $exp->has_status('CONTROL_CONVERTED_TO_BED')){
+#       #Make this status specific for now, just in case
+#     
+#       if($exp->has_status('CONVERTING_CONTROL_TO_BED')){
+#          $self->input_job->transient_error(0); #So we don't retry  
+#           #Would be nice to set a retry delay of 60 mins
+#           throw($exp->name.' is in the CONVERTING_CONTROL_TO_BED state, another job may already be converting these controls'.
+#             "\nPlease wait until ".$exp->name.' has the CONTROL_CONVERTED_TO_BED status before resubmitting this job');
+#       }
+#       else{
+#         #Potential race condition here will fail on store
+#         $exp->adaptor->store_status('CONVERTING_CONTROL_TO_BED', $exp); 
        
-        $self->get_alignment_files_by_ResultSet_formats($rset,
-                                                        ['bed'],
-                                                        1); #control flag
+       my $path = $self->get_alignment_path_prefix_by_ResultSet($rset, 1);
+       
+       my $bam_file = $path . '.bam';
+       my $bed_file = $path . '.bed';
+       
+       if(! -e $bam_file) {
+	confess("Can't find bam file $bam_file!");
+       }
+       
+       if (! -e $bed_file) {
+	my $cmd = qq(bamToBed -i $bam_file > ${bed_file}.part);
+	run_system_cmd($cmd);
+	$cmd = qq(mv ${bed_file}.part $bed_file);
+	run_system_cmd($cmd);
+       }
+       
+#        die($path);
+       
+#         $self->get_alignment_files_by_ResultSet_formats($rset,
+#                                                         ['bed'],
+#                                                         1); #control flag
 
-        # This is creating the prepared bed file in a subdir
-      
-        #todo check success of this in case another job has pipped us
-        $exp->adaptor->store_status('CONTROL_CONVERTED_TO_BED',   $exp); 
-        $exp->adaptor->revoke_status('CONVERTING_CONTROL_TO_BED', $exp, 1);#Validate status flag
-      }   
-    }
+#         # This is creating the prepared bed file in a subdir
+#       
+#         #todo check success of this in case another job has pipped us
+#         $exp->adaptor->store_status('CONTROL_CONVERTED_TO_BED',   $exp); 
+#         $exp->adaptor->revoke_status('CONVERTING_CONTROL_TO_BED', $exp, 1);#Validate status flag
+#       }   
+#     }
   }
-
   
+  my $path = $self->get_alignment_path_prefix_by_ResultSet($rset);
+
+  my $bam_file = $path . '.bam';
+  my $bed_file = $path . '.bed';
+  
+  if(! -e $bam_file) {
+    confess("Can't find bam file $bam_file!");
+  }
+  
+  if (! -e $bed_file) {
+    my $cmd = qq(bamToBed -i $bam_file > ${bed_file}.part);
+    run_system_cmd($cmd);
+    $cmd = qq(mv ${bed_file}.part $bed_file);
+    run_system_cmd($cmd);
+  }
   
   #This currently keeps the sam files! Which is caning the lfs quota                                                                    
   warn "Hardcoded PreprocessAlignments to get bed only, as sam intermediate was eating quota";
    
-  my $align_files = $self->get_alignment_files_by_ResultSet_formats($rset, ['bam', 'bed']);
+  #my $align_files = $self->get_alignment_files_by_ResultSet_formats($rset, ['bam', 'bed']);
+  my $align_files = $self->get_alignment_files_by_ResultSet_formats($rset, ['bam']);
                                                                 #    $self->param_required('feature_formats'),
                                                                 #    undef, #control flag
                                                                 #    1);     #all formats
+  $align_files->{bed} = $bed_file;
                                                                                                                                                                             
   $self->set_param_method('bam_file', $align_files->{bam}, 'required');  # For bai test/creation 
                                                                     
@@ -188,70 +222,52 @@ sub fetch_input {   # fetch parameters...
 sub run {   # Check parameters and do appropriate database/file operations... 
   my $self = shift;
   my $Imp  = $self->get_Importer;
-    
-  if(! $self->param_silent('merge')){ #Prepare or write slice col
-   
-    if( ! $Imp->prepared ){  #Preparing data...
-
-      # Test/create bai index file
-      if(! -e $self->bam_file.'.bai'){
-        my $cmd = 'samtools index '.$self->bam_file;  # -b option not require for this version
-        run_system_cmd($cmd);
-      } 
-
-      $Imp->read_and_import_data('prepare');
-   
-
-      # This now only rebokes the IMPORTED status
-      # so we will need to manage that before we can remove the Importer usage
-      # We're not actually storing anything yet, so that can be done in
-      # the PeakCaller/BigWigWriter?
-      # This was also creating the prepared bed for collection generation
-      # Looks like this wasn't being used for CCAT peak calling
-      # There small potential that an unsorted bed file could be used for CCAT
-      # Although this is always generated from the sorted bam at present,
-      # as opposed to a pre-computed unverified/sorted bed file.
-
-    
-      my $output_id = {%{$self->batch_params}, 
-                       # These are already param_required by fetch_Set_input
-                       dbID         => $self->param('dbID'),
-                       set_name     => $self->param('set_name'),  # mainly for readability
-                       set_type     => $self->param('set_type'),
-                       filter_from_format => undef,                 
-                     }; 
-
-      #Need to add a final semaphored job, to do the clean up
-      #bed files only, as we keep the bams
-      $self->branch_job_group(2, [$output_id]); #BigWigWriter data flow
-
-      my $fset = $self->FeatureSet;
-      
-      # TODO
-      # Do we need to be able to do this conditionally?
-      # If we are re-running then data flowing here will create duplicate jobs
-      # on any branch which has run successfully before
-      # this is fine if the input_id is the same and we are using the same hive
-      # as it will not create the job
-      # But if we have reseeded with slightly different batch_params
-      # or we are using a new hive, then this will try to rerun
-      # the next analyses
-      
-      # Maybe we don't even won't to run the peaks, even though we have a feature set?
-      # see pipeline dev notes
-      
-      if(defined $fset){
-        $self->branch_job_group('run_'.$fset->analysis->logic_name, [{%$output_id}]);
-      }
-    } 
-    else { #These are the fanned slice jobs  
-      $self->throw_no_retry('CollectionWriter no longer supports collection generation');
-    } 
-  }
-  else{
+  
+  if($self->param_silent('merge')){
     $self->throw_no_retry('CollectionWriter no longer supports collection merge mode');
   }
+  if($Imp->prepared ){
+    $self->throw_no_retry('CollectionWriter no longer supports collection generation');
+  }
+  #Prepare or write slice col
+  
+  # Test/create bai index file
+  if(! -e $self->bam_file.'.bai'){
+    my $cmd = 'samtools index '.$self->bam_file;  # -b option not require for this version
+    run_system_cmd($cmd);
+  } 
 
+  $Imp->read_and_import_data('prepare');
+
+  # This now only rebokes the IMPORTED status
+  # so we will need to manage that before we can remove the Importer usage
+  # We're not actually storing anything yet, so that can be done in
+  # the PeakCaller/BigWigWriter?
+  # This was also creating the prepared bed for collection generation
+  # Looks like this wasn't being used for CCAT peak calling
+  # There small potential that an unsorted bed file could be used for CCAT
+  # Although this is always generated from the sorted bam at present,
+  # as opposed to a pre-computed unverified/sorted bed file.
+
+
+  my $output_id = {%{$self->batch_params}, 
+		    # These are already param_required by fetch_Set_input
+		    dbID         => $self->param('dbID'),
+		    set_name     => $self->param('set_name'),  # mainly for readability
+		    set_type     => $self->param('set_type'),
+		    filter_from_format => undef,                 
+		  }; 
+
+  #Need to add a final semaphored job, to do the clean up
+  #bed files only, as we keep the bams
+  $self->branch_job_group(2, [$output_id]); #BigWigWriter data flow
+
+  my $fset = $self->FeatureSet;
+  
+  if(defined $fset){
+    $self->branch_job_group('run_'.$fset->analysis->logic_name, [{%$output_id}]);
+  }
+  
   return;
 }
 
