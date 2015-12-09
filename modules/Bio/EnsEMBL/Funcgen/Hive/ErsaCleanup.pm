0ed3335120945c9dffff52aadc1368f530ec4f00
Michael Nuhn <mnuhn@ebi.ac.uk>
2015-12-09 10:56:18 +0000

    Get rid of .sav files in the peaks directory

--- modules/Bio/EnsEMBL/Funcgen/Hive/ErsaCleanup.pm
+++ modules/Bio/EnsEMBL/Funcgen/Hive/ErsaCleanup.pm
@@ -8,7 +8,7 @@ Bio::EnsEMBL::Funcgen::Hive::ErsaCleanup
 
 package Bio::EnsEMBL::Funcgen::Hive::ErsaCleanup;
 
-use base ('Bio::EnsEMBL::Funcgen::Hive::Base');
+use base ('Bio::EnsEMBL::Funcgen::Hive::BaseDB');
 
 use warnings;
 use strict;
@@ -16,6 +16,15 @@ use Bio::EnsEMBL::Funcgen::Utils::EFGUtils qw( run_system_cmd );
 
 sub run {
   my $self = shift; 
+  
+  my $peaks_output_dir = $self->peaks_output_dir;
+  
+  # Removes .sav files from the peaks directory. These are saved R sessions
+  # for which there is no use case.
+  #
+  my $rm_sav_cmd = qq(bash -o pipefail -c "find $peaks_output_dir -name '*.sav' | xargs rm -f ");
+  
+  run_system_cmd($rm_sav_cmd);
   
   my $file_to_delete_after_cell_line_has_been_processed = $self->param('file_to_delete_after_cell_line_has_been_processed');
   
