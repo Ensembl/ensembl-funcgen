582238883731ff23471b71b6383ae4651bb4547e
Ilias Lavidas <ilavidas@ebi.ac.uk>
2016-01-04 19:24:03 +0200

    skip BindingMatrix_MotifFeature.t

--- travisci/harness.sh
+++ travisci/harness.sh
@@ -6,8 +6,7 @@ echo "Running test suite"
 echo "Using $PERL5LIB"
 
 # skipping existing failing tests
-# SKIP_TESTS="--skip AnnotatedFeature.t,Annotated_SetFeatureAdaptor.t,BaseFeatureAdaptor.t,BindingMatrix_MotifFeature.t,CoordSystem.t,DataSet.t,DNAMethylationFeature.t,ExperimentalGroup.t,FeatureSet.t,FeatureType.t,InputSet_Set_BaseAdaptor.t,MultiTestDB.t,RegulatoryFeature.t,ResultFeature.t,ResultSet.t,Storable.t"
-SKIP_TESTS="--skip Annotated_SetFeatureAdaptor.t,BaseFeatureAdaptor.t,CoordSystem.t,FeatureSet.t,FeatureType.t,InputSet_Set_BaseAdaptor.t,MultiTestDB.t,RegulatoryFeature.t,ResultSet.t,Storable.t"
+SKIP_TESTS="--skip Annotated_SetFeatureAdaptor.t,BaseFeatureAdaptor.t,BindingMatrix_MotifFeature.t,CoordSystem.t,FeatureSet.t,FeatureType.t,InputSet_Set_BaseAdaptor.t,MultiTestDB.t,RegulatoryFeature.t,ResultSet.t,Storable.t"
 
 if [ "$COVERALLS" = 'true' ]; then
   PERL5OPT='-MDevel::Cover=+ignore,bioperl,+ignore,ensembl-test,+ignore,ensembl,+ignore,\.t$$' perl $PWD/ensembl-test/scripts/runtests.pl -verbose $PWD/modules/t/ $SKIP_TESTS
