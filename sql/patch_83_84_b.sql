e975ea8548f815eee01bb6db836e665cbd7b1be5
Ilias Lavidas <ilavidas@ebi.ac.uk>
2016-01-29 10:30:04 +0000

    add sql schema patches for e84

---
+++ sql/patch_83_84_b.sql
@@ -0,0 +1,23 @@
+-- Copyright [1999-2016] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+/**
+@header patch_83_84_b.sql - Drop unique key for cell_type.efo_id
+@desc   It's possible for two or more entries to have the same efo_id (i.e. different biological replicates of the same cell type)
+*/
+
+ALTER TABLE cell_type DROP INDEX efo_idx;
+
+-- patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_83_84_b.sql|Drop unique key for cell_type.efo_id');
