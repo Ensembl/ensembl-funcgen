37239c46cdead3cfff035884a56de6c67c93c7e0
Michael Nuhn <mnuhn@ebi.ac.uk>
2016-02-09 13:20:43 +0000

    Merged into Bio::EnsEMBL::Funcgen::Hive::Config::Base

--- modules/Bio/EnsEMBL/Funcgen/Hive/Config/Base.pm
+++ modules/Bio/EnsEMBL/Funcgen/Hive/Config/Base.pm
@@ -42,6 +42,17 @@ sub default_options {
   
   return {
       %{$self->SUPER::default_options},
+      
+	dnadb_pass          => $self->o('ENV', 'DNADB_PASS'),
+	pass                => $self->o('ENV', 'DB_PASS'),
+	dnadb_port          => undef,
+	port                => undef,
+
+	# These can probably go:
+	#
+	ssh                 => undef, #Connect to DBs using ssh(use in Importer)
+	result_set_only    => 0, #why is this 0 rather than undef?
+
       use_tracking_db     => 1,
       species             => undef,
 
@@ -75,6 +86,21 @@ sub pipeline_wide_parameters {
 
     hive_output_dir => $self->o('hive_output_dir'),
     use_tracking_db => $self->o('use_tracking_db'),
+
+      dnadb   => {
+         -dnadb_host   => $self->o('dnadb_host'),
+         -dnadb_pass   => $self->o('dnadb_pass'),
+         -dnadb_port   => $self->o('dnadb_port'),
+         -dnadb_user   => $self->o('dnadb_user'),
+         -dnadb_name   => $self->o('dnadb_name'),
+        },
+      out_db  => {
+         -host   => $self->o('host'),
+         -port   => $self->o('port'),
+         -user   => $self->o('user'),
+         -pass   => $self->o('pass'),
+         -dbname => $self->o('dbname'),
+        },
   };
 }
 
