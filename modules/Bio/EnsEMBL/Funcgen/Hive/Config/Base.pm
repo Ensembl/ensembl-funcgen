bba3642364be2869eb6e600729f8f6b1772d9068
Michael Nuhn <mnuhn@ebi.ac.uk>
2016-02-10 15:43:34 +0000

    A merge job was suspended. Looks like the merge analysis can use more than two cpus at times.

--- modules/Bio/EnsEMBL/Funcgen/Hive/Config/Base.pm
+++ modules/Bio/EnsEMBL/Funcgen/Hive/Config/Base.pm
@@ -14,18 +14,28 @@ sub default_options {
       use_tracking_db   => 1,
       work_root_dir     => $self->o('data_root_dir').'/output/'.$self->o('pipeline_name'),
       hive_output_dir   => $self->o('data_root_dir').'/output/'.$self->o('pipeline_name').'/hive_debug',
+      port              => 3306,
+      dnadb_port        => 3306,
+      pipeline_name     => 'ersa',
    };
 }
 
 sub pipeline_wide_parameters {
   my ($self) = @_;
 
+  my $dnadb_pass = $self->o('dnadb_pass');
+  # This is code for no password on the command line.
+  #
+  if (($dnadb_pass eq "''") || ($dnadb_pass eq "\"\"")) {
+    $dnadb_pass = undef;
+  }
+  
   return {
     %{$self->SUPER::pipeline_wide_parameters},
 
     dnadb   => {
 	-dnadb_host   => $self->o('dnadb_host'),
-	-dnadb_pass   => $self->o('dnadb_pass'),
+	-dnadb_pass   => $dnadb_pass,
 	-dnadb_port   => $self->o('dnadb_port'),
 	-dnadb_user   => $self->o('dnadb_user'),
 	-dnadb_name   => $self->o('dnadb_name'),
@@ -67,6 +77,7 @@ sub resource_classes {
      normal_20GB_2cpu        => {'LSF' => ' -n2 -M20000 -R"select[mem>20000] rusage[mem=20000] span[hosts=1]"' }, 
      normal_25GB_2cpu        => {'LSF' => ' -n2 -M25000 -R"select[mem>25000] rusage[mem=25000] span[hosts=1]"' }, 
      normal_30GB_2cpu        => {'LSF' => ' -n2 -M30000 -R"select[mem>30000] rusage[mem=30000] span[hosts=1]"' },      
+     normal_30GB_3cpu        => {'LSF' => ' -n3 -M30000 -R"select[mem>30000] rusage[mem=30000] span[hosts=1]"' },      
      normal_10gb_monitored   => {'LSF' => " -M10000 -R\"select[mem>10000] rusage[mem=10000]\"" },
      normal_5GB_2cpu_monitored => {'LSF' => " -n2 -M5000 -R\"select[mem>5000] rusage[mem=5000] span[hosts=1]\"" },
      normal_10gb             => { 'LSF' => ' -M10000 -R"select[mem>10000] rusage[mem=10000]"' },
