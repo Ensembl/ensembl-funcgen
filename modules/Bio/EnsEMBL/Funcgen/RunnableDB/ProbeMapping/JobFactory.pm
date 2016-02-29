c3e611891b5bc1608ed3b327813c10a0721a511a
Michael Nuhn <mnuhn@ebi.ac.uk>
2016-02-29 16:53:45 +0000

    Create all_array_names parameter for rollback

--- modules/Bio/EnsEMBL/Funcgen/RunnableDB/ProbeMapping/JobFactory.pm
+++ modules/Bio/EnsEMBL/Funcgen/RunnableDB/ProbeMapping/JobFactory.pm
@@ -17,6 +17,10 @@ sub run {
     my @list = grep !/^\.\.?$/, readdir(D);
     closedir(D);
 
+    my $tracking_dba_hash = $self->param('tracking_dba_hash');
+    use Bio::EnsEMBL::Funcgen::RunnableDB::ProbeMapping::Utils qw( create_funcgen_adaptor );
+    my $funcgen_adaptor = create_funcgen_adaptor($tracking_dba_hash);
+    
     my $input_id = [];
     DIR: foreach my $subdirectory (@list) {
     
@@ -27,12 +31,28 @@ sub run {
       next DIR if (! -d $full_path);
       next DIR if ($full_path =~ /GENOMICSEQS/);
       next DIR if ($full_path =~ /TRANSCRIPTSEQS/);
+      
+      my $arrayref_of_arrayref_with_one_element_which_is_the_name = $funcgen_adaptor->dbc->db_handle->selectall_arrayref(
+	qq(select name from array where class="$subdirectory")
+      );
 
-      push @$input_id, {    
-	array_format => $subdirectory,
-	QUERYSEQS    => File::Spec->catfile($full_path, 'arrays.'.$subdirectory.'.fasta'),
+      # Dereference the individual array refs so we get an array of array 
+      # names
+      #
+      my @array_of_array_names = map { @$_ } @$arrayref_of_arrayref_with_one_element_which_is_the_name;
+
+      if (! @array_of_array_names) {
+	die("Can't find an array with class $subdirectory");
+      }
+      
+      my $all_array_names = join " ", @array_of_array_names;
+
+      push @$input_id, {
+	array_format    => $subdirectory,
+	QUERYSEQS       => File::Spec->catfile($full_path, 'arrays.'.$subdirectory.'.fasta'),
+	all_array_names => $all_array_names,
       };
-    }    
+    }
     foreach my $current_input_id (@$input_id) {
       $self->dataflow_output_id( $current_input_id, 1 );
     }
