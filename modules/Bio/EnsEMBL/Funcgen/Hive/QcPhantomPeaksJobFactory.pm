08a9a6f845e8433bd8efa40646ba4d0e43f58925
Michael Nuhn <mnuhn@ebi.ac.uk>
2016-03-15 10:31:19 +0000

    Removed obsolete documentation

--- modules/Bio/EnsEMBL/Funcgen/Hive/QcPhantomPeaksJobFactory.pm
+++ modules/Bio/EnsEMBL/Funcgen/Hive/QcPhantomPeaksJobFactory.pm
@@ -1,45 +1,3 @@
-
-=pod 
-
-=head1 NAME
-
-Bio::EnsEMBL::Funcgen::Hive::QcPhantomPeaksJobFactory
-
-=head1 DESCRIPTION
-
-default_stuff='out_db => {"-dbname" => "mn1_faang2_tracking_homo_sapiens_funcgen_81_38","-host" => "ens-genomics1","-pass" => "ensembl","-port" => 3306,"-user" => "ensadmin"}, work_root_dir => "/lustre/scratch109/ensembl/funcgen/mn1/ersa/faang/debug", data_root_dir => "/lustre/scratch109/ensembl/funcgen/mn1/ersa/faang/", pipeline_name => "blah", use_tracking_db => 1, dnadb => {"-dnadb_host" => "ens-livemirror","-dnadb_name" => "homo_sapiens_core_82_38","-dnadb_pass" => "","-dnadb_port" => 3306,"-dnadb_user" => "ensro"}'
-
-standaloneJob.pl Bio::EnsEMBL::Funcgen::Hive::QcPhantomPeaksJobFactory -input_id "{ $default_stuff, input_subset_id => 1234, }"
-
-export R_LIBS=/software/ensembl/funcgen/R-modules
-
-        {   -logic_name => 'QcRunPhantomPeaks',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -meadow_type=> 'LSF',
-            -parameters => { 
-		  cmd => 
-		    qq( Rscript /software/ensembl/funcgen/spp_package/run_spp.R )
-		  . qq(    -c=#bam_file# )
-		  . qq(    -savp -out=#phantom_peak_out_file# )
-            },
-            -flow_into => { 
-	      '1' => [ 'QCLoadPhantomPeaksToDB' ],
-            },
-        },
-        
-        {   -logic_name => 'QCLoadPhantomPeaksToDB',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -meadow_type=> 'LOCAL',
-	    -parameters => {
-                'cmd' =>
-		    qq( load_phantom_peak_file.pl )
-		  . qq(    --result_set_id #result_set_id# )
-		  . qq(    --result_file #phantom_peak_out_file# )
-		  . qq(    --user #tracking_db_user# --pass #tracking_db_pass# --host #tracking_db_host# --dbname #tracking_db_name# )
-            },
-            
-=cut
-
 package Bio::EnsEMBL::Funcgen::Hive::QcPhantomPeaksJobFactory;
 
 use warnings;
@@ -49,10 +7,8 @@ use base qw( Bio::EnsEMBL::Funcgen::Hive::BaseDB );
 
 sub run {
   my $self = shift;
-  #my $input_subset_id = $self->param('input_subset_id');
 
   my $rset;
-  
   my $set_type = $self->param_required('set_type');
   if ($set_type eq 'ResultSet') {
     $rset = $self->fetch_Set_input('ResultSet'); 
@@ -64,10 +20,6 @@ sub run {
 
   my $input_id = $self->create_input_id($rset);
   
-  use Data::Dumper;
-  $Data::Dumper::Maxdepth = 0;
-  print Dumper($input_id);
-
   $self->dataflow_output_id($input_id, 2);
   return;
 }
@@ -79,18 +31,26 @@ sub create_input_id {
   my $result_set = shift;
   my $result_set_id = $result_set->dbID;
 
-  my $align_prefix   = $self->get_alignment_path_prefix_by_ResultSet($result_set, undef, 1);#validate aligned flag 
-  #my $control_prefix = $self->get_alignment_path_prefix_by_ResultSet($result_set, 1, 1);#and control flag 
+  my $is_control = $self->param('is_control') ? 1 : undef;
   
-  my $signal_bam_file  = $align_prefix   . '.bam';
-  #my $control_bam_file = $control_prefix . '.bam';
+  my $align_prefix   = $self->get_alignment_path_prefix_by_ResultSet($result_set, $is_control, 1);  
+  my $bam_file  = $align_prefix   . '.bam';
   
-  if (! -e $signal_bam_file) {
-    die("$signal_bam_file doesn't exist!");
+  if (! -e $bam_file) {
+
+    use Bio::EnsEMBL::Utils::Logger;
+    my $logger = Bio::EnsEMBL::Utils::Logger->new();
+    $logger->error(
+      "The bam file $bam_file doesn't exist. This can happen, if it has just "
+      . "been created and the file system has not been updated yet on all "
+      . "nodes. In cases like these it may be resolved by restarting after "
+      . "waiting a bit. This job will sleep for 10 seconds now and then die. "
+      . "Hopefully upon retry it will work.\n"
+    );
+    sleep(10);
+    die("$bam_file doesn't exist. Dying now and hoping for more luck in "
+      . "the next life.");
   }
-#   if (! -e $control_bam_file) {
-#     die("$control_bam_file doesn't exist!");
-#   }
   
   my $work_dir = $self->param_required('work_root_dir');
   my $temp_dir = "$work_dir/temp/Qc/PhantomPeaks/$result_set_id";
@@ -100,10 +60,7 @@ sub create_input_id {
   make_path($temp_dir);
   
   use File::Basename;
-  (my $signal_bam_file_base_name,  my $signal_bam_directory)  = fileparse($signal_bam_file);
-#  (my $control_bam_file_base_name, my $control_bam_directory) = fileparse($control_bam_file);
-  
- # die unless($signal_bam_directory eq $control_bam_directory);
+  (my $bam_file_base_name,  my $signal_bam_directory)  = fileparse($bam_file);
 
   my $input_id_common = [
       # Directory into which the bam files will be copied
@@ -118,23 +75,13 @@ sub create_input_id {
       tracking_db_port   => $out_db->dbc->port,
   ];
   
-  my $signal_phantom_peaks_file  = "$temp_dir/${signal_bam_file_base_name}.signal_phantom_peaks.txt";
-#  my $control_phantom_peaks_file = "$temp_dir/${control_bam_file_base_name}.control_phantom_peaks.txt";
+  my $signal_phantom_peaks_file  = "$temp_dir/${bam_file_base_name}.phantom_peaks.txt";
   
-  my $input_id = [
-    {
+  my $input_id = {
       @$input_id_common,
       phantom_peak_out_file  => $signal_phantom_peaks_file,
-      bam_file => $signal_bam_file,
-      is_control => undef,
-    },
-#     {
-#       @$input_id_common,
-#       phantom_peak_out_file  => $control_phantom_peaks_file,
-#       bam_file => $control_bam_file,
-#       is_control => 1,
-#     }
-  ];
+      bam_file => $bam_file,
+  };
   return $input_id;
 }
 
