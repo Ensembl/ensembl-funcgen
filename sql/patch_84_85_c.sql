8ff70b392102e6c4fcf6f0bbf18095eba25bf18b
Ilias Lavidas <ilavidas@ebi.ac.uk>
2016-03-18 12:14:47 +0000

    add missing lines to license

--- sql/patch_84_85_c.sql
+++ sql/patch_84_85_c.sql
@@ -1,3 +1,6 @@
+-- Copyright [1999-2016] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
 -- You may obtain a copy of the License at
 --
