9b0f1de8742ac3192b702ff164542a8830ad728e
Michael Nuhn <mnuhn@ebi.ac.uk>
2016-04-06 09:28:19 +0100

    Renamed to file_to_delete

--- modules/Bio/EnsEMBL/Funcgen/Hive/ErsaCleanup.pm
+++ modules/Bio/EnsEMBL/Funcgen/Hive/ErsaCleanup.pm
@@ -17,8 +17,6 @@ use Bio::EnsEMBL::Funcgen::Utils::EFGUtils qw( run_system_cmd );
 sub run {
   my $self = shift; 
   
-  my $peaks_output_dir = $self->peaks_output_dir;
-  
   # Removes .sav files from the peaks directory. These are saved R sessions
   # for which there is no use case.
   #
@@ -26,12 +24,13 @@ sub run {
   # so we have to be selective and use strain names here to narrow this down
   # to the .sav files belonging to cell lines that are actually done.
   #
+  # my $peaks_output_dir = $self->peaks_output_dir;
   # my $rm_sav_cmd = qq(bash -o pipefail -c "find $peaks_output_dir -name '*.sav' | xargs rm -f ");
   # run_system_cmd($rm_sav_cmd);
   
-  my $file_to_delete_after_cell_line_has_been_processed = $self->param('file_to_delete_after_cell_line_has_been_processed');
+  my $file_to_delete = $self->param('file_to_delete');
   
-  foreach my $current_file (@$file_to_delete_after_cell_line_has_been_processed) {
+  foreach my $current_file (@$file_to_delete) {
     my $cmd = "rm -f $current_file";
     print $cmd . "\n";
     run_system_cmd($cmd); 
