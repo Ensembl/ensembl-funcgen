418f0caadc7b357ae4b125c070effa83668e6f5c
Michael Nuhn <mnuhn@ebi.ac.uk>
2016-05-13 10:16:47 +0100

    More patches

---
+++ sql/patch_84_85_p.sql
@@ -0,0 +1,24 @@
+-- Copyright [1999-2016] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+/**
+@header patch_84_85_o.sql - Delete MultiCell epigenome
+@desc   Delete all MultiCell regulatory features feature set entries.
+*/
+
+delete from feature_set where name = "RegulatoryFeatures:MultiCell";
+delete from epigenome where name = "MultiCell";
+
+insert into meta (species_id, meta_key, meta_value) values (null, 'patch', 'patch_84_85_p.sql|Delete MultiCell epigenome.');
+
