df30d11bc7cfd7691340aa26ab2ad2a579bccc9a
Ilias Lavidas <ilavidas@ebi.ac.uk>
2016-05-17 10:29:47 +0100

    add patch for dropping empty unused tables

---
+++ sql/patch_84_85_r.sql
@@ -0,0 +1,30 @@
+-- Copyright [1999-2016] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+/**
+@header patch_83_84_r.sql - Drop unused empty tables
+@desc   These tables are empty and of no use. No need to keep them
+*/
+
+DROP TABLE associated_group;
+DROP TABLE channel;
+DROP TABLE experimental_chip;
+DROP TABLE input_set;
+DROP TABLE input_set_input_subset;
+DROP TABLE mage_xml;
+DROP TABLE result;
+DROP TABLE result_feature;
+
+-- patch identifier
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_83_84_r.sql|Drop unused empty tables');
