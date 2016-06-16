379646328bbeafc9fbbc374408354716487e0ee9
Michael <mn1@users.noreply.github.com>
2016-06-16 12:19:44 +0100

    Update ErsaPrePipelineChecks.pm

--- modules/Bio/EnsEMBL/Funcgen/Hive/ErsaPrePipelineChecks.pm
+++ modules/Bio/EnsEMBL/Funcgen/Hive/ErsaPrePipelineChecks.pm
@@ -19,7 +19,7 @@ sub run {
     
     # Check that the values in the replicate column aren't duplicated.
     
-    my $find_non_unique_replicate_specifications = 'select epigenome_id, experiment_id, biological_replicate, technical_replicate, count(*) c from input_subset group by epigenome_id, experiment_id, biological_replicate, technical_replicate having c > 1';
+    my $find_non_unique_replicate_specifications = 'select epigenome_id, experiment_id, biological_replicate, technical_replicate, count(*) c from input_subset where is_control=0 group by epigenome_id, experiment_id, biological_replicate, technical_replicate having c > 1';
     
     my $sth = $dbc->prepare($find_non_unique_replicate_specifications);
     $sth->execute;
