dd78953431796fd1b3cf65ebc96c354df690b84f
Michael Nuhn <mnuhn@ebi.ac.uk>
2016-09-09 15:00:42 +0100

    More work on ftp pipeline

--- modules/Bio/EnsEMBL/Funcgen/HiveConfig/Ftp/CreateMetaFiles.pm
+++ modules/Bio/EnsEMBL/Funcgen/HiveConfig/Ftp/CreateMetaFiles.pm
@@ -2,24 +2,8 @@ package Bio::EnsEMBL::Funcgen::HiveConfig::Ftp::CreateMetaFiles;
 
 use strict;
 use warnings;
-use base 'Bio::EnsEMBL::Hive::PipeConfig::EnsemblGeneric_conf';
+use base 'Bio::EnsEMBL::Funcgen::HiveConfig::Ftp::Base';
 use Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf;
-
-sub pipeline_wide_parameters {
-    my ($self) = @_;
-    return {
-      %{$self->SUPER::pipeline_wide_parameters},
-
-      ftp_base_dir  => $self->o('ftp_base_dir'),
-      reg_conf      => $self->o('reg_conf'),
-      temp_dir      => '#ftp_base_dir#/tempdir/annotated_features',
-    };
-}
-
-sub beekeeper_extra_cmdline_options {
-    my ($self) = @_;
-    return '-reg_conf ' . $self->o('reg_conf') . ' -keep_alive -can_respecialize 1';
-}
 
 sub pipeline_analyses {
     my $self = shift;
--- modules/Bio/EnsEMBL/Funcgen/HiveConfig/Ftp/ExportMotifFeatures.pm
+++ modules/Bio/EnsEMBL/Funcgen/HiveConfig/Ftp/ExportMotifFeatures.pm
@@ -2,23 +2,8 @@ package Bio::EnsEMBL::Funcgen::HiveConfig::Ftp::ExportMotifFeatures;
 
 use strict;
 use warnings;
-use base 'Bio::EnsEMBL::Hive::PipeConfig::EnsemblGeneric_conf';
+use base 'Bio::EnsEMBL::Funcgen::HiveConfig::Ftp::Base';
 use Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf;
-
-sub pipeline_wide_parameters {
-    my ($self) = @_;
-    return {
-      %{$self->SUPER::pipeline_wide_parameters},
-
-      ftp_base_dir  => $self->o('ftp_base_dir'),
-      reg_conf      => $self->o('reg_conf'),
-    };
-}
-
-sub beekeeper_extra_cmdline_options {
-    my ($self) = @_;
-    return '-reg_conf ' . $self->o('reg_conf') . ' -keep_alive -can_respecialize 1';
-}
 
 sub pipeline_analyses {
     my $self = shift;
