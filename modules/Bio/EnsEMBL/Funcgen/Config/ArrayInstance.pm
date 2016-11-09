4ad15cf9c3da649c971b4c2d94d44f565bec03a9
Michael Nuhn <mnuhn@ebi.ac.uk>
2016-11-09 16:46:41 +0000

    New modules for probe mapping

--- modules/Bio/EnsEMBL/Funcgen/Config/ArrayInstance.pm
+++ modules/Bio/EnsEMBL/Funcgen/Config/ArrayInstance.pm
@@ -2,13 +2,12 @@ package Bio::EnsEMBL::Funcgen::Config::ArrayInstance;
 
 use strict;
 use Data::Dumper;
-use List::Util qw(any);
 use Bio::EnsEMBL::Funcgen::Config::ImportArrays;
 use Bio::EnsEMBL::Analysis::Tools::Utilities qw(parse_config);
 
 sub new {
   my ( $class, @args ) = @_;
-  my $self = bless {}, $class;
+  my $self = bless {}, $class;  
   return $self;
 }
 
@@ -16,7 +15,7 @@ sub read_and_check_config {
 
   my $self = shift;
   my $array_format = shift;
-
+  
   my $former_logic_name = 'IMPORT_'. uc($array_format) .'_ARRAYS';
   parse_config($self, $ARRAY_CONFIG, $former_logic_name);
 
@@ -30,15 +29,10 @@ sub read_and_check_config {
 sub get_ARRAY_PARAMS_by_array_name {
   my ( $self, $array_name ) = @_;
 
-  if (any { $_ eq $array_name } @{$self->{'_CONFIG_ARRAYS_WITH_DEFAULT_PARAMS'}})
-  {
-    $self->{'_CONFIG_ARRAY_PARAMS'}->{$array_name} = $self->{'_CONFIG_ARRAY_PARAMS'}->{'Default'};
-    $self->{'_CONFIG_ARRAY_PARAMS'}->{$array_name}->{'-name'} = $array_name;
-   } elsif(! exists $self->{'_CONFIG_ARRAY_PARAMS'}{$array_name}) {
-    use Carp;
-    confess("No ARRAY_PARAMS config available for $array_name.  You must add this to the ImportArrays config before importing");
- }
-
+  if(! exists $self->{'_CONFIG_ARRAY_PARAMS'}{$array_name}) {
+        use Carp;
+        confess("No ARRAY_PARAMS config available for $array_name.  You must add this to the ImportArrays config before importing");
+  }
   return $self->{'_CONFIG_ARRAY_PARAMS'}{$array_name};
 }
 
@@ -73,12 +67,6 @@ sub INPUT_FORMAT{
 sub ARRAY_PARAMS {
   my ( $self, $value ) = @_;
   $self->{'_CONFIG_ARRAY_PARAMS'} = $value  if defined $value;
-  return $self->{'_CONFIG_ARRAY_PARAMS'};
-}
-
-sub ARRAYS_WITH_DEFAULT_PARAMS {
-  my ( $self, $value ) = @_;
-  $self->{'_CONFIG_ARRAYS_WITH_DEFAULT_PARAMS'} = $value  if defined $value;
   return $self->{'_CONFIG_ARRAY_PARAMS'};
 }
 
