87cacaccc743eb94aa145dafc8399331150626ba
Michael Nuhn <mnuhn@ebi.ac.uk>
2017-03-07 13:32:51 +0000

    Renamed modules: They are not that new anymore.

--- modules/Bio/EnsEMBL/Funcgen/HiveConfig/ProbeMapping/AlignProbes_conf.pm
+++ modules/Bio/EnsEMBL/Funcgen/HiveConfig/ProbeMapping/AlignProbes_conf.pm
@@ -3,6 +3,7 @@ package Bio::EnsEMBL::Funcgen::HiveConfig::ProbeMapping::AlignProbes_conf;
 use strict;
 use warnings;
 
+use Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf;           # Allow this particular config to use conditional dataflow and INPUT_PLUS
 use base ('Bio::EnsEMBL::Funcgen::HiveConfig::ProbeMapping::Base');
 
 sub pipeline_analyses {
@@ -10,80 +11,151 @@ sub pipeline_analyses {
     
     return [
       {
-          -logic_name  => 'start',
+          -logic_name  => 'start_align_probes',
           -module     => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
-          -input_ids => [
-            { species => 'homo_sapiens', },
-          ],
           -flow_into => {
               MAIN => 'job_factory_probe_align',
           },
       },
         {   -logic_name  => 'job_factory_probe_align',
-            -module      => 'Bio::EnsEMBL::Funcgen::RunnableDB::FastaFactory',
+            -module      => 'Bio::EnsEMBL::Hive::RunnableDB::FastaFactory',
             -parameters => {
               inputfile        => '#unmapped_sequences_file#',
               output_dir       => '#tempdir#/#species#/probe_chunks',
-              max_chunk_length => 1000,
+              max_chunk_length => 100000,
               output_prefix    => 'probe_chunk_',
               output_suffix    => '.fasta',
               hash_directories => 1,
             },
             -flow_into => {
-                MAIN => [ 
-                  'probe_align_genomic',
-                  'probe_align_transcript', 
+                2 => [ 
+                  { 'multiply_probe_align_jobs' => INPUT_PLUS },
                 ],
             },
         },
+      {
+          -logic_name  => 'multiply_probe_align_jobs',
+          -module     => 'Bio::EnsEMBL::Funcgen::Hive::AddJobParameter',
+          -parameters => {
+            add => {
+              species => '#species#',
+            }
+          },
+          -flow_into => {
+              2 => [
+                'start_align_probes_genomic',
+                'start_align_probes_transcript'
+               ],
+          },
+      },
+      {
+          -logic_name  => 'start_align_probes_genomic',
+          -module     => 'Bio::EnsEMBL::Funcgen::Hive::AddJobParameter',
+          -parameters => {
+            add => {
+              type    => 'genomic',
+            }
+          },
+          -flow_into => {
+              2 => 'probe_align_genomic',
+          },
+      },
+      {
+          -logic_name  => 'start_align_probes_transcript',
+          -module     => 'Bio::EnsEMBL::Funcgen::Hive::AddJobParameter',
+          -parameters => {
+            add => {
+              type    => 'transcript',
+            }
+          },
+          -flow_into => {
+              2 => 'probe_align_transcript',
+          },
+      },
         {   -logic_name  => 'probe_align_genomic',
-            -module      => 'Bio::EnsEMBL::Funcgen::RunnableDB::ProbeMapping::ProbeAlign',
+            -module      => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
             -rc_name     => '8Gb_job',
-            -analysis_capacity => 20,
-            -batch_size        => 100,
+#             -analysis_capacity => 1,
+            -batch_size        => 10,
             -parameters => {
-                QUERYSEQS    => '#chunk_name#',
-                TARGETSEQS   => '#toplevel_sequences_file#',
-                mapping_type => 'genomic', # genomic or transcript
+                cmd => '
+                  exonerate \
+                  --querytype dna \
+                  --targettype dna \
+                  --query #chunk_name# \
+                  --target #toplevel_sequences_file# \
+                  --showsugar false --showvulgar false --showalignment false --ryo "RESULT: %S %pi %ql %tl %em scores:{%Ps:}\\n" \
+                  > #chunk_name#_genomic.exonerate.txt
+                '
             },
             -flow_into => {
-                MAIN => 'probe_align_genomic_himem',
+                MAIN => 'start_store_probe_features',
+                '-1' => 'probe_align_genomic_himem',
             },
         },
         {   -logic_name  => 'probe_align_genomic_himem',
-            -module      => 'Bio::EnsEMBL::Funcgen::RunnableDB::ProbeMapping::ProbeAlign',
+            -module      => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
             -rc_name     => '64Gb_job',
-            -analysis_capacity => 20,
-            -batch_size        => 100,
+            -batch_size        => 10,
             -parameters => {
-                QUERYSEQS    => '#chunk_name#',
-                TARGETSEQS   => '#toplevel_sequences_file#',
-                mapping_type => 'genomic', # genomic or transcript
+                cmd => '
+                  exonerate \
+                  --querytype dna \
+                  --targettype dna \
+                  --query #chunk_name# \
+                  --target #toplevel_sequences_file# \
+                  --showsugar false --showvulgar false --showalignment false --ryo "RESULT: %S %pi %ql %tl %em scores:{%Ps:}\\n" \
+                  > #chunk_name#_genomic.exonerate.txt
+                '
+            },
+            -flow_into => {
+                MAIN => 'start_store_probe_features',
             },
         },
         {   -logic_name  => 'probe_align_transcript',
-            -module      => 'Bio::EnsEMBL::Funcgen::RunnableDB::ProbeMapping::ProbeAlign',
-            -batch_size => 100,
+            -module      => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+            -batch_size => 10,
             -rc_name     => '8Gb_job',
+#             -analysis_capacity => 1,
             -parameters => {
-                QUERYSEQS    => '#chunk_name#',
-                TARGETSEQS   => '#gene_sequences_file#',
-                mapping_type => 'transcript', # genomic or transcript
+                cmd => '
+                  exonerate \
+                  --querytype dna \
+                  --targettype dna \
+                  --query #chunk_name# \
+                  --target #gene_sequences_file# \
+                  --showsugar false --showvulgar false --showalignment false --ryo "RESULT: %S %pi %ql %tl %em scores:{%Ps:}\\n" \
+                  > #chunk_name#_transcript.exonerate.txt
+                '
             },
             -flow_into => {
-                MAIN => 'probe_align_transcript_himem',
+                MAIN => 'start_store_probe_features',
+                '-1' => 'probe_align_transcript_himem',
             },
         },
         {   -logic_name  => 'probe_align_transcript_himem',
-            -module      => 'Bio::EnsEMBL::Funcgen::RunnableDB::ProbeMapping::ProbeAlign',
+            -module      => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+            -batch_size => 10,
             -rc_name     => '64Gb_job',
-            -batch_size => 100,
             -parameters => {
-                QUERYSEQS    => '#chunk_name#',
-                TARGETSEQS   => '#gene_sequences_file#',
-                mapping_type => 'transcript', # genomic or transcript
+                cmd => '
+                  exonerate \
+                  --querytype dna \
+                  --targettype dna \
+                  --query #chunk_name# \
+                  --target #gene_sequences_file# \
+                  --showsugar false --showvulgar false --showalignment false --ryo "RESULT: %S %pi %ql %tl %em scores:{%Ps:}\\n" \
+                  > #chunk_name#_transcript.exonerate.txt
+                '
+            },
+            -flow_into => {
+                MAIN => 'start_store_probe_features',
             },
         },
+      {
+          -logic_name  => 'start_store_probe_features',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
+      },
     ];
 }
 
--- modules/Bio/EnsEMBL/Funcgen/HiveConfig/ProbeMapping/ImportArrays_conf.pm
+++ modules/Bio/EnsEMBL/Funcgen/HiveConfig/ProbeMapping/ImportArrays_conf.pm
@@ -3,101 +3,154 @@ package Bio::EnsEMBL::Funcgen::HiveConfig::ProbeMapping::ImportArrays_conf;
 use strict;
 use warnings;
 
-use base ('Bio::EnsEMBL::Funcgen::HiveConfig::Probe2Transcript_conf');
+use base ('Bio::EnsEMBL::Funcgen::HiveConfig::ProbeMapping::Base');
+
+=head1
+
+  export HIVE_URL='mysql://ensadmin:ensembl@ens-genomics2:3306/mmn1_tracking_homo_sapiens_funcgen_87_38_hive'
+  ftp_pipeline_parameters="-pipeline_url $HIVE_URL"
+  
+  init_pipeline.pl Bio::EnsEMBL::Funcgen::HiveConfig::ProbeMapping::ImportArraysNew_conf $ftp_pipeline_parameters -hive_force_init 1
+
+=cut
 
 sub pipeline_analyses {
     my $self = shift;
     
     return [
-        {   -logic_name  => 'PrePipelineChecks',
-            -module      => 'Bio::EnsEMBL::Funcgen::RunnableDB::ProbeMapping::PrePipelineChecks',
-            -input_ids => [ {} ],
+        {
+            -logic_name  => 'start_import',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
             -flow_into => {
-                MAIN => 'MkTmpDir',
+                MAIN => 'pre_pipeline_checks',
+            },
+        },
+        {   -logic_name  => 'pre_pipeline_checks',
+            -module      => 'Bio::EnsEMBL::Funcgen::RunnableDB::ProbeMapping::PrePipelineChecks',
+            -flow_into => {
+                MAIN => 'make_temp_dir',
             },
         },
         {
-	    -logic_name  => 'MkTmpDir',
+            -logic_name  => 'make_temp_dir',
             -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
             -parameters => {
-                cmd       => 'mkdir -p #tempdir#',
+                cmd       => 'mkdir -p #tempdir#/#species#',
             },
             -flow_into => {
-                MAIN => 'CreateDB',
+                MAIN => 'rollback_array',
             },
-        },
-        {   -logic_name  => 'CreateDB',
-            -module      => 'Bio::EnsEMBL::Funcgen::RunnableDB::ProbeMapping::CreateDB',
-            -flow_into => {
-                MAIN => 'JobFactoryImportArrays',
-            },
-        },
-        {   -logic_name  => 'JobFactoryImportArrays',
-            -module      => 'Bio::EnsEMBL::Funcgen::RunnableDB::ProbeMapping::JobFactory',
-            -parameters => {
-                probe_directories => $self->o('probe_directories'),
-            },
-	    -flow_into => {
-		MAIN => 'RollbackArray',
-	    },
         },
         {
-	    -logic_name  => 'RollbackArray',
-            -meadow_type => 'LOCAL',
+            -logic_name  => 'rollback_array',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SqlCmd',
+            -parameters => {
+                sql     => [
+                  "truncate array;",
+                  "truncate array_chip;",
+                  "truncate probe;",
+                  "truncate probe_feature;",
+                  "truncate probe_seq;",
+                  "truncate probe_set;",
+                  "truncate probe_transcript;",
+                  "truncate probe_set_transcript;",
+                  "truncate probe_feature_transcript;",
+                  "truncate unmapped_object;",
+                  "truncate unmapped_reason;",
+                  "delete analysis_description from analysis_description, analysis where analysis.analysis_id=analysis_description.analysis_id and logic_name like '%Probe%Align';",
+                  "delete from analysis where logic_name like '%Probe%Align';",
+                  "delete analysis_description from analysis_description, analysis where analysis.analysis_id=analysis_description.analysis_id and logic_name = 'probe2transcript';",
+                  "delete from analysis where logic_name = 'probe2transcript';",
+                ],
+                db_conn => 'funcgen:#species#',
+            },
+            -flow_into => {
+               MAIN => 'create_probe_mapping_analyses',
+            },
+        },
+        {
+            -logic_name  => 'create_probe_mapping_analyses',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+            -parameters => {
+                cmd       => 
+                    'create_probe_mapping_analyses.pl'
+                  . ' --registry #reg_conf#'
+                  . ' --species  #species#'
+            },
+            -flow_into => {
+                MAIN => 'job_factory_import_arrays',
+            },
+        },
+
+        {
+          -logic_name  => 'job_factory_import_arrays',
+          -module      => 'Bio::EnsEMBL::Funcgen::RunnableDB::ProbeMapping::JobFactory',
+          -parameters => {
+              probe_directories => '#probe_directory#/#species#',
+          },
+          -flow_into => {
+            MAIN => 'parse_probe_fasta_file',
+          },
+        },
+        {
+            -logic_name  => 'parse_probe_fasta_file',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+            -parameters => {
+                cmd       => '
+                  import_parse_probe_fasta_file.pl \
+                    --array_name      #array_class# \
+                    --probe_file      #probe_file# \
+                    --parsed_output   #tempdir#/#species#/#array_class#_parsed_probes.pl
+                ',
+            },
+            -flow_into => {
+                MAIN => 'create_array_objects',
+            },
+        },
+        {
+            -logic_name  => 'create_array_objects',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+            -parameters => {
+                cmd       => '
+                  import_create_array_objects.pl \
+                    --array_name        #array_class# \
+                    --parsed_probe_data #tempdir#/#species#/#array_class#_parsed_probes.pl \
+                    --output_file       #tempdir#/#species#/#array_class#_array_objects.pl
+                  ',
+            },
+            -flow_into => {
+                MAIN => 'store_array_objects',
+            },
+        },
+        {
+            -logic_name  => 'store_array_objects',
             -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
             -analysis_capacity => 1,
             -parameters => {
-                cmd => 'rollback_array.pl'
-		  . ' --species '     . $self->o('species')		  
-		  . ' -dbhost '       . $self->o('tracking_host')
-		  . ' -dbname '       . $self->o('tracking_dbname')
-		  . ' -dbuser '       . $self->o('tracking_user')
-		  . ' -dbport '       . $self->o('tracking_port')
-		  
-		  . ( $self->o('tracking_pass') ? ' -dbpass ' . $self->o('tracking_pass') : '' )
-		  
-		  . ' -arrays #all_array_names# -force',
+                cmd       => '
+                  import_store_array_objects.pl \
+                    --registry           #reg_conf# \
+                    --species            #species# \
+                    --array_objects_file #tempdir#/#species#/#array_class#_array_objects.pl
+                ',
             },
-            -flow_into => {
-               '1->A' => 'ImportArrays',
-               'A->1' => 'hc_import_arrays',
-            },
+          -flow_into => {
+              MEMLIMIT => 'store_array_objects_himem',
+          },
         },
-        {   -logic_name  => 'hc_import_arrays',
-            -module      => 'Bio::EnsEMBL::Hive::RunnableDB::SqlHealthcheck',
-            -parameters  => {
-              query         => 'select distinct array.name from array join array_chip using (array_id) left join probe using (array_chip_id) where probe.probe_id is null and array.class="#array_format#"',
-              expected_size => '=0',
-              db_conn       => $self->_create_db_url_from_dba_hash($self->o('tracking_dba_hash')),
-              description   => 'Check that the arrays imported from #array_format# have probes in the database.';
+        {
+            -logic_name  => 'store_array_objects_himem',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+            -analysis_capacity => 1,
+            -parameters => {
+                cmd       => '
+                  import_store_array_objects.pl \
+                    --registry           #reg_conf# \
+                    --species            #species# \
+                    --array_objects_file #tempdir#/#species#/#array_class#_array_objects.pl
+                ',
             },
-        },
-        {   -logic_name  => 'ImportArrays',
-            -module      => 'Bio::EnsEMBL::Funcgen::RunnableDB::ProbeMapping::ImportArrays',
-            -flow_into => {
-               # MEMLIMIT
-               -1 => [ 'ImportArrays8Gb' ],
-            },
-        },
-        {   -logic_name  => 'ImportArrays8Gb',
-            -module      => 'Bio::EnsEMBL::Funcgen::RunnableDB::ProbeMapping::ImportArrays',
-            -rc_name    => '8Gb_job',
-            -flow_into => {
-	       # MEMLIMIT
-               -1 => [ 'ImportArrays16Gb' ],
-            },
-        },
-        {   -logic_name  => 'ImportArrays16Gb',
-            -module      => 'Bio::EnsEMBL::Funcgen::RunnableDB::ProbeMapping::ImportArrays',
-            -rc_name    => '16Gb_job',
-            -flow_into => {
-	       # MEMLIMIT
-               -1 => [ 'ImportArrays64Gb' ],
-            },
-        },
-        {   -logic_name  => 'ImportArrays64Gb',
-            -module      => 'Bio::EnsEMBL::Funcgen::RunnableDB::ProbeMapping::ImportArrays',
-            -rc_name    => '64Gb_job',
+            -rc_name     => '16Gb_job',
         },
     ];
 }
