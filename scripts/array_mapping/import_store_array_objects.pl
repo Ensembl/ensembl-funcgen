74eeff8df93164a229665af0f2de84e4a20fc3a0
Michael Nuhn <mnuhn@ebi.ac.uk>
2017-03-13 15:41:18 +0000

    Bugfix for correct probe set sizes, thanks @dbolser-ebi for reporting

--- scripts/array_mapping/import_store_array_objects.pl
+++ scripts/array_mapping/import_store_array_objects.pl
@@ -101,6 +101,7 @@ my $fetch_probe_set_from_db = sub {
     
     if (defined $probe_set_from_db) {
       $probe_set = $probe_set_from_db;
+      $probe_set->size($probe_set->size + 1);
     } else {
       $probe_set->size(1);
       $probe_set_adaptor->store($probe_set);
