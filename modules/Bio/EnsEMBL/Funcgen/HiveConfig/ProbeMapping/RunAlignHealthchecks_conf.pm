fca91f59164809e5225bf8bc0534ff4266db4fae
Michael Nuhn <mnuhn@ebi.ac.uk>
2017-03-22 14:59:37 +0000

    Tweaked analysis names

--- modules/Bio/EnsEMBL/Funcgen/HiveConfig/ProbeMapping/RunAlignHealthchecks_conf.pm
+++ modules/Bio/EnsEMBL/Funcgen/HiveConfig/ProbeMapping/RunAlignHealthchecks_conf.pm
@@ -15,11 +15,11 @@ sub pipeline_analyses {
           -logic_name  => 'start_align_healthchecks',
           -module     => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
           -flow_into => {
-              MAIN => 'check_complex_probe_features_from_transcripts_matches'
+              MAIN => 'check_gapped_probe_features_from_transcript_matches'
           },
       },
       {
-          -logic_name  => 'check_complex_probe_features_from_transcripts_matches',
+          -logic_name  => 'check_gapped_probe_features_from_transcript_matches',
           -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
           -parameters => {
               cmd => 'check_probe_feature_sequences.pl '
@@ -30,11 +30,11 @@ sub pipeline_analyses {
                 . ' --max_check ' . $max_probe_features_to_check
           },
           -flow_into => {
-              MAIN => 'check_simple_probe_features_from_transcripts_matches',
+              MAIN => 'check_ungapped_probe_features_from_transcript_matches',
           },
         },
       {
-          -logic_name  => 'check_simple_probe_features_from_transcripts_matches',
+          -logic_name  => 'check_ungapped_probe_features_from_transcript_matches',
           -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
           -parameters => {
               cmd => 'check_probe_feature_sequences.pl '
