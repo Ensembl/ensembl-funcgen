435526480eac65ccdbbba41e22485d3afcbbf6c1
Myrto Kostadima <kostadim@ebi.ac.uk>
2017-05-16 15:31:15 +0100

    Fixed headers in the two new files and deleted older files with deprecated examples of how to load VISTA enhancers

--- scripts/external_features/project_external_features_vista.pl
+++ scripts/external_features/project_external_features_vista.pl
@@ -1,5 +1,44 @@
 #!/usr/bin/env perl
 
+=head1 LICENSE
+
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2017] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=head1 CONTACT
+
+  Please email comments or questions to the public Ensembl
+  developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
+
+  Questions may also be sent to the Ensembl help desk at
+  <http://www.ensembl.org/Help/Contact>.
+
+=cut
+
+# Project the VISTA enhancer genomic coordinates from one assembly to another prior to loading them on the funcgen db 
+#
+# Example usage:
+# project_external_features_vista.pl \
+#       -registry registry.pm \
+#       -species=mus_musculus \
+#       -old_assembly="NCBIM37" \
+#       -new_assembly="GRCm38"
+#       -in=/nfs/production/panda/ensembl/funcgen/source_files/old/warehouse/ensembl07/source_files/external_features/VISTA/VISTA_4Apr17_header \
+#       -out=VISTA_4Apr17_header_mouse_89_38 \
+
 use strict;
 use warnings;
 use feature qw(say);
@@ -9,14 +48,6 @@ use Bio::EnsEMBL::Registry;
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Getopt::Long;
 
-## Example usage:
-# project_external_features_vista.pl \
-#       -registry registry.pm \
-#       -species=mus_musculus \
-#       -old_assembly="NCBIM37" \
-#       -new_assembly="GRCm38"
-#       -in=/nfs/production/panda/ensembl/funcgen/source_files/old/warehouse/ensembl07/source_files/external_features/VISTA/VISTA_4Apr17_header \
-#       -out=VISTA_4Apr17_header_mouse_89_38 \
 
 my ($registry, $species, $old_assembly, $new_assembly, $in, $out);
 
