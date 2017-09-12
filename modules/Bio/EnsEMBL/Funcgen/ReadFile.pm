3ca505c966f60712221b184aa759614665f358d6
Michael Nuhn <mnuhn@ebi.ac.uk>
2017-09-12 13:34:03 +0100

    ReadFile: Minor indentation changes

--- modules/Bio/EnsEMBL/Funcgen/ReadFile.pm
+++ modules/Bio/EnsEMBL/Funcgen/ReadFile.pm
@@ -39,15 +39,14 @@ use base 'Bio::EnsEMBL::Funcgen::GenericGetSetFunctionality';
 
 sub _constructor_parameters {
   return {
-    name          => 'name',
-    is_paired_end => 'is_paired_end',
-    paired_with   => 'paired_with',
-    file_size     => 'file_size',
-    read_length   => 'read_length',
-    md5sum        => 'md5sum',
-    file          => 'file',
-    notes         => 'notes',
-
+    name           => 'name',
+    is_paired_end  => 'is_paired_end',
+    paired_with    => 'paired_with',
+    file_size      => 'file_size',
+    read_length    => 'read_length',
+    md5sum         => 'md5sum',
+    file           => 'file',
+    notes          => 'notes',
     analysis                             => 'set_Analysis',
     read_file_experimental_configuration => 'set_ReadFileExperimentalConfiguration',
   };
@@ -55,14 +54,14 @@ sub _constructor_parameters {
 
 sub _simple_accessors {
   return [
-    { method_name => 'name',          hash_key => '_name',          },
-    { method_name => 'is_paired_end', hash_key => '_is_paired_end', },
-    { method_name => 'paired_with',   hash_key => '_paired_with',   },
-    { method_name => 'file_size',     hash_key => '_file_size',     },
-    { method_name => 'read_length',   hash_key => '_read_length',   },
-    { method_name => 'md5sum',        hash_key => '_md5sum',        },
-    { method_name => 'file',          hash_key => '_file',          },
-    { method_name => 'notes',         hash_key => '_notes',         },
+    { method_name => 'name',           hash_key => '_name',           },
+    { method_name => 'is_paired_end',  hash_key => '_is_paired_end',  },
+    { method_name => 'paired_with',    hash_key => '_paired_with',    },
+    { method_name => 'file_size',      hash_key => '_file_size',      },
+    { method_name => 'read_length',    hash_key => '_read_length',    },
+    { method_name => 'md5sum',         hash_key => '_md5sum',         },
+    { method_name => 'file',           hash_key => '_file',           },
+    { method_name => 'notes',          hash_key => '_notes',          },
   ]
 }
 
