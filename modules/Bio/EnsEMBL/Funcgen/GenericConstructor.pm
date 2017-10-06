b5909705fa23ed12008599633f1aae055db75038
Michael Nuhn <mnuhn@ebi.ac.uk>
2017-10-06 10:00:11 +0100

    Added some duct tape too keep this together until ENSCORESW-2464 has been fixed.

--- modules/Bio/EnsEMBL/Funcgen/GenericConstructor.pm
+++ modules/Bio/EnsEMBL/Funcgen/GenericConstructor.pm
@@ -35,7 +35,7 @@ package Bio::EnsEMBL::Funcgen::GenericConstructor;
 use strict;
 use warnings;
 use Bio::EnsEMBL::Utils::Exception qw( throw );
-use Bio::EnsEMBL::Utils::Argument  qw( rearrange );
+#use Bio::EnsEMBL::Utils::Argument  qw( rearrange );
 
 use Role::Tiny;
 
@@ -78,7 +78,7 @@ sub _initialise_fields {
   
   return if !@accepted_constructor_parameters;
 
-  my @value = rearrange([ @accepted_constructor_parameters ], @parameters);
+  my @value = rearrange_pp([ @accepted_constructor_parameters ], @parameters);
   
   for (my $index = 0; $index<@value; $index++) {
   
@@ -112,6 +112,41 @@ sub _initialise_fields {
     }
   }
   return;
+}
+
+# Using pure perl implementation copied over from 
+# Bio::EnsEMBL::Utils::Argument until
+# https://www.ebi.ac.uk/panda/jira/browse/ENSCORESW-2464
+# has been resolved.
+#
+sub rearrange_pp {
+  my $order = shift;
+
+  if ( $order eq "Bio::EnsEMBL::Utils::Argument" ) {
+    # skip object if one provided
+    $order = shift;
+  }
+
+  # If we've got parameters, we need to check to see whether
+  # they are named or simply listed. If they are listed, we
+  # can just return them.
+  unless ( @_ && $_[0] && substr( $_[0], 0, 1 ) eq '-' ) {
+    return @_;
+  }
+  
+  # Push undef onto the end if % 2 != 0 to stop warnings
+  push @_,undef unless $#_ %2;
+  my %param;
+  while( @_ ) {
+    #deletes all dashes & uppercases at the same time
+    (my $key = shift) =~ tr/a-z\055/A-Z/d;
+    $param{$key} = shift;
+  }
+  
+  # What we intend to do is loop through the @{$order} variable,
+  # and for each value, we use that as a key into our associative
+  # array, pushing the value at that key onto our return array.
+  return map { $param{uc($_)} } @$order;
 }
 
 1;
