705475bc6984305711cb6d61aaae6104946872f5
Michael Nuhn <mnuhn@ebi.ac.uk>
2017-11-23 17:30:40 +0000

    Added and reinstated methods needed by the webcode

--- modules/Bio/EnsEMBL/Funcgen/DBSQL/PeakCallingAdaptor.pm
+++ modules/Bio/EnsEMBL/Funcgen/DBSQL/PeakCallingAdaptor.pm
@@ -49,4 +49,166 @@ sub _tables {
   return ['peak_calling', 'pc']
 }
 
+sub fetch_all_by_Epigenome {
+    my $self      = shift;
+    my $epigenome = shift;
+    
+    if (! defined $epigenome) {
+      throw("Epigenome was undefined");
+    }
+    my $constraint = 'epigenome_id  = ' . $epigenome->dbID;
+    return $self->fetch_all($constraint);
+}
+
+sub _fetch_all_by_constraints {
+    my $self            = shift;
+    my $constraint_hash = shift;
+    
+    my @constraints;
+    
+    if (exists $constraint_hash->{epigenomes}) {
+      for my $epigenome (@{$constraint_hash->{epigenomes}}) {
+        push @constraints, 'epigenome_id  = ' . $epigenome->dbID;
+      }
+    }
+    if (exists $constraint_hash->{feature_types}) {
+      for my $feature_type (@{$constraint_hash->{feature_types}}) {
+        push @constraints, 'feature_type_id  = ' . $feature_type->dbID;
+      }
+    }
+    if (exists $constraint_hash->{projects}) {
+      for my $project (@{$constraint_hash->{projects}}) {
+      
+        my $experiment_group_name = $project->name;
+      
+        push @constraints, "experiment_id in (select experiment_id from experiment join experimental_group using (experimental_group_id) where experimental_group.name = '".$experiment_group_name."')";
+      }
+    }
+    if (exists $constraint_hash->{evidence_types}) {
+      for my $evidence_type (@{$constraint_hash->{evidence_types}}) {
+        if ($evidence_type eq 'DNase1 & TFBS') {
+          push @constraints, "feature_type_id in (select feature_type_id from feature_type where class in ('Open Chromatin', 'Transcription Factor'))";
+        }
+        if ($evidence_type eq 'Hists & Pols') {
+          push @constraints, "feature_type_id in (select feature_type_id from feature_type where class in ('Histone', 'Polymerase'))";
+        }
+      }
+    }
+    
+    my $constraint = join ' and ', @constraints;
+    
+    print STDERR "\n";
+    use Data::Dumper;
+    print STDERR Dumper($constraint_hash);
+    print STDERR $constraint;
+    print STDERR "\n";
+    
+    return $self->fetch_all($constraint);
+}
+
+# Legacy method that had to be carried over, because web relies on it to 
+# display sources.
+#
+sub _fetch_feature_set_filter_counts {
+  my $self = shift;
+
+   my $sql =<<SQL
+    SELECT 
+      count(*), 
+      eg.name, 
+      eg.description, 
+      eg.is_project, 
+      ft.class, 
+      epi.name, 
+      epi.description 
+    FROM 
+      experimental_group eg, 
+      experiment e, 
+      peak_calling fs, 
+      feature_type ft, 
+      epigenome epi 
+    WHERE 
+      fs.experiment_id = e.experiment_id 
+      AND e.experimental_group_id = eg.experimental_group_id 
+      AND fs.feature_type_id = ft.feature_type_id 
+      AND fs.epigenome_id = epi.epigenome_id 
+    GROUP BY 
+      eg.name, 
+      eg.is_project, 
+      ft.class, 
+      epi.name
+SQL
+;
+
+  #warn $sql;
+  #Need to write HC around this as we sometimes get less than expect.
+
+
+  my @rows = @{$self->db->dbc->db_handle->selectall_arrayref($sql)};
+  my $ft_a = $self->db->get_FeatureTypeAdaptor;
+  my $ftype_info = $ft_a->get_regulatory_evidence_info;
+
+  my %filter_info =
+    (
+     #Project=> {},
+     #'Cell/Tissue' => {},
+     All =>
+     {
+      All =>{ count       => 0,
+              description => 'All experiments',
+            }
+     }
+
+    );
+
+  foreach my $row(@rows){
+
+    my ($count, $project, $proj_desc, $is_proj, $ft_class, $epigenome_name, $epigenome_desc) = @$row;
+
+    #All counts
+    $filter_info{All}{All}{count} += $count;
+
+    #Project counts
+    if($is_proj){
+
+      if(! exists $filter_info{Project}{$project}){
+        $filter_info{Project}{$project} =
+          { count       => 0,
+            description => $proj_desc,
+          };
+      }
+
+      $filter_info{Project}{$project}{count} += $count;
+    }
+
+    #Cell/Tissue counts
+    if(! exists $filter_info{'Cell/Tissue'}{$epigenome_name}){
+      $filter_info{'Cell/Tissue'}{$epigenome_name} =
+        { count       => 0,
+          description => $epigenome_desc,
+        };
+    }
+    $filter_info{'Cell/Tissue'}{$epigenome_name}{count} += $count;
+
+    #Evidence class counts
+    #Do we want to split this into ft.class
+    #i.e. split 'DNase1 & TFBS'
+    my $evidence_type  = $ft_a->get_regulatory_evidence_type($ft_class);
+    my $ft_class_label =  $ftype_info->{$evidence_type}{label};
+
+    if(! exists $filter_info{'Evidence type'}{$ft_class_label}){
+      $filter_info{'Evidence type'}{$ft_class_label} =
+        { count       => 0,
+          description => $ftype_info->{$evidence_type}{long_name},
+        };
+    }
+    $filter_info{'Evidence type'}{$ft_class_label}{count} += $count;
+  }
+
+  return \%filter_info;
+
+  #Do we need to add an 'in_build' filter /data field?
+
+}
+
 1;
