1db726a6ee482398a9a98d5bd1bb890013f87cfe
Michael Nuhn <mnuhn@ebi.ac.uk>
2017-12-20 11:15:39 +0000

    Updates to load_fastqc_summary_file.pl

--- scripts/sequencing/load_fastqc_summary_file.pl
+++ scripts/sequencing/load_fastqc_summary_file.pl
@@ -29,41 +29,7 @@ limitations under the License.
 
 =head1 SYNOPSIS
 
-CREATE TABLE `input_subset_fastqc` (
-  `input_subset_qc_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `read_file_id` int(10) unsigned NOT NULL,
-  `status` varchar(100) NOT NULL,
-  `title` varchar(100) NOT NULL,
-  `file_name` varchar(100) NOT NULL,
-  PRIMARY KEY (`input_subset_qc_id`),
-  UNIQUE KEY `name_exp_idx` (`read_file_id`,`title`)
-) ENGINE=MyISAM
-
-INPUT_SUBSET_ID=3436
-
-
-
-TEMP_DIR=/lustre/scratch109/ensembl/funcgen/mn1/qc/${INPUT_SUBSET_ID}
-mkdir -p $TEMP_DIR
-# GZIPPED_FASTQ_FILE=$(mysql -N -B $DB_MYSQL_ARGS -e "select local_url from input_subset_tracking where read_file_id = ${INPUT_SUBSET_ID}")
-
-
-DB_MYSQL_ARGS="-hens-genomics2 -P3306 -uensadmin -psecret mn1_faang_tracking_homo_sapiens_funcgen_81_38"
-GZIPPED_FASTQ_FILE=$(mysql -N -B $DB_MYSQL_ARGS -e "select local_url from input_subset_tracking where read_file_id = ${INPUT_SUBSET_ID}")
-
-echo $GZIPPED_FASTQ_FILE
-
-fastqc -o $TEMP_DIR $GZIPPED_FASTQ_FILE
-
-FASTQC_DIR=$(find $TEMP_DIR -maxdepth 1 -mindepth 1 -type d)
-
-SUMMARY_FILE=$FASTQC_DIR/summary.txt
-
-echo $SUMMARY_FILE
-
-./scripts/sequencing/load_fastqc_summary_file.pl --read_file_id $INPUT_SUBSET_ID --summary_file $SUMMARY_FILE | mysql $DB_MYSQL_ARGS
-
-./scripts/sequencing/load_fastqc_summary_file.pl --read_file_id 3 --summary_file /lustre/scratch109/ensembl/funcgen/mn1/ersa/faang/alignments/homo_sapiens/GRCh38/3526/histone_control_fastqc/summary.txt | mysql $DB_MYSQL_ARGS
+load_fastqc_summary_file.pl       --read_file_id #read_file_id#         --summary_file #fastqc_summary_file#  --work_dir #tempdir# --registry #reg_conf# --species #species#
 
 =head1 DESCRIPTION
 
@@ -76,11 +42,15 @@ use Getopt::Long;
 my $summary_file;
 my $read_file_id;
 my $work_dir;
+my $registry;
+my $species;
 
 my %config_hash = (
-  "summary_file"    => \$summary_file,
+  "summary_file" => \$summary_file,
   "read_file_id" => \$read_file_id,
-  "work_dir"        => \$work_dir,
+  "registry"     => \$registry,
+  "work_dir"     => \$work_dir,
+  "species"      => \$species,
 );
 
 my $result = GetOptions(
@@ -88,45 +58,60 @@ my $result = GetOptions(
   'read_file_id=s',
   'summary_file=s',
   'work_dir=s',
+  'species=s',
+  'registry=s',
 );
 
 die unless(-e $summary_file);
 die unless($read_file_id);
+die("No registry!") unless($registry);
 
-print &create_table_sql;
+use Bio::EnsEMBL::Registry;
+Bio::EnsEMBL::Registry->load_all($registry, 1, 1, 0, 1);
 
 open IN, $summary_file;
 
+my %parsed;
+
 while (my $current_line = <IN>) {
   chomp $current_line;
-  #print " - $current_line\n";
   my @f = split "\t", $current_line;
-  #print Dumper(\@f);
-  my $sql = "INSERT INTO read_file_fastqc (read_file_id,status,title,file_name,path) VALUES (".$read_file_id.", '".$f[0]."', '".$f[1]."', '".$f[2]."', '".$work_dir."')";
-  
-  print "$sql;\n";
+  $parsed{$f[1]} = $f[0];
 }
 
-=head2 create_table_sql
-=cut
-sub create_table_sql {
+close(IN);
 
-my $sql = <<SQL
-CREATE TABLE if not exists `read_file_fastqc` (
-  `input_subset_qc_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `read_file_id` int(10) unsigned NOT NULL,
-  `status` varchar(100) NOT NULL,
-  `title` varchar(100) NOT NULL,
-  `file_name` varchar(100) NOT NULL,
-  `path` varchar(512) NOT NULL,
-  PRIMARY KEY (`input_subset_qc_id`),
-  UNIQUE KEY `name_exp_idx` (`read_file_id`,`title`)
-) ENGINE=MyISAM;
+# $VAR1 = {
+#           'Sequence Duplication Levels' => 'PASS',
+#           'Per base N content' => 'PASS',
+#           'Per sequence GC content' => 'PASS',
+#           'Overrepresented sequences' => 'WARN',
+#           'Per base sequence content' => 'PASS',
+#           'Kmer Content' => 'FAIL',
+#           'Per sequence quality scores' => 'WARN',
+#           'Sequence Length Distribution' => 'PASS',
+#           'Per tile sequence quality' => 'FAIL',
+#           'Basic Statistics' => 'PASS',
+#           'Per base sequence quality' => 'WARN',
+#           'Adapter Content' => 'PASS'
+#         };
 
-SQL
-;
-  return $sql;
-}
+use Bio::EnsEMBL::Funcgen::FastQC;
+my $fastqc = Bio::EnsEMBL::Funcgen::FastQC->new(
+    -read_file_id                     => $read_file_id,
+    -basic_statistics                 => $parsed{'Basic Statistics'},
+    -per_base_sequence_quality        => $parsed{'Per base sequence quality'},
+    -per_tile_sequence_quality        => $parsed{'Per tile sequence quality'},
+    -per_sequence_quality_scores      => $parsed{'Per sequence quality scores'},
+    -per_base_sequence_content        => $parsed{'Per base sequence content'},
+    -per_sequence_gc_content          => $parsed{'Per sequence GC content'},
+    -per_base_n_content               => $parsed{'Per base N content'},
+    -sequence_length_distribution     => $parsed{'Sequence Length Distribution'},
+    -sequence_duplication_levels      => $parsed{'Sequence Duplication Levels'},
+    -overrepresented_sequences        => $parsed{'Overrepresented sequences'},
+    -adapter_content                  => $parsed{'Adapter Content'},
+    -kmer_content                     => $parsed{'Kmer Content'},
+);
 
-
-
+my $fastqc_adaptor = Bio::EnsEMBL::Registry->get_adaptor($species, 'funcgen', 'fastqc');
+$fastqc_adaptor->store($fastqc);
