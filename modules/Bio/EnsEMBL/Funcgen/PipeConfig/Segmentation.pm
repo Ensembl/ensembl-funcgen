4831a79326f4f603013b2dcf3c56027a123bc4c9
Michael Nuhn <mnuhn@ebi.ac.uk>
2018-01-15 14:20:49 +0000

    Segmentation pipeline

---
+++ modules/Bio/EnsEMBL/Funcgen/PipeConfig/Segmentation.pm
@@ -0,0 +1,213 @@
+=pod 
+
+=head1 NAME
+
+    Bio::EnsEMBL::Funcgen::PipeConfig::Segmentation
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 LICENSE
+
+    Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+    Copyright [2016-2017] EMBL-European Bioinformatics Institute
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software distributed under the License
+    is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and limitations under the License.
+
+=head1 CONTACT
+
+=cut
+package Bio::EnsEMBL::Funcgen::PipeConfig::Segmentation;
+
+use strict;
+use warnings;
+
+use Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf;
+use base ('Bio::EnsEMBL::Funcgen::PipeConfig::ChIPSeqAnalysis::Base');
+
+sub pipeline_wide_parameters {
+  my $self = shift;
+  
+  return {
+    %{$self->SUPER::pipeline_wide_parameters},
+    pipeline_name            => $self->o('pipeline_name'),
+    tempdir                  => $self->o('tempdir'),
+    tempdir_segmentation     => $self->o('tempdir') . '/segmentation',
+    tempdir_regulatory_build => $self->o('tempdir') . '/regulatory_build',
+    data_root_dir            => $self->o('data_root_dir'),
+    reference_data_root_dir  => $self->o('reference_data_root_dir'),
+    reg_conf                 => $self->o('reg_conf'),
+    ChromHMM                 => '/nfs/production/panda/ensembl/funcgen/ChromHMM/ChromHMM.jar',
+  };
+}
+
+sub pipeline_analyses {
+    my ($self) = @_;
+    return [
+      {   -logic_name => 'start_segmentation',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
+          -flow_into => { 
+            'MAIN->A' => 'segmentation_job_factory',
+            'A->MAIN' => 'segmentation_done'
+          },
+      },
+      {   -logic_name => 'segmentation_job_factory',
+          -module     => 'Bio::EnsEMBL::Funcgen::RunnableDB::Segmentation::SegmentationJobFactory',
+          -parameters => {
+            tempdir => '#tempdir_segmentation#/#species#/',
+          },
+          -flow_into => {
+            '2->A' => 'make_celltable_dir',
+            'A->2' => 'generate_segmentation_parameter_file',
+          },
+      },
+      
+      {   -logic_name => 'make_celltable_dir',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+          -parameters => {
+              cmd => qq(mkdir -p #tempdir_segmentation#/#species#/celltables),
+          },
+          -flow_into => { 
+            MAIN     => 'for_every_epigenome',
+          },
+      },
+      {   -logic_name  => 'for_every_epigenome',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::JobFactory',
+          -parameters => {
+              db_conn    => 'funcgen:#species#',
+              inputquery => 'select name as epigenome, production_name as epigenome_production_name from epigenome',
+          },
+          -flow_into => {
+              2 => { 'generate_cell_table_file' => INPUT_PLUS },
+          },
+      },
+
+      {   -logic_name => 'generate_cell_table_file',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+          -parameters => {
+            cmd => qq( generate_cell_table_file.pl      )
+              . qq( --species         #species#         )
+              . qq( --registry        #reg_conf#        )
+              . qq( --cell_table_file #tempdir_segmentation#/#species#/celltables/#epigenome_production_name#.txt )
+              . qq( --epigenome       #epigenome#       )
+              
+          },
+          -flow_into => { 
+            MAIN     => { 'binarize' => INPUT_PLUS },
+          },
+      },
+      {   -logic_name => 'binarize',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+          -rc_name    => 'binarization',
+          -parameters => { 
+            cmd => qq(java -Xmx14500M )
+              . qq( -jar #ChromHMM# )
+              . qq( BinarizeBam                       )
+              . qq( -p 20                             )
+              . qq( #chromosome_length_file#          )
+              . qq( #data_root_dir_species_assembly#  )
+              . qq( #tempdir_segmentation#/#species#/celltables/#epigenome_production_name#.txt )
+              . qq( #binarized_bam_dir#               ),
+          },
+      },
+      {   -logic_name => 'generate_segmentation_parameter_file',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+          -parameters => {
+            cmd => q( echo -e "segmentation\t#species#_segmentation\tChromHMM\t#learnmodel_output_dir#" > #segmentation_parameter_file# )
+          },
+          -flow_into => { 
+            MAIN     => 'learn_model',
+          },
+      },
+      {   -logic_name => 'learn_model',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+          -rc_name    => 'learn_model',
+          -parameters => {
+            cmd => 
+                qq( export DISPLAY= ;             )
+                . qq( java -Xmx30000M             )
+                . qq( -jar #ChromHMM#             )
+                . qq( LearnModel                  )
+                . qq( -r 300 -p 12                )
+                . qq( -l #chromosome_length_file# )
+                . qq( #binarized_bam_dir#         )
+                . qq( #learnmodel_output_dir#     )
+                . qq( 25                          )
+                . qq( #assembly#                  )
+          },
+          -flow_into => {
+            MAIN => 'make_regbuild_dir',
+          },
+      },
+      {   -logic_name => 'make_regbuild_dir',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+          -parameters => {
+              cmd => qq(mkdir -p #tempdir_regulatory_build#/#species#/#assembly#),
+          },
+          -flow_into => { 
+            MAIN     => 'build_regulatory_features',
+          },
+      },
+      {   -logic_name => 'build_regulatory_features',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+          -rc_name    => '4Gb_job',
+          -parameters => {
+            cmd => 
+                qq( new_build_regulatory_features.pl             )
+                . qq( --out #tempdir_regulatory_build#/#species# )
+                . qq( -dump #segmentation_parameter_file#     )
+                . qq( -a #assembly#                )
+                . qq( --db   #funcgen_dbname#      )
+                . qq( --user #funcgen_username#    )
+                . qq( --host #funcgen_host#        )
+                . qq( --pass #funcgen_password#    )
+                . qq( --port #funcgen_port#        )
+                . qq( --dnadb_name #core_dbname#   )
+                . qq( --dnadb_host #core_host#     )
+                . qq( --dnadb_user #core_username# )
+                . qq( --dnadb_pass #core_password# )
+                . qq( --dnadb_port #core_port#     )
+                . qq( --chrom_lengths #chromosome_length_file# )
+          },
+          -flow_into => {
+            MAIN => 'load_build',
+          },
+      },
+      {   -logic_name => 'load_build',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+          -rc_name    => '24Gb_job',
+          -parameters => {
+            cmd => 
+                qq( load_build.pl )
+                . qq( --dbname   #funcgen_dbname# )
+                . qq( --user     #funcgen_username#    )
+                . qq( --host     #funcgen_host#        )
+                . qq( --pass     #funcgen_password#    )
+                . qq( --port     #funcgen_port#        )
+                . qq( --base_dir #tempdir_regulatory_build#/#species#/#assembly# )
+          },
+      },
+      {   -logic_name => 'segmentation_done',
+          -module     => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
+      },
+    ];
+}
+
+sub resource_classes {
+    my ($self) = @_;
+    return {
+         %{$self->SUPER::resource_classes},  # inherit 'default' from the parent class
+         'binarization' => {'LSF' => '-M16000 -R"select[mem>16000] rusage[mem=16000]" -n 20'               },
+         'learn_model'  => {'LSF' => '-M31000 -R"span[hosts=1] select[mem>31000] rusage[mem=31000]" -n 12' },
+    };
+}
+
+1;
---
+++ modules/Bio/EnsEMBL/Funcgen/RunnableDB/Segmentation/SegmentationJobFactory.pm
@@ -0,0 +1,90 @@
+=pod 
+=head1 NAME
+
+Bio::EnsEMBL::Funcgen::RunnableDB::Segmentation::SegmentationJobFactory
+
+=head1 DESCRIPTION
+=cut
+
+package Bio::EnsEMBL::Funcgen::RunnableDB::Segmentation::SegmentationJobFactory;
+
+use warnings;
+use strict;
+
+use base 'Bio::EnsEMBL::Hive::Process';
+
+use Bio::EnsEMBL::Funcgen::Hive::RefBuildFileLocator;
+use Bio::EnsEMBL::Funcgen::Utils::GoodUtils qw( create_species_assembly_path );
+
+sub run {
+  my $self = shift;
+  
+  my $tempdir                 = $self->param_required('tempdir');
+  my $species                 = $self->param_required('species');
+  my $data_root_dir           = $self->param_required('data_root_dir');
+  my $reference_data_root_dir = $self->param_required('reference_data_root_dir');
+  
+  my $species_assembly_path = create_species_assembly_path($species);
+  
+  my $data_root_dir_species_assembly = $data_root_dir . '/' . $species_assembly_path;
+  
+  #my $celltable_dir         = $tempdir . '/celltables/';
+  my $binarized_bam_dir     = $tempdir . '/binarization/';
+  my $learnmodel_output_dir = $tempdir . '/learn_model/';
+  
+  my $coordsystem_adaptor = Bio::EnsEMBL::Registry->get_adaptor($species, 'core', 'coordsystem');
+  my $default_chromosome_coordsystem = $coordsystem_adaptor->fetch_by_name('chromosome');
+  my $default_assembly = $default_chromosome_coordsystem->version;
+
+  my $chromosome_length_file = 
+    $reference_data_root_dir
+    . '/'
+    . 
+      Bio::EnsEMBL::Funcgen::Hive::RefBuildFileLocator
+        ->new
+        ->locate({
+          species   => $species,
+          file_type => 'chromosome_lengths_by_species_assembly',
+          assembly  => $default_assembly
+        });
+  
+  use Data::Dumper;
+  
+  my $core_adaptor = Bio::EnsEMBL::Registry->get_DBAdaptor($species, 'core');
+  my $core_dbc = $core_adaptor->dbc;
+
+  my $funcgen_adaptor = Bio::EnsEMBL::Registry->get_DBAdaptor($species, 'funcgen');
+  my $funcgen_dbc = $funcgen_adaptor->dbc;
+  
+  my $segmentation_parameter_file = $tempdir . '/segmentation_parameter_file.txt';
+
+  my $input_id = {
+    chromosome_length_file         => $chromosome_length_file,
+    data_root_dir_species_assembly => $data_root_dir_species_assembly,
+    #celltable_dir                  => $celltable_dir,
+    binarized_bam_dir              => $binarized_bam_dir,
+    learnmodel_output_dir          => $learnmodel_output_dir,
+    assembly                       => $default_assembly,
+    species                        => $species,
+    
+    segmentation_parameter_file => $segmentation_parameter_file,
+    
+    core_host     => $core_dbc->host,
+    core_port     => $core_dbc->port,
+    core_username => $core_dbc->username,
+    core_password => $core_dbc->password,
+    core_dbname   => $core_dbc->dbname,
+
+    funcgen_host     => $funcgen_dbc->host,
+    funcgen_port     => $funcgen_dbc->port,
+    funcgen_username => $funcgen_dbc->username,
+    funcgen_password => $funcgen_dbc->password,
+    funcgen_dbname   => $funcgen_dbc->dbname,
+
+  };
+  
+  $self->dataflow_output_id($input_id, 2);
+  return;
+}
+
+1;
