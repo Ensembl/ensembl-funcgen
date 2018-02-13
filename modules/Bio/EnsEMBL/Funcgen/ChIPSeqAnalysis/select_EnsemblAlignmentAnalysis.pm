9c1916aa58f243b379219429413029b204ffd969
Michael Nuhn <mnuhn@ebi.ac.uk>
2018-02-13 15:49:21 +0000

    For selecting the right alignment method, depending on whether the reads are single or paired end.

---
+++ modules/Bio/EnsEMBL/Funcgen/ChIPSeqAnalysis/select_EnsemblAlignmentAnalysis.pm
@@ -0,0 +1,35 @@
+package Bio::EnsEMBL::Funcgen::ChIPSeqAnalysis::select_EnsemblAlignmentAnalysis;
+
+use strict;
+use Data::Dumper;
+use Bio::EnsEMBL::Funcgen::ChIPSeqAnalysis::Constants qw ( :all );
+use Carp;
+use Role::Tiny;
+
+sub select_EnsemblAlignmentAnalysis {
+
+  my $param = shift;
+
+  my $experiment = $param->{experiment};
+  my $has_single_end_reads_only = $param->{has_single_end_reads_only};
+  my $has_paired_end_reads_only = $param->{has_paired_end_reads_only};
+
+  my $has_mix_of_read_types = ! $has_single_end_reads_only && ! $has_paired_end_reads_only;
+
+  if ($has_mix_of_read_types) {
+    #confess("Experiment ". $experiment->name ." has a mix of read types!");
+    print STDERR ("Experiment ". $experiment->name ." has a mix of read types!");
+    return ENSEMBL_HODGEPODGE_ALIGNMENT_ANALYSIS;
+  }
+  
+  if ($has_single_end_reads_only) {
+    return ENSEMBL_SINGLE_END_ALIGNMENT_ANALYSIS;
+  }
+  if ($has_paired_end_reads_only) {
+    return ENSEMBL_PAIRED_END_ALIGNMENT_ANALYSIS;
+  }
+  
+  confess("has_single_end_reads_only and has_paired_end_reads_only can't both be true!");
+}
+
+1;
