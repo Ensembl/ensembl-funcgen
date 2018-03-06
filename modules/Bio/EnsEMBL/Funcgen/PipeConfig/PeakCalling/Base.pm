90199910c6bd5b8d0b833ad9b4cd4b31565dd389
Michael Nuhn <mnuhn@ebi.ac.uk>
2018-03-06 13:04:56 +0000

    Added configuration to use the production queue

--- modules/Bio/EnsEMBL/Funcgen/PipeConfig/PeakCalling/Base.pm
+++ modules/Bio/EnsEMBL/Funcgen/PipeConfig/PeakCalling/Base.pm
@@ -27,7 +27,7 @@ sub pipeline_wide_parameters {
     %{$self->SUPER::pipeline_wide_parameters},
     pipeline_name           => $self->o('pipeline_name'),
     tempdir                 => $self->o('tempdir'),
-    tempdir_peak_calling         => $self->o('tempdir') . '/peak_calling',
+    tempdir_peak_calling    => $self->o('tempdir') . '/peak_calling',
     data_root_dir           => $self->o('data_root_dir'),
     ensembl_release_version => $self->o('ensembl_release_version'),
     reference_data_root_dir => $self->o('reference_data_root_dir'),
@@ -120,7 +120,8 @@ sub generate_parallel_alignment_analyses {
 
 sub resource_classes {
     my ($self) = @_;
-    return {
+    
+    my $default_resource_classes = {
         %{$self->SUPER::resource_classes},  # inherit 'default' from the parent class
 
          '250Mb_job'      => {'LSF' => '-M250   -R"select[mem>250]   rusage[mem=250]"'   }     ,
@@ -139,6 +140,28 @@ sub resource_classes {
          '64Gb_job'       => {'LSF' => '-M64000 -R"select[mem>64000] rusage[mem=64000]"'      },
          '64Gb_job_3cpus' => {'LSF' => '-M64000 -R"select[mem>64000] rusage[mem=64000]" -n 3' },
     };
+    
+    my $production_resource_classes = {
+        %{$self->SUPER::resource_classes},  # inherit 'default' from the parent class
+
+         '250Mb_job'      => {'LSF' => '-q production-rh7 -M250   -R"select[mem>250]   rusage[mem=250]"'   }     ,
+         '500Mb_job'      => {'LSF' => '-q production-rh7 -M500   -R"select[mem>500]   rusage[mem=500]"'        },
+         '1Gb_job'        => {'LSF' => '-q production-rh7 -M1000  -R"select[mem>1000]  rusage[mem=1000]"'       },
+         '2Gb_job'        => {'LSF' => '-q production-rh7 -M2000  -R"select[mem>2000]  rusage[mem=2000]"'       },
+         '4Gb_job'        => {'LSF' => '-q production-rh7 -M4000  -R"select[mem>4000]  rusage[mem=4000]"'       },
+         '4Gb_job_2cpus'  => {'LSF' => '-q production-rh7 -M4000  -R"select[mem>4000]  rusage[mem=4000]" -n 2'  },
+         '8Gb_job'        => {'LSF' => '-q production-rh7 -M8000  -R"select[mem>8000]  rusage[mem=8000]"'       },
+         '16Gb_job'       => {'LSF' => '-q production-rh7 -M16000 -R"select[mem>16000] rusage[mem=16000]"'      },
+         '24Gb_job'       => {'LSF' => '-q production-rh7 -M24000 -R"select[mem>24000] rusage[mem=24000]"'      },
+         '32Gb_job'       => {'LSF' => '-q production-rh7 -M32000 -R"select[mem>32000] rusage[mem=32000]"'      },
+         '32Gb_job_2cpus' => {'LSF' => '-q production-rh7 -M32000 -R"select[mem>32000] rusage[mem=32000]" -n 2' },
+         '32Gb_job_3cpus' => {'LSF' => '-q production-rh7 -M32000 -R"select[mem>32000] rusage[mem=32000]" -n 3' },
+         '48Gb_job'       => {'LSF' => '-q production-rh7 -M48000 -R"select[mem>48000] rusage[mem=48000]"'      },
+         '64Gb_job'       => {'LSF' => '-q production-rh7 -M64000 -R"select[mem>64000] rusage[mem=64000]"'      },
+         '64Gb_job_3cpus' => {'LSF' => '-q production-rh7 -M64000 -R"select[mem>64000] rusage[mem=64000]" -n 3' },
+    };
+    
+    return $production_resource_classes;
 }
 
 1;
