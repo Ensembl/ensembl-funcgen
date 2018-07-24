2dfa74c2193f44dd60a193f3bc0ab0c75abbf9ec
Michael Nuhn <mnuhn@ebi.ac.uk>
2018-07-24 11:19:10 +0100

    Added method with Ensembl look and feel

--- modules/Bio/EnsEMBL/Funcgen/Experiment.pm
+++ modules/Bio/EnsEMBL/Funcgen/Experiment.pm
@@ -369,6 +369,19 @@ sub _fetch_Idr {
 
 sub epigenome { return shift->{epigenome}; }
 
+=head2 epigenome
+
+  Example    : my $epigenome_name = $exp->get_Epigenome->name;
+  Description: Getter for the Epigenome
+  Returntype : Bio::EnsEMBL::Funcgen::Epigenome
+  Exceptions : None
+  Caller     : General
+  Status     : Stable
+
+=cut
+
+sub get_Epigenome { return shift->{epigenome}; }
+
 
 =head2 feature_type
 
