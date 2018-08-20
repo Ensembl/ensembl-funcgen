c220abfd42ac6a07694e2806f968f2b2c28f4daf
Jose C. Marugan <jcmarca@ebi.ac.uk>
2018-08-20 23:24:04 +0100

    download files

---
+++ scripts/tracking/download_files.sh
@@ -0,0 +1,27 @@
+TOTLINES=$(wc -l < input_file_xtra_filtered_human_only_known_tagets_rel_94_8.tsv)
+TOTLINES=$((TOTLINES-1))
+echo "Total Lines: "$TOTLINES
+
+count=0
+while IFS=$'\t' read -r -a myArray
+do
+ LINK=$(echo "${myArray[24]}")
+ FILE=$(echo "${myArray[11]}")
+
+ if [[ $count -gt 0 && $count -lt 1001 ]]
+ then
+ 	if [ ! -e $FILE ]
+ 	then
+	    echo "Downloading file: " $LINK
+	    wget $LINK -P /nfs/production/panda/ensembl/funcgen/source_files/fastq/homo_sapiens/encode/v94/
+	    echo "Downloaded " $count " of " $TOTLINES
+	    #echo $LINK
+ 	fi
+ 
+     #break
+ fi
+ count=$((count+1))
+ 
+done < input_file_xtra_filtered_human_only_known_tagets_rel_94_8.tsv
+
+
