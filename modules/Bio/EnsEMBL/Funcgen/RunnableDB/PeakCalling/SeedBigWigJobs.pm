b3069375a3a6f6bcb742aa0ef5b5a1efd713bc91
Michael Nuhn <mnuhn@ebi.ac.uk>
2018-08-28 13:33:17 +0100

    Minor

--- modules/Bio/EnsEMBL/Funcgen/RunnableDB/PeakCalling/SeedBigWigJobs.pm
+++ modules/Bio/EnsEMBL/Funcgen/RunnableDB/PeakCalling/SeedBigWigJobs.pm
@@ -15,7 +15,6 @@ sub run {
   my $self = shift;
   my $species      = $self->param_required('species');
   my $plan         = $self->param_required('execution_plan');
-  my $tempdir      = $self->param_required('tempdir_peak_calling');
   
   use Bio::EnsEMBL::Funcgen::PeakCallingPlan::ExecutionPlanUtils qw (
         lock_execution_plan
