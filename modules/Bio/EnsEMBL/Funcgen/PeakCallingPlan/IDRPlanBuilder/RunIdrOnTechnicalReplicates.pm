19e457f0e9485b7b545f7137113e08ef35755ee7
Michael Nuhn <mnuhn@ebi.ac.uk>
2018-09-20 16:21:20 +0100

    Bugfix, 'multiple' replicates belonging to the same replicate weren't handled correctly.

--- modules/Bio/EnsEMBL/Funcgen/PeakCallingPlan/IDRPlanBuilder/RunIdrOnTechnicalReplicates.pm
+++ modules/Bio/EnsEMBL/Funcgen/PeakCallingPlan/IDRPlanBuilder/RunIdrOnTechnicalReplicates.pm
@@ -59,50 +59,6 @@ sub construct {
     'ReadFile'
   );
 
-  my $read_file_experimental_configuration_list = 
-    $read_file_experimental_configuration_adaptor
-      ->fetch_all_by_Experiment($experiment);
-
-  my $alignment = [];
-
-  my $remove_duplicates_plan_builder
-    = Bio::EnsEMBL::Funcgen::PeakCallingPlan::RemoveDuplicatesPlanBuilder
-      ->new;
-  $remove_duplicates_plan_builder->set_output_format ( BAM_FORMAT );
-  $remove_duplicates_plan_builder->set_experiment    ( $experiment->name );
-
-  my $has_single_end_reads_only = 1;
-  my $has_paired_end_reads_only = 1;
-  
-  READ_FILE_EXPERIMENTAL_CONFIGURATION:
-  foreach my $read_file_experimental_configuration 
-    (@$read_file_experimental_configuration_list) {
-    
-    my $read_file = $read_file_experimental_configuration->get_ReadFile;
-    
-    if ($read_file->is_paired_end) {
-      $has_single_end_reads_only = undef;
-    }
-    if (! $read_file->is_paired_end) {
-      $has_paired_end_reads_only = undef;
-    }
-  }
-
-  my $ensembl_alignment_analysis = select_EnsemblAlignmentAnalysis({
-    experiment => $experiment,
-    has_single_end_reads_only => $has_single_end_reads_only,
-    has_paired_end_reads_only => $has_paired_end_reads_only,
-  });
-
-  my @align_replicates_plan;
-  foreach my $read_file_experimental_configuration 
-    (@$read_file_experimental_configuration_list) {
-  
-    my $read_file = $read_file_experimental_configuration->get_ReadFile;
-    my $technical_replicate_number
-      = $read_file_experimental_configuration
-        ->technical_replicate;
-    
     use Bio::EnsEMBL::Funcgen::Utils::RefBuildFileLocator;
     my $bwa_index_locator = Bio::EnsEMBL::Funcgen::Utils::RefBuildFileLocator->new;
 
@@ -112,38 +68,90 @@ sub construct {
                 $experiment->epigenome->gender
             );
 
-    $alignment_namer->unset_biological_replicate_number;
-    $alignment_namer->technical_replicate_number($technical_replicate_number);
-
     my $is_control;
-    
     if ($experiment->is_control) {
         $is_control = TRUE;
     } else {
         $is_control = FALSE;
     }
 
+  my $technical_replicate_numbers = 
+    $read_file_experimental_configuration_adaptor
+      ->fetch_all_technical_replicate_numbers_from_Experiment($experiment);
+
+  my @align_replicates_plan;
+  my $alignment = [];
+  
+  foreach my $technical_replicate_number (@$technical_replicate_numbers) {
+
+    my $remove_duplicates_plan_builder
+        = Bio::EnsEMBL::Funcgen::PeakCallingPlan::RemoveDuplicatesPlanBuilder
+        ->new;
+    $remove_duplicates_plan_builder->set_output_format ( BAM_FORMAT );
+    $remove_duplicates_plan_builder->set_experiment    ( $experiment->name );
+
+    my $has_single_end_reads_only = 1;
+    my $has_paired_end_reads_only = 1;
+    
+    my $read_file_experimental_configuration_list = [
+      grep {
+        # Skip these, because they are fetched, when the first is considered.
+        $_->paired_end_tag != 2
+      }
+      @{
+        $read_file_experimental_configuration_adaptor
+            ->fetch_all_by_Experiment_and_technical_replicate_number(
+                $experiment, 
+                $technical_replicate_number
+            )
+      }
+    ];
+
+    READ_FILE_EXPERIMENTAL_CONFIGURATION:
+    foreach my $read_file_experimental_configuration 
+        (@$read_file_experimental_configuration_list) {
+        
+        my $read_file = $read_file_experimental_configuration->get_ReadFile;
+        
+        if ($read_file->is_paired_end) {
+        $has_single_end_reads_only = undef;
+        }
+        if (! $read_file->is_paired_end) {
+        $has_paired_end_reads_only = undef;
+        }
+    }
+
+    my $ensembl_alignment_analysis = select_EnsemblAlignmentAnalysis({
+        experiment => $experiment,
+        has_single_end_reads_only => $has_single_end_reads_only,
+        has_paired_end_reads_only => $has_paired_end_reads_only,
+    });
+    
+    my @names_of_reads_to_merge;
+    foreach my $read_file_experimental_configuration (@$read_file_experimental_configuration_list) {
+    
+        my $read_file = $read_file_experimental_configuration->get_ReadFile;
+        push @names_of_reads_to_merge, summarise_ReadFile($read_file);
+    }
+    
+    $alignment_namer->unset_biological_replicate_number;
+    $alignment_namer->technical_replicate_number($technical_replicate_number);
+
     use Bio::EnsEMBL::Funcgen::PeakCallingPlan::AlignmentPlanFactory;
     my $alignment_plan_factory = Bio::EnsEMBL::Funcgen::PeakCallingPlan::AlignmentPlanFactory
     ->new(
-      -names_of_reads_to_merge => [ 
-        summarise_ReadFile( 
-          $read_file_adaptor->fetch_by_name(
-            $read_file->name
-          )
-        )
-      ],
-      -description             => "Technical replicate $technical_replicate_number",
-      -name                    => $alignment_namer->base_name_with_duplicates,
-      -to_gender               => $to_gender,
-      -to_assembly             => $assembly,
-      -ensembl_analysis        => $ensembl_alignment_analysis,
-      -from_experiment         => $experiment->name,
-      -output_real             => $alignment_namer->bam_file_with_duplicates,
-      -output_stored           => $alignment_namer->bam_file_with_duplicates_stored,
-      -output_format           => BAM_FORMAT,
-      -is_control              => $is_control,
-      -has_all_reads           => FALSE,
+    -names_of_reads_to_merge => \@names_of_reads_to_merge,
+    -description             => "Technical replicate $technical_replicate_number",
+    -name                    => $alignment_namer->base_name_with_duplicates,
+    -to_gender               => $to_gender,
+    -to_assembly             => $assembly,
+    -ensembl_analysis        => $ensembl_alignment_analysis,
+    -from_experiment         => $experiment->name,
+    -output_real             => $alignment_namer->bam_file_with_duplicates,
+    -output_stored           => $alignment_namer->bam_file_with_duplicates_stored,
+    -output_format           => BAM_FORMAT,
+    -is_control              => $is_control,
+    -has_all_reads           => FALSE,
     );
     
     my $alignment_plan = $alignment_plan_factory->product;
@@ -160,10 +168,12 @@ sub construct {
     
     push @align_replicates_plan, create_ref($remove_duplicates_plan);
     
+    print Dumper($alignment_plan);
+    
     push @$alignment, $alignment_plan;
     push @$alignment, $remove_duplicates_plan;
   }
-  
+
   my $idr_plan = {
     alignment_replicates => \@align_replicates_plan,
     type                 => IDR_ANALYSIS,
@@ -176,6 +186,7 @@ sub construct {
   
   return $idr_plan;
 }
+
 
 1;
 
