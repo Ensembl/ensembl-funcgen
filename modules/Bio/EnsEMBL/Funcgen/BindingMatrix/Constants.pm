c64b3ddb639a2ac21f051261c70caf0960255d60
Ilias Lavidas <ilavidas@ebi.ac.uk>
2018-11-29 16:29:13 +0000

    add constant

--- modules/Bio/EnsEMBL/Funcgen/BindingMatrix/Constants.pm
+++ modules/Bio/EnsEMBL/Funcgen/BindingMatrix/Constants.pm
@@ -58,16 +58,14 @@ our @EXPORT_OK = qw(
     WEIGHTS
     BITS
     VALID_UNITS
+    VALID_NUCLEOTIDES
 );
 
 our %EXPORT_TAGS = ( all => \@EXPORT_OK );
 
 use constant {
-    FREQUENCIES   => 'Frequencies',
-    PROBABILITIES => 'Probabilities',
-    WEIGHTS       => 'Weights',
-    BITS          => 'Bits',
-    VALID_UNITS   => ['Frequencies', 'Probabilities', 'Weights', 'Bits']
+    VALID_UNITS       => [ 'Frequencies', 'Probabilities', 'Weights', 'Bits' ],
+    VALID_NUCLEOTIDES => [ 'A', 'C', 'G', 'T' ]
 };
 
 1;
