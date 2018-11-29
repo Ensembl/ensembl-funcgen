290089ad6201f97accde052142f1fc94ab9c31f5
Ilias Lavidas <ilavidas@ebi.ac.uk>
2018-11-29 16:30:45 +0000

    remove check which is enforced on the DB level

--- modules/Bio/EnsEMBL/Funcgen/DBSQL/BindingMatrixAdaptor.pm
+++ modules/Bio/EnsEMBL/Funcgen/DBSQL/BindingMatrixAdaptor.pm
@@ -84,13 +84,6 @@ sub fetch_by_name {
 
     my $result = $self->generic_fetch($constraint);
 
-    if ( scalar @$result > 1 ) {
-        throw(    'Binding Matrix '
-                . $name
-                . ' is not unique in the database.'
-                . ' Only one result has been returned' );
-    }
-
     return $result->[0];
 }
 
