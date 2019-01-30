8189aa95d81937a6c1bae2cd491823a69d8003ee
Michael Nuhn <mnuhn@ebi.ac.uk>
2019-01-30 11:54:16 +0000

    Switched to production-rh74 as the default queue for the regulatory build pipeline

--- modules/Bio/EnsEMBL/Funcgen/PipeConfig/PeakCalling/ResourceClasses.pm
+++ modules/Bio/EnsEMBL/Funcgen/PipeConfig/PeakCalling/ResourceClasses.pm
@@ -33,8 +33,6 @@ sub _swarm_resource {
     my $gb_value  = $self->_gigabytes_to_bytes ( $gb   );
     my $cpu_value = $self->_cpu_to_nanocpu     ( $cpus );
 
-    #return qq({"Limits":{"MemoryBytes":$gb_value,"NanoCPUs":$cpu_value},"Reservations":{"NanoCPUs":$cpu_value,"MemoryBytes":$gb_value}});
-
     return stringify(
         {
             'Limits'  => {
@@ -51,115 +49,91 @@ sub _swarm_resource {
 
 sub resource_classes {
     my ($self) = @_;
+
+    my $lsf_queue_option = '-q production-rh74';
     
-    my $default_resource_classes = {
+    my $resource_classes = {
         %{$self->SUPER::resource_classes},  # inherit 'default' from the parent class
 
-         '250Mb_job'      => {'LSF' => '-M250   -R"select[mem>250]   rusage[mem=250]"'   }     ,
-         '500Mb_job'      => {'LSF' => '-M500   -R"select[mem>500]   rusage[mem=500]"'        },
-         '1Gb_job'        => {'LSF' => '-M1000  -R"select[mem>1000]  rusage[mem=1000]"'       },
-         '2Gb_job'        => {'LSF' => '-M2000  -R"select[mem>2000]  rusage[mem=2000]"'       },
-         '4Gb_job'        => {'LSF' => '-M4000  -R"select[mem>4000]  rusage[mem=4000]"'       },
-         '4Gb_job_2cpus'  => {'LSF' => '-M4000  -R"select[mem>4000]  rusage[mem=4000]" -n 2'  },
-         '8Gb_job'        => {'LSF' => '-M8000  -R"select[mem>8000]  rusage[mem=8000]"'       },
-         '16Gb_job'       => {'LSF' => '-M16000 -R"select[mem>16000] rusage[mem=16000]"'      },
-         '24Gb_job'       => {'LSF' => '-M24000 -R"select[mem>24000] rusage[mem=24000]"'      },
-         '32Gb_job'       => {'LSF' => '-M32000 -R"select[mem>32000] rusage[mem=32000]"'      },
-         '32Gb_job_2h'    => {'LSF' => '-M32000 -R"select[mem>32000] rusage[mem=32000]" -W 2:00' },
-         '32Gb_job_8h'    => {'LSF' => '-M32000 -R"select[mem>32000] rusage[mem=32000]" -W 8:00' },
-         '32Gb_job_2cpus' => {'LSF' => '-M32000 -R"select[mem>32000] rusage[mem=32000]" -n 2' },
-         '32Gb_job_3cpus' => {'LSF' => '-M32000 -R"select[mem>32000] rusage[mem=32000]" -n 3' },
-         '48Gb_job'       => {'LSF' => '-M48000 -R"select[mem>48000] rusage[mem=48000]"'      },
-         '64Gb_job'       => {'LSF' => '-M64000 -R"select[mem>64000] rusage[mem=64000]"'      },
-         '64Gb_job_3cpus' => {'LSF' => '-M64000 -R"select[mem>64000] rusage[mem=64000]" -n 3' },
-
-         'binarization' => {'LSF' => '-M16000 -R"select[mem>16000] rusage[mem=16000]" -n 5'                },
-         'learn_model'  => {'LSF' => '-M31000 -R"span[hosts=1] select[mem>31000] rusage[mem=31000]" -n 12' },
-    };
-    
-    my $production_resource_classes = {
-        %{$self->SUPER::resource_classes},  # inherit 'default' from the parent class
-
-        'default'        => {'LSF' => '-q production-rh7'   }     ,
+        'default'         => {'LSF' => $lsf_queue_option   },
         '250Mb_job'       => {
-            'LSF'         => [ '-q production-rh7 -M250   -R"select[mem>250]   rusage[mem=250]"' ],
+            'LSF'         => [ qq($lsf_queue_option -M250   -R"select[mem>250]   rusage[mem=250]") ],
             'DockerSwarm' => [ $self->_swarm_resource(1, 0.25) ],
         },
         '500Mb_job'       => {
-            'LSF'         => [ '-q production-rh7 -M500   -R"select[mem>500]   rusage[mem=500]"' ],
+            'LSF'         => [ qq($lsf_queue_option -M500   -R"select[mem>500]   rusage[mem=500]") ],
             'DockerSwarm' => [ $self->_swarm_resource(1, 0.5) ],
         },
         '1Gb_job'         => {
-            'LSF'         => [ '-q production-rh7 -M1000  -R"select[mem>1000]  rusage[mem=1000]"' ],
+            'LSF'         => [ qq($lsf_queue_option -M1000  -R"select[mem>1000]  rusage[mem=1000]") ],
             'DockerSwarm' => [ $self->_swarm_resource(1, 1 - 0.5) ],
         },
         '2Gb_job'         => {
-            'LSF'         => [ '-q production-rh7 -M2000  -R"select[mem>2000]  rusage[mem=2000]"' ],
+            'LSF'         => [ qq($lsf_queue_option -M2000  -R"select[mem>2000]  rusage[mem=2000]") ],
             'DockerSwarm' => [ $self->_swarm_resource(1, 2 -1) ],
         },
         '4Gb_job'         => {
-            'LSF'         => [ '-q production-rh7 -M4000  -R"select[mem>4000]  rusage[mem=4000]"' ],
+            'LSF'         => [ qq($lsf_queue_option -M4000  -R"select[mem>4000]  rusage[mem=4000]") ],
             'DockerSwarm' => [ $self->_swarm_resource(1, 4 -1) ],
         },
-
-        '4Gb_job_2cpus'  => {'LSF' => '-q production-rh7 -M4000  -R"select[mem>4000]  rusage[mem=4000]" -n 2'  },
+        '4Gb_job_2cpus'   => {
+            'LSF'         => qq($lsf_queue_option -M4000  -R"select[mem>4000]  rusage[mem=4000]" -n 2),
+            'DockerSwarm' => [ $self->_swarm_resource(1, 4 -1) ],
+        },
         '8Gb_job'         => {
-            'LSF'         => [ '-q production-rh7 -M8000  -R"select[mem>8000]  rusage[mem=8000]"' ],
+            'LSF'         => [ qq($lsf_queue_option -M8000  -R"select[mem>8000]  rusage[mem=8000]") ],
             'DockerSwarm' => [ $self->_swarm_resource(1, 8 -1) ],
         },
         '16Gb_job'        => {
-            'LSF'         => [ '-q production-rh7 -M16000 -R"select[mem>16000] rusage[mem=16000]"' ],
+            'LSF'         => [ qq($lsf_queue_option -M16000 -R"select[mem>16000] rusage[mem=16000]") ],
             'DockerSwarm' => [ $self->_swarm_resource(1, 16 -1) ],
         },
         '24Gb_job'        => {
-            'LSF'         => [ '-q production-rh7 -M24000 -R"select[mem>24000] rusage[mem=24000]"' ],
+            'LSF'         => [ qq($lsf_queue_option -M24000 -R"select[mem>24000] rusage[mem=24000]") ],
             'DockerSwarm' => [ $self->_swarm_resource(1, 24 -1) ],
         },
         '32Gb_job'        => {
-            'LSF'         => [ '-q production-rh7 -M32000 -R"select[mem>32000] rusage[mem=32000]"' ],
+            'LSF'         => [ qq($lsf_queue_option -M32000 -R"select[mem>32000] rusage[mem=32000]") ],
             'DockerSwarm' => [ $self->_swarm_resource(1, 32 -1) ],
         },
-        '32Gb_job_2h'    => {
-            'LSF' => '-q production-rh7 -M32000 -R"select[mem>32000] rusage[mem=32000]" -W 2:00',
+        '32Gb_job_2h'     => {
+            'LSF'         => qq($lsf_queue_option -M32000 -R"select[mem>32000] rusage[mem=32000]" -W 2:00),
             'DockerSwarm' => [ $self->_swarm_resource(1, 32 -1) ],
         },
         '32Gb_job_8h'     => {
-            'LSF'         => '-q production-rh7 -M32000 -R"select[mem>32000] rusage[mem=32000]" -W 8:00',
+            'LSF'         => qq($lsf_queue_option -M32000 -R"select[mem>32000] rusage[mem=32000]" -W 8:00),
             'DockerSwarm' => [ $self->_swarm_resource(1, 32 -1) ],
         },
         '32Gb_job_2cpus'  => {
-            'LSF'         => '-q production-rh7 -M32000 -R"select[mem>32000] rusage[mem=32000]" -n 2',
+            'LSF'         => qq($lsf_queue_option -M32000 -R"select[mem>32000] rusage[mem=32000]" -n 2),
             'DockerSwarm' => [ $self->_swarm_resource(2, 32 -1) ],
         },
         '32Gb_job_3cpus'  => {
-            'LSF'         => '-q production-rh7 -M32000 -R"select[mem>32000] rusage[mem=32000]" -n 3',
+            'LSF'         => qq($lsf_queue_option -M32000 -R"select[mem>32000] rusage[mem=32000]" -n 3),
             'DockerSwarm' => [ $self->_swarm_resource(3, 32 -1) ],
         },
         '48Gb_job'        => {
-            'LSF'         => [ '-q production-rh7 -M48000 -R"select[mem>48000] rusage[mem=48000]"' ],
+            'LSF'         => [ qq($lsf_queue_option -M48000 -R"select[mem>48000] rusage[mem=48000]") ],
             'DockerSwarm' => [ $self->_swarm_resource(1, 48 -1) ],
         },
         '64Gb_job'        => {
-            'LSF'         => [ '-q production-rh7 -M64000 -R"select[mem>64000] rusage[mem=64000]"' ],
+            'LSF'         => [ qq($lsf_queue_option -M64000 -R"select[mem>64000] rusage[mem=64000]") ],
             'DockerSwarm' => [ $self->_swarm_resource(1, 64 -1 ) ],
         },
         '64Gb_job_3cpus'  => {
-            'LSF'         => '-q production-rh7 -M64000 -R"select[mem>64000] rusage[mem=64000]" -n 3',
+            'LSF'         => qq($lsf_queue_option -M64000 -R"select[mem>64000] rusage[mem=64000]" -n 3),
             'DockerSwarm' => [ $self->_swarm_resource(3, 64 -1 ) ],
         },
         'binarization'    => {
-            'LSF'         => '-q production-rh7 -M16000 -R"select[mem>16000] rusage[mem=16000]" -n 5',
+            'LSF'         => qq($lsf_queue_option -M16000 -R"select[mem>16000] rusage[mem=16000]" -n 5),
             'DockerSwarm' => [ $self->_swarm_resource(5, 16 -1 ) ],
         },
-        'learn_model'  => {
-            'LSF' => '-q production-rh7 -M31000 -R"span[hosts=1] select[mem>31000] rusage[mem=31000]" -n 12',
+        'learn_model'     => {
+            'LSF'         => qq($lsf_queue_option -M31000 -R"span[hosts=1] select[mem>31000] rusage[mem=31000]" -n 12),
             'DockerSwarm' => [ $self->_swarm_resource(12, 31 -1 ) ],
         },
-
     };
-    
-    return $production_resource_classes;
+    return $resource_classes;
 }
 
 1;
-
