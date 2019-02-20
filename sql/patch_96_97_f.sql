cc2e18946f04e03671007d51db86a9603cf4d542
Jose C. Marugan <jcmarca@ebi.ac.uk>
2019-02-20 09:52:47 +0000

    update schema patch

--- sql/patch_96_97_f.sql
+++ sql/patch_96_97_f.sql
@@ -1,5 +1,5 @@
 -- Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
--- Copyright [2016-2018] EMBL-European Bioinformatics Institute
+-- Copyright [2016-2019] EMBL-European Bioinformatics Institute
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
 -- You may obtain a copy of the License at
@@ -13,11 +13,15 @@
 -- limitations under the License.
 
 /**
-@header patch_96_97_b.sql - Changed to text
-@desc 	Changed to text
+@header patch_96_97_f.sql - Add search_terms and full_name columns to epigenome table and rename display_label column to short_name
+@desc 	Add search_terms and full_name columns to epigenome table and rename display_label column to short_name
 */
 
-alter table regulatory_build change column name name text;
+ALTER TABLE epigenome ADD COLUMN search_terms MEDIUMTEXT DEFAULT NULL;
+ALTER TABLE epigenome ADD COLUMN full_name MEDIUMTEXT DEFAULT NULL;
+ALTER TABLE epigenome DROP INDEX display_label_idx;
+ALTER TABLE epigenome CHANGE `display_label` `short_name` varchar(120) NOT NULL;
+ALTER TABLE epigenome ADD CONSTRAINT short_name_idx UNIQUE (short_name);
 
 -- patch identifier
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_96_97_b.sql|Changed to text');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_96_97_f.sql|Add search_terms and full_name columns to epigenome table and rename display_label column to short_name');
