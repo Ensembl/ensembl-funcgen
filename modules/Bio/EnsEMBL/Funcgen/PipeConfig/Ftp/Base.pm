bcc03effa67b162857c7e48883cc399ef0ba96a1
Michael Nuhn <mnuhn@ebi.ac.uk>
2019-03-07 13:39:07 +0000

    tidy up

--- modules/Bio/EnsEMBL/Funcgen/PipeConfig/Ftp/Base.pm
+++ modules/Bio/EnsEMBL/Funcgen/PipeConfig/Ftp/Base.pm
@@ -38,23 +38,4 @@ sub beekeeper_extra_cmdline_options {
     return '-reg_conf ' . $self->o('reg_conf') . ' -keep_alive -can_respecialize 1';
 }
 
-# sub resource_classes {
-#     my ($self) = @_;
-#     return {
-#         %{$self->SUPER::resource_classes},  # inherit 'default' from the parent class
-# 
-#          '250Mb_job'    => {'LSF' => '-M250   -R"select[mem>250]   rusage[mem=250]"' },
-#          '500Mb_job'    => {'LSF' => '-M500   -R"select[mem>500]   rusage[mem=500]"' },
-#          '1Gb_job'      => {'LSF' => '-M1000  -R"select[mem>1000]  rusage[mem=1000]"' },
-#          '2Gb_job'      => {'LSF' => '-M2000  -R"select[mem>2000]  rusage[mem=2000]"' },
-#          '4Gb_job'      => {'LSF' => '-M4000  -R"select[mem>4000]  rusage[mem=4000]"' },
-#          '8Gb_job'      => {'LSF' => '-M8000  -R"select[mem>8000]  rusage[mem=8000]"' },
-#          '16Gb_job'     => {'LSF' => '-M16000 -R"select[mem>16000] rusage[mem=16000]"' },
-#          '24Gb_job'     => {'LSF' => '-M24000 -R"select[mem>24000] rusage[mem=24000]"' },
-#          '32Gb_job'     => {'LSF' => '-M32000 -R"select[mem>32000] rusage[mem=32000]"' },
-#          '48Gb_job'     => {'LSF' => '-M48000 -R"select[mem>48000] rusage[mem=48000]"' },
-#          '64Gb_job'     => {'LSF' => '-M64000 -R"select[mem>64000] rusage[mem=64000]"' },
-#     };
-# }
-
 1;
