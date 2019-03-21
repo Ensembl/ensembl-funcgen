7477d95309482f763abbb5c2176cf0442e877ba0
Jose C. Marugan <jcmarca@ebi.ac.uk>
2019-03-21 11:22:00 +0000

    test database patched

--- modules/t/test-genome-DBs/homo_sapiens/funcgen/table.sql
+++ modules/t/test-genome-DBs/homo_sapiens/funcgen/table.sql
@@ -197,7 +197,7 @@ CREATE TABLE `epigenome` (
   `epigenome_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(120) NOT NULL,
   `short_name` varchar(120) NOT NULL,
-  `description` varchar(80) DEFAULT NULL,
+  `description` mediumtext DEFAULT NULL,
   `production_name` varchar(120) DEFAULT NULL,
   `gender` enum('male','female','hermaphrodite','mixed','unknown') DEFAULT 'unknown',
   `search_terms` mediumtext,
