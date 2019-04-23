e862e0347a777f3f7c0fc5536f5575d2e7876d52
Michael Nuhn <mnuhn@ebi.ac.uk>
2019-04-23 15:43:39 +0100

    Added datachecks to the end of the pipeline

--- modules/Bio/EnsEMBL/Funcgen/PipeConfig/ProbeMapping/Backbone_conf.pm
+++ modules/Bio/EnsEMBL/Funcgen/PipeConfig/ProbeMapping/Backbone_conf.pm
@@ -137,11 +137,24 @@ sub pipeline_analyses {
             -module     => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
             -flow_into   => {
                '1->A' => 'start_populate_meta_coords',
-               'A->1' => 'backbone_pipeline_finished'
+               'A->1' => 'backbone_fire_probemapping_hc'
             },
         },
         {
             -logic_name  => 'start_populate_meta_coords',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
+        },
+        
+        {
+            -logic_name  => 'backbone_fire_probemapping_hc',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
+            -flow_into   => {
+               '1->A' => 'start_probemapping_hc',
+               'A->1' => 'backbone_pipeline_finished'
+            },
+        },
+        {
+            -logic_name  => 'start_probemapping_hc',
             -module     => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
         },
         
---
+++ modules/Bio/EnsEMBL/Funcgen/PipeConfig/ProbeMapping/ProbeMappingHc.pm
@@ -0,0 +1,60 @@
+=pod
+
+=head1 NAME
+
+    Bio::EnsEMBL::Funcgen::PipeConfig::ProbeMapping::ProbeMappingHc
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 LICENSE
+
+    Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+    Copyright [2016-2019] EMBL-European Bioinformatics Institute
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software distributed under the License
+    is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and limitations under the License.
+
+=head1 CONTACT
+
+=cut
+
+package Bio::EnsEMBL::Funcgen::PipeConfig::ProbeMapping::ProbeMappingHc;
+
+use strict;
+use warnings;
+
+use Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf;
+use base ('Bio::EnsEMBL::Funcgen::PipeConfig::ProbeMapping::Base');
+
+sub pipeline_analyses {
+    my ($self) = @_;
+    return [
+        {   -logic_name => 'start_probemapping_hc',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
+            -flow_into => { 
+              MAIN => 'run_probemapping_checks',
+            },
+        },
+        {
+          -logic_name       => 'run_probemapping_checks',
+          -module           => 'Bio::EnsEMBL::DataCheck::Pipeline::RunDataChecks',
+          -max_retry_count  => 0,
+          -parameters => {
+            registry_file    => '#reg_conf#',
+            species          => '#species#',
+            group            => 'funcgen',
+            datacheck_groups => [ 'probe_mapping' ],
+          },
+        },
+    ];
+}
+
+1;
