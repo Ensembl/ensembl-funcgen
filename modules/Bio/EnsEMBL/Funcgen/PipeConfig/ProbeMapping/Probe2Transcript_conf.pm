e84c97b3dd359aa76b2945aa4107fee1bc36dd65
Michael Nuhn <mnuhn@ebi.ac.uk>
2019-06-05 15:20:50 +0100

    :arrow_upper_right: Restart with more memory, if there wasn't enough

--- modules/Bio/EnsEMBL/Funcgen/PipeConfig/ProbeMapping/Probe2Transcript_conf.pm
+++ modules/Bio/EnsEMBL/Funcgen/PipeConfig/ProbeMapping/Probe2Transcript_conf.pm
@@ -290,6 +290,21 @@ sub pipeline_analyses {
                   . ' --analysis_logic_name  probe2transcript'
                   . ' --probeset_rejections_file ' . $probe_feature_transcript_rejection_file
             },
+          -flow_into => {
+              MEMLIMIT => 'load_probe_feature_to_transcript_rejections_himem',
+          },
+        },
+        {   -logic_name  => 'load_probe_feature_to_transcript_rejections_himem',
+            -rc_name     => '64Gb_job',
+            -module      => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+            -parameters  => {
+                cmd => 
+                    'load_probeset_to_transcript_rejections.pl '
+                  . ' --registry    #reg_conf#'
+                  . ' --species     #species#'
+                  . ' --analysis_logic_name  probe2transcript'
+                  . ' --probeset_rejections_file ' . $probe_feature_transcript_rejection_file
+            },
         },
 
         {   -logic_name  => 'load_probe_feature_to_transcript_assignments',
