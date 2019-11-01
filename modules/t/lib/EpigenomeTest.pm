4d72d175ac3c5fbbaa20d53d11c67117e184da45
Ilias Lavidas <ilavidas@ebi.ac.uk>
2019-11-01 17:17:44 +0000

    new unit test for Epigenome

---
+++ modules/t/lib/EpigenomeTest.pm
@@ -0,0 +1,67 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2019] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=head1 CONTACT
+
+  Please email comments or questions to the public Ensembl
+  developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
+
+  Questions may also be sent to the Ensembl help desk at
+  <http://www.ensembl.org/Help/Contact>.
+
+=cut
+
+package EpigenomeTest;
+
+use strict;
+use warnings;
+
+use Test::More;
+use Test::Exception;
+
+use parent qw(Bio::EnsEMBL::Funcgen::Test);
+
+sub parameters :Test(setup) {
+    my $self = shift;
+
+    my %mandatory_constructor_parameters = (
+        -name => 'dummy_epigenome_name'
+    );
+
+    $self->{mandatory_constructor_parameters} =
+        \%mandatory_constructor_parameters;
+
+    my %optional_constructor_parameters = (
+        -short_name      => 'dummy_short_name',
+        -description     => 'dummy_description',
+        -production_name => 'dummy_production_name',
+        -search_terms    => 'dummy_terms',
+        -full_name       => 'dummy_full_name'
+    );
+
+    my %constructor_parameters = (%mandatory_constructor_parameters,
+                                  %optional_constructor_parameters);
+
+    $self->{constructor_parameters} = \%constructor_parameters;
+
+    my %parameters = (
+    );
+
+    $self->{parameters} = \%parameters;
+}
+
+1;
