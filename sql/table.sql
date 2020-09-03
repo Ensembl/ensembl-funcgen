2cfd4dbe95dbf38136725f370673ec049256cfc5
Ilias Lavidas <ilavidas@ebi.ac.uk>
2020-09-03 14:40:09 +0100

    add patch for schema version

--- sql/table.sql
+++ sql/table.sql
@@ -1658,9 +1658,8 @@ CREATE TABLE `meta` (
 INSERT INTO meta (meta_key, meta_value, species_id) VALUES ('schema_type', 'funcgen', NULL);
 
 -- Update and remove these for each release to avoid erroneous patching
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_version', '102');
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_101_102_a.sql|schema_version');
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_101_102_b.sql|Drop underlying_structure table');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_version', '103');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_102_103_a.sql|schema_version');
 
 /**
 @table meta_coord
