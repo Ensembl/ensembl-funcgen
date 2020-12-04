41543cd507e6843636c3bd7de892027af386b1f4
Jose C. Marugan <jcmarca@ebi.ac.uk>
2020-12-04 16:53:37 +0000

    schema update 104

--- sql/table.sql
+++ sql/table.sql
@@ -1658,8 +1658,8 @@ CREATE TABLE `meta` (
 INSERT INTO meta (meta_key, meta_value, species_id) VALUES ('schema_type', 'funcgen', NULL);
 
 -- Update and remove these for each release to avoid erroneous patching
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_version', '103');
-INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_102_103_a.sql|schema_version');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_version', '104');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_103_104_a.sql|schema_version');
 
 /**
 @table meta_coord
