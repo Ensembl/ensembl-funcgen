1f5c5b1e6603d1e067dd91ca7a8737cb23e00479
Paulo Lins <plins@ebi.ac.uk>
2022-05-23 14:57:32 +0100

    create epigenome track

---
+++ modules/Bio/EnsEMBL/Funcgen/DBSQL/EpigenomeTrackAdaptor.pm
@@ -0,0 +1,281 @@
+#
+# Ensembl module for Bio::EnsEMBL::DBSQL::Funcgen::AnnotatedFeatureAdaptor
+#
+
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2022] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=head1 CONTACT
+
+  Please email comments or questions to the public Ensembl
+  developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
+
+  Questions may also be sent to the Ensembl help desk at
+  <http://www.ensembl.org/Help/Contact>.
+
+
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
+
+package Bio::EnsEMBL::Funcgen::DBSQL::EpigenomeTrackAdaptor;
+
+use strict;
+use base 'Bio::EnsEMBL::Funcgen::DBSQL::GenericAdaptor';
+use Bio::EnsEMBL::Utils::Exception qw( throw );
+
+sub object_class {
+    return 'Bio::EnsEMBL::Funcgen::EpigenomeTrack';
+}
+
+sub _tables {
+  return ['epigenome_track', 'et']
+}
+
+sub fetch_all_failed {
+  my $self         = shift;
+  
+  my $features = $self->fetch_all(
+    "run_failed is true"
+  );
+  return $features;
+}
+
+sub fetch_all_by_Epigenome {
+    my $self      = shift;
+    my $epigenome = shift;
+    
+    if (! defined $epigenome) {
+      throw("Epigenome was undefined");
+    }
+    my $constraint = 'epigenome_id  = ' . $epigenome->dbID;
+    return $self->fetch_all($constraint);
+}
+
+# sub _fetch_all_by_Epigenome_ExperimentalGroup {
+#     my $self      = shift;
+#     my $epigenome = shift;
+#     my $experimental_group_wanted = shift;
+    
+#     my $epigenome_tracks = $self->fetch_all_by_Epigenome($epigenome);
+    
+#     my @result;
+#     foreach my $epigenome_track (@$epigenome_tracks) {
+    
+#       my $experiment = $epigenome_track->get_Experiment;
+#       my $experimental_group = $experiment->experimental_group;
+      
+#       if ($experimental_group->name eq $experimental_group_wanted->name) {
+#         push @result, $epigenome_track;
+#       }
+    
+#     }
+#     return \@result;
+# }
+
+# sub fetch_all_by_FeatureType {
+#     my $self       = shift;
+#     my $feature_type = shift;
+    
+#     my $constraint = 'feature_type_id  = ' . $feature_type->dbID;
+#     return $self->fetch_all($constraint);
+# }
+
+
+# sub fetch_by_EpigenomeTrackStatistic {
+#     my $self                   = shift;
+#     my $epigenome_track_statistic = shift;
+#     return $self->fetch_by_dbID($epigenome_track_statistic->epigenome_track_id);
+# }
+
+# sub count_peaks_by_EpigenomeTrack {
+#   my $self = shift;
+#   my $epigenome_track = shift;
+#   return $self->db->get_PeakAdaptor->count_all('epigenome_track_id = ' . $epigenome_track->dbID);
+# }
+
+# sub fetch_all_by_Epigenome_FeatureType {
+#   my $self = shift;
+  
+#   my $epigenome    = shift;
+#   my $feature_type = shift;
+  
+#   if (! defined $epigenome) {
+#     throw("Not an epigenome!");
+#   }
+#   if (! defined $feature_type) {
+#     throw("Not a feature type!");
+#   }
+  
+#   my $constraint = join ' and ', (
+#     'epigenome_id    = ' . $epigenome->dbID,
+#     'feature_type_id = ' . $feature_type->dbID
+#   );
+#   return $self->fetch_all($constraint);
+# }
+
+# # Ugly legacy stuff from here onwards, used by web, so we can't get rid of it 
+# # easily. Don't use any of this in future code development.
+
+# sub _fetch_all_by_constraints {
+#     my $self            = shift;
+#     my $constraint_hash = shift;
+    
+#     my @constraints;
+    
+#     if (exists $constraint_hash->{epigenomes}) {
+#       for my $epigenome (@{$constraint_hash->{epigenomes}}) {
+#         push @constraints, 'epigenome_id  = ' . $epigenome->dbID;
+#       }
+#     }
+#     if (exists $constraint_hash->{feature_types}) {
+#       for my $feature_type (@{$constraint_hash->{feature_types}}) {
+#         push @constraints, 'feature_type_id  = ' . $feature_type->dbID;
+#       }
+#     }
+#     if (exists $constraint_hash->{projects}) {
+#       for my $project (@{$constraint_hash->{projects}}) {
+      
+#         my $experiment_group_name = $project->name;
+      
+#         push @constraints, "experiment_id in (select experiment_id from experiment join experimental_group using (experimental_group_id) where experimental_group.name = '".$experiment_group_name."')";
+#       }
+#     }
+#     if (exists $constraint_hash->{evidence_types}) {
+#       for my $evidence_type (@{$constraint_hash->{evidence_types}}) {
+#         if ($evidence_type eq 'DNase1 & TFBS') {
+#           push @constraints, "feature_type_id in (select feature_type_id from feature_type where class in ('Open Chromatin', 'Transcription Factor'))";
+#         }
+#         if ($evidence_type eq 'Hists & Pols') {
+#           push @constraints, "feature_type_id in (select feature_type_id from feature_type where class in ('Histone', 'Polymerase'))";
+#         }
+#       }
+#     }
+#     my $constraint = join ' and ', @constraints;
+#     return $self->fetch_all($constraint);
+# }
+
+# # Legacy method that had to be carried over, because web relies on it to 
+# # display sources.
+# #
+# sub _fetch_feature_set_filter_counts {
+#   my $self = shift;
+
+#    my $sql =<<SQL
+#     SELECT 
+#       count(*), 
+#       eg.name, 
+#       eg.description, 
+#       eg.is_project, 
+#       ft.class, 
+#       epi.name, 
+#       epi.description 
+#     FROM 
+#       experimental_group eg, 
+#       experiment e, 
+#       epigenome_track fs, 
+#       feature_type ft, 
+#       epigenome epi 
+#     WHERE 
+#       fs.experiment_id = e.experiment_id 
+#       AND e.experimental_group_id = eg.experimental_group_id 
+#       AND fs.feature_type_id = ft.feature_type_id 
+#       AND fs.epigenome_id = epi.epigenome_id 
+#     GROUP BY 
+#       eg.name, 
+#       eg.is_project, 
+#       ft.class, 
+#       epi.name
+# SQL
+# ;
+
+#   #warn $sql;
+#   #Need to write HC around this as we sometimes get less than expect.
+
+
+#   my @rows = @{$self->db->dbc->db_handle->selectall_arrayref($sql)};
+#   my $ft_a = $self->db->get_FeatureTypeAdaptor;
+#   my $ftype_info = $ft_a->get_regulatory_evidence_info;
+
+#   my %filter_info =
+#     (
+#      #Project=> {},
+#      #'Cell/Tissue' => {},
+#      All =>
+#      {
+#       All =>{ count       => 0,
+#               description => 'All experiments',
+#             }
+#      }
+
+#     );
+
+#   foreach my $row(@rows){
+
+#     my ($count, $project, $proj_desc, $is_proj, $ft_class, $epigenome_name, $epigenome_desc) = @$row;
+
+#     #All counts
+#     $filter_info{All}{All}{count} += $count;
+
+#     #Project counts
+#     if($is_proj){
+
+#       if(! exists $filter_info{Project}{$project}){
+#         $filter_info{Project}{$project} =
+#           { count       => 0,
+#             description => $proj_desc,
+#           };
+#       }
+
+#       $filter_info{Project}{$project}{count} += $count;
+#     }
+
+#     #Cell/Tissue counts
+#     if(! exists $filter_info{'Cell/Tissue'}{$epigenome_name}){
+#       $filter_info{'Cell/Tissue'}{$epigenome_name} =
+#         { count       => 0,
+#           description => $epigenome_desc,
+#         };
+#     }
+#     $filter_info{'Cell/Tissue'}{$epigenome_name}{count} += $count;
+
+#     #Evidence class counts
+#     #Do we want to split this into ft.class
+#     #i.e. split 'DNase1 & TFBS'
+#     my $evidence_type  = $ft_a->get_regulatory_evidence_type($ft_class);
+#     my $ft_class_label =  $ftype_info->{$evidence_type}{label};
+
+#     if(! exists $filter_info{'Evidence type'}{$ft_class_label}){
+#       $filter_info{'Evidence type'}{$ft_class_label} =
+#         { count       => 0,
+#           description => $ftype_info->{$evidence_type}{long_name},
+#         };
+#     }
+#     $filter_info{'Evidence type'}{$ft_class_label}{count} += $count;
+#   }
+
+#   return \%filter_info;
+
+#   #Do we need to add an 'in_build' filter /data field?
+
+# }
+
+1;
---
+++ modules/Bio/EnsEMBL/Funcgen/EpigenomeTrack.pm
@@ -0,0 +1,262 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2022] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=head1 CONTACT
+
+  Please email comments or questions to the public Ensembl
+  developers list at <http://lists.ensembl.org/mailman/listinfo/dev>.
+
+  Questions may also be sent to the Ensembl help desk at
+  <http://www.ensembl.org/Help/Contact>.
+
+=head1 NAME
+
+Bio::EnsEMBL::Funcgen::EpigenomeTrack - Object that represents a peak calling
+
+=head1 SYNOPSIS
+
+  use strict;
+  use warnings;
+  use Bio::EnsEMBL::Registry;
+  use List::Util qw( min );
+  use Data::Dumper;
+
+  my $registry = 'Bio::EnsEMBL::Registry';
+
+  $registry->load_registry_from_db(
+      -host => 'ensembldb.ensembl.org',
+      -user => 'anonymous'
+  );
+
+  my $epigenome_track_adaptor = Bio::EnsEMBL::Registry->get_adaptor('homo_sapiens', 'funcgen', 'EpigenomeTrack');
+
+  my $all_epigenome_tracks = $epigenome_track_adaptor->fetch_all;
+
+  my $number_of_epigenome_tracks_available = @$all_epigenome_tracks;
+
+  print "There are $number_of_epigenome_tracks_available peak callings available for querying:\n";
+
+  # Print the first ten
+  my $max_features_to_print = 10;
+
+  for my $i ( 1.. min($max_features_to_print, $number_of_epigenome_tracks_available) ) {
+
+    my $current_epigenome_track = $all_epigenome_tracks->[$i];
+    print "  - " . $current_epigenome_track->display_label . "\n";
+    
+  }
+
+=head1 DESCRIPTION
+
+This object represents a peak calling from a ChIP-seq or other high-throughput
+assay. It links to 
+
+  - the set of Peaks that were generated and 
+  - the alignment that the peak calling was done on and
+  - the peak caller that was used via the analysis.
+
+=cut
+
+package Bio::EnsEMBL::Funcgen::EpigenomeTrack;
+
+use strict;
+use warnings;
+use Bio::EnsEMBL::Utils::Exception qw( deprecate );
+use Bio::EnsEMBL::Funcgen::GenericGetSetFunctionality qw(
+  _generic_get_or_set
+  _generic_set
+  _generic_get
+  _generic_fetch
+);
+
+use Role::Tiny::With;
+with 'Bio::EnsEMBL::Funcgen::GenericConstructor';
+
+sub _constructor_parameters {
+  return {
+   dbID                      => 'dbID',
+   adaptor                   => 'adaptor',
+   feature_type_id           => 'feature_type_id',
+   analysis_id               => 'analysis_id',
+   epigenome_id              => 'epigenome_id',
+   data_file_id              => 'data_file_id'
+  };
+}
+
+sub dbID            { return shift->_generic_get_or_set('dbID',             @_); }
+sub adaptor         {return shift->_generic_get_or_set('adaptor',           @_); }
+sub name            { return shift->_generic_get_or_set('name',             @_); }
+sub feature_type_id { return shift->_generic_get_or_set('feature_type_id',  @_); }
+sub analysis_id     { return shift->_generic_get_or_set('analysis_id',      @_); }
+sub epigenome_id    { return shift->_generic_get_or_set('epigenome_id',     @_); }
+sub data_file_id    { return shift->_generic_get_or_set('data_file_id',     @_); }
+
+
+=head2 get_FeatureType
+
+  Example    : my $feature_type = $epigenome_track->get_FeatureType;
+  Description: Fetches the feature type of the peak calling. This is the
+               type of feature the experiment was assaying for.
+  Returntype : Bio::EnsEMBL::Funcgen::FeatureType
+  Exceptions : None
+  Caller     : general
+  Status     : Stable
+
+=cut
+
+sub get_FeatureType {
+  return shift->_generic_fetch('feature_type', 'get_FeatureTypeAdaptor', 'feature_type_id');
+}
+
+
+sub get_Idr {
+  my $self = shift;
+
+  my $idr_adaptor = $self->adaptor->db->get_IdrAdaptor;
+  if (! defined $idr_adaptor) {
+    throw("Couldn't get an IdrAdaptor!");
+  }
+  my $idr = $idr_adaptor->_fetch_by_experiment_id($self->experiment_id);
+  return $idr;
+}
+
+=head2 get_EpigenomeTrackStatistic_by_statistic
+
+  Arg [1]    : String - statistic to get, available options:
+               total_length, average_length, num_peaks
+  Example    : my $statistic = $epigenome_track->get_EpigenomeTrackStatistic_by_statistic;
+  Description: Gets requested statistic for peak calling
+  Returntype : Bio::EnsEMBL::Funcgen::Alignment
+  Exceptions : None
+  Caller     : general
+  Status     : Stable
+
+=cut
+
+sub get_EpigenomeTrackStatistic_by_statistic {
+  my ($self, $statistic) = @_;
+
+  my $epigenome_track_statistic_adaptor = $self->adaptor->db->get_EpigenomeTrackStatisticAdaptor;
+  if (! defined $epigenome_track_statistic_adaptor) {
+    throw("Couldn't get an EpigenomeTrackStatisticAdaptor!");
+  }
+  my $epigenome_track_statistic = $epigenome_track_statistic_adaptor->_fetch_by_EpigenomeTrack_statistic($self, $statistic);
+  return $epigenome_track_statistic;
+}
+
+sub get_Frip {
+  my $self = shift;
+
+  my $frip_adaptor = $self->adaptor->db->get_FripAdaptor;
+  if (! defined $frip_adaptor) {
+    throw("Couldn't get an IdrAdaptor!");
+  }
+  my $frip = $frip_adaptor->fetch_by_EpigenomeTrack($self);
+  return $frip;
+}
+
+sub get_Chance {
+  my $self = shift;
+
+  my $signal_alignment  = $self->get_signal_Alignment;
+  my $control_alignment = $self->get_control_Alignment;
+
+  my $chance_adaptor = $self->adaptor->db->get_ChanceAdaptor;
+  if (! defined $chance_adaptor) {
+    throw("Couldn't get an ChanceAdaptor!");
+  }
+  my $chance = $chance_adaptor
+    ->fetch_by_signal_control_Alignments(
+      $signal_alignment, 
+      $control_alignment
+    );
+  return $chance;
+}
+
+
+=head2 get_Analysis
+
+  Example    : my $analysis = $epigenome_track->get_Analysis;
+  Description: Gets the analysis of the peak calling. This is the analysis
+               representing the peak caller that was used to analyse the
+               alignment.
+  Returntype : Bio::EnsEMBL::Analysis
+  Exceptions : None
+  Caller     : general
+  Status     : Stable
+
+=cut
+sub get_Analysis {
+  return shift->_generic_fetch('analysis', 'get_AnalysisAdaptor', 'analysis_id');
+}
+
+=head2 get_Epigenome
+
+  Example    : my $epigenome = $epigenome_track->get_Epigenome;
+  Description: Gets the epigenome that was used in the assay.
+  Returntype : Bio::EnsEMBL::Funcgen::Epigenome
+  Exceptions : None
+  Caller     : general
+  Status     : Stable
+
+=cut
+
+sub get_Epigenome {
+    my $self = shift;
+    return $self->_generic_fetch('epigenome', 'get_EpigenomeAdaptor', 'epigenome_id');
+}
+
+=head2 summary_as_hash
+
+  Example       : $summary = $epigenome_track->summary_as_hash;
+  Description   : Returns summary in a hash reference.
+  Returns       : Hashref of descriptive strings
+  Status        : Intended for internal use (REST)
+
+=cut
+
+sub summary_as_hash {
+  my $self   = shift;
+  
+  my $epigenome         = $self->get_Epigenome;
+  my $feature_type      = $self->get_FeatureType;
+  my $analysis          = $self->get_Analysis;
+#   my $data_file         = $self->get_DataFile;
+  my $idr               = $self->get_Idr;
+  
+  my $control_summary;
+  my $control_alignment = $self->get_control_Alignment;
+  if (defined $control_alignment) {
+    $control_summary = $control_alignment ->summary_as_hash;
+  } else {
+    $control_summary = undef;
+  }
+  
+  my $summary = {
+    epigenome         => $epigenome         ->summary_as_hash,
+    feature_type      => $feature_type      ->summary_as_hash,
+    idr               => $idr               ->summary_as_hash,
+  };
+  
+  my $chance = $self->get_Chance;
+  if ($chance) {
+    $summary->{chance} = $chance->summary_as_hash
+  }
+  return $summary;
+}
+
+1;
