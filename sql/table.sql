927b19b9bc0b6a18695bbab52fa17250dc9febd6
Paulo Lins <plins@ebi.ac.uk>
2022-06-01 06:56:50 +0100

    add new patch to table.sql file

--- sql/table.sql
+++ sql/table.sql
@@ -1687,6 +1687,7 @@ INSERT INTO meta (meta_key, meta_value, species_id) VALUES ('schema_type', 'func
 -- Update and remove these for each release to avoid erroneous patching
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'schema_version', '108');
 INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_107_108_a.sql|schema_version');
+INSERT INTO meta (species_id, meta_key, meta_value) VALUES (NULL, 'patch', 'patch_107_108_b.sql|New epigenome_track table');
 
 /**
 @table meta_coord
